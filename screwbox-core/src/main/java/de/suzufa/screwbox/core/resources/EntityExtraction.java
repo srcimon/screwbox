64204792b5e54d7f92611881bd41ad61ccfd898f
simonbas <sebastian.simon@koeln.de>
2022-06-04 11:06:08 +0200

    prototyping new entity extractor

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/resources/EntityExtraction.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/resources/EntityExtraction.java
@@ -1,15 +1,29 @@
 package de.suzufa.screwbox.core.resources;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.suzufa.screwbox.core.entityengine.Entity;
 
 public class EntityExtraction<I, O> {
 
-    private List<O> entities;
-    private EntityExtractor<I> caller;
+    private final List<O> entities;
+    private final EntityExtractor<I> caller;
+    private final List<Entity> extractedEntities = new ArrayList<>();
 
-    public EntityExtraction(List<O> extractedEntities, EntityExtractor<I> caller) {// TODO: CYCLIC DEPENDENCY!
+    public EntityExtraction(final List<O> extractedEntities, final EntityExtractor<I> caller) {// TODO: CYCLIC
+                                                                                               // DEPENDENCY!
         this.entities = extractedEntities;
         this.caller = caller;
     }
 
+    public EntityExtraction<I, O> convertVia(final EntityConverter<O> converter) {
+        entities.stream().filter(converter::accepts).map(converter::convert).forEach(extractedEntities::add);
+        return this;
+    }
+
+    public EntityExtractor<I> and() {
+        caller.addEntities(extractedEntities);
+        return caller;
+    }
 }
--- screwbox-core/src/main/java/de/suzufa/screwbox/core/resources/EntityExtractor.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/resources/EntityExtractor.java
@@ -1,10 +1,15 @@
 package de.suzufa.screwbox.core.resources;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.suzufa.screwbox.core.entityengine.Entity;
 
 public class EntityExtractor<T> {
 
     private final T input;
+
+    private final List<Entity> entities = new ArrayList<>();
 
     public static <T> EntityExtractor<T> extractFrom(T input) {
         return new EntityExtractor<>(input);
@@ -14,8 +19,17 @@ public class EntityExtractor<T> {
         this.input = input;
     }
 
+    void addEntities(List<Entity> entities) {
+        this.entities.addAll(entities);
+    }
+
     public <O> EntityExtraction<T, O> extractVia(Extractor<T, O> extractor) {
         List<O> extractedEntities = extractor.extractFrom(input);
-        return new EntityExtraction<>(extractedEntities, this);
+        EntityExtraction<T, O> entityExtraction = new EntityExtraction<>(extractedEntities, this);
+        return entityExtraction;
+    }
+
+    public List<Entity> allEntities() {
+        return entities;
     }
 }
