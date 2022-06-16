27bab1d670933cde18204db5a9266424c8a81177
simonbas <sebastian.simon@koeln.de>
2022-06-16 10:18:36 +0200

    added javadoc to ScrewBox

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/ScrewBox.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/ScrewBox.java
@@ -2,6 +2,11 @@ package de.suzufa.screwbox.core;
 
 import static java.util.Objects.nonNull;
 
+/**
+ * Best game engine ever made (not). The entry point for starting a game.
+ * 
+ * @see #createEngine()
+ */
 public final class ScrewBox {
 
     private static Engine engine;
@@ -9,6 +14,9 @@ public final class ScrewBox {
     private ScrewBox() {
     }
 
+    /**
+     * Creates an {@link Engine} instance. Can only be called once.
+     */
     public static Engine createEngine() {
         if (nonNull(engine)) {
             throw new IllegalStateException("only one instance of ScrewBox can be created");
