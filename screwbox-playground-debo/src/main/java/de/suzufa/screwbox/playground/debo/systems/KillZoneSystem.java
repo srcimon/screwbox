1d26c827a515740b557a499c9d884298774ba173
simonbas <sebastian.simon@koeln.de>
2022-06-17 07:43:49 +0200

    refactored killzonesystem

--- screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/systems/KillZoneSystem.java
+++ screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/systems/KillZoneSystem.java
@@ -1,11 +1,13 @@
 package de.suzufa.screwbox.playground.debo.systems;
 
+import java.util.Optional;
+
 import de.suzufa.screwbox.core.Engine;
 import de.suzufa.screwbox.core.entityengine.Archetype;
 import de.suzufa.screwbox.core.entityengine.Entity;
 import de.suzufa.screwbox.core.entityengine.EntitySystem;
-import de.suzufa.screwbox.core.entityengine.components.TransformComponent;
 import de.suzufa.screwbox.core.entityengine.components.SignalComponent;
+import de.suzufa.screwbox.core.entityengine.components.TransformComponent;
 import de.suzufa.screwbox.playground.debo.components.DeathEventComponent;
 import de.suzufa.screwbox.playground.debo.components.KillZoneComponent;
 import de.suzufa.screwbox.playground.debo.components.PlayerMarkerComponent;
@@ -19,10 +21,9 @@ public class KillZoneSystem implements EntitySystem {
     public void update(Engine engine) {
         for (Entity area : engine.entityEngine().fetchAll(TRIGGER_AREAS)) {
             if (area.get(SignalComponent.class).isTriggered) {
-                for (Entity playerEntity : engine.entityEngine().fetchAll(PLAYER)) {
-                    if (!playerEntity.hasComponent(DeathEventComponent.class)) {
-                        playerEntity.add(new DeathEventComponent(area.get(KillZoneComponent.class).deathType));
-                    }
+                Optional<Entity> fetch = engine.entityEngine().fetch(PLAYER);
+                if (fetch.isPresent() && !fetch.get().hasComponent(DeathEventComponent.class)) {
+                    fetch.get().add(new DeathEventComponent(area.get(KillZoneComponent.class).deathType));
                 }
             }
         }
