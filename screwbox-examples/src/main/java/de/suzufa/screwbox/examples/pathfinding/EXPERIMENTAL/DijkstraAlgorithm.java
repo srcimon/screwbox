4301e833b815e2d17239ff0ea617517611a05e08
simonbas <sebastian.simon@koeln.de>
2022-06-24 08:50:01 +0200

    moved functionality to raster

--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/DijkstraAlgorithm.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/DijkstraAlgorithm.java
@@ -15,37 +15,6 @@ public class DijkstraAlgorithm implements PathfindingAlgorithm {
         return points;
     }
 
-    // TODO: findNeighbours is part of the raster!!!!
-    // TODO: Sub Algorithm wihtout diagonal movements
-    private List<RasterPoint> FindNeighbors(Raster map, RasterPoint point) {
-        List<RasterPoint> neighbors = new ArrayList<>();
-        RasterPoint downLeft = point.offset(-1, 1);
-        RasterPoint downRight = point.offset(1, 1);
-        RasterPoint upLeft = point.offset(-1, -1);
-        RasterPoint upRight = point.offset(1, -1);
-        RasterPoint down = point.offset(0, 1);
-        RasterPoint up = point.offset(0, -1);
-        RasterPoint left = point.offset(-1, 0);
-        RasterPoint right = point.offset(1, 0);
-        if (map.isFree(down))
-            neighbors.add(down);
-        if (map.isFree(up))
-            neighbors.add(up);
-        if (map.isFree(left))
-            neighbors.add(left);
-        if (map.isFree(right))
-            neighbors.add(right);
-        if (map.isFree(downRight) && map.isFree(down) && map.isFree(right))
-            neighbors.add(downRight);
-        if (map.isFree(downLeft) && map.isFree(down) && map.isFree(left))
-            neighbors.add(downLeft);
-        if (map.isFree(upLeft) && map.isFree(up) && map.isFree(left))
-            neighbors.add(upLeft);
-        if (map.isFree(upRight) && map.isFree(up) && map.isFree(right))
-            neighbors.add(upRight);
-        return neighbors;
-    }
-
     private List<RasterPoint> FindPath(Raster map, RasterPoint start, RasterPoint end) {
         boolean finished = false;
         List<RasterPoint> used = new ArrayList<>();
@@ -54,7 +23,7 @@ public class DijkstraAlgorithm implements PathfindingAlgorithm {
             List<RasterPoint> newOpen = new ArrayList<>();
             for (int i = 0; i < used.size(); ++i) {
                 RasterPoint point = used.get(i);
-                for (RasterPoint neighbor : FindNeighbors(map, point)) {
+                for (RasterPoint neighbor : map.FindNeighbors(point)) {
                     if (!used.contains(neighbor) && !newOpen.contains(neighbor)) {
                         newOpen.add(neighbor);
                     }
--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/Raster.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/Raster.java
@@ -1,5 +1,8 @@
 package de.suzufa.screwbox.examples.pathfinding.EXPERIMENTAL;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.suzufa.screwbox.core.Bounds;
 import de.suzufa.screwbox.core.Vector;
 
@@ -10,14 +13,14 @@ public class Raster {
     private final int height;
     private final int gridSize;
 
-    public Raster(Bounds bounds, int gridSize) {
+    public Raster(final Bounds bounds, final int gridSize) {
         width = (int) bounds.width() / gridSize;
         height = (int) bounds.height() / gridSize;
         this.gridSize = gridSize;
         isBlocked = new boolean[width][height];
     }
 
-    public boolean isFree(RasterPoint point) {
+    public boolean isFree(final RasterPoint point) {
         if (point.y < 0 || point.y > isBlocked[0].length - 1) {
             return false;
         }
@@ -28,27 +31,27 @@ public class Raster {
     }
 
     @Deprecated
-    public boolean isBlocked(int x, int y) {
+    public boolean isBlocked(final int x, final int y) {
         // TODO: outOfRangeCheck
         return isBlocked[x][y];
     }
 
-    public Vector getVector(RasterPoint point) {
+    public Vector getVector(final RasterPoint point) {
         return Vector.of((point.x + 0.5) * gridSize, (point.y + 0.5) * gridSize);
     }
 
-    public RasterPoint getPoint(Vector position) {
+    public RasterPoint getPoint(final Vector position) {
         return new RasterPoint((int) position.x() / gridSize, (int) position.y() / gridSize, null);
     }
 
-    public void blockArea(Bounds bounds) {
-        Vector boundsOrigin = bounds.origin();
-        int xMin = (int) boundsOrigin.x() / gridSize;
-        int yMin = (int) boundsOrigin.y() / gridSize;
+    public void blockArea(final Bounds bounds) {
+        final Vector boundsOrigin = bounds.origin();
+        final int xMin = (int) boundsOrigin.x() / gridSize;
+        final int yMin = (int) boundsOrigin.y() / gridSize;
 
-        Vector bottomRight = bounds.bottomRight();
-        int xMax = (int) bottomRight.x() / gridSize;
-        int yMax = (int) bottomRight.y() / gridSize;
+        final Vector bottomRight = bounds.bottomRight();
+        final int xMax = (int) bottomRight.x() / gridSize;
+        final int yMax = (int) bottomRight.y() / gridSize;
         for (int x = xMin; x < xMax; x++) {
             for (int y = yMin; y < yMax; y++) {
                 isBlocked[x][y] = true;
@@ -62,5 +65,35 @@ public class Raster {
 
     public int height() {
         return height;
+    }
+
+    // TODO: Sub Algorithm wihtout diagonal movements
+    public List<RasterPoint> FindNeighbors(final RasterPoint point) {
+        final List<RasterPoint> neighbors = new ArrayList<>();
+        final RasterPoint downLeft = point.offset(-1, 1);
+        final RasterPoint downRight = point.offset(1, 1);
+        final RasterPoint upLeft = point.offset(-1, -1);
+        final RasterPoint upRight = point.offset(1, -1);
+        final RasterPoint down = point.offset(0, 1);
+        final RasterPoint up = point.offset(0, -1);
+        final RasterPoint left = point.offset(-1, 0);
+        final RasterPoint right = point.offset(1, 0);
+        if (isFree(down))
+            neighbors.add(down);
+        if (isFree(up))
+            neighbors.add(up);
+        if (isFree(left))
+            neighbors.add(left);
+        if (isFree(right))
+            neighbors.add(right);
+        if (isFree(downRight) && isFree(down) && isFree(right))
+            neighbors.add(downRight);
+        if (isFree(downLeft) && isFree(down) && isFree(left))
+            neighbors.add(downLeft);
+        if (isFree(upLeft) && isFree(up) && isFree(left))
+            neighbors.add(upLeft);
+        if (isFree(upRight) && isFree(up) && isFree(right))
+            neighbors.add(upRight);
+        return neighbors;
     }
 }
