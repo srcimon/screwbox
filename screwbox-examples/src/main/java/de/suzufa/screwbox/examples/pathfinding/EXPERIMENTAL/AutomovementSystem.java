3c24eff0285ce028722444ee34184f83c6c27c98
simonbas <sebastian.simon@koeln.de>
2022-06-25 10:42:04 +0200

    refactored automovement system

--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/AutomovementSystem.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/pathfinding/EXPERIMENTAL/AutomovementSystem.java
@@ -10,23 +10,25 @@ import de.suzufa.screwbox.core.entityengine.components.TransformComponent;
 
 public class AutomovementSystem implements EntitySystem {
 
-    private static final Archetype MOVINGS = Archetype.of(AutomovementComponent.class, PhysicsBodyComponent.class,
+    private static final Archetype AUTO_MOVERS = Archetype.of(AutomovementComponent.class, PhysicsBodyComponent.class,
             TransformComponent.class);
 
     @Override
     public void update(final Engine engine) {
-        for (final Entity entity : engine.entityEngine().fetchAll(MOVINGS)) {
-            final Vector position = entity.get(TransformComponent.class).bounds.position();
-            final var automovement = entity.get(AutomovementComponent.class);
-            if (automovement.path != null) {
-                if (position.distanceTo(automovement.path.end()) < 1) {
-                    entity.get(PhysicsBodyComponent.class).momentum = Vector.zero();
+        for (final Entity mover : engine.entityEngine().fetchAll(AUTO_MOVERS)) {
+            final Vector position = mover.get(TransformComponent.class).bounds.position();
+            final var automovement = mover.get(AutomovementComponent.class);
+            Path path = automovement.path;
+
+            if (path != null) {
+                if (position.distanceTo(path.end()) < 1) {
+                    mover.get(PhysicsBodyComponent.class).momentum = Vector.zero();
                 } else {
-                    if (automovement.path.nodeCount() > 1 && position.distanceTo(automovement.path.start()) < 1) {
-                        automovement.path.dropStart();
+                    if (path.nodeCount() > 1 && position.distanceTo(path.start()) < 1) {
+                        path.dropStart();
                     }
-                    final Vector direction = automovement.path.start().substract(position);
-                    entity.get(PhysicsBodyComponent.class).momentum = direction.capLength(automovement.speed);
+                    final Vector direction = path.start().substract(position);
+                    mover.get(PhysicsBodyComponent.class).momentum = direction.capLength(automovement.speed);
                 }
 
             }
