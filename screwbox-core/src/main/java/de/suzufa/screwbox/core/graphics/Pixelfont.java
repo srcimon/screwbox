13bb894d7f2ca6cacd3566c7b384f6e0067b5fc9
simonbas <sebastian.simon@koeln.de>
2022-07-16 12:00:07 +0200

    added pixelfont functionality

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/graphics/Pixelfont.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/graphics/Pixelfont.java
@@ -1,5 +1,6 @@
 package de.suzufa.screwbox.core.graphics;
 
+import static java.lang.String.format;
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
@@ -12,10 +13,15 @@ public class Pixelfont {
     private final Map<Character, Sprite> characters = new HashMap<>();
     private Dimension characterSize;
 
-    public static Pixelfont from(final List<Character> characters, final List<Sprite> sprites) {
-        final Pixelfont pixelfont = new Pixelfont();
-
-        return pixelfont;
+    public void addCharacters(final List<Character> characters, final List<Sprite> sprites) {
+        if (characters.size() != sprites.size()) {
+            throw new IllegalArgumentException(
+                    format("Count of characters (%d) is different than count of sprites (%d).",
+                            characters.size(), sprites.size()));
+        }
+        for (int i = 0; i < characters.size(); i++) {
+            addCharacter(characters.get(i), sprites.get(i));
+        }
     }
 
     public void addCharacter(final char character, final Sprite sprite) {
--- screwbox-core/src/test/java/de/suzufa/screwbox/core/graphics/PixelfontTest.java
+++ screwbox-core/src/test/java/de/suzufa/screwbox/core/graphics/PixelfontTest.java
@@ -3,6 +3,8 @@ package de.suzufa.screwbox.core.graphics;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -71,5 +73,30 @@ class PixelfontTest {
         assertThatThrownBy(() -> pixelfont.addCharacter('X', sprite))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("Character already present in font: X");
+    }
+
+    @Test
+    void addCharacters_sameAmoutOfCharactersAndSprites_addsAll() {
+        Sprite sprite = Sprite.invisible();
+
+        List<Character> characters = List.of('A', 'B', 'C');
+        List<Sprite> sprites = List.of(sprite, sprite, sprite);
+
+        pixelfont.addCharacters(characters, sprites);
+
+        assertThat(pixelfont.hasCharacter('A')).isTrue();
+        assertThat(pixelfont.hasCharacter('B')).isTrue();
+        assertThat(pixelfont.hasCharacter('C')).isTrue();
+        assertThat(pixelfont.characterCount()).isEqualTo(3);
+    }
+
+    @Test
+    void addCharacters_moreCharactersThanSprites_throwsException() {
+        List<Character> characters = List.of('A', 'B', 'C');
+        List<Sprite> sprites = List.of(Sprite.invisible());
+
+        assertThatThrownBy(() -> pixelfont.addCharacters(characters, sprites))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Count of characters (3) is different than count of sprites (1).");
     }
 }
