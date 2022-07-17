8e39ca07adb4b9640c98476a901c44a307ba5a43
simonbas <sebastian.simon@koeln.de>
2022-07-17 10:54:55 +0200

    formatted record

--- screwbox-tiled/src/main/java/de/suzufa/screwbox/tiled/internal/entity/LayerEntity.java
+++ screwbox-tiled/src/main/java/de/suzufa/screwbox/tiled/internal/entity/LayerEntity.java
@@ -5,11 +5,33 @@ import static java.util.Objects.isNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public record LayerEntity(List<ChunkEntity> chunks, String compression, List<Integer> data, String draworder,
-        String encoding, int height, int id, String image, double parallaxx, double parallaxy, List<LayerEntity> layers,
-        String name, List<ObjectEntity> objects, double offsetx, double offsety, double opacity,
-        List<PropertyEntity> properties, int startx, int starty, String tintcolor, String transparentcolor, String type,
-        boolean visible, int width, int x, int y) {
+public record LayerEntity(
+        List<ChunkEntity> chunks,
+        String compression,
+        List<Integer> data,
+        String draworder,
+        String encoding,
+        int height,
+        int id,
+        String image,
+        double parallaxx,
+        double parallaxy,
+        List<LayerEntity> layers,
+        String name,
+        List<ObjectEntity> objects,
+        double offsetx,
+        double offsety,
+        double opacity,
+        List<PropertyEntity> properties,
+        int startx,
+        int starty,
+        String tintcolor,
+        String transparentcolor,
+        String type,
+        boolean visible,
+        int width,
+        int x,
+        int y) {
 
     public List<ChunkEntity> chunks() {
         return isNull(chunks) ? new ArrayList<>() : chunks;
