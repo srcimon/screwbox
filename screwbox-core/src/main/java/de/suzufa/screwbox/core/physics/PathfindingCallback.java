7612fbd84d41c4cf9fbc00975af24080a3956047
simonbas <sebastian.simon@koeln.de>
2022-08-25 21:44:16 +0200

    added javadoc to callback

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/physics/PathfindingCallback.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/physics/PathfindingCallback.java
@@ -2,10 +2,21 @@ package de.suzufa.screwbox.core.physics;
 
 import de.suzufa.screwbox.core.Path;
 
+/**
+ * Callback for asynchronous pathfinding requests.
+ * 
+ * @see Physics#findPathAsync(Vector, Vector, PathfindingCallback)
+ */
 public interface PathfindingCallback {
 
+    /**
+     * Is called when a path has been found. Path is never null.
+     */
     void onPathFound(Path path);
 
+    /**
+     * Is called when no path was found.
+     */
     default void onPathNotFound() {
 
     }
