5f5a19128d3dbdc17b5e1d661bd606635a2ff7a3
simonbas <sebastian.simon@koeln.de>
2022-08-26 17:28:35 +0200

    fix redundant null check

--- screwbox-tiled/src/main/java/de/suzufa/screwbox/tiled/internal/JsonLoader.java
+++ screwbox-tiled/src/main/java/de/suzufa/screwbox/tiled/internal/JsonLoader.java
@@ -17,31 +17,32 @@ import de.suzufa.screwbox.tiled.internal.entity.TilesetEntity;
 
 public class JsonLoader {
 
-    public MapEntity loadMap(String fileName) {
-        Objects.requireNonNull(fileName, "fileName must not be null");
-        File mapFile = ResourceLoader.resourceFile(fileName);
+    public MapEntity loadMap(final String fileName) {
+        final File mapFile = ResourceLoader.resourceFile(fileName);
 
-        MapEntity map = deserialize(mapFile, MapEntity.class);
-        embedExternalTilesets(map, getDirectory(fileName));
-        embedObjectTemplates(map, getDirectory(fileName));
+        final MapEntity map = deserialize(mapFile, MapEntity.class);
+        final String directory = getDirectory(fileName);
+        embedExternalTilesets(map, directory);
+        embedObjectTemplates(map, directory);
         return map;
     }
 
-    private String getDirectory(String fileName) {
-        String[] parts = fileName.split("/");
-        String file = parts[parts.length - 1];
+    private String getDirectory(final String fileName) {
+        final String[] parts = fileName.split("/");
+        final String file = parts[parts.length - 1];
         return fileName.replace(file, "");
     }
 
-    private void embedObjectTemplates(MapEntity map, String directory) {
-        for (LayerEntity layer : map.getLayers()) {
+    private void embedObjectTemplates(final MapEntity map, final String directory) {
+        for (final LayerEntity layer : map.getLayers()) {
             if (layer.objects() != null) {
                 for (int i = 0; i < layer.objects().size(); i++) {
-                    ObjectEntity object = layer.objects().get(i);
+                    final ObjectEntity object = layer.objects().get(i);
                     if (object.getTemplate() != null) {
-                        File templateFile = ResourceLoader.resourceFile(directory + object.getTemplate());
-                        ObjectTemplateEntity objectTemplate = deserialize(templateFile, ObjectTemplateEntity.class);
-                        ObjectEntity replacement = objectTemplate.object();
+                        final File templateFile = ResourceLoader.resourceFile(directory + object.getTemplate());
+                        final ObjectTemplateEntity objectTemplate = deserialize(templateFile,
+                                ObjectTemplateEntity.class);
+                        final ObjectEntity replacement = objectTemplate.object();
                         replacement.setId(object.getId());
                         replacement.setX(object.getX());
                         replacement.setY(object.getY());
@@ -59,13 +60,13 @@ public class JsonLoader {
 
     }
 
-    private void embedExternalTilesets(MapEntity map, String directory) {
-        List<TilesetEntity> fullTilesets = new ArrayList<>();
-        for (TilesetEntity tileset : map.getTilesets()) {
+    private void embedExternalTilesets(final MapEntity map, final String directory) {
+        final List<TilesetEntity> fullTilesets = new ArrayList<>();
+        for (final TilesetEntity tileset : map.getTilesets()) {
             if (tileset.getSource() == null) {
                 fullTilesets.add(tileset);
             } else {
-                TilesetEntity externalTileset = loadTileset(directory + tileset.getSource());
+                final TilesetEntity externalTileset = loadTileset(directory + tileset.getSource());
                 externalTileset.setFirstgid(tileset.getFirstgid());
                 fullTilesets.add(externalTileset);
             }
@@ -73,21 +74,21 @@ public class JsonLoader {
         map.setTilesets(fullTilesets);
     }
 
-    public TilesetEntity loadTileset(String fileName) {
+    public TilesetEntity loadTileset(final String fileName) {
         Objects.requireNonNull(fileName, "fileName must not be null");
-        File tilesetFile = ResourceLoader.resourceFile(fileName);
-        TilesetEntity deserialize = deserialize(tilesetFile, TilesetEntity.class);
-        String fileNameInFileName = fileName.split("/")[fileName.split("/").length - 1];
-        String correctPath = fileName.replace(fileNameInFileName, deserialize.getImage());
+        final File tilesetFile = ResourceLoader.resourceFile(fileName);
+        final TilesetEntity deserialize = deserialize(tilesetFile, TilesetEntity.class);
+        final String fileNameInFileName = fileName.split("/")[fileName.split("/").length - 1];
+        final String correctPath = fileName.replace(fileNameInFileName, deserialize.getImage());
         deserialize.setImage(correctPath);
         return deserialize;
     }
 
-    private <T> T deserialize(File mapFile, Class<T> clazz) {
+    private <T> T deserialize(final File mapFile, final Class<T> clazz) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
+            final ObjectMapper objectMapper = new ObjectMapper();
             return objectMapper.readValue(mapFile, clazz);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new IllegalArgumentException("file could not be loaded: " + mapFile.getName(), e);
         }
     }
