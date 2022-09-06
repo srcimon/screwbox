9bad03c12b2e0d03fb2ff81294625d00adb9a088
simonbas <sebastian.simon@koeln.de>
2022-09-06 20:03:29 +0200

    some prototyping

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/entityengine/systems/WaterReflectionSystem.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/entityengine/systems/WaterReflectionSystem.java
@@ -1,28 +1,84 @@
 package de.suzufa.screwbox.core.entityengine.systems;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import de.suzufa.screwbox.core.Bounds;
 import de.suzufa.screwbox.core.Engine;
+import de.suzufa.screwbox.core.Vector;
 import de.suzufa.screwbox.core.entityengine.Archetype;
 import de.suzufa.screwbox.core.entityengine.Entity;
 import de.suzufa.screwbox.core.entityengine.EntitySystem;
+import de.suzufa.screwbox.core.entityengine.UpdatePriority;
+import de.suzufa.screwbox.core.entityengine.components.SpriteComponent;
 import de.suzufa.screwbox.core.entityengine.components.TransformComponent;
 import de.suzufa.screwbox.core.entityengine.components.WaterComponent;
 import de.suzufa.screwbox.core.entityengine.components.WaterReflectionComponent;
+import de.suzufa.screwbox.core.graphics.FlipMode;
 
 public class WaterReflectionSystem implements EntitySystem {
 
-    private static final Archetype WATERS = Archetype.of(WaterComponent.class, TransformComponent.class);
+    private static final Archetype WATERS = Archetype.of(
+            WaterComponent.class, TransformComponent.class);
+
     private static final Archetype RELECTED_ENTITIES = Archetype.of(
-            WaterReflectionComponent.class, TransformComponent.class);
+            WaterReflectionComponent.class, TransformComponent.class, SpriteComponent.class);
+
+    private static final record SpriteBatchEntry(SpriteComponent spriteComponent, Vector position)
+            implements Comparable<SpriteBatchEntry> {
+
+        @Override
+        public int compareTo(final SpriteBatchEntry o) {
+            return Integer.compare(spriteComponent.drawOrder, o.spriteComponent.drawOrder);
+        }
+
+    }
 
     @Override
     public void update(Engine engine) {
-        List<Entity> reflectedEntities = engine.entityEngine().fetchAll(RELECTED_ENTITIES);
+        List<Entity> reflectableEntities = engine.entityEngine().fetchAll(RELECTED_ENTITIES);
 
-        for (Entity waters : engine.entityEngine().fetchAll(WATERS)) {
+        for (Entity water : engine.entityEngine().fetchAll(WATERS)) {
+            var transform = water.get(TransformComponent.class);
+            var reflectedArea = transform.bounds.moveBy(Vector.yOnly(-transform.bounds.height()));
+            final List<SpriteBatchEntry> spriteBatch = new ArrayList<>();
 
+            for (var reflectableEntity : reflectableEntities) {
+                var reflectableBounds = reflectableEntity.get(TransformComponent.class).bounds;
+                if (reflectableBounds.intersects(reflectedArea)) {
+
+                }
+
+                final SpriteComponent spriteComponent = reflectableEntity.get(SpriteComponent.class);
+                final var sprite = spriteComponent.sprite;
+                final var spriteDimension = sprite.size();
+                final var spriteBounds = Bounds.atOrigin(
+                        reflectableBounds.position().x() - spriteDimension.width() / 2.0,
+                        reflectableBounds.position().y() - spriteDimension.height() / 2.0,
+                        spriteDimension.width() * spriteComponent.scale,
+                        spriteDimension.height() * spriteComponent.scale);
+
+                spriteBatch.add(new SpriteBatchEntry(spriteComponent, spriteBounds.origin()));
+            }
+            Collections.sort(spriteBatch);
+            for (final SpriteBatchEntry entry : spriteBatch) {
+                final SpriteComponent spriteC = entry.spriteComponent;
+                engine.graphics().world().drawSprite(
+                        spriteC.sprite,
+                        entry.position.addY(reflectedArea.height()),
+                        spriteC.scale,
+                        spriteC.opacity.substract(0.4),
+                        spriteC.rotation,
+                        FlipMode.VERTICAL);// TODO:invert
+            }
         }
+
+    }
+
+    @Override
+    public UpdatePriority updatePriority() {
+        return UpdatePriority.PRESENTATION_EFFECTS;
     }
 
 }
