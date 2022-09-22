e39972c394174e399b903241f0a54e95c7082a48
simonbas <sebastian.simon@koeln.de>
2022-09-22 18:25:25 +0200

    fix

--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/systems/GridRenderSystem.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/systems/GridRenderSystem.java
@@ -16,14 +16,16 @@ public class GridRenderSystem implements EntitySystem {
 
     @Override
     public void update(final Engine engine) {
-
         final var gridComponent = engine.entities().forcedFetch(GRID_HOLDER).get(GridComponent.class);
         final World world = engine.graphics().world();
         Grid grid = gridComponent.grid;
-        for (final var node : grid.nodes()) {
+        for (final var node : grid.nodes()) {// TODO: ONLY NODES ON SCREEN
             final Bounds worldBounds = grid.toWorldBounds(node);
             if (!grid.isFree(node)) {
-                final var color = grid.blockedNeighbors(node).size() > 2 ? Color.RED : Color.WHITE;
+                int neighbors = grid.blockedNeighbors(node).size();
+                final var color = neighbors > 2
+                        ? neighbors > 3 ? Color.BLUE : Color.RED
+                        : Color.WHITE;
                 world.drawCircle(worldBounds.position(), (int) worldBounds.width(), color);
             }
         }
