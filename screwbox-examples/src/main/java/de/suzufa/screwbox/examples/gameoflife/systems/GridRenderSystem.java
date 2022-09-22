41b600691bff28ac03c6597a19d359a38bb83e88
simonbas <sebastian.simon@koeln.de>
2022-09-22 18:17:38 +0200

    render cursor on top

--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/systems/GridRenderSystem.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/systems/GridRenderSystem.java
@@ -20,9 +20,6 @@ public class GridRenderSystem implements EntitySystem {
         final var gridComponent = engine.entities().forcedFetch(GRID_HOLDER).get(GridComponent.class);
         final World world = engine.graphics().world();
         Grid grid = gridComponent.grid;
-        Vector snappedMousePosition = grid.snap(engine.mouse().worldPosition());
-
-        world.drawCircle(snappedMousePosition, 2, Color.YELLOW);
         for (final var node : grid.nodes()) {
             final Bounds worldBounds = grid.toWorldBounds(node);
             if (!grid.isFree(node)) {
@@ -31,6 +28,8 @@ public class GridRenderSystem implements EntitySystem {
             }
         }
 
+        Vector snappedMousePosition = grid.snap(engine.mouse().worldPosition());
+        world.drawCircle(snappedMousePosition, 2, Color.YELLOW);
     }
 
 }
