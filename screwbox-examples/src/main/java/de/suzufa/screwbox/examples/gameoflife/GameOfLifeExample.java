401046dcdff6099af7956e02467248ce6174c408
simonbas <sebastian.simon@koeln.de>
2022-09-22 12:09:51 +0200

    refactoring

--- screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/GameOfLifeExample.java
+++ screwbox-examples/src/main/java/de/suzufa/screwbox/examples/gameoflife/GameOfLifeExample.java
@@ -1,9 +1,10 @@
 package de.suzufa.screwbox.examples.gameoflife;
 
+import static de.suzufa.screwbox.core.graphics.PredefinedCursor.DEFAULT;
+
 import de.suzufa.screwbox.core.Engine;
 import de.suzufa.screwbox.core.ScrewBox;
 import de.suzufa.screwbox.core.entities.systems.LogFpsSystem;
-import de.suzufa.screwbox.core.graphics.PredefinedCursor;
 import de.suzufa.screwbox.examples.gameoflife.systems.GridSystem;
 import de.suzufa.screwbox.examples.gameoflife.systems.MouseCameraSystem;
 
@@ -17,8 +18,8 @@ public class GameOfLifeExample {
                 .add(new LogFpsSystem())
                 .add(new MouseCameraSystem());
 
-        engine.graphics().window().setCursor(PredefinedCursor.DEFAULT);
-
+        engine.graphics().window().setCursor(DEFAULT);
+        engine.graphics().configuration().toggleFullscreen();
         engine.start();
     }
 }
