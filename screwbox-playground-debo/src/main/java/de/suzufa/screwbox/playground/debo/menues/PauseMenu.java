58857d1fc0949d4d00666ab2f482be400c89138b
simonbas <sebastian.simon@koeln.de>
2022-10-09 10:29:19 +0200

    reorder PauseMenu

--- screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/menues/PauseMenu.java
+++ screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/menues/PauseMenu.java
@@ -8,8 +8,8 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 import de.suzufa.screwbox.core.Engine;
-import de.suzufa.screwbox.core.entities.Entity;
 import de.suzufa.screwbox.core.entities.Entities;
+import de.suzufa.screwbox.core.entities.Entity;
 import de.suzufa.screwbox.core.ui.UiMenu;
 import de.suzufa.screwbox.core.ui.UiMenuItem;
 import de.suzufa.screwbox.playground.debo.scenes.GameScene;
@@ -28,16 +28,6 @@ public class PauseMenu extends UiMenu {
                 new PauseMenuResumeGame().onActivate(engine);
             }
 
-            private void serialize(List<Entity> entities, String filename) {
-                try (FileOutputStream fos = new FileOutputStream(filename)) {
-                    try (ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-                        oos.writeObject(entities);
-                    }
-                } catch (IOException e) {
-                    throw new IllegalStateException("Could not serialize Entities.", e);
-                }
-            }
-
         });
         add(new UiMenuItem("Load Game") {
 
@@ -51,16 +41,6 @@ public class PauseMenu extends UiMenu {
                 entities.add(allEntities);
                 new PauseMenuResumeGame().onActivate(engine);
 
-            }
-
-            private List<Entity> deserialize(String filename) {
-                try (FileInputStream fis = new FileInputStream(filename)) {
-                    try (ObjectInputStream oos = new ObjectInputStream(fis)) {
-                        return (List<Entity>) oos.readObject();
-                    }
-                } catch (IOException | ClassNotFoundException e) {
-                    throw new IllegalStateException("Could not deserialize File.", e);
-                }
             }
 
         });
@@ -85,6 +65,26 @@ public class PauseMenu extends UiMenu {
                 engine.stop();
             }
         });
+    }
+
+    private List<Entity> deserialize(String filename) {
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            try (ObjectInputStream oos = new ObjectInputStream(fis)) {
+                return (List<Entity>) oos.readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new IllegalStateException("Could not deserialize File.", e);
+        }
+    }
+
+    private void serialize(List<Entity> entities, String filename) {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                oos.writeObject(entities);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not serialize Entities.", e);
+        }
     }
 
 }
