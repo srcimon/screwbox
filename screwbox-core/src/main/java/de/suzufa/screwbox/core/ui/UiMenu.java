320b4eb3bd5f7878037a9bcd981a3e4b8180801a
simonbas <sebastian.simon@koeln.de>
2022-10-11 10:47:25 +0200

    fixed stack overflow bugs on non active menues

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/ui/UiMenu.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/ui/UiMenu.java
@@ -10,7 +10,7 @@ public class UiMenu {
     private final List<UiMenuItem> items = new ArrayList<>();
     private int selectedItemIndex = 0;
 
-    public final void add(UiMenuItem item) {
+    public final void add(final UiMenuItem item) {
         items.add(item);
     }
 
@@ -21,7 +21,7 @@ public class UiMenu {
         return items;
     }
 
-    public final boolean isSelectedItem(UiMenuItem item) {
+    public final boolean isSelectedItem(final UiMenuItem item) {
         return items().get(selectedItemIndex).equals(item);
     }
 
@@ -29,38 +29,41 @@ public class UiMenu {
         return items().get(selectedItemIndex);
     }
 
-    public final void nextItem(Engine engine) {
-        if (selectedItemIndex < items().size() - 1) {
-            selectedItemIndex++;
-        }
-        while (!isActive(selectedItemIndex, engine)) {
-            nextItem(engine);
-        }
-        // TODO: endless loop bug
-        // TODO: add Test
+    public final void nextItem(final Engine engine) {
+        selectedItemIndex = fetchNextSelectableItem(engine);
     }
 
-    public final void previousItem(Engine engine) {
-        if (selectedItemIndex > 0) {
-            selectedItemIndex--;
-        }
-
-        while (!isActive(selectedItemIndex, engine)) {
-            previousItem(engine);
-        }
-        // TODO: endless loop bug
-        // TODO: add Test
+    public final void previousItem(final Engine engine) {
+        selectedItemIndex = fetchPreviousSelectableItem(engine);
     }
 
-    public boolean isActive(UiMenuItem item, Engine engine) {
+    private int fetchNextSelectableItem(final Engine engine) {
+        for (int index = selectedItemIndex + 1; index < items().size(); index++) {
+            if (isActive(index, engine)) {
+                return index;
+            }
+        }
+        return selectedItemIndex;
+    }
+
+    private int fetchPreviousSelectableItem(final Engine engine) {
+        for (int index = selectedItemIndex - 1; index > 0; index--) {
+            if (isActive(index, engine)) {
+                return index;
+            }
+        }
+        return selectedItemIndex;
+    }
+
+    public boolean isActive(final UiMenuItem item, final Engine engine) {
         return isActive(itemIndex(item), engine);
     }
 
-    boolean isActive(int index, Engine engine) {
+    boolean isActive(final int index, final Engine engine) {
         return items.get(index).activeCondition().test(engine);
     }
 
-    public void onExit(Engine engine) {
+    public void onExit(final Engine engine) {
         // does nothing
     }
 
@@ -72,9 +75,9 @@ public class UiMenu {
         return selectedItemIndex;
     }
 
-    public final int itemIndex(UiMenuItem menuItem) {
+    public final int itemIndex(final UiMenuItem menuItem) {
         int index = 0;
-        for (var item : items) {
+        for (final var item : items) {
             if (item.equals(menuItem)) {
                 return index;
             }
@@ -83,7 +86,7 @@ public class UiMenu {
         throw new IllegalArgumentException("Menu doesn't contain specified MenuItem.");
     }
 
-    public final void selectItem(UiMenuItem menuItem) {
+    public final void selectItem(final UiMenuItem menuItem) {
         selectedItemIndex = itemIndex(menuItem);
     }
 
