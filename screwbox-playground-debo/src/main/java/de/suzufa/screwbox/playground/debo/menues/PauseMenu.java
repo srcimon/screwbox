29fa80b9fd1d194a2151080f0ba0d9640c01450c
simonbas <sebastian.simon@koeln.de>
2022-10-11 12:23:06 +0200

    refactored pausemenu

--- screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/menues/PauseMenu.java
+++ screwbox-playground-debo/src/main/java/de/suzufa/screwbox/playground/debo/menues/PauseMenu.java
@@ -1,30 +1,41 @@
 package de.suzufa.screwbox.playground.debo.menues;
 
 import de.suzufa.screwbox.core.Engine;
+import de.suzufa.screwbox.core.entities.Archetype;
+import de.suzufa.screwbox.core.entities.Entity;
 import de.suzufa.screwbox.core.ui.UiMenu;
 import de.suzufa.screwbox.core.ui.UiMenuItem;
+import de.suzufa.screwbox.playground.debo.components.BackgroundHolderComponent;
 import de.suzufa.screwbox.playground.debo.scenes.GameScene;
 import de.suzufa.screwbox.playground.debo.scenes.StartScene;
 
 public class PauseMenu extends UiMenu {
 
+    private static final Archetype BACKGROUND = Archetype.of(BackgroundHolderComponent.class);
     private static final String SAVEGAME_NAME = "savegame.sav";
 
     public PauseMenu() {
-        add(new PauseMenuResumeGame());
+        add(new UiMenuItem("Resume game").onActivate(this::resumeGame));
 
         add(new UiMenuItem("Save Game").onActivate(engine -> {
             engine.savegame().create(SAVEGAME_NAME, GameScene.class);
-            new PauseMenuResumeGame().trigger(engine);
+            resumeGame(engine);
         }));
 
         add(new UiMenuItem("Load Game").onActivate(engine -> {
             engine.savegame().load(SAVEGAME_NAME, GameScene.class);
-            new PauseMenuResumeGame().trigger(engine);
+            resumeGame(engine);
         }).activeCondition(engine -> engine.savegame().exists(SAVEGAME_NAME)));
 
         add(new UiMenuItem("Options").onActivate(engine -> engine.ui().openMenu(new OptionsMenu(new PauseMenu()))));
         add(new UiMenuItem("Back to menu").onActivate(engine -> engine.scenes().switchTo(StartScene.class)));
         add(new UiMenuItem("Quit Game").onActivate(Engine::stop));
+    }
+
+    private void resumeGame(Engine engine) {
+        engine.ui().closeMenu();
+        Entity backgroundEntity = engine.entities().forcedFetch(BACKGROUND);
+        backgroundEntity.get(BackgroundHolderComponent.class).background = null;
+        engine.scenes().switchTo(GameScene.class);
     }
 }
