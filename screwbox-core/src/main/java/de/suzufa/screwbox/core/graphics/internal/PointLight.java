20fca7d8c541cbe63fe187be077b240fc801d345
simonbas <sebastian.simon@koeln.de>
2022-11-09 09:47:16 +0100

    Revert "more multithreading"
    This reverts commit 86d182465205100b7d8426176096c584c631c9ee.

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/graphics/internal/PointLight.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/graphics/internal/PointLight.java
@@ -1,11 +1,10 @@
 package de.suzufa.screwbox.core.graphics.internal;
 
 import java.util.List;
-import java.util.concurrent.Future;
 
 import de.suzufa.screwbox.core.graphics.Color;
 import de.suzufa.screwbox.core.graphics.Offset;
 
-public record PointLight(Offset position, int radius, Future<List<Offset>> area, Color color) {
+public record PointLight(Offset position, int radius, List<Offset> area, Color color) {
 
 }
