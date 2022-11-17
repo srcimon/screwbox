98ef5a542bf3b80a8a7b76b665406b93717212fe
simonbas <sebastian.simon@koeln.de>
2022-11-17 21:48:30 +0100

    added todo

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/assets/Assets.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/assets/Assets.java
@@ -7,6 +7,7 @@ public interface Assets {
     // TODO: return progress rather than progress in service?
     Assets startLoadingFromPackage(String packageName); // TODO: accept class
 
+    // TODO: i dont want that: needed for gamescene tests / avoidable?
     Assets loadFromPackage(String packageName);
 
     List<AssetLocation<?>> scanPackageForAssetLocations(String packageName);// TODO: accept class
