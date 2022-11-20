a6db2615731946ffd5ad3728b954edab7cf4340b
simonbas <sebastian.simon@koeln.de>
2022-11-20 20:12:46 +0100

    refactoring

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/utils/Reflections.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/utils/Reflections.java
@@ -54,23 +54,28 @@ public final class Reflections {
         return retval;
     }
 
-    private static Collection<String> getResources(final String element, final Pattern pattern) {
+    private static List<String> getResources(final String element, final Pattern pattern) {
         final File file = new File(element);
-        return file.isDirectory()
-                ? getResourcesFromDirectory(file, pattern)
-                : getResourcesFromJarFile(file, pattern);
+        Collection<String> resources = file.isDirectory()
+                ? getResourcesFromDirectory(file)
+                : getResourcesFromJarFile(file);
+
+        var matchingResources = new ArrayList<String>();
+        for (var resource : resources) {
+            if (pattern.matcher(resource).matches()) {
+                matchingResources.add(resource);
+            }
+        }
+        return matchingResources;
     }
 
-    private static Collection<String> getResourcesFromJarFile(final File file, final Pattern pattern) {
+    private static Collection<String> getResourcesFromJarFile(final File file) {
         final ArrayList<String> retval = new ArrayList<>();
         try (ZipFile zf = getZipFile(file)) {
             final var entries = zf.entries();
             while (entries.hasMoreElements()) {
                 final ZipEntry ze = entries.nextElement();
-                final String fileName = ze.getName();
-                if (pattern.matcher(fileName).matches()) {
-                    retval.add(fileName);
-                }
+                retval.add(ze.getName());
             }
             return retval;
         } catch (final IOException e) {
@@ -86,17 +91,14 @@ public final class Reflections {
         }
     }
 
-    private static Collection<String> getResourcesFromDirectory(final File directory, final Pattern pattern) {
+    private static List<String> getResourcesFromDirectory(final File directory) {
         final List<String> retval = new ArrayList<>();
         for (final File file : directory.listFiles()) {
             if (file.isDirectory()) {
-                retval.addAll(getResourcesFromDirectory(file, pattern));
+                retval.addAll(getResourcesFromDirectory(file));
             } else {
                 try {
-                    final String fileName = file.getCanonicalPath();
-                    if (pattern.matcher(fileName).matches()) {
-                        retval.add(fileName);
-                    }
+                    retval.add(file.getCanonicalPath());
                 } catch (final IOException e) {
                     throw new IllegalStateException("could not read resources from directory", e);
                 }
