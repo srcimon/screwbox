fe8a23850e05e5001d271e56cbcedb8cfc8bb379
simonbas <sebastian.simon@koeln.de>
2022-11-22 17:56:27 +0100

    refactoring

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/scenes/internal/SceneContainer.java
+++
@@ -1,8 +0,0 @@
-package de.suzufa.screwbox.core.scenes.internal;
-
-import de.suzufa.screwbox.core.entities.internal.DefaultEntities;
-import de.suzufa.screwbox.core.scenes.Scene;
-
-public record SceneContainer(Scene scene, DefaultEntities entities) {
-
-}
