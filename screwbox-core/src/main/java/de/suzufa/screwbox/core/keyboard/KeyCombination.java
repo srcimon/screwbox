26df730c7398097900a87fc7bc937f0e4f7aaf4e
simonbas <sebastian.simon@koeln.de>
2022-11-25 22:47:11 +0100

    tests and javadoc

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/keyboard/KeyCombination.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/keyboard/KeyCombination.java
@@ -1,24 +1,37 @@
 package de.suzufa.screwbox.core.keyboard;
 
-import java.util.Collection;
+import static de.suzufa.screwbox.core.utils.ListUtil.containsDuplicates;
+
 import java.util.List;
 
+/**
+ * A combination of multiple {@link Key}s.
+ */
 public class KeyCombination {
 
-    private final Collection<Key> keys;
+    private final List<Key> keys;
 
+    /**
+     * Creats a new instance of the given {@link Key}s. Must have at leas one key.
+     */
     public static KeyCombination ofKeys(final Key... keys) {
-        if (keys.length == 0) {
-            throw new IllegalArgumentException("combination must contain a key");
-        }
         return new KeyCombination(List.of(keys));
     }
 
     private KeyCombination(final List<Key> keys) {
+        if (keys.isEmpty()) {
+            throw new IllegalArgumentException("combination must contain a key");
+        }
+        if (containsDuplicates(keys)) {
+            throw new IllegalArgumentException("combination must not contain duplicate keys");
+        }
         this.keys = keys;
     }
 
-    public Collection<Key> keys() {
+    /**
+     * Returns all {@link Key}s in that {@link KeyCombination}.
+     */
+    public List<Key> keys() {
         return keys;
     }
 }
