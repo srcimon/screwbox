3dd2275323bf8e4e1a5e3b3711aac947cc9f8428
simonbas <sebastian.simon@koeln.de>
2022-12-11 10:30:41 +0100

    refactor gamescene

--- screwbox-examples/platformer-example/src/test/java/de/suzufa/screwbox/examples/platformer/scenes/GameSceneTest.java
+++ screwbox-examples/platformer-example/src/test/java/de/suzufa/screwbox/examples/platformer/scenes/GameSceneTest.java
@@ -2,9 +2,11 @@ package de.suzufa.screwbox.examples.platformer.scenes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.suzufa.screwbox.core.Engine;
 import de.suzufa.screwbox.core.entities.Entities;
@@ -14,13 +16,15 @@ import de.suzufa.screwbox.core.entities.internal.DefaultEntityManager;
 import de.suzufa.screwbox.core.entities.internal.DefaultSystemManager;
 import de.suzufa.screwbox.examples.platformer.components.PlayerMarkerComponent;
 
+@ExtendWith(MockitoExtension.class)
 class GameSceneTest {
+
+    @Mock
+    Engine engine;
 
     @ParameterizedTest
     @ValueSource(strings = { "maps/0-1_intro.json", "maps/1-1_teufelsinsel.json", "maps/1-2_misty_caves.json" })
     void allMapsCanBeConvertetToEntities(String mapName) {
-        var engine = Mockito.mock(Engine.class);
-
         var entityManager = new DefaultEntityManager();
         var systemManager = new DefaultSystemManager(engine, entityManager);
         Entities entities = new DefaultEntities(entityManager, systemManager);
