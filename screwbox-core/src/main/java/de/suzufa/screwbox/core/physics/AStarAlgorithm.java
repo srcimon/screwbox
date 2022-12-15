dd340de0faa76297a67097a51e9dd55554a29bdc
simonbas <sebastian.simon@koeln.de>
2022-12-15 22:16:48 +0100

    refactoring

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/physics/AStarAlgorithm.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/physics/AStarAlgorithm.java
@@ -21,6 +21,11 @@ import de.suzufa.screwbox.core.Grid.Node;
  */
 public class AStarAlgorithm implements PathfindingAlgorithm {
 
+    @Override
+    public List<Node> findPath(final Grid grid, final Node start, final Node end) {
+        return new AStarSearch(grid, start, end).findPath();
+    }
+
     private record WeightedNode(Node node, Double cost) implements Comparable<WeightedNode> {
 
         @Override
@@ -29,36 +34,56 @@ public class AStarAlgorithm implements PathfindingAlgorithm {
         }
     }
 
-    @Override
-    public List<Node> findPath(final Grid grid, final Node start, final Node end) {
-        final Set<Node> closed = new HashSet<>();
-        final Map<Node, Double> costs = new HashMap<>(Map.of(start, 0.0));
-        final Map<Node, Double> costsToStart = new HashMap<>();
-        final Queue<WeightedNode> open = new PriorityQueue<>(List.of(new WeightedNode(start, 0.0)));
+    private class AStarSearch {
 
-        while (!open.isEmpty()) {
-            final Node currentNode = open.remove().node;
-            if (!closed.contains(currentNode)) {
-                closed.add(currentNode);
-                if (currentNode.equals(end)) {
-                    return grid.backtrack(currentNode);
+        private Grid grid;
+        private Node start;
+        private Node end;
+        private Set<Node> closed;
+        private Map<Node, Double> costs;
+        private Map<Node, Double> costsToStart;
+        private Queue<WeightedNode> open;
+
+        public AStarSearch(Grid grid, Node start, Node end) {
+            this.grid = grid;
+            this.start = start;
+            this.end = end;
+            closed = new HashSet<>();
+            costs = new HashMap<>(Map.of(start, 0.0));
+            costsToStart = new HashMap<>();
+            open = new PriorityQueue<>(List.of(new WeightedNode(start, 0.0)));
+        }
+
+        public List<Node> findPath() {
+            while (!open.isEmpty()) {
+                final Node currentNode = open.remove().node;
+                if (!closed.contains(currentNode)) {
+                    closed.add(currentNode);
+                    if (currentNode.equals(end)) {
+                        return grid.backtrack(currentNode);
+                    }
+                    processNode(currentNode);
                 }
-                final var costToStart = costsToStart.get(currentNode);
-                for (final Node neighbor : grid.reachableNeighbors(currentNode)) {
-                    if (!closed.contains(neighbor)) {
-                        final double startCost = isNull(costToStart) ? currentNode.distance(start) : costToStart;
-                        final double totalCost = startCost + neighbor.distance(currentNode) + neighbor.distance(end);
-                        final Double costNeighbour = costs.get(neighbor);
-                        if (isNull(costNeighbour) || totalCost < costNeighbour) {
-                            costsToStart.put(neighbor, totalCost);
-                            costs.put(neighbor, totalCost);
-                            open.add(new WeightedNode(neighbor, totalCost));
-                        }
+            }
+            return emptyList();
+        }
+
+        private void processNode(final Node currentNode) {
+            final var costToStart = costsToStart.get(currentNode);
+            for (final Node neighbor : grid.reachableNeighbors(currentNode)) {
+                if (!closed.contains(neighbor)) {
+                    final double startCost = isNull(costToStart) ? currentNode.distance(start) : costToStart;
+                    final double totalCost = startCost + neighbor.distance(currentNode)
+                            + neighbor.distance(end);
+                    final Double costNeighbour = costs.get(neighbor);
+                    if (isNull(costNeighbour) || totalCost < costNeighbour) {
+                        costsToStart.put(neighbor, totalCost);
+                        costs.put(neighbor, totalCost);
+                        open.add(new WeightedNode(neighbor, totalCost));
                     }
                 }
             }
         }
-        return emptyList();
     }
 
 }
