6c98886dc568cfdb72d09bb97d195ef8e3a6c34a
simonbas <sebastian.simon@koeln.de>
2023-01-01 18:34:48 +0100

    added javadoc

--- screwbox-core/src/main/java/de/suzufa/screwbox/core/utils/MathUtil.java
+++ screwbox-core/src/main/java/de/suzufa/screwbox/core/utils/MathUtil.java
@@ -1,18 +1,31 @@
 package de.suzufa.screwbox.core.utils;
 
+/**
+ * Utility functions for math operations.
+ *
+ */
 public final class MathUtil {
 
     private MathUtil() {
     }
 
+    /**
+     * Returns value but never a value below min and never a value exceeding max.
+     */
     public static double clamp(final double min, final double value, final double max) {
         return Math.min(Math.max(value, min), max);
     }
 
+    /**
+     * Returns {@code true} if both values have the same sign.
+     */
     public static boolean sameSign(final double value, final double other) {
         return value >= 0 == other >= 0;
     }
 
+    /**
+     * Returns 1 if value is positive or -1 if value is negative.
+     */
     public static double modifier(final double value) {
         return value >= 0 ? 1 : -1;
     }
