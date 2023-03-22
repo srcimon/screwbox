b54ab02788224b71a9b1bb6d44ab7270e4ea6e9a
simonbas <sebastian.simon@koeln.de>
2023-03-22 19:54:16 +0100

    refactoring

--- screwbox-examples/slideshow-example/src/main/java/io/github/simonbas/screwbox/examples/slideshow/SlideshowExample.java
+++ screwbox-examples/slideshow-example/src/main/java/io/github/simonbas/screwbox/examples/slideshow/SlideshowExample.java
@@ -2,7 +2,9 @@ package io.github.simonbas.screwbox.examples.slideshow;
 
 import io.github.simonbas.screwbox.core.Engine;
 import io.github.simonbas.screwbox.core.ScrewBox;
+import io.github.simonbas.screwbox.core.scenes.Scenes;
 import io.github.simonbas.screwbox.core.ui.WobblyUiLayouter;
+import io.github.simonbas.screwbox.core.window.WindowDropEvent;
 import io.github.simonbas.screwbox.examples.slideshow.scenes.InputReceivedScene;
 import io.github.simonbas.screwbox.examples.slideshow.scenes.WaitingForInputScene;
 
@@ -11,17 +13,21 @@ public class SlideshowExample {
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Slideshow Example");
 
-        engine.scenes().add(new WaitingForInputScene());
+        Scenes scenes = engine.scenes();
+
+        scenes.add(new WaitingForInputScene());
 
         engine.ui().setLayouter(new WobblyUiLayouter());
-        engine.window().addDropListener(dropEvent -> {
-            if (engine.scenes().isActive(InputReceivedScene.class)) {
-                engine.scenes().switchTo(WaitingForInputScene.class);
-                engine.scenes().remove(InputReceivedScene.class);
-            }
-            engine.scenes().add(new InputReceivedScene(dropEvent.files()));
-            engine.scenes().switchTo(InputReceivedScene.class);
-        });
+        engine.window().addDropListener(dropEvent -> switchToInputReceived(scenes, dropEvent));
         engine.start(WaitingForInputScene.class);
+    }
+
+    private static void switchToInputReceived(Scenes scenes, WindowDropEvent dropEvent) {
+        if (scenes.isActive(InputReceivedScene.class)) {
+            scenes.switchTo(WaitingForInputScene.class);
+            scenes.remove(InputReceivedScene.class);
+        }
+        scenes.add(new InputReceivedScene(dropEvent.files()));
+        scenes.switchTo(InputReceivedScene.class);
     }
 }
