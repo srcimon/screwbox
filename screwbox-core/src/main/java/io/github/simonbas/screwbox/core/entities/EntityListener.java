69afbbade557c221d873b98861606f8b26d63905
simonbas <sebastian.simon@koeln.de>
2023-03-22 13:33:25 +0100

    added some more javadoc

--- screwbox-core/src/main/java/io/github/simonbas/screwbox/core/entities/EntityListener.java
+++ screwbox-core/src/main/java/io/github/simonbas/screwbox/core/entities/EntityListener.java
@@ -2,10 +2,18 @@ package io.github.simonbas.screwbox.core.entities;
 
 import java.util.EventListener;
 
-//TODO add EntityEvent
+/**
+ * Listener interface for receiving {@link EntityEvent}s.
+ */
 public interface EntityListener extends EventListener {
 
+    /**
+     * invoked when a {@link Component} was added to the {@link Entity}.
+     */
     void componentAdded(EntityEvent event);
 
+    /**
+     * invoked when a {@link Component} was removed from the {@link Entity}.
+     */
     void componentRemoved(EntityEvent event);
 }
--- screwbox-core/src/main/java/io/github/simonbas/screwbox/core/graphics/GraphicsConfigurationListener.java
+++ screwbox-core/src/main/java/io/github/simonbas/screwbox/core/graphics/GraphicsConfigurationListener.java
@@ -2,7 +2,13 @@ package io.github.simonbas.screwbox.core.graphics;
 
 import java.util.EventListener;
 
+/**
+ * Listener interface for receiving {@link GraphicsConfigurationEvent}s.
+ */
 public interface GraphicsConfigurationListener extends EventListener {
 
+    /**
+     * invoked when a property of the {@link GraphicsConfiguration} is changed.
+     */
     void configurationChanged(GraphicsConfigurationEvent event);
 }
