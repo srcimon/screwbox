647cca93ef8211e414917c3e788467ce9e4840c6
simonbas <sebastian.simon@koeln.de>
2023-04-07 14:49:47 +0200

    added tests for DefaultLoadingScene

--- screwbox-core/src/main/java/io/github/simonbas/screwbox/core/scenes/DefaultLoadingScene.java
+++ screwbox-core/src/main/java/io/github/simonbas/screwbox/core/scenes/DefaultLoadingScene.java
@@ -1,8 +1,10 @@
 package io.github.simonbas.screwbox.core.scenes;
 
+import io.github.simonbas.screwbox.core.Time;
 import io.github.simonbas.screwbox.core.entities.Entities;
 import io.github.simonbas.screwbox.core.graphics.Color;
 import io.github.simonbas.screwbox.core.graphics.Offset;
+import io.github.simonbas.screwbox.core.graphics.Screen;
 
 import static io.github.simonbas.screwbox.core.graphics.Color.RED;
 import static io.github.simonbas.screwbox.core.graphics.Color.WHITE;
@@ -12,15 +14,17 @@ public class DefaultLoadingScene implements Scene {
 
     @Override
     public void initialize(Entities entities) {
-        entities.add(engine -> {
-            final int distance = 25;
-            final double timeSeed = engine.loop().lastUpdate().milliseconds() / 400.0;
-            for (int x = -distance; x <= distance; x += distance) {
-                int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
-                Color color = x == 0 ? RED : WHITE;
-                Offset position = engine.graphics().screen().center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
-                engine.graphics().screen().fillRectangle(position, square(size), color);
-            }
-        });
+        entities.add(engine -> drawLoadingAnimation(engine.graphics().screen()));
+    }
+
+    private void drawLoadingAnimation(final Screen screen) {
+        final int distance = 25;
+        final double timeSeed = Time.now().milliseconds() / 400.0;
+        for (int x = -distance; x <= distance; x += distance) {
+            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
+            Color color = x == 0 ? RED : WHITE;
+            Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
+            screen.fillRectangle(position, square(size), color);
+        }
     }
 }
