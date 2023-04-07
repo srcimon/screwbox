b383bcb83336bffce9837263e19c8c07015d00a8
simonbas <sebastian.simon@koeln.de>
2023-04-07 20:32:05 +0200

    refactoring StartGameMenu

--- screwbox-examples/platformer-example/src/main/java/io/github/simonbas/screwbox/examples/platformer/menues/StartGameMenu.java
+++ screwbox-examples/platformer-example/src/main/java/io/github/simonbas/screwbox/examples/platformer/menues/StartGameMenu.java
@@ -17,7 +17,7 @@ public class StartGameMenu extends UiMenu {
             engine.ui().closeMenu();
         }).activeCondition(engine -> engine.savegame().exists("savegame.sav"));
 
-        addItem("Options").onActivate(engine -> engine.ui().openMenu(new OptionsMenu(new StartGameMenu())));
+        addItem("Options").onActivate(engine -> engine.ui().openMenu(new OptionsMenu(this)));
         addItem("Quit").onActivate(Engine::stop);
     }
 
