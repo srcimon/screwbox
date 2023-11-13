691dd4f360dbd5a7939e245c9cb5685a03005a00
simonbas <sebastian.simon@koeln.de>
2023-11-13 21:00:34 +0100

    added todo

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/WindowDropListener.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/WindowDropListener.java
@@ -5,6 +5,7 @@ import java.util.EventListener;
 /**
  * Listener interface for receiving {@link WindowDropEvent}s.
  */
+//TODO This is kind of a mess - eventing may result in concurrent modification exception - better change to pull method from window - this is not very resource effecient but it's better than a bug
 public interface WindowDropListener extends EventListener {
 
     /**
