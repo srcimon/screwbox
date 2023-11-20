1d0c2de4d569fa6148fb820f8b76c8f4cf5c6ec9
simonbas <sebastian.simon@koeln.de>
2023-11-20 18:58:23 +0100

    Rename Helper Class and upgrade back to java 21

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/utils/MacOsSupport.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/utils/MacOsSupport.java
@@ -1,12 +1,12 @@
 package io.github.srcimon.screwbox.core.utils;
 
 /**
- * Gives information on the current operation system.
+ * Gives support for MacOs specific features.
  */
-public class OsInfo {
+public class MacOsSupport {
 
     /**
-     * Returns true if the current operation system is osx.
+     * Returns true if the current operation system is MacOs.
      */
     public static boolean isMacOs() {
         return "Mac OS X".equalsIgnoreCase(System.getProperty("os.name", "UNKNOWN-OS"));
