3328d42234dcb1f0f751c026d0769d2ef7ba88b5
simonbas <sebastian.simon@koeln.de>
2023-11-21 21:51:07 +0100

    fixed bug

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/internal/MacOsWindowFrame.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/internal/MacOsWindowFrame.java
@@ -19,7 +19,9 @@ public class MacOsWindowFrame extends WindowFrame {
             final var application = Class.forName("com.apple.eawt.Application");
             final var fullScreenMethod = application.getMethod("requestToggleFullScreen", Window.class);
             fullScreenMethod.invoke(application.getConstructor().newInstance(), this);
-        } catch (final Exception e) {
+        } catch (final InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (Exception e) {
             throw new IllegalStateException(
                     "Please add jvm parameters to allow native fullscreen on MacOs: --add-opens java.desktop/com.apple.eawt=ALL-UNNAMED",
                     e);
