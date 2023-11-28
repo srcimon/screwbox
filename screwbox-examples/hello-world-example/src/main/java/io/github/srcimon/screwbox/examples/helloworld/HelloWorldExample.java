eb28010f365fb1545b6209a75f71b847814c76db
simonbas <sebastian.simon@koeln.de>
2023-11-28 22:59:22 +0100

    fix sonar issue

--- screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
+++ screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
@@ -2,9 +2,6 @@ package io.github.srcimon.screwbox.examples.helloworld;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
-import io.github.srcimon.screwbox.core.entities.systems.QuitOnKeyPressSystem;
-import io.github.srcimon.screwbox.core.graphics.Screen;
-import io.github.srcimon.screwbox.core.keyboard.Key;
 
 import static io.github.srcimon.screwbox.core.graphics.Pixelfont.defaultFont;
 
@@ -15,7 +12,7 @@ public class HelloWorldExample {
 
         screwBox.entities()
                 .add(engine -> {
-                    Screen screen = engine.graphics().screen();
+                    var screen = engine.graphics().screen();
                     screen.drawTextCentered(screen.center(), "HELLO WORLD!", defaultFont(), 2);
                 });
 
