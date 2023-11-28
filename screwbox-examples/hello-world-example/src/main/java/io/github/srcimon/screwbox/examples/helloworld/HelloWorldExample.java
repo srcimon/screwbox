0241806714143c10687e7e0798114607f1176f3d
simonbas <sebastian.simon@koeln.de>
2023-11-28 22:58:35 +0100

    much more simple hello world example

--- screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
+++ screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
@@ -3,24 +3,22 @@ package io.github.srcimon.screwbox.examples.helloworld;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
 import io.github.srcimon.screwbox.core.entities.systems.QuitOnKeyPressSystem;
-import io.github.srcimon.screwbox.core.graphics.MouseCursor;
+import io.github.srcimon.screwbox.core.graphics.Screen;
 import io.github.srcimon.screwbox.core.keyboard.Key;
-import io.github.srcimon.screwbox.examples.helloworld.systems.PrintHelloWorldSystem;
+
+import static io.github.srcimon.screwbox.core.graphics.Pixelfont.defaultFont;
 
 public class HelloWorldExample {
 
     public static void main(String[] args) {
-        // create the engine
-        Engine engine = ScrewBox.createEngine("Hello World Example");
+        Engine screwBox = ScrewBox.createEngine("Hello World Example");
 
-        // hide cursor in window mode
-        engine.window().setWindowCursor(MouseCursor.HIDDEN);
+        screwBox.entities()
+                .add(engine -> {
+                    Screen screen = engine.graphics().screen();
+                    screen.drawTextCentered(screen.center(), "HELLO WORLD!", defaultFont(), 2);
+                });
 
-        // add systems to the game loop
-        engine.entities()
-                .add(new PrintHelloWorldSystem()) // print hello world next to mouse position
-                .add(new QuitOnKeyPressSystem(Key.ESCAPE)); // quits the engine on pressing ESC
-
-        engine.start(); // start the previously configured engine
+        screwBox.start();
     }
 }
