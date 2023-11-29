8e0b1cd10afe5d871617940b83ba83575047bec1
simonbas <sebastian.simon@koeln.de>
2023-11-29 19:33:20 +0100

    Revert "blur background"
    This reverts commit f1ba292bdcc634af18395a8d07271f1b9ae18d25.

--- screwbox-examples/platformer-example/src/main/java/io/github/srcimon/screwbox/examples/platformer/systems/PauseSystem.java
+++ screwbox-examples/platformer-example/src/main/java/io/github/srcimon/screwbox/examples/platformer/systems/PauseSystem.java
@@ -1,13 +1,9 @@
 package io.github.srcimon.screwbox.examples.platformer.systems;
 
 import io.github.srcimon.screwbox.core.Engine;
-import io.github.srcimon.screwbox.core.Time;
 import io.github.srcimon.screwbox.core.entities.Archetype;
 import io.github.srcimon.screwbox.core.entities.Entity;
 import io.github.srcimon.screwbox.core.entities.EntitySystem;
-import io.github.srcimon.screwbox.core.graphics.Sprite;
-import io.github.srcimon.screwbox.core.graphics.internal.BlurImageFilter;
-import io.github.srcimon.screwbox.core.graphics.internal.ImageUtil;
 import io.github.srcimon.screwbox.core.keyboard.Key;
 import io.github.srcimon.screwbox.examples.platformer.components.ScreenshotComponent;
 import io.github.srcimon.screwbox.examples.platformer.scenes.PauseScene;
@@ -23,8 +19,7 @@ public class PauseSystem implements EntitySystem {
                 || !engine.window().hasFocus()) {
 
             Entity screenshotEntity = engine.entities().forcedFetch(SCREENSHOT);
-            var newImage = new BlurImageFilter(8).apply(ImageUtil.toBufferedImage(engine.graphics().screen().takeScreenshot().image(Time.now())));
-            screenshotEntity.get(ScreenshotComponent.class).screenshot = Sprite.fromImage(newImage);
+            screenshotEntity.get(ScreenshotComponent.class).screenshot = engine.graphics().screen().takeScreenshot();
             engine.audio().stopAllSounds();
             engine.scenes().switchTo(PauseScene.class);
         }
