88e37e60edad8a61d47653e30585881d2ba6f57d
simonbas <sebastian.simon@koeln.de>
2023-12-10 10:52:41 +0100

    added other savegame method to environment

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/internal/DefaultEnvironment.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/internal/DefaultEnvironment.java
@@ -2,20 +2,24 @@ package io.github.srcimon.screwbox.core.environment.internal;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.environment.*;
+import io.github.srcimon.screwbox.core.utils.TimeoutCache;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
+import static io.github.srcimon.screwbox.core.Duration.ofSeconds;
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
 public class DefaultEnvironment implements Environment {
+
+    private final TimeoutCache<String, Boolean> savegameCache = new TimeoutCache<>(ofSeconds(1));
 
     private final EntityManager entityManager;
     private final SystemManager systemManager;
@@ -71,7 +75,7 @@ public class DefaultEnvironment implements Environment {
     public Optional<Entity> fetch(final Archetype archetype) {
         final var entities = entityManager.entitiesMatching(archetype);
         if (entities.size() == 1) {
-            return Optional.of(entities.get(0));
+            return Optional.of(entities.getFirst());
         }
         return Optional.empty();
     }
@@ -119,21 +123,6 @@ public class DefaultEnvironment implements Environment {
     @Override
     public List<EntitySystem> systems() {
         return systemManager.allSystems();
-    }
-
-    @Override
-    public Environment createSavegame(final String name) {
-        requireNonNull(name, "name must not be null");
-        try (final OutputStream outputStream = new FileOutputStream(name)) {
-            try (final OutputStream zippedOutputStream = new GZIPOutputStream(outputStream)) {
-                try (final ObjectOutputStream objectOutputStream = new ObjectOutputStream(zippedOutputStream)) {
-                    objectOutputStream.writeObject(entities());
-                }
-            }
-        } catch (final IOException e) {
-            throw new IllegalStateException("could not create savegame: " + name, e);
-        }
-        return this;
     }
 
     public void updateTimes(final int count) {
@@ -209,6 +198,59 @@ public class DefaultEnvironment implements Environment {
             addSystem(entitySystem);
         }
         return this;
+    }
+
+
+    @Override
+    public Environment createSavegame(final String name) {
+        requireNonNull(name, "name must not be null");
+        try (final var outputStream = new FileOutputStream(name)) {
+            try (final var zippedOutputStream = new GZIPOutputStream(outputStream)) {
+                try (final var objectOutputStream = new ObjectOutputStream(zippedOutputStream)) {
+                    objectOutputStream.writeObject(entities());
+                }
+            }
+        } catch (final IOException e) {
+            throw new IllegalStateException("could not create savegame: " + name, e);
+        }
+        return this;
+    }
+
+    @Override
+    public Environment loadSavegame(final String name) {
+        requireNonNull(name, "name must not be null");
+        clearEntities();
+        try (final var inputStream = new FileInputStream(name)) {
+            try (final var zippedInputStream = new GZIPInputStream(inputStream)) {
+                try (final var objectInputStream = new ObjectInputStream(zippedInputStream)) {
+                    final var allEntities = (List<Entity>) objectInputStream.readObject();
+                    addEntities(allEntities);
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new IllegalStateException("could not load entities", e);
+        }
+        return this;
+    }
+
+    @Override
+    public Environment deleteSavegame(final String name) {
+        try {
+            requireNonNull(name, "name must not be null");
+            final Path path = Path.of(name);
+            Files.delete(path);
+            savegameCache.clear(name);
+        } catch (IOException e) {
+            throw new IllegalStateException("could not delete savegame: " + name, e);
+        }
+        return this;
+
+    }
+
+    @Override
+    public boolean savegameExists(String name) {
+        requireNonNull(name, "name must not be null");
+        return savegameCache.getOrElse(name, () -> Files.exists(Path.of(name)));
     }
 
 }
