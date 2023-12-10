267f48fa4e4c03ebecb4a9cd4dfa034044f4afc7
simonbas <sebastian.simon@koeln.de>
2023-12-10 19:17:30 +0100

    fix hellow world

--- screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
+++ screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
@@ -3,7 +3,6 @@ package io.github.srcimon.screwbox.examples.helloworld;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
 
-import static io.github.srcimon.screwbox.core.graphics.Color.random;
 import static io.github.srcimon.screwbox.core.graphics.Pixelfont.defaultFont;
 
 public class HelloWorldExample {
@@ -13,7 +12,7 @@ public class HelloWorldExample {
 
         screwBox.environment().addSystem(engine -> {
             var screen = engine.graphics().screen();
-            screen.drawTextCentered(screen.center(), "HELLO WORLD!", defaultFont(random()), 2);
+            screen.drawTextCentered(screen.center(), "HELLO WORLD!", defaultFont(), 2);
         });
 
         screwBox.start();
