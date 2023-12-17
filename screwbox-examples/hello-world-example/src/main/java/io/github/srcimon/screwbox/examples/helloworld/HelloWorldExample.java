fcc66683a0c2bdca3cd447bb71cc7231f8f6c162
simonbas <sebastian.simon@koeln.de>
2023-12-17 22:39:58 +0100

    fixed

--- screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
+++ screwbox-examples/hello-world-example/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldExample.java
@@ -2,11 +2,6 @@ package io.github.srcimon.screwbox.examples.helloworld;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
-import io.github.srcimon.screwbox.core.Vector;
-import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
-import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
-import io.github.srcimon.screwbox.core.graphics.Color;
-import io.github.srcimon.screwbox.core.graphics.Sprite;
 
 import static io.github.srcimon.screwbox.core.graphics.Pixelfont.defaultFont;
 
@@ -15,11 +10,10 @@ public class HelloWorldExample {
     public static void main(String[] args) {
         Engine screwBox = ScrewBox.createEngine("Hello World Example");
 
-        screwBox.environment().setup().enableRendering().enablePhysics();
-        screwBox.environment().addEntity(
-                new TransformComponent(Vector.zero()),
-                new RenderComponent(Sprite.fromFile("default_font.png").replaceColor(Color.BLACK, Color.RED)));
-
+        screwBox.environment().addSystem(engine -> {
+            var screen = engine.graphics().screen();
+            screen.drawTextCentered(screen.center(), "HELLO WORLD!", defaultFont(), 2);
+        });
 
         screwBox.start();
     }
