4781d477385f023a81447e68d074d7a7defe605e
simonbas <sebastian.simon@koeln.de>
2024-02-01 07:21:22 +0100

    work on new loading scene

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
@@ -8,6 +8,7 @@ import io.github.srcimon.screwbox.core.graphics.Pixelfont;
 import io.github.srcimon.screwbox.core.graphics.Screen;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static io.github.srcimon.screwbox.core.graphics.Color.RED;
 import static io.github.srcimon.screwbox.core.graphics.Color.WHITE;
@@ -15,7 +16,7 @@ import static io.github.srcimon.screwbox.core.graphics.Size.square;
 
 public class DefaultLoadingScene implements Scene {
 
-    private List<String> texts = List.of("S","C","R","E","W","B","O","X");
+    private List<String> texts = List.of("L","O", "A","D","i","n","g", " ", "S", "C","R","E","W","B","O","X");
     @Override
     public void populate(final Environment environment) {
         environment.addSystem(engine -> drawLoadingAnimation(engine.graphics().screen()));
@@ -25,11 +26,17 @@ public class DefaultLoadingScene implements Scene {
         final int distance = 25;
         final double timeSeed = Time.now().milliseconds() / 400.0;
         int x = -distance * texts.size() / 2;
+        Color c = RED;
         for(var character : texts) {
             x+=distance;
-            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
+            if(c.equals(RED)) {
+                c = WHITE;
+            } else {
+                c = RED;
+            }
+            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 10);
             Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
-            screen.drawTextCentered(position, character, Pixelfont.defaultFont(), size / 4.0);
+            screen.drawTextCentered(position, character, Pixelfont.defaultFont(c), size / 4.0);
         }
 //        for (int x = -distance; x <= distance; x += distance) {
 //            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
