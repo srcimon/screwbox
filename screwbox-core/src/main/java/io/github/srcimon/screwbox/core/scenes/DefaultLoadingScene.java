bf63fb628df9b4b2fa327e00e8071cfd7fd481d7
simonbas <sebastian.simon@koeln.de>
2024-02-01 07:15:15 +0100

    work on new loading scene

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
@@ -4,7 +4,10 @@ import io.github.srcimon.screwbox.core.Time;
 import io.github.srcimon.screwbox.core.environment.Environment;
 import io.github.srcimon.screwbox.core.graphics.Color;
 import io.github.srcimon.screwbox.core.graphics.Offset;
+import io.github.srcimon.screwbox.core.graphics.Pixelfont;
 import io.github.srcimon.screwbox.core.graphics.Screen;
+
+import java.util.List;
 
 import static io.github.srcimon.screwbox.core.graphics.Color.RED;
 import static io.github.srcimon.screwbox.core.graphics.Color.WHITE;
@@ -12,6 +15,7 @@ import static io.github.srcimon.screwbox.core.graphics.Size.square;
 
 public class DefaultLoadingScene implements Scene {
 
+    private List<String> texts = List.of("S","C","R","E","W","B","O","X");
     @Override
     public void populate(final Environment environment) {
         environment.addSystem(engine -> drawLoadingAnimation(engine.graphics().screen()));
@@ -20,11 +24,18 @@ public class DefaultLoadingScene implements Scene {
     private void drawLoadingAnimation(final Screen screen) {
         final int distance = 25;
         final double timeSeed = Time.now().milliseconds() / 400.0;
-        for (int x = -distance; x <= distance; x += distance) {
+        int x = -distance * texts.size() / 2;
+        for(var character : texts) {
+            x+=distance;
             int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
-            Color color = x == 0 ? RED : WHITE;
             Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
-            screen.fillRectangle(position, square(size), color);
+            screen.drawTextCentered(position, character, Pixelfont.defaultFont(), size / 4.0);
         }
+//        for (int x = -distance; x <= distance; x += distance) {
+//            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
+//            Color color = x == 0 ? RED : WHITE;
+//            Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
+//            screen.fillRectangle(position, square(size), color);
+//        }
     }
 }
