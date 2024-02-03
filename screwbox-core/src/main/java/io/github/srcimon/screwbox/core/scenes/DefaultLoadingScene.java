7d70fd850a6a6494d5f01c2488e1e4f7ae8d436b
simonbas <sebastian.simon@koeln.de>
2024-02-03 10:57:44 +0100

    improve loading scene

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/DefaultLoadingScene.java
@@ -16,7 +16,8 @@ import static io.github.srcimon.screwbox.core.graphics.Size.square;
 
 public class DefaultLoadingScene implements Scene {
 
-    private List<String> texts = List.of("L","O", "A","D","i","n","g", " ", "S", "C","R","E","W","B","O","X");
+    private List<String> texts = List.of( "L","O","A","D","I","N","G");
+    private List<String> texts2 = List.of( "S","C","R","E","W","B","O","X");
     @Override
     public void populate(final Environment environment) {
         environment.addSystem(engine -> drawLoadingAnimation(engine.graphics().screen()));
@@ -24,25 +25,21 @@ public class DefaultLoadingScene implements Scene {
 
     private void drawLoadingAnimation(final Screen screen) {
         final int distance = 25;
-        final double timeSeed = Time.now().milliseconds() / 400.0;
+        final double timeSeed = Time.now().milliseconds() / 600.0;
         int x = -distance * texts.size() / 2;
-        Color c = RED;
         for(var character : texts) {
             x+=distance;
-            if(c.equals(RED)) {
-                c = WHITE;
-            } else {
-                c = RED;
-            }
-            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 10);
+            int size = (int) (Math.abs(Math.sin(x * 2 + timeSeed)) * 10);
             Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
-            screen.drawTextCentered(position, character, Pixelfont.defaultFont(c), size / 4.0);
+            screen.drawTextCentered(position, character, Pixelfont.defaultFont(), size / 4.0);
         }
-//        for (int x = -distance; x <= distance; x += distance) {
-//            int size = (int) (Math.abs(Math.sin(x * 4 + timeSeed)) * 14);
-//            Color color = x == 0 ? RED : WHITE;
-//            Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0));
-//            screen.fillRectangle(position, square(size), color);
-//        }
+
+         x = -distance * texts2.size() / 2;
+        for(var character : texts2) {
+            x+=distance;
+            int size = (int) (Math.abs(Math.sin(x * 2 + timeSeed)) * 10);
+            Offset position = screen.center().add((int) (-size / 2.0) + x, (int) (-size / 2.0) + 20);
+            screen.drawTextCentered(position, character, Pixelfont.defaultFont(RED), size / 4.0);
+        }
     }
 }
