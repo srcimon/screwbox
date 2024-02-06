e777a014c13dfb0534e08efed30977c638aa5b0a
simonbas <sebastian.simon@koeln.de>
2024-02-06 09:50:50 +0100

    add js page

---
+++ docs/app.js
@@ -0,0 +1,13 @@
+  let observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+       entry.target.classList.add('show');
+    } else {
+       entry.target.classList.remove('show');
+    }
+    });
+});
+
+let hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
