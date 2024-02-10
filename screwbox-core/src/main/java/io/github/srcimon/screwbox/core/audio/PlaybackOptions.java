e5973ee38712327af7af808b8088475d383393d8
simonbas <sebastian.simon@koeln.de>
2024-02-10 10:13:17 +0100

    javadoc for PlaybackOptions

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/PlaybackOptions.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/PlaybackOptions.java
@@ -1,17 +1,36 @@
 package io.github.srcimon.screwbox.core.audio;
 
-//TODO TEST JAVADOC AND RENAME
+import io.github.srcimon.screwbox.core.Percent;
+
+//TODO Test
+
+/**
+ * Sets options for the playback of a specific {@link Sound} via {@link Audio#playEffect(Sound, PlaybackOptions)} or {@link Audio#playMusic(Sound, PlaybackOptions)}.
+ */
 public class PlaybackOptions {
 
+    private final int times;
+    private Percent volume = Percent.max();
+
+    /**
+     * Playback {@link Sound} until stopped via {@link Audio#stop(Sound)}.
+     */
     public static PlaybackOptions playLooped() {
         return new PlaybackOptions(Integer.MAX_VALUE);
     }
 
+    /**
+     * Playback {@link Sound} only once.
+     */
     public static PlaybackOptions playOnce() {
         return playTimes(1);
     }
 
+    /**
+     * Playback {@link Sound} looped for the given times.
+     */
     public static PlaybackOptions playTimes(final int times) {
+        //TODO Validate
         return new PlaybackOptions(times);
     }
 
@@ -19,10 +38,27 @@ public class PlaybackOptions {
         this.times = times;
     }
 
-    private final int times;
+    /**
+     * Sets the volume of the playback. The actual playback volume is also determined by {@link AudioConfiguration}.
+     */
+    public PlaybackOptions volume(final Percent volume) {
+        this.volume = volume;
+        return this;
+    }
 
-    //TOODO: RENAME
+    /**
+     * Get the playback volume.
+     */
+    public Percent volume() {
+        return volume;
+    }
+
+    /**
+     * Get the number of time the sound is played.
+     */
     public int times() {
         return times;
     }
+
+
 }
