bc684c03719944e42ee26aaebfabf26887cbf47c
simonbas <sebastian.simon@koeln.de>
2024-02-12 12:07:22 +0100

    update visualization

--- examples/soundscape/src/main/java/io/github/srcimon/screwbox/examples/soundscape/SoundscapeApp.java
+++ examples/soundscape/src/main/java/io/github/srcimon/screwbox/examples/soundscape/SoundscapeApp.java
@@ -3,6 +3,7 @@ package io.github.srcimon.screwbox.examples.soundscape;
 import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.ScrewBox;
+import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.audio.Sound;
 import io.github.srcimon.screwbox.core.graphics.Color;
 import io.github.srcimon.screwbox.core.graphics.Pixelfont;
@@ -13,17 +14,21 @@ public class SoundscapeApp {
 
     public static void main(String[] args) {
         var screwBox = ScrewBox.createEngine("Soundscape");
-        screwBox.graphics().configuration().setUseAntialiasing(true);
+
         screwBox.environment()
-                .addSystem(engine -> {
+                .addSystem(engine -> { // play sound effect on mouse position
                     if (engine.mouse().isPressedLeft()) {
                         engine.audio().playEffect(Sound.dummyEffect(), engine.mouse().position());
                     }
-                }).addSystem(engine -> {
+                }).addSystem(engine -> { // visualize active sound effects
                     for (var playback : engine.audio().activePlaybacks()) {
                         Percent percentDone = Percent.of(Duration.since(playback.start()).milliseconds() * 1.0 / playback.sound().duration().milliseconds());
-                        engine.graphics().world().drawCircle(playback.position().get(),
-                                engine.audio().configuration().soundDistance() * percentDone.value(), Color.BLUE.opacity(percentDone.invert()), 8);
+
+                        engine.graphics().world().drawCircle(
+                                playback.position().get(),
+                                engine.audio().configuration().soundDistance() * percentDone.value(),
+                                Color.WHITE.opacity(percentDone.invert()),
+                                8);
                     }
                 });
 
