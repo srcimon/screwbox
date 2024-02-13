6aae1b0ed5542d8e862bfa861ef4ca797c83b5af
simonbas <sebastian.simon@koeln.de>
2024-02-13 19:53:27 +0100

    added javadoc

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/Playback.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/Playback.java
@@ -8,6 +8,10 @@ import io.github.srcimon.screwbox.core.Vector;
 import java.util.Optional;
 
 //TODO Test and javadoc
+
+/**
+ * Information about currently playing {@link Sound}. Retrieved via {@link Audio#activePlaybacks()}.
+ */
 public class Playback {
 
     private final Sound sound;
@@ -15,28 +19,46 @@ public class Playback {
     private final Vector position;
     private final Time startTime = Time.now();
 
+    /**
+     * Constructor used by the engine.
+     */
     public Playback(final Sound sound, final SoundOptions options, final Vector position) {
         this.sound = sound;
         this.options = options;
         this.position = position;
     }
 
+    /**
+     * The current progress of the {@link Playback} using {@link #startTime()} and {@link Sound#duration()}.
+     */
     public Percent progress() {
         return Percent.of(1.0 * Duration.since(startTime).nanos() / sound.duration().nanos());
     }
 
+    /**
+     * The time the {@link Playback} started.
+     */
     public Time startTime() {
         return startTime;
     }
 
+    /**
+     * The {@link Sound} that is played.
+     */
     public Sound sound() {
         return sound;
     }
 
+    /**
+     * The {@link SoundOptions} that were used to play the {@link Sound}.
+     */
     public SoundOptions options() {
         return options;
     }
 
+    /**
+     * The position of the {@link Sound} when a {@link Sound} was played via {@link Audio#playSound(Sound, Vector)}.
+     */
     public Optional<Vector> position() {
         return Optional.ofNullable(position);
     }
