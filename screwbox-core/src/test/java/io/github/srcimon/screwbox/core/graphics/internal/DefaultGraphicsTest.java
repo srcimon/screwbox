88fbdd7685cad9b2dfd23f1aa2beb8e9931769cd
simonbas <sebastian.simon@koeln.de>
2024-02-17 18:15:54 +0100

    added test

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultGraphicsTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultGraphicsTest.java
@@ -1,5 +1,6 @@
 package io.github.srcimon.screwbox.core.graphics.internal;
 
+import io.github.srcimon.screwbox.core.graphics.AspectRatio;
 import io.github.srcimon.screwbox.core.graphics.Size;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.awt.*;
 import java.util.List;
 
+import static io.github.srcimon.screwbox.core.graphics.AspectRatio.WIDESCREEN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -33,5 +35,19 @@ class DefaultGraphicsTest {
         List<Size> supportedResolutions = defaultGraphics.supportedResolutions();
 
         assertThat(supportedResolutions).containsExactly(Size.of(1024, 768), Size.of(800, 600));
+    }
+
+    @Test
+    void supportedResolutions_onlyWidescreen_returnsOnlyWidescreenResolutions() {
+        when(graphicsDevice.getDisplayModes()).thenReturn(List.of(
+                        new DisplayMode(800, 600, 16, 60),
+                        new DisplayMode(800, 600, 32, 60),
+                        new DisplayMode(1600, 900, 32, 60),
+                        new DisplayMode(1024, 768, 32, 60))
+                .toArray(new DisplayMode[]{}));
+
+        List<Size> supportedResolutions = defaultGraphics.supportedResolutions(WIDESCREEN);
+
+        assertThat(supportedResolutions).containsExactly(Size.of(1600, 900));
     }
 }
