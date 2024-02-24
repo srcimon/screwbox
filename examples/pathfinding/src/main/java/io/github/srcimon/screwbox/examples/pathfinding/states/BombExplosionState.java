d0b996a12036f5e5f0129faa3bfba15ecc684b88
simonbas <sebastian.simon@koeln.de>
2024-02-24 15:30:58 +0100

    fixings

--- examples/pathfinding/src/main/java/io/github/srcimon/screwbox/examples/pathfinding/states/BombExplosionState.java
+++ examples/pathfinding/src/main/java/io/github/srcimon/screwbox/examples/pathfinding/states/BombExplosionState.java
@@ -31,7 +31,7 @@ public class BombExplosionState implements EntityState {
         Sprite sprite = SPRITE.get().freshInstance();
         entity.get(RenderComponent.class).sprite = sprite;
         endOfAnimation = engine.loop().lastUpdate().plus(sprite.duration());
-        engine.audio().playSound(EXPLOSION);
+        engine.audio().playSound(EXPLOSION, entity.get(TransformComponent.class).bounds.position());
         Bounds bounds = entity.get(TransformComponent.class).bounds.inflated(8);
         List<Entity> entitiesInExplosionRange = engine.physics()
                 .searchInRange(bounds)
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultGraphics.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultGraphics.java
@@ -141,15 +141,9 @@ public class DefaultGraphics implements Graphics, Updatable {
 
     @Override
     public Vector moveCameraWithinVisualBounds(Vector delta, Bounds bounds) {
-        double width = bounds.width() - world.visibleArea().width();
-        double height = bounds.height() - world.visibleArea().height();
-//        if(width <= 0 || height <= 0) {
-//            System.out.println(width);
-//            return cameraPosition();
-//        }
         final var visualBounds = Bounds.atPosition(bounds.position(),
-                Math.max(0.1, width),
-                Math.max(0.1, height));
+                Math.max(1, bounds.width() - world.visibleArea().width()),
+                Math.max(1, bounds.height() - world.visibleArea().height()));
 
         final double movementX = MathUtil.clamp(
                 visualBounds.minX() -cameraPosition().x(),
