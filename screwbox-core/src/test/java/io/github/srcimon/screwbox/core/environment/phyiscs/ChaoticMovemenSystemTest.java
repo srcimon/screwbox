0b70cc47ff900108658ffd88147f1cc363c38d02
simonbas <sebastian.simon@koeln.de>
2024-02-27 09:49:01 +0100

    added Test

---
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/environment/phyiscs/ChaoticMovemenSystemTest.java
@@ -0,0 +1,34 @@
+package io.github.srcimon.screwbox.core.environment.phyiscs;
+
+import io.github.srcimon.screwbox.core.Duration;
+import io.github.srcimon.screwbox.core.Time;
+import io.github.srcimon.screwbox.core.environment.internal.DefaultEnvironment;
+import io.github.srcimon.screwbox.core.environment.physics.ChaoticMovementComponent;
+import io.github.srcimon.screwbox.core.environment.physics.ChaoticMovementSystem;
+import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
+import io.github.srcimon.screwbox.core.loop.Loop;
+import io.github.srcimon.screwbox.core.test.EnvironmentExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(EnvironmentExtension.class)
+class ChaoticMovemenSystemTest {
+
+    @Test
+    void update_addsMomentumToPhysicsEntitiesWithChaoticMovement(DefaultEnvironment environment, Loop loop) {
+        when(loop.lastUpdate()).thenReturn(Time.now());
+
+        PhysicsComponent physics = new PhysicsComponent();
+
+        environment.addEntity(physics, new ChaoticMovementComponent(20, Duration.ofSeconds(1)))
+                .addSystem(new ChaoticMovementSystem());
+
+        environment.update();
+
+        assertThat(physics.momentum.x()).isNotZero().isBetween(-20.0, 20.0);
+        assertThat(physics.momentum.y()).isNotZero().isBetween(-20.0, 20.0);
+    }
+}
