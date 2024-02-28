2e8ad165201fe7313c5979c955b4d170b39346f8
simonbas <sebastian.simon@koeln.de>
2024-02-28 20:11:39 +0100

    Enhanced graphics of `AutomovementDebugSystem`

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/debug/AutomovementDebugSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/debug/AutomovementDebugSystem.java
@@ -3,9 +3,9 @@ package io.github.srcimon.screwbox.core.environment.debug;
 import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Path;
+import io.github.srcimon.screwbox.core.environment.*;
 import io.github.srcimon.screwbox.core.environment.physics.AutomovementComponent;
 import io.github.srcimon.screwbox.core.graphics.*;
-import io.github.srcimon.screwbox.core.environment.*;
 
 import static java.util.Objects.nonNull;
 
@@ -18,18 +18,20 @@ public class AutomovementDebugSystem implements EntitySystem {
     @Override
     public void update(Engine engine) {
         World world = engine.graphics().world();
-        var grid = engine.physics().grid().get();
+
 
         for (Entity entity : engine.environment().fetchAll(PATH_CONTAINING)) {
             Path path = entity.get(AutomovementComponent.class).path;
 
             if (nonNull(path)) {
-                for (var node : grid.nodes()) {
-                    Bounds bounds = grid.worldArea(node);
-                    if (bounds.position().nearestOf(path.nodes()).distanceTo(bounds.position()) < grid.gridSize() * 2) {
-                        engine.graphics().world().drawRectangle(bounds, RectangleDrawOptions.filled(grid.isBlocked(node) ? Color.RED.opacity(0.5) : Color.GREEN.opacity(0.5)));
+                engine.physics().grid().ifPresent(grid -> {
+                    for (var node : grid.nodes()) {
+                        Bounds bounds = grid.worldArea(node);
+                        if (bounds.position().nearestOf(path.nodes()).distanceTo(bounds.position()) < grid.gridSize() * 2) {
+                            engine.graphics().world().drawRectangle(bounds, RectangleDrawOptions.filled(grid.isBlocked(node) ? Color.RED.opacity(0.5) : Color.GREEN.opacity(0.5)));
+                        }
                     }
-                }
+                });
                 for (var segment : path.segments()) {
                     world.drawLine(segment, LineDrawOptions.color(Color.YELLOW).strokeWidth(2));
                 }
