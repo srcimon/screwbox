dad81ace5a2b355a8161e556fe8d9a167500a2cc
simonbas <sebastian.simon@koeln.de>
2024-03-03 13:34:23 +0100

    fixing camera system when leaving bounds

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/camera/CameraSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/camera/CameraSystem.java
@@ -1,5 +1,6 @@
 package io.github.srcimon.screwbox.core.environment.camera;
 
+import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
@@ -19,21 +20,25 @@ public class CameraSystem implements EntitySystem {
     public void update(final Engine engine) {
         engine.environment().tryFetchSingleton(TARGET).ifPresent(targetEntity -> {
             final var cameraPosition = engine.graphics().camera().position();
-            final var targetPosition = targetEntity.get(TransformComponent.class).bounds.position();
+            final var targetBounds = targetEntity.get(TransformComponent.class).bounds;
 
             final var target = targetEntity.get(CameraTargetComponent.class);
-            if (target.allowJumping && targetPosition.distanceTo(cameraPosition) > engine.graphics().world().visibleArea().width() / 2.0) {
-                engine.graphics().camera().setPosition(targetPosition);
+            final var configuration = engine.environment().tryFetchSingletonComponent(CameraBoundsComponent.class);
+            if (target.allowJumping && targetBounds.position().distanceTo(cameraPosition) > engine.graphics().world().visibleArea().width() / 2.0) {
+                if(configuration.isPresent()) {
+                    if(configuration.get().cameraBounds.expand(-targetBounds.extents().length()).contains(targetBounds.position())) {
+                        engine.graphics().camera().setPosition(targetBounds.position());
+                    }
+                }
                 return;
             }
 
             final Vector distance = cameraPosition
-                    .substract(targetPosition)
+                    .substract(targetBounds.position())
                     .substract(target.shift);
 
             final Vector cameraMovement = distance.multiply(clamp(-1, engine.loop().delta(-1 * target.followSpeed), 1));
 
-            final var configuration = engine.environment().tryFetchSingletonComponent(CameraBoundsComponent.class);
             if (configuration.isPresent()) {
                 engine.graphics().camera().moveWithinVisualBounds(cameraMovement, configuration.get().cameraBounds);
             } else {
