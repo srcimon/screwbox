e2b4581787f4e0eb8997ddb4470ec88b9e43167d
simonbas <sebastian.simon@koeln.de>
2024-03-13 22:19:49 +0100

    add new drawing method

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteDrawOptions.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteDrawOptions.java
@@ -5,19 +5,8 @@ import io.github.srcimon.screwbox.core.Rotation;
 
 //TODO: camerashakeoptions immutable
 //TODO javadoc and test
-public class SpriteDrawOptions {
+public record SpriteDrawOptions(double scale, Percent opacity, Rotation rotation, Flip flip) {
 
-    private final double scale;
-    private final Percent opacity ;
-    private final Rotation rotation ;
-    private final  Flip flip;
-
-    private SpriteDrawOptions(double scale, Percent opacity, Rotation rotation, Flip flip) {
-        this.scale = scale;
-        this.opacity = opacity;
-        this.rotation = rotation;
-        this.flip = flip;
-    }
     private SpriteDrawOptions(final double scale) {
         this(scale, Percent.max(), Rotation.none(), Flip.NONE);
     }
@@ -48,22 +37,6 @@ public class SpriteDrawOptions {
     public SpriteDrawOptions flip(final Flip flip) {
         //TODO validation
         return new SpriteDrawOptions(scale, opacity, rotation, flip);
-    }
-
-    public double scale() {
-        return scale;
-    }
-
-    public Percent opacity() {
-        return opacity;
-    }
-
-    public Rotation rotation() {
-        return rotation;
-    }
-
-    public Flip flip() {
-        return flip;
     }
 
 }
