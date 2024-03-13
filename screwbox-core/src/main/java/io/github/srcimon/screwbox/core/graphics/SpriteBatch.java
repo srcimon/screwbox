d6c93baff35e1ace96c71ffe50134def6c3be5e8
simonbas <sebastian.simon@koeln.de>
2024-03-13 21:41:58 +0100

    add new drawing method

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteBatch.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteBatch.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+//TODO Javadoc
 public class SpriteBatch {
 
     private final List<SpriteBatchEntry> entries = new ArrayList<>();
