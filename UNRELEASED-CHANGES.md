4422bcf466ea84c7af16c4e8091003f3ac886ae5
simonbas <sebastian.simon@koeln.de>
2024-04-02 22:04:25 +0200

    added Javadoc and changelog

--- UNRELEASED-CHANGES.md
+++ UNRELEASED-CHANGES.md
@@ -1,5 +1,6 @@
 ### 🚀 Features & improvements
 
+- Optimized drawing of sprite fill using `SpriteFillOptions` (#242)
 - Slightly improved rendering performance due to performance optimized rendering hints
 - Major performance improvement on drawing image fill
 
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteFillOptions.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/SpriteFillOptions.java
@@ -2,17 +2,41 @@ package io.github.srcimon.screwbox.core.graphics;
 
 import io.github.srcimon.screwbox.core.Percent;
 
-//TODO Javadoc
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Customize the drawing of filling the {@link Screen} with {@link Sprite}s.
+ *
+ * @param offset  the {@link Offset} used to fill the {@link Screen}
+ * @param scale   the scale of the {@link Sprite}s used to fill the {@link Screen}
+ * @param opacity the opacity used to fill the {@link Screen}
+ * @see Screen#fillWith(Sprite, SpriteFillOptions)
+ */
 public record SpriteFillOptions(Offset offset, double scale, Percent opacity) {
 
+    //TODO test
+    public SpriteFillOptions {
+        requireNonNull(offset, "offset must not be null");
+        requireNonNull(opacity, "opacity must not be null");
+    }
+
+    /**
+     * Creates a new instance with given {@link #scale()}.
+     */
     public static SpriteFillOptions scale(final double scale) {
         return new SpriteFillOptions(Offset.origin(), scale, Percent.max());
     }
 
+    /**
+     * Creates a new instance with given {@link #offset()}
+     */
     public SpriteFillOptions offset(final Offset offset) {
         return new SpriteFillOptions(offset, scale, opacity);
     }
 
+    /**
+     * Creates a new instance with given {@link #opacity()}.
+     */
     public SpriteFillOptions opacity(final Percent opacity) {
         return new SpriteFillOptions(offset, scale, opacity);
     }
