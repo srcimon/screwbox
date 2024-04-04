7985e6d7667e5e524c347a1729d8fd008f0b3d23
simonbas <sebastian.simon@koeln.de>
2024-04-04 07:43:14 +0200

    skinny sans

--- examples/hello-world/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldApp.java
+++ examples/hello-world/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldApp.java
@@ -3,7 +3,6 @@ package io.github.srcimon.screwbox.examples.helloworld;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
 import io.github.srcimon.screwbox.core.assets.Fonts;
-import io.github.srcimon.screwbox.core.graphics.Color;
 
 public class HelloWorldApp {
 
@@ -13,9 +12,10 @@ public class HelloWorldApp {
 
         screwBox.environment().addSystem(engine -> {
             var screen = engine.graphics().screen();
-            screen.drawTextCentered(screen.center(), "Aber ABER HELLO WORLD!", Fonts.SKINNY_SANS_WHITE.get(),  4);
+            screen.drawTextCentered(screen.center(), "The quick brown fox jumps over the lazy dog. Jinxed wizards pluck ivy from the big quilt.", Fonts.SKINNY_SANS_WHITE.get(), 2);
+            screen.drawTextCentered(screen.center().addY(20), "Jaded zombies acted quaintly but kept driving their dizzy oxen forward.", Fonts.SKINNY_SANS_WHITE.get(), 2);
+            screen.drawTextCentered(screen.center().addY(40), "We promptly judged antique ivory buckles for the next prize.", Fonts.SKINNY_SANS_WHITE.get(), 2);
         });
-
 
         screwBox.start();
     }
