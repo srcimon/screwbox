5c946637ff28b86bf3f3ad9c74c3e194ec735c82
simonbas <sebastian.simon@koeln.de>
2024-04-04 19:45:35 +0200

    skinny sans

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/Fonts.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/Fonts.java
@@ -21,7 +21,7 @@ public class Fonts {
 
     public static final Asset<Pixelfont> SKINNY_SANS_BLACK = asset(() -> loadFont("assets/pixelfonts/skinny_sans.png", Size.square(8),
             'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q'
-            , 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', ' '));
+            , 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',  ' ', '.', ',', ':', '!', '?', '-'));
 
     public static final Asset<Pixelfont> SKINNY_SANS_WHITE = asset(() -> SKINNY_SANS_BLACK.get().replaceBlack(Color.WHITE));
 
