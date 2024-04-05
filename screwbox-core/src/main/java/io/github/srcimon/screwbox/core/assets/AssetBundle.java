231198d58b5fe8c7e793a63963a0cf9f52189619
simonbas <sebastian.simon@koeln.de>
2024-04-05 19:51:37 +0200

    simplify

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
@@ -1,8 +1,26 @@
 package io.github.srcimon.screwbox.core.assets;
 
-import java.util.function.Supplier;
+import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.graphics.Pixelfont;
+import io.github.srcimon.screwbox.core.graphics.Size;
+import io.github.srcimon.screwbox.core.graphics.Sprite;
 
-//TODO only implemented by enum???
-public interface AssetBundle<T > extends Supplier<T> {
+import java.util.Arrays;
+
+import static io.github.srcimon.screwbox.core.assets.Asset.asset;
+
+public final class AssetBundle {
+
+    public static final Asset<Pixelfont> FONT_SCREWBOX_BLACK = asset(() -> loadFont("assets/pixelfonts/default_font.png", Size.square(8), 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'));
+    public static final Asset<Pixelfont> FONT_SCREWBOX_WHITE = asset(() -> FONT_SCREWBOX_BLACK.get().replaceColor(Color.BLACK, Color.WHITE));
+    public static final Asset<Pixelfont> FONT_SKINNY_SANS_BLACK = asset(() -> loadFont("assets/pixelfonts/skinny_sans.png", Size.square(8), 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'));
+    public static final Asset<Pixelfont> FONT_SKINNY_SANS_WHITE = asset(() -> FONT_SKINNY_SANS_BLACK.get().replaceColor(Color.BLACK, Color.WHITE));
+
+    private static Pixelfont loadFont(String resouce, Size size, Character... characters) {
+        final Pixelfont font = new Pixelfont();
+        final var sprites = Sprite.multipleFromFile(resouce, size);
+        font.addCharacters(Arrays.asList(characters), sprites.stream().map(Sprite::cropHorizontal).toList());
+        return font;
+    }
 
 }
