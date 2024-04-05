c33132fd9d7ecfd62e3c094af5d6d3320e9da464
simonbas <sebastian.simon@koeln.de>
2024-04-05 20:27:24 +0200

    simplify

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
@@ -12,9 +12,7 @@ import static io.github.srcimon.screwbox.core.assets.Asset.asset;
 public final class AssetBundle {
 
     public static final Asset<Pixelfont> FONT_SCREWBOX_BLACK = asset(() -> loadFont("assets/pixelfonts/default_font.png", Size.square(8), 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'));
-    public static final Asset<Pixelfont> FONT_SCREWBOX_WHITE = asset(() -> FONT_SCREWBOX_BLACK.get().replaceColor(Color.BLACK, Color.WHITE));
     public static final Asset<Pixelfont> FONT_SKINNY_SANS_BLACK = asset(() -> loadFont("assets/pixelfonts/skinny_sans.png", Size.square(8), 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'));
-    public static final Asset<Pixelfont> FONT_SKINNY_SANS_WHITE = asset(() -> FONT_SKINNY_SANS_BLACK.get().replaceColor(Color.BLACK, Color.WHITE));
 
     private static Pixelfont loadFont(String resouce, Size size, Character... characters) {
         final Pixelfont font = new Pixelfont();
---
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/Fonts.java
@@ -0,0 +1,43 @@
+package io.github.srcimon.screwbox.core.assets;
+
+import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.graphics.Pixelfont;
+import io.github.srcimon.screwbox.core.graphics.Size;
+import io.github.srcimon.screwbox.core.graphics.Sprite;
+import io.github.srcimon.screwbox.core.utils.Cache;
+
+import java.util.Arrays;
+
+import static io.github.srcimon.screwbox.core.assets.Asset.asset;
+
+public enum Fonts {
+
+    SCREWBOX(asset(() -> loadFont("assets/pixelfonts/default_font.png", Size.square(8), 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'))),
+    SKINNY_SANS(asset(() -> loadFont("assets/pixelfonts/skinny_sans.png", Size.square(8), 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-')));
+
+    private final Cache<Color, Pixelfont> cache = new Cache<>();
+    private final Asset<Pixelfont> asset;
+
+    Fonts(final Asset<Pixelfont> asset) {
+        this.asset = asset;
+    }
+
+    public Pixelfont black() {
+        return customColor(Color.BLACK);
+    }
+
+    public Pixelfont white() {
+        return customColor(Color.WHITE);
+    }
+
+    public Pixelfont customColor(final Color color) {
+        return cache.getOrElse(color, () -> asset.get().replaceColor(Color.BLACK, color));
+    }
+
+    private static Pixelfont loadFont(String resouce, Size size, Character... characters) {
+        final Pixelfont font = new Pixelfont();
+        final var sprites = Sprite.multipleFromFile(resouce, size);
+        font.addCharacters(Arrays.asList(characters), sprites.stream().map(Sprite::cropHorizontal).toList());
+        return font;
+    }
+}
