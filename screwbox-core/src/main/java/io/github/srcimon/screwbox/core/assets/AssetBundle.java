605545cc1f6402deae1eeb986fd2c23aea469e28
simonbas <sebastian.simon@koeln.de>
2024-04-06 19:59:54 +0200

    added javadoc

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/AssetBundle.java
@@ -1,11 +1,24 @@
 package io.github.srcimon.screwbox.core.assets;
 
 import java.util.function.Supplier;
-//TODO RENAME  FontsBundle, SpritesBundle, SoundsBundle
+
+/**
+ * Marks enums (e.g. {@link FontsBundle}) to serve as container for {@link Assets}.
+ * Marking non enums with this interface will result in {@link IllegalStateException}.
+ * 
+ * @see Assets#prepareClassPackage(Class) 
+ */
 public interface AssetBundle<T> extends Supplier<T> {
 
+    /**
+     * The {@link Asset} represented by each enum value.
+     */
     Asset<T> asset();
 
+    /**
+     * Getter to get the {@link Asset} content directly.
+     * @return
+     */
     @Override
     default T get() {
         return asset().get();
