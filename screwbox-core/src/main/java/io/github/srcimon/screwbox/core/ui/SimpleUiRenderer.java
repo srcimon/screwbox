08ecfd6bf233b92492b7ff95490a621c5db14e4a
simonbas <sebastian.simon@koeln.de>
2024-04-07 15:03:55 +0200

    started to remove old methods

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/SimpleUiRenderer.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/SimpleUiRenderer.java
@@ -1,32 +1,34 @@
 package io.github.srcimon.screwbox.core.ui;
 
+import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.assets.FontsBundle;
 import io.github.srcimon.screwbox.core.graphics.Pixelfont;
 import io.github.srcimon.screwbox.core.graphics.Screen;
 import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
+import io.github.srcimon.screwbox.core.graphics.TextDrawOptions;
 
 import static io.github.srcimon.screwbox.core.graphics.Color.WHITE;
 import static io.github.srcimon.screwbox.core.graphics.Color.YELLOW;
 
 public class SimpleUiRenderer implements UiRenderer {
 
-    private static final Pixelfont NOT_SELECTED = FontsBundle.BOLDZILLA.getWhite();
-    private static final Pixelfont SELECTED = FontsBundle.BOLDZILLA.getCustomColor(YELLOW);
-    private static final Pixelfont INACTIVE = FontsBundle.BOLDZILLA.getCustomColor(WHITE.opacity(0.2));
+    private static final Asset<Pixelfont> NOT_SELECTED = FontsBundle.BOLDZILLA.white();
+    private static final Asset<Pixelfont> SELECTED = FontsBundle.BOLDZILLA.customColor(YELLOW);
+    private static final Asset<Pixelfont> INACTIVE = FontsBundle.BOLDZILLA.customColor(WHITE.opacity(0.2));
 
     @Override
     public void renderSelectableItem(String label, ScreenBounds bounds, Screen screen) {
-        screen.drawTextCentered(bounds.center(), label, NOT_SELECTED, 2);
+        screen.drawText(bounds.center(), label, TextDrawOptions.font(NOT_SELECTED).scale(2));
     }
 
     @Override
     public void renderSelectedItem(String label, ScreenBounds bounds, Screen screen) {
-        screen.drawTextCentered(bounds.center(), label, SELECTED, 2.5);
+        screen.drawText(bounds.center(), label, TextDrawOptions.font(SELECTED).scale(2.5));
     }
 
     @Override
     public void renderInactiveItem(String label, ScreenBounds bounds, Screen screen) {
-        screen.drawTextCentered(bounds.center(), label, INACTIVE, 2);
+        screen.drawText(bounds.center(), label, TextDrawOptions.font(INACTIVE).scale(2));
     }
 
 }
