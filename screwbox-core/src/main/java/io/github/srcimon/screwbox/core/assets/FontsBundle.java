bf0f576e4b50c8e8128945930ed9e3531031cb41
simonbas <sebastian.simon@koeln.de>
2024-04-07 15:13:16 +0200

    started to remove old methods

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/FontsBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/assets/FontsBundle.java
@@ -20,7 +20,7 @@ public enum FontsBundle implements AssetBundle<Pixelfont> {
     BOLDZILLA(Asset.asset(() -> loadFont("assets/pixelfonts/BOLDZILLA.png", Size.square(8),
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-'))),
 
-    SKINNY_SANS(Asset.asset(() -> loadFont("assets/pixelfonts/SKINNY_SANS.png", Size.square(8),
+    SKINNY_SANS(Asset.asset(() -> loadFontCropped("assets/pixelfonts/SKINNY_SANS.png", Size.square(8),
             'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ',', ':', '!', '?', '-')));
 
     private final Cache<Color, Asset<Pixelfont>> cache = new Cache<>();
@@ -64,6 +64,12 @@ public enum FontsBundle implements AssetBundle<Pixelfont> {
     }
 
     private static Pixelfont loadFont(final String resouce, final Size size, final Character... characters) {
+        final Pixelfont font = new Pixelfont();
+        final var sprites = Sprite.multipleFromFile(resouce, size);
+        font.addCharacters(Arrays.asList(characters), sprites);
+        return font;
+    }
+    private static Pixelfont loadFontCropped(final String resouce, final Size size, final Character... characters) {
         final Pixelfont font = new Pixelfont();
         final var sprites = Sprite.multipleFromFile(resouce, size);
         font.addCharacters(Arrays.asList(characters), sprites.stream().map(Sprite::cropHorizontal).toList());
