89c9af7f41a954e9c62f34b57bbdd046076836bd
simonbas <sebastian.simon@koeln.de>
2024-04-08 19:36:06 +0200

    remove particles

--- examples/hello-world/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldApp.java
+++ examples/hello-world/src/main/java/io/github/srcimon/screwbox/examples/helloworld/HelloWorldApp.java
@@ -1,66 +1,21 @@
 package io.github.srcimon.screwbox.examples.helloworld;
 
-import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
-import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.ScrewBox;
-import io.github.srcimon.screwbox.core.Vector;
-import io.github.srcimon.screwbox.core.environment.Entity;
-import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
-import io.github.srcimon.screwbox.core.environment.debug.LogFpsSystem;
-import io.github.srcimon.screwbox.core.environment.particles.ParticleDebugSystem;
-import io.github.srcimon.screwbox.core.environment.particles.ParticleEmitterComponent;
-import io.github.srcimon.screwbox.core.environment.particles.ParticleFactory;
-import io.github.srcimon.screwbox.core.environment.particles.ParticleSystem;
-import io.github.srcimon.screwbox.core.environment.physics.ChaoticMovementComponent;
-import io.github.srcimon.screwbox.core.environment.tweening.TweenOpacityComponent;
-import io.github.srcimon.screwbox.core.graphics.Sprite;
-import io.github.srcimon.screwbox.core.keyboard.Key;
 
-import static io.github.srcimon.screwbox.core.Duration.ofMillis;
-import static io.github.srcimon.screwbox.core.utils.Sheduler.withInterval;
+import static io.github.srcimon.screwbox.core.assets.FontsBundle.BOLDZILLA;
+import static io.github.srcimon.screwbox.core.graphics.TextDrawOptions.font;
 
-//TODO fixup example
-//TODO MAKE SMOKE SPRITE PART O BOUNDLE
 public class HelloWorldApp {
 
     public static void main(String[] args) {
         Engine screwBox = ScrewBox.createEngine("Hello World");
 
-        ParticleFactory particleFactory = ParticleFactory
-                .template(Sprite.fromFile("sprite-0001.png"))
-                .useRandomStartRotation()
-                .scale(5)
-                .maxAge(Duration.ofSeconds(5))
-                .addCustomizer(() -> new ChaoticMovementComponent(60, Duration.ofMillis(1500), Vector.y(-100)))
-                .addCustomizer(() -> new TweenOpacityComponent(Percent.zero(), Percent.of(0.2)));
-
-
-        screwBox.environment()
-                .addSystem(new LogFpsSystem())
-                .enablePhysics()
-                .enableRendering()
-                .enableTweening()
-                .addSystem(engine -> {
-                    if (engine.keyboard().isPressed(Key.ENTER)) {
-                        engine.environment().createSavegame("X");
-                    }
-                })
-                .addSystem(new ParticleSystem())
-                .addSystem(engine -> {
-                    Entity emitter = engine.environment().fetchById(1);
-                    var transform = emitter.get(TransformComponent.class);
-                    transform.bounds = transform.bounds.expand(engine.mouse().unitsScrolled() * 4);
-                    emitter.moveTo(engine.mouse().position());
-                })
-                .addSystem(engine -> {
-                    if (engine.mouse().isPressedLeft()) {
-                        engine.environment().toggleSystem(new ParticleDebugSystem());
-                    }
-                })
-                .addEntity(1,
-                        new TransformComponent(0, 0, 64, 64),
-                        new ParticleEmitterComponent(withInterval(ofMillis(15)), particleFactory));
+        screwBox.environment().addSystem(engine -> {
+            var screen = engine.graphics().screen();
+            var drawOptions = font(BOLDZILLA).scale(4).alignCenter();
+            screen.drawText(screen.center(), "Hello world!", drawOptions);
+        });
 
         screwBox.start();
     }
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/particles/ParticleComponent.java
+++
@@ -1,6 +0,0 @@
-package io.github.srcimon.screwbox.core.environment.particles;
-
-import io.github.srcimon.screwbox.core.environment.Component;
-
-public class ParticleComponent implements Component {
-}
