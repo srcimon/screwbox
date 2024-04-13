446173c00af9b47e29df9f3830307ad12c320b07
simonbas <sebastian.simon@koeln.de>
2024-04-13 22:15:59 +0200

    added tests

--- UNRELEASED-CHANGES.md
+++ UNRELEASED-CHANGES.md
@@ -3,12 +3,11 @@ This is a huge content update.
 ### 🚀 Features & improvements
 
 - Optimized drawing of text with pixelfonts via `TextDrawOptions` (#266)
-- Added new characters to `SKINNY_SANS`: `(`, `)`,`[`,`]`
-- Added particle effects related systems and componets (#134)
-- Added `TweenScaleComponent` / `System`
-- Added new example showing particle capabilities (#280)
-- Added new assets to `SpritesBundle`: `SMOKE_16`, `BOX_STRIPED_32`, `DOT_BLUE_16`, `DOT_YELLOW_16`, `DOT_RED_16`
+- Added support for particle effects (#134)
 - Added task bar icon (#283)
+- Added `TweenScaleComponent` / `System`
+- Added new assets to `SpritesBundle`: `SMOKE_16`, `BOX_STRIPED_32`, `DOT_BLUE_16`, `DOT_YELLOW_16`, `DOT_RED_16`
+- Added new characters to `SKINNY_SANS`: `(`, `)`,`[`,`]`
 - Added `Rotation.random()`
 - Added `TweenMode.SIN_IN_OUT_TWICE`
 
--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/particles/ParticleOptionsTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/particles/ParticleOptionsTest.java
@@ -1,6 +1,11 @@
 package io.github.srcimon.screwbox.core.particles;
 
+import io.github.srcimon.screwbox.core.Duration;
+import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.assets.SpritesBundle;
+import io.github.srcimon.screwbox.core.environment.Entity;
+import io.github.srcimon.screwbox.core.environment.tweening.TweenComponent;
+import io.github.srcimon.screwbox.core.environment.tweening.TweenOpacityComponent;
 import io.github.srcimon.screwbox.core.environment.tweening.TweenScaleComponent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -53,5 +58,34 @@ class ParticleOptionsTest {
         assertThatThrownBy(() -> noMoreCustomizable.customize("modifier-nr-", entity -> entity.name("some name")))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("added more than 100 modifiers");
+    }
+
+    @Test
+    void lifetimeSeconds_setsTweenModeDuration() {
+        Entity particle = applyOptionsOnTemplateParticle(options.lifetimeSeconds(4));
+
+        assertThat(particle.get(TweenComponent.class).duration).isEqualTo(Duration.ofSeconds(4));
+    }
+
+    @Test
+    void randomLifeTimeSeconds_validRange_setsTweenModeDurationOnRandomValueInRange() {
+        Entity particle = applyOptionsOnTemplateParticle(options.randomLifeTimeSeconds(4, 20));
+
+        assertThat(particle.get(TweenComponent.class).duration.seconds()).isBetween(4l, 20l);
+    }
+
+    @Test
+    void animateOpacity_addsTweenOpacityComponent() {
+        Entity particle = applyOptionsOnTemplateParticle(options.animateOpacity());
+
+        assertThat(particle.hasComponent(TweenOpacityComponent.class)).isTrue();
+        assertThat(particle.get(TweenOpacityComponent.class).from).isEqualTo(Percent.zero());
+        assertThat(particle.get(TweenOpacityComponent.class).to).isEqualTo(Percent.max());
+    }
+
+    private Entity applyOptionsOnTemplateParticle(ParticleOptions result) {
+        Entity particle = new Entity().add(new TweenComponent(Duration.ofSeconds(100)));
+        result.modifiers().forEach(modifier -> modifier.accept(particle));
+        return particle;
     }
 }
