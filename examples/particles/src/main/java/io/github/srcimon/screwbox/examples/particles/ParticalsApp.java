32a5a1c52b89c74a57f69c7655b36a8266541817
simonbas <sebastian.simon@koeln.de>
2024-04-16 21:17:55 +0200

    prototype particle interaction

--- examples/particles/src/main/java/io/github/srcimon/screwbox/examples/particles/ParticalsApp.java
+++ examples/particles/src/main/java/io/github/srcimon/screwbox/examples/particles/ParticalsApp.java
@@ -3,43 +3,37 @@ package io.github.srcimon.screwbox.examples.particles;
 import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.ScrewBox;
-import io.github.srcimon.screwbox.core.Vector;
-import io.github.srcimon.screwbox.core.assets.ParticleOptionsBundle;
-import io.github.srcimon.screwbox.core.assets.SpritesBundle;
 import io.github.srcimon.screwbox.core.environment.core.LogFpsSystem;
 import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
 import io.github.srcimon.screwbox.core.environment.particles.ParticleInteractionComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
-import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
 import io.github.srcimon.screwbox.core.utils.Sheduler;
+
+import static io.github.srcimon.screwbox.core.assets.ParticleOptionsBundle.CONFETTI;
 
 public class ParticalsApp {
 
     public static void main(String[] args) {
-        Engine screwBox = ScrewBox.createEngine("Hello World");
-        Sheduler spawn = Sheduler.withInterval(Duration.ofMillis(50));
+        Engine screwBox = ScrewBox.createEngine("Particles");
+
+        final Sheduler particleSpawnSheduler = Sheduler.withInterval(Duration.ofMillis(20));
+
         screwBox.environment()
                 .enableTweening()
                 .enableParticles()
                 .enablePhysics()
+                .addSystem(engine -> {
+                    if (particleSpawnSheduler.isTick()) {
+                        engine.particles().spawn(engine.graphics().world().visibleArea(), CONFETTI);
+                    }
+                })
                 .addSystem(new LogFpsSystem())
-                .addSystem(e -> e.environment().fetchAllHaving(ParticleInteractionComponent.class).forEach(
-                        entity -> entity.moveTo(e.mouse().position())))
-                .enableRendering()
-                .addEntity(new PhysicsComponent(),
-                        new RenderComponent(SpritesBundle.DOT_YELLOW_16),
+                .addEntity(1, "particle interactor",
+                        new PhysicsComponent(),
                         new TransformComponent(-60, -60, 120, 120),
-                        new ParticleInteractionComponent(80));
-
-
-        screwBox.environment().addSystem(engine -> {
-            if (spawn.isTick()) {
-                engine.particles().spawnMultiple(3, engine.graphics().world().visibleArea(), ParticleOptionsBundle.CONFETTI.get()
-                        .sprites(SpritesBundle.DOT_BLUE_16)
-
-                );
-            }
-        });
+                        new ParticleInteractionComponent(80))
+                .addSystem(engine -> engine.environment().fetchById(1).moveTo(engine.mouse().position()))
+                .enableRendering();
 
         screwBox.start();
     }
