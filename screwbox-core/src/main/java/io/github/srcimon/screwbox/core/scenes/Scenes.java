87dccc546fdd3bec2c1d5e126c3db91d25ae7736
simonbas <sebastian.simon@koeln.de>
2024-04-20 12:02:17 +0200

    added JavaDoc to Scenes

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/Scenes.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/Scenes.java
@@ -2,10 +2,19 @@ package io.github.srcimon.screwbox.core.scenes;
 
 import io.github.srcimon.screwbox.core.environment.Environment;
 
+/**
+ * Manage different game situations like pause or options in different {@link Scenes}.
+ */
 public interface Scenes {
 
     Scenes addOrReplace(Scene scene);
 
+    /**
+     * Returns {@code true} if the scene of the specified class is present.
+     *
+     * @param sceneClass class of the {@link Scene}
+     * @return true if {@link Scene} is present
+     */
     boolean contains(Class<? extends Scene> sceneClass);
 
     Scenes add(Scene... scenes);
