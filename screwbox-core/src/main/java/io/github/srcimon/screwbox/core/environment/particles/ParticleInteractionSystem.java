922ae1dd11dec4372ffbbc59683fe806a420cac8
simonbas <sebastian.simon@koeln.de>
2024-04-21 15:40:30 +0200

    fixed Sonar issues

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/particles/ParticleInteractionSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/particles/ParticleInteractionSystem.java
@@ -2,9 +2,12 @@ package io.github.srcimon.screwbox.core.environment.particles;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.environment.Archetype;
+import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
+
+import java.util.List;
 
 import static java.util.Objects.isNull;
 
@@ -21,25 +24,29 @@ public class ParticleInteractionSystem implements EntitySystem {
         }
         final var particles = engine.environment().fetchAll(PARTICLES);
         for (final var interactor : interactors) {
-            final var interaction = interactor.get(ParticleInteractionComponent.class);
-            if (isNull(interaction.lastPos)) {
-                interaction.lastPos = interactor.position();
-            }
-            final var momentum = interactor.position()
-                    .substract(interaction.lastPos)
-                    .multiply(1 / engine.loop().delta() * interaction.modifier.value());
+            applyMomentumOnParticles(particles, interactor, engine.loop().delta());
+        }
+    }
 
+    private void applyMomentumOnParticles(final List<Entity> particles, final Entity interactor, final double delta) {
+        final var interaction = interactor.get(ParticleInteractionComponent.class);
+        if (isNull(interaction.lastPos)) {
             interaction.lastPos = interactor.position();
-            if (!momentum.isZero()) {
-                final double momentumLength = momentum.length();
-                final var interactionBounds = interactor.bounds().expand(interaction.range);
+        }
+        final var momentum = interactor.position()
+                .substract(interaction.lastPos)
+                .multiply(1 / delta * interaction.modifier.value());
 
-                for (final var particle : particles) {
-                    if (particle.bounds().intersects(interactionBounds)) {
-                        var physics = particle.get(PhysicsComponent.class);
-                        if (physics.momentum.length() < momentumLength) {
-                            physics.momentum = physics.momentum.add(momentum.multiply(engine.loop().delta()));
-                        }
+        interaction.lastPos = interactor.position();
+        if (!momentum.isZero()) {
+            final double momentumLength = momentum.length();
+            final var interactionBounds = interactor.bounds().expand(interaction.range);
+
+            for (final var particle : particles) {
+                if (particle.bounds().intersects(interactionBounds)) {
+                    var physics = particle.get(PhysicsComponent.class);
+                    if (physics.momentum.length() < momentumLength) {
+                        physics.momentum = physics.momentum.add(momentum.multiply(delta));
                     }
                 }
             }
