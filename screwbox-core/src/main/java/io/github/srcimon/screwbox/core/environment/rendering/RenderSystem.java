bc53748f917f90bb02e156bafdfdde029dfd6bf8
simonbas <sebastian.simon@koeln.de>
2024-04-25 07:15:56 +0200

    avoid using batch api

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderSystem.java
@@ -2,23 +2,55 @@ package io.github.srcimon.screwbox.core.environment.rendering;
 
 import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.environment.Order;
 import io.github.srcimon.screwbox.core.environment.SystemOrder;
 import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
-import io.github.srcimon.screwbox.core.graphics.SpriteBatch;
+import io.github.srcimon.screwbox.core.graphics.Sprite;
+import io.github.srcimon.screwbox.core.graphics.SpriteDrawOptions;
+import io.github.srcimon.screwbox.core.graphics.World;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Order(SystemOrder.PRESENTATION_WORLD)
 public class RenderSystem implements EntitySystem {
+
+    public class Batch {
+
+        private final List<BatchEntry> entries = new ArrayList<>();
+
+        public record BatchEntry(Sprite sprite, Vector position, SpriteDrawOptions options, int drawOrder)
+                implements Comparable<BatchEntry> {
+
+            @Override
+            public int compareTo(final BatchEntry o) {
+                return Integer.compare(drawOrder, o.drawOrder);
+            }
+        }
+
+        public void addEntry(final Sprite sprite, final Vector position, SpriteDrawOptions options, final int drawOrder) {
+            this.entries.add(new BatchEntry(sprite, position, options, drawOrder));
+        }
+
+        public List<BatchEntry> entriesInDrawOrder() {
+            Collections.sort(entries);
+            return entries;
+        }
+
+    }
 
     private static final Archetype RENDERS = Archetype.of(RenderComponent.class, TransformComponent.class);
 
     @Override
     public void update(final Engine engine) {
-        final SpriteBatch spriteBatch = new SpriteBatch();
-        final Bounds visibleArea = engine.graphics().world().visibleArea();
+        final Batch batch = new Batch();
+        final World world = engine.graphics().world();
+        final Bounds visibleArea = world.visibleArea();
 
         for (final Entity entity : engine.environment().fetchAll(RENDERS)) {
             final RenderComponent render = entity.get(RenderComponent.class);
@@ -27,9 +59,11 @@ public class RenderSystem implements EntitySystem {
             final var spriteBounds = Bounds.atPosition(entity.position(), width, height);
 
             if (spriteBounds.intersects(visibleArea)) {
-                spriteBatch.addEntry(render.sprite, spriteBounds.origin(), render.options, render.drawOrder);
+                batch.addEntry(render.sprite, spriteBounds.origin(), render.options, render.drawOrder);
             }
         }
-        engine.graphics().world().drawSpriteBatch(spriteBatch);
+        for (final var entry : batch.entriesInDrawOrder()) {
+            world.drawSprite(entry.sprite(), entry.position(), entry.options());
+        }
     }
 }
