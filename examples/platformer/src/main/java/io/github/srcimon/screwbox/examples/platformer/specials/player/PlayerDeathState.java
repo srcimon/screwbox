b478a2b06b9521284bbef25772e28b3a09ee3f41
simonbas <sebastian.simon@koeln.de>
2024-05-03 17:57:04 +0200

    removed scene transition component and system

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/specials/player/PlayerDeathState.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/specials/player/PlayerDeathState.java
@@ -1,6 +1,5 @@
 package io.github.srcimon.screwbox.examples.platformer.specials.player;
 
-import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Time;
 import io.github.srcimon.screwbox.core.assets.Asset;
@@ -14,12 +13,9 @@ import io.github.srcimon.screwbox.examples.platformer.components.DeathEventCompo
 import io.github.srcimon.screwbox.examples.platformer.components.PlayerControlComponent;
 import io.github.srcimon.screwbox.examples.platformer.components.ResetSceneComponent;
 import io.github.srcimon.screwbox.examples.platformer.components.TextComponent;
-import io.github.srcimon.screwbox.core.graphics.transitions.*;
 
 import java.io.Serial;
-import java.util.List;
 
-import static io.github.srcimon.screwbox.core.utils.ListUtil.randomFrom;
 import static io.github.srcimon.screwbox.tiled.Tileset.spriteAssetFromJson;
 
 public class PlayerDeathState implements EntityState {
@@ -32,13 +28,6 @@ public class PlayerDeathState implements EntityState {
     private static final Asset<Sound> OUCH_SOUND = Sound.assetFromFile("sounds/ouch.wav");
     private static final Asset<Sound> BLUPP_SOUND = Sound.assetFromFile("sounds/blupp.wav");
 
-    private static final List<ScreenTransition> TRANSITIONS = List.of(
-            new FadeOutTransition(new HorizontalLinesTransition(20)),
-            new FadeOutTransition(new SwipeTransition()),
-            new FadeOutTransition(new FadingScreenTransition()),
-            new FadeOutTransition(new CircleTransition()),
-            new FadeOutTransition(new MosaikTransition(30, 20)));
-
     @Override
     public void enter(Entity entity, Engine engine) {
         entity.remove(PlayerControlComponent.class);
@@ -49,7 +38,6 @@ public class PlayerDeathState implements EntityState {
         }
 
         entity.get(RenderComponent.class).sprite = SPRITE.get().freshInstance();
-        entity.add(new ScreenTransitionComponent(randomFrom(TRANSITIONS), Duration.ofSeconds(3)));
         entity.add(new TextComponent("GAME OVER", ""));
         entity.add(new ResetSceneComponent(Time.now().addSeconds(3)));
     }
--- examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/systems/ChangeMapSystem.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/systems/ChangeMapSystem.java
@@ -1,46 +1,34 @@
 package io.github.srcimon.screwbox.examples.platformer.systems;
 
-import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
-import io.github.srcimon.screwbox.core.Time;
-import io.github.srcimon.screwbox.core.environment.*;
+import io.github.srcimon.screwbox.core.environment.Archetype;
+import io.github.srcimon.screwbox.core.environment.Entity;
+import io.github.srcimon.screwbox.core.environment.EntitySystem;
+import io.github.srcimon.screwbox.core.environment.Order;
+import io.github.srcimon.screwbox.core.environment.SystemOrder;
 import io.github.srcimon.screwbox.core.environment.logic.SignalComponent;
-import io.github.srcimon.screwbox.core.graphics.transitions.*;
 import io.github.srcimon.screwbox.core.keyboard.Key;
 import io.github.srcimon.screwbox.examples.platformer.components.ChangeMapComponent;
 import io.github.srcimon.screwbox.examples.platformer.scenes.GameScene;
-
-import java.util.List;
-
-import static io.github.srcimon.screwbox.core.utils.ListUtil.randomFrom;
 
 @Order(SystemOrder.SIMULATION_BEGIN)
 public class ChangeMapSystem implements EntitySystem {
 
     private static final Archetype CHANGE_MAP_ZONES = Archetype.of(ChangeMapComponent.class, SignalComponent.class);
 
-    private static final List<ScreenTransition> TRANSITIONS = List.of(
-            new FadeOutTransition(new HorizontalLinesTransition(20)),
-            new FadeOutTransition(new SwipeTransition()),
-            new FadeOutTransition(new FadingScreenTransition()),
-            new FadeOutTransition(new CircleTransition()),
-            new FadeOutTransition(new MosaikTransition(30, 20)));
-
+    //TODO BUGFIX
     @Override
     public void update(Engine engine) {
-        for (Entity entity : engine.environment().fetchAll(CHANGE_MAP_ZONES)) {
-            ChangeMapComponent changeMapComponent = entity.get(ChangeMapComponent.class);
-            if (changeMapComponent.time.isSet()) {
-                if (Time.now().isAfter(changeMapComponent.time)) {
+        if (engine.keyboard().isPressed(Key.SPACE)) {
+            System.out.println("SEARCH");
+            for (Entity entity : engine.environment().fetchAll(CHANGE_MAP_ZONES)) {
+                if (entity.get(SignalComponent.class).isTriggered) {
+                    System.out.println("FOUND TRIGGERED");
                     engine.scenes().add(new GameScene(entity.get(ChangeMapComponent.class).fileName));
                     engine.scenes().switchTo(GameScene.class);
                 }
-            } else if (engine.keyboard().isPressed(Key.SPACE) && entity.get(SignalComponent.class).isTriggered) {
-                engine.environment().addEntity(new Entity().add(
-                        new ScreenTransitionComponent(randomFrom(TRANSITIONS), Duration.ofMillis(3200))));
-                changeMapComponent.time = Time.now().addSeconds(3);
-            }
 
+            }
         }
     }
 
