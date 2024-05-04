d255c120dd442e0ebeabda8b7ca83e832c36a76f
simonbas <sebastian.simon@koeln.de>
2024-05-04 12:16:26 +0200

    made immutable

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/menues/StartGameMenu.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/examples/platformer/menues/StartGameMenu.java
@@ -5,7 +5,8 @@ import io.github.srcimon.screwbox.core.scenes.SceneTransition;
 import io.github.srcimon.screwbox.core.ui.UiMenu;
 import io.github.srcimon.screwbox.examples.platformer.scenes.GameScene;
 
-import static io.github.srcimon.screwbox.core.scenes.AnimationBundle.SCREENSHOT_FADE;
+import static io.github.srcimon.screwbox.core.scenes.AnimationBundle.CIRCLES;
+import static io.github.srcimon.screwbox.core.scenes.AnimationBundle.COLORFADE_BLACK;
 
 public class StartGameMenu extends UiMenu {
 
@@ -33,8 +34,10 @@ public class StartGameMenu extends UiMenu {
     private void startMap(String map, Engine engine) {
         engine.scenes()
                 .add(new GameScene(map))
-                .switchTo(GameScene.class, SceneTransition.noExtroAnimation()
-                        .introAnimation(SCREENSHOT_FADE)
+                .switchTo(GameScene.class, SceneTransition
+                        .extroAnimation(COLORFADE_BLACK)
+                        .extroDurationMillis(100)
+                        .introAnimation(CIRCLES)
                         .introDurationMillis(1200));
     }
 }
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/AnimationBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/AnimationBundle.java
@@ -11,24 +11,25 @@ import java.util.function.Supplier;
 
 public enum AnimationBundle implements AssetBundle<Animation> {
 
-    COLORFADE_BLACK(() -> (screen, value) -> screen.fillWith(Color.BLACK.opacity(value))),
-    CIRCLES(() -> (screen, value) -> {
+    COLORFADE_BLACK(() -> (screen, progress) ->
+            screen.fillWith(Color.BLACK.opacity(progress))),
+    CIRCLES(() -> (screen, progress) -> {
         int size = Math.max(screen.size().width(), screen.size().height()) / 20;
         int xDelta = screen.size().width() / (screen.size().width() / size);
         int yDelta = screen.size().height() / (screen.size().height() / size);
 
         for (int x = 0; x < screen.size().width() + xDelta; x += xDelta) {
             for (int y = 0; y < screen.size().height() + yDelta; y += yDelta) {
-                screen.drawCircle(Offset.at(x, y), (int) (value.value() * size), CircleDrawOptions.filled(Color.BLACK));
+                screen.drawCircle(Offset.at(x, y), (int) (progress.value() * size), CircleDrawOptions.filled(Color.BLACK));
             }
         }
     }),
-    SCREENSHOT_FADE(() -> (screen, value) ->
-            screen.drawSprite(screen.lastScreenshot().orElseThrow(), Offset.origin(), SpriteDrawOptions.originalSize().opacity(value))),
-    SLIDE_UP(() -> (screen, value) ->
+    SCREENSHOT_FADE(() -> (screen, progress) ->
+            screen.drawSprite(screen.lastScreenshot().orElseThrow(), Offset.origin(), SpriteDrawOptions.originalSize().opacity(progress))),
+    SCREENSHOT_SLIDE_UP(() -> (screen, progress) ->
             screen.drawSprite(
                     screen.lastScreenshot().orElseThrow(),
-                    Offset.origin().addY((int) (screen.size().height() * -value.invert().value())),
+                    Offset.origin().addY((int) (screen.size().height() * -progress.invert().value())),
                     SpriteDrawOptions.originalSize()));
 
     private final Asset<Animation> animation;
