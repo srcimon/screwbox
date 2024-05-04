17545a8bf7732d6c002c4f51deafa5b5e91fb7ad
simonbas <sebastian.simon@koeln.de>
2024-05-04 11:27:05 +0200

    made immutable

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/ExtroAnimationBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/ExtroAnimationBundle.java
@@ -4,18 +4,20 @@ import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.assets.AssetBundle;
 import io.github.srcimon.screwbox.core.graphics.Color;
 
+import java.util.function.Supplier;
+
 public enum ExtroAnimationBundle implements AssetBundle<ExtroAnimation> {
 
-    FADE_TO_BLACK((screen, value) -> screen.fillWith(Color.BLACK.opacity(value)));
+    FADE_TO_BLACK(() -> (screen, value) -> screen.fillWith(Color.BLACK.opacity(value)));
 
-    private final ExtroAnimation animation;
+    private final Asset<ExtroAnimation> animation;
 
-    ExtroAnimationBundle(final ExtroAnimation animation) {
-        this.animation = animation;
+    ExtroAnimationBundle(final Supplier<ExtroAnimation> animation) {
+        this.animation = Asset.asset(animation);
     }
 
     @Override
     public Asset<ExtroAnimation> asset() {
-        return Asset.asset(() -> animation);
+        return animation;
     }
 }
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/IntroAnimationBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/scenes/IntroAnimationBundle.java
@@ -6,23 +6,28 @@ import io.github.srcimon.screwbox.core.graphics.Color;
 import io.github.srcimon.screwbox.core.graphics.Offset;
 import io.github.srcimon.screwbox.core.graphics.SpriteDrawOptions;
 
+import java.util.function.Supplier;
+
 public enum IntroAnimationBundle implements AssetBundle<IntroAnimation> {
 
-    FADE_FROM_BLACK((screen, value, screenshot) -> screen.fillWith(Color.BLACK.opacity(value))),
-    FADE_OUT((screen, value, screenshot) -> screen.drawSprite(screenshot, Offset.origin(), SpriteDrawOptions.originalSize().opacity(value))),
-    SLIDE_UP((screen, value, screenshot) -> screen.drawSprite(
-            screenshot,
-            Offset.origin().addY((int) (screen.size().height() * -value.invert().value())),
-            SpriteDrawOptions.originalSize()));
+    FADE_FROM_BLACK(() -> (screen, value, screenshot) ->
+            screen.fillWith(Color.BLACK.opacity(value))),
+    FADE_OUT(() -> (screen, value, screenshot) ->
+            screen.drawSprite(screenshot, Offset.origin(), SpriteDrawOptions.originalSize().opacity(value))),
+    SLIDE_UP(() -> (screen, value, screenshot) ->
+            screen.drawSprite(
+                    screenshot,
+                    Offset.origin().addY((int) (screen.size().height() * -value.invert().value())),
+                    SpriteDrawOptions.originalSize()));
 
-    private final IntroAnimation animation;
+    private final Asset<IntroAnimation> animation;
 
-    IntroAnimationBundle(final IntroAnimation animation) {
-        this.animation = animation;
+    IntroAnimationBundle(final Supplier<IntroAnimation> animation) {
+        this.animation = Asset.asset(animation);
     }
 
     @Override
     public Asset<IntroAnimation> asset() {
-        return Asset.asset(() -> animation);
+        return animation;
     }
 }
