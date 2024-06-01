92a038d872e7c5cdae3820d24caa23fba5d378a4
simonbas <sebastian.simon@koeln.de>
2024-06-01 18:17:42 +0200

    refactoring

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/scenes/StartScene.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/scenes/StartScene.java
@@ -2,12 +2,12 @@ package io.github.srcimon.screwbox.platformer.scenes;
 
 import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Duration;
+import io.github.srcimon.screwbox.core.Ease;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.environment.Environment;
-import io.github.srcimon.screwbox.core.Ease;
 import io.github.srcimon.screwbox.core.graphics.Sprite;
 import io.github.srcimon.screwbox.core.particles.ParticleOptions;
 import io.github.srcimon.screwbox.core.scenes.Scene;
@@ -17,17 +17,17 @@ import io.github.srcimon.screwbox.platformer.menues.StartGameMenu;
 import java.util.List;
 
 import static io.github.srcimon.screwbox.core.Duration.ofSeconds;
-import static io.github.srcimon.screwbox.tiled.Tileset.fromJson;
+import static io.github.srcimon.screwbox.tiled.Tileset.spriteFromJson;
 
 public class StartScene implements Scene {
 
     private static final Asset<List<Sprite>> BACKGROUNDS = Asset.asset(() -> List.of(
-            fromJson("tilesets/specials/player.json").findByName("idle"),
-            fromJson("tilesets/enemies/slime.json").findByName("moving"),
-            fromJson("tilesets/enemies/tracer.json").findByName("active"),
-            fromJson("tilesets/specials/cat.json").findByName("walking"),
-            fromJson("tilesets/collectables/cherries.json").first(),
-            fromJson("tilesets/props/box.json").first()));
+            spriteFromJson("tilesets/specials/player.json", "idle"),
+            spriteFromJson("tilesets/enemies/slime.json", "moving"),
+            spriteFromJson("tilesets/enemies/tracer.json", "active"),
+            spriteFromJson("tilesets/specials/cat.json", "walking"),
+            spriteFromJson("tilesets/collectables/cherries.json"),
+            spriteFromJson("tilesets/props/box.json")));
 
     @Override
     public void populate(Environment environment) {
