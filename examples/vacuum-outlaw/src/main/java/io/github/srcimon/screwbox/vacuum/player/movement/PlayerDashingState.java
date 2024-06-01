7f1da2267f74b902c4b6a05e503f584952f7bb1d
simonbas <sebastian.simon@koeln.de>
2024-06-01 18:12:08 +0200

    fix?

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/movement/PlayerDashingState.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/movement/PlayerDashingState.java
@@ -1,10 +1,30 @@
 package io.github.srcimon.screwbox.vacuum.player.movement;
 
+import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Percent;
+import io.github.srcimon.screwbox.core.assets.Asset;
+import io.github.srcimon.screwbox.core.audio.SoundBundle;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
+import io.github.srcimon.screwbox.core.environment.particles.ParticleEmitterComponent;
+import io.github.srcimon.screwbox.core.particles.ParticleOptions;
+
+import static io.github.srcimon.screwbox.core.assets.Asset.asset;
+import static io.github.srcimon.screwbox.tiled.Tileset.spriteAssetFromJson;
 
 public class PlayerDashingState implements EntityState {
+
+    private static final Asset<ParticleOptions> DASH_PARTICLE = asset(() -> ParticleOptions.unknownSource()
+            .sprite(spriteAssetFromJson("tilesets/objects/player.json", "dashing"))
+            .lifetimeMilliseconds(400)
+            .animateOpacity(Percent.zero(), Percent.half()));
+
+    @Override
+    public void enter(Entity entity, Engine engine) {
+        engine.audio().playSound(SoundBundle.JUMP);
+        entity.add(new ParticleEmitterComponent(Duration.ofMillis(60), ParticleEmitterComponent.SpawnMode.POSITION, DASH_PARTICLE));
+    }
 
     @Override
     public EntityState update(Entity entity, Engine engine) {
@@ -13,4 +33,8 @@ public class PlayerDashingState implements EntityState {
                 : new PlayerWalkingState();
     }
 
+    @Override
+    public void exit(Entity entity, Engine engine) {
+        entity.remove(ParticleEmitterComponent.class);
+    }
 }
--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/movement/PlayerWalkingState.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/movement/PlayerWalkingState.java
@@ -6,11 +6,12 @@ import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
 import io.github.srcimon.screwbox.core.graphics.Sprite;
-import io.github.srcimon.screwbox.tiled.Tileset;
+
+import static io.github.srcimon.screwbox.tiled.Tileset.spriteAssetFromJson;
 
 public class PlayerWalkingState implements EntityState {
 
-    private static final Asset<Sprite> SPRITE = Tileset.spriteAssetFromJson("tilesets/objects/player.json", "idle");
+    private static final Asset<Sprite> SPRITE = spriteAssetFromJson("tilesets/objects/player.json", "idle");
 
     @Override
     public void enter(Entity entity, Engine engine) {
