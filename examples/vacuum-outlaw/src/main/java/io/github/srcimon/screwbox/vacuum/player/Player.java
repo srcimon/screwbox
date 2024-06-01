6c3728d9d9db5afc74156701f41a57f7dacd4e44
simonbas <sebastian.simon@koeln.de>
2024-06-01 19:12:17 +0200

    Tiled tiles use `class` as name instead of custom property `name` (#333)

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/Player.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/Player.java
@@ -8,6 +8,7 @@ import io.github.srcimon.screwbox.core.environment.logic.StateComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.CameraTargetComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
+import io.github.srcimon.screwbox.core.environment.rendering.RotateSpriteComponent;
 import io.github.srcimon.screwbox.core.graphics.SpriteBundle;
 import io.github.srcimon.screwbox.tiled.GameObject;
 import io.github.srcimon.screwbox.vacuum.player.movement.PlayerWalkingState;
@@ -19,6 +20,7 @@ public class Player implements SourceImport.Converter<GameObject> {
         return new Entity(object.id()).name("player")
                 .add(new TransformComponent(object.position(), 8, 8))
                 .add(new PhysicsComponent())
+                .add(new RotateSpriteComponent())
                 .add(new StateComponent(new PlayerWalkingState()))
                 .add(new ShadowCasterComponent(false))
                 .add(new RenderComponent(SpriteBundle.SLIME_MOVING, object.layer().order()))
