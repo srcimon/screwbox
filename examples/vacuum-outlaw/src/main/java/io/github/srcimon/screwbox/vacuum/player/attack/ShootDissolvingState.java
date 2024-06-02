b1acb36f9a22b5f071000d95ee9f9da73bb22452
simonbas <sebastian.simon@koeln.de>
2024-06-02 19:25:43 +0200

    add cursor

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/attack/ShootDissolvingState.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/player/attack/ShootDissolvingState.java
@@ -2,12 +2,14 @@ package io.github.srcimon.screwbox.vacuum.player.attack;
 
 import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
 import io.github.srcimon.screwbox.core.environment.physics.CollisionDetectionComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
 import io.github.srcimon.screwbox.core.environment.tweening.TweenComponent;
 import io.github.srcimon.screwbox.core.environment.tweening.TweenDestroyComponent;
+import io.github.srcimon.screwbox.core.environment.tweening.TweenLightComponent;
 import io.github.srcimon.screwbox.core.environment.tweening.TweenScaleComponent;
 
 public class ShootDissolvingState implements EntityState {
@@ -18,6 +20,7 @@ public class ShootDissolvingState implements EntityState {
         entity.remove(CollisionDetectionComponent.class);
         entity.add(new TweenComponent(Duration.ofMillis(200)));
         entity.add(new TweenScaleComponent(0, 1));
+        entity.add(new TweenLightComponent(Percent.zero(), Percent.max()));
         entity.add(new TweenDestroyComponent());
     }
 
