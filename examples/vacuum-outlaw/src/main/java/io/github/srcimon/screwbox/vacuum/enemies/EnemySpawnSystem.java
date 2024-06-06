038ca1a63b14a3d30e6bfab1620f1a31b1ef4470
simonbas <sebastian.simon@koeln.de>
2024-06-06 18:58:16 +0200

    added enemies

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/enemies/EnemySpawnSystem.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/enemies/EnemySpawnSystem.java
@@ -8,13 +8,18 @@ import io.github.srcimon.screwbox.vacuum.enemies.slime.Slime;
 public class EnemySpawnSystem implements EntitySystem {
 
     private static final Archetype SPAWN_POINTS = Archetype.of(SpawnPointComponent.class);
+    private static final Archetype ENEMIES = Archetype.of(EnemyComponent.class);
 
     @Override
     public void update(Engine engine) {
+        var enemyCount = engine.environment().fetchAll(ENEMIES).size();
         for (final var spawnPoint : engine.environment().fetchAll(SPAWN_POINTS)) {
             final var spawnConfig = spawnPoint.get(SpawnPointComponent.class);
             if (spawnConfig.sheduler.isTick()) {
-                engine.environment().importSource(spawnPoint).as(new Slime());
+                if(enemyCount < 2) {
+                    enemyCount++;
+                    engine.environment().importSource(spawnPoint).as(new Slime());
+                }
             }
         }
     }
