aed699905eb0339fd2aa0df573bade9635db2bba
simonbas <sebastian.simon@koeln.de>
2024-06-07 23:25:33 +0200

    fixed test class

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/window/WindowTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/window/WindowTest.java
@@ -1,11 +1,17 @@
 package io.github.srcimon.screwbox.core.window;
 
+import io.github.srcimon.screwbox.core.graphics.Frame;
+import io.github.srcimon.screwbox.core.graphics.Sprite;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class WindowTest {
@@ -16,5 +22,21 @@ class WindowTest {
     @Test
     void isClosed_isNotOpen_isTrue() {
         assertThat(window.isClosed()).isTrue();
+    }
+
+    @Test
+    void isClosed_isOpen_isFalse() {
+        when(window.isOpen()).thenReturn(true);
+
+        assertThat(window.isClosed()).isFalse();
+    }
+
+    @Test
+    void setCursor_multiImageSprite_exception() {
+        Sprite multiImageSprite = new Sprite(List.of(Frame.invisible(), Frame.invisible()));
+
+        assertThatThrownBy(() -> window.setCursor(multiImageSprite))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("The sprite has more than one frame.");
     }
 }
