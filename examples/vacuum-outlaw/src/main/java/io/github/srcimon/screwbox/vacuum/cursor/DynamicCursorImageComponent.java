7625d6be5fa6faeab2b05c55076bfac9f057effd
simonbas <sebastian.simon@koeln.de>
2024-06-07 11:05:32 +0200

    fixed cursor issue

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/cursor/DynamicCursorImageComponent.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/cursor/DynamicCursorImageComponent.java
@@ -6,5 +6,5 @@ import io.github.srcimon.screwbox.core.environment.Component;
 
 public class DynamicCursorImageComponent implements Component {
 
-    public boolean isShowingReloading = false;
+    public boolean isShowingReloading = true;
 }
