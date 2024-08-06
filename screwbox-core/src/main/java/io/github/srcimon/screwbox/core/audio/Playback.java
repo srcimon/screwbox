77c3c697eb036f8bb8e6ee654412dfae4f90af67
simonbas <sebastian.simon@koeln.de>
2024-08-06 19:26:00 +0200

    added JavaDoc to playback

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/Playback.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/Playback.java
@@ -3,8 +3,17 @@ package io.github.srcimon.screwbox.core.audio;
 import java.io.Serializable;
 import java.util.UUID;
 
-//TODO javadoc
+/**
+ * Contains information on a {@link Sound} played via {@link Audio}.
+ * Can be used to control the playback after it has been started.
+ *
+ * @see Audio#stopPlayback(Playback)
+ * @see Audio#updatePlaybackOptions(Playback, SoundOptions)
+ *
+ * @param id      unique identifier of the playback
+ * @param sound   {@link Sound} played
+ * @param options {@link SoundOptions} used to play the {@link Sound}.
+ */
 public record Playback(UUID id, Sound sound, SoundOptions options) implements Serializable {
-
 
 }
