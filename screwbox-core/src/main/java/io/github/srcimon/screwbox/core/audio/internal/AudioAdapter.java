ee64614050c6cf3de6f0fe95bf5743b721be4318
simonbas <sebastian.simon@koeln.de>
2024-08-06 22:53:49 +0200

    added tests

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/internal/AudioAdapter.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/audio/internal/AudioAdapter.java
@@ -9,6 +9,45 @@ import java.io.IOException;
 
 public class AudioAdapter {
 
+    public static void setVolume(final SourceDataLine line, final Percent volume) {
+        final FloatControl gainControl = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume.value()));
+    }
+
+    public static void setPan(final SourceDataLine line, final double pan) {
+        final FloatControl gainControl = (FloatControl) line.getControl(FloatControl.Type.PAN);
+        gainControl.setValue((float) pan);
+    }
+
+    public static AudioInputStream getAudioInputStream(final byte[] content) {
+        try (final var inputStream = new ByteArrayInputStream(content)) {
+            return AudioSystem.getAudioInputStream(inputStream);
+        } catch (UnsupportedAudioFileException | IOException e) {
+            throw new IllegalArgumentException("could not load audio content", e);
+        }
+    }
+
+    public static AudioFormat getAudioFormat(final byte[] content) {
+        try (final var inputStream = getAudioInputStream(content)) {
+            return inputStream.getFormat();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("could not get audio format", e);
+        }
+    }
+
+    public static byte[] convertToStereo(final AudioInputStream monoInputStream) {
+        final var originalFormat = monoInputStream.getFormat();
+        final var stereoFormat = new AudioFormat(originalFormat.getEncoding(), originalFormat.getSampleRate(),
+                originalFormat.getSampleSizeInBits(), 2, originalFormat.getFrameSize() * 2, originalFormat.getFrameRate(), false);
+        try (final ByteArrayOutputStream stereoOutputStream = new ByteArrayOutputStream();
+             final AudioInputStream stereoInputStream = AudioSystem.getAudioInputStream(stereoFormat, monoInputStream)) {
+            AudioSystem.write(stereoInputStream, AudioFileFormat.Type.WAVE, stereoOutputStream);
+            return stereoOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new IllegalStateException("could not convert mono to stereo audio", e);
+        }
+    }
+
     public SourceDataLine createSourceLine(final AudioFormat format) {
         try {
             final var info = new DataLine.Info(SourceDataLine.class, format);
@@ -30,46 +69,6 @@ public class AudioAdapter {
             return line;
         } catch (final LineUnavailableException e) {
             throw new IllegalStateException("error creating audio target line", e);
-        }
-    }
-
-    static void setVolume(final SourceDataLine line, final Percent volume) {
-        final FloatControl gainControl = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
-        gainControl.setValue(20f * (float) Math.log10(volume.value()));
-    }
-
-    static void setPan(final SourceDataLine line, final double pan) {
-        final FloatControl gainControl = (FloatControl) line.getControl(FloatControl.Type.PAN);
-        gainControl.setValue((float) pan);
-    }
-
-    public static AudioInputStream getAudioInputStream(final byte[] content) {
-        try (final var inputStream = new ByteArrayInputStream(content)) {
-            return AudioSystem.getAudioInputStream(inputStream);
-        } catch (UnsupportedAudioFileException | IOException e) {
-            throw new IllegalArgumentException("could not load audio content", e);
-        }
-    }
-
-    public static AudioFormat getAudioFormat(final byte[] content) {
-        try (final var inputStream = getAudioInputStream(content)) {
-            return inputStream.getFormat();
-        } catch (IOException e) {
-            throw new IllegalArgumentException("could not get audio format", e);
-        }
-    }
-
-    //TODO try to test?
-    public static byte[] convertToStereo(final AudioInputStream monoInputStream) {
-        final var originalFormat = monoInputStream.getFormat();
-        final var stereoFormat = new AudioFormat(originalFormat.getEncoding(), originalFormat.getSampleRate(),
-                originalFormat.getSampleSizeInBits(), 2, originalFormat.getFrameSize() * 2, originalFormat.getFrameRate(), false);
-        try (final ByteArrayOutputStream stereoOutputStream = new ByteArrayOutputStream();
-             final AudioInputStream stereoInputStream = AudioSystem.getAudioInputStream(stereoFormat, monoInputStream)) {
-            AudioSystem.write(stereoInputStream, AudioFileFormat.Type.WAVE, stereoOutputStream);
-            return stereoOutputStream.toByteArray();
-        } catch (IOException e) {
-            throw new IllegalStateException("could not convert mono to stereo audio", e);
         }
     }
 }
--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/audio/internal/AudioAdapterTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/audio/internal/AudioAdapterTest.java
@@ -1,6 +1,7 @@
 package io.github.srcimon.screwbox.core.audio.internal;
 
 import io.github.srcimon.screwbox.core.Percent;
+import io.github.srcimon.screwbox.core.audio.SoundBundle;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.SourceDataLine;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,7 +32,7 @@ class AudioAdapterTest {
     void setVolume_fourtyPercent_appliesCalculatedFloatValueToLine() {
         when(line.getControl(FloatControl.Type.MASTER_GAIN)).thenReturn(floatControl);
 
-        audioAdapter.setVolume(line, Percent.of(0.4));
+        AudioAdapter.setVolume(line, Percent.of(0.4));
 
         verify(floatControl).setValue(-7.9588003f);
     }
@@ -38,8 +41,27 @@ class AudioAdapterTest {
     void setPan_validValue_appliesValueToLine() {
         when(line.getControl(FloatControl.Type.PAN)).thenReturn(floatControl);
 
-        audioAdapter.setPan(line, 0.2);
+        AudioAdapter.setPan(line, 0.2);
 
         verify(floatControl).setValue(0.2f);
+    }
+
+    @Test
+    void getAudioFormat_validSound_returnsFormat() {
+        byte[] sound = SoundBundle.JUMP.get().content();
+
+        var audioFormat = AudioAdapter.getAudioFormat(sound);
+
+        assertThat(audioFormat.getFrameRate()).isEqualTo(44100.0f);
+        assertThat(audioFormat.getChannels()).isEqualTo(2);
+    }
+
+    @Test
+    void getAudioFormat_noSoundData_throwsException() {
+        byte[] noSound = "no-sound".getBytes();
+
+        assertThatThrownBy(() -> AudioAdapter.getAudioFormat(noSound))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("could not load audio content");
     }
 }
