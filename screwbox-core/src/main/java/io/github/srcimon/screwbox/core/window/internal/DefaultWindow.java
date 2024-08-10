96dd9066304fb4488ae0f838247bf65275abaf5d
simonbas <sebastian.simon@koeln.de>
2024-08-10 14:52:11 +0200

    Revert "Reduced unwanted cursor changes when changing the window title"
    This reverts commit 8eb8a3b3f15768b0d317f3e1c39a9b9956e77dce.

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/internal/DefaultWindow.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/window/internal/DefaultWindow.java
@@ -21,7 +21,6 @@ import java.util.function.Supplier;
 import static io.github.srcimon.screwbox.core.graphics.GraphicsConfigurationEvent.ConfigurationProperty.RESOLUTION;
 import static io.github.srcimon.screwbox.core.graphics.GraphicsConfigurationEvent.ConfigurationProperty.WINDOW_MODE;
 import static java.util.Objects.nonNull;
-import static java.util.Objects.requireNonNull;
 
 public class DefaultWindow implements Window, Updatable {
 
@@ -65,11 +64,8 @@ public class DefaultWindow implements Window, Updatable {
 
     @Override
     public Window setTitle(final String title) {
-        requireNonNull(title, "window title must not be null");
-        if (!title().equals(title)) {
-            frame.setTitle(title);
-            updateCursor();
-        }
+        frame.setTitle(title);
+        updateCursor();
         return this;
     }
 
@@ -233,7 +229,7 @@ public class DefaultWindow implements Window, Updatable {
     }
 
     private Supplier<Cursor> createCustomCursor(final Sprite sprite) {
-        return () -> Toolkit.getDefaultToolkit()
-                .createCustomCursor(sprite.singleImage(), new Point(0, 0), "custom cursor");
+        return () -> Toolkit.getDefaultToolkit().createCustomCursor(sprite.singleImage(), new Point(0, 0),
+                "custom cursor");
     }
 }
