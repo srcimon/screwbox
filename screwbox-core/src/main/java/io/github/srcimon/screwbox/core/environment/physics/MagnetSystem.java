0f54d4f4ccab2213af57fd509ff707a9dfe6138b
simonbas <sebastian.simon@koeln.de>
2024-09-26 22:11:26 +0200

    Performance tune

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/physics/MagnetSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/physics/MagnetSystem.java
@@ -18,22 +18,20 @@ public class MagnetSystem implements EntitySystem {
             return;
         }
         final var bodies = engine.environment().fetchAll(BODIES);
-        double delta = engine.loop().delta();
+        final var delta = engine.loop().delta();
         for (final var magnet : magnets) {
-            Vector magnetPosition = magnet.position();
-            var magnetComponent = magnet.get(MagnetComponent.class);
+            final Vector magnetPosition = magnet.position();
+            final var magnetComponent = magnet.get(MagnetComponent.class);
             for (final var body : bodies) {
                 if (!magnet.equals(body)) {
-                    var bodyTransform = body.get(TransformComponent.class);
                     var bodyPhysics = body.get(PhysicsComponent.class);
-                    var distance = bodyTransform.bounds.position().distanceTo(magnetPosition);
+                    var distance = body.position().distanceTo(magnetPosition);
                     var force = Math.max(0, (magnetComponent.range - distance) / magnetComponent.range)
                             * magnetComponent.force * delta * bodyPhysics.magnetModifier;
                     bodyPhysics.momentum = bodyPhysics.momentum
-                            .add(magnetPosition.substract(bodyTransform.bounds.position()).multiply(force));
+                            .add(magnetPosition.substract(body.position()).multiply(force));
                 }
             }
         }
     }
-
 }
