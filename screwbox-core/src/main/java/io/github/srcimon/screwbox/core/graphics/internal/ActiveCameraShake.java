d6b776850e8bb542709e15ea0cb900ac1e7ceeb9
simonbas <sebastian.simon@koeln.de>
2024-10-04 23:46:14 +0200

    refactor

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/ActiveCameraShake.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/ActiveCameraShake.java
@@ -16,43 +16,38 @@ class ActiveCameraShake {
     private final Noise yNoise;
     private final Noise shakeNoise;
     private final Time start = Time.now();
-    private final Duration duration;
-    private final double xStrength;
-    private final double yStrength;
-    private final Rotation screenShake;
+    private final CameraShakeOptions options;
+
     public ActiveCameraShake(final CameraShakeOptions options) {
         xNoise = Noise.variableInterval(options.interval());
         yNoise = Noise.variableInterval(options.interval());
         shakeNoise = Noise.variableInterval(options.interval());
-        duration = options.duration();
-        xStrength = options.xStrength();
-        yStrength = options.yStrength();
-        screenShake = options.screenShake();//TODO reduce variables?
+        this.options = options;
     }
 
     Vector calculateDistortion(final Time now, final double zoom) {
         final var progress = calculateProgress(now);
 
-        return $(xNoise.value(now) * xStrength * progress.invert().value() / zoom,
-                yNoise.value(now) * yStrength * progress.invert().value() / zoom);
+        return $(xNoise.value(now) * options.xStrength() * progress.invert().value() / zoom,
+                yNoise.value(now) * options.yStrength() * progress.invert().value() / zoom);
     }
 
     boolean hasEnded(final Time now) {
-        return !duration.isNone() && now.isAfter(duration.addTo(start));
+        return !options.duration().isNone() && now.isAfter(options.duration().addTo(start));
     }
 
     private Percent calculateProgress(final Time now) {
-        if (duration.isNone()) {
+        if (options.duration().isNone()) {
             return Percent.zero();
         }
         final Duration elapsed = Duration.between(start, now);
-        final var end = duration.addTo(start);
+        final var end = options.duration().addTo(start);
         return Percent.of(1.0 * elapsed.nanos() / Duration.between(start, end).nanos());
     }
 
     //TODO test
     public Rotation caclulateRotation(final Time now) {
         final var progress = calculateProgress(now);
-        return Rotation.degrees(screenShake.degrees() * shakeNoise.value(now) * progress.invert().value());
+        return Rotation.degrees(options.screenShake().degrees() * shakeNoise.value(now) * progress.invert().value());
     }
 }
