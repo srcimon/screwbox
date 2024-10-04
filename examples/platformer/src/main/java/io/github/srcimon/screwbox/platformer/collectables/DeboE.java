e60bca42c3718ce6b36a28464f4bbead9ae166a3
simonbas <sebastian.simon@koeln.de>
2024-10-04 11:36:44 +0200

    refactoring

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/collectables/DeboE.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/collectables/DeboE.java
@@ -1,16 +1,12 @@
 package io.github.srcimon.screwbox.platformer.collectables;
 
-import io.github.srcimon.screwbox.core.Duration;
-import io.github.srcimon.screwbox.core.Ease;
 import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.SourceImport.Converter;
+import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
 import io.github.srcimon.screwbox.core.environment.physics.CollisionDetectionComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.FixedSpinComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
-import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
-import io.github.srcimon.screwbox.core.environment.tweening.TweenComponent;
-import io.github.srcimon.screwbox.core.environment.tweening.TweenSpinComponent;
 import io.github.srcimon.screwbox.core.graphics.Sprite;
 import io.github.srcimon.screwbox.platformer.components.CollectableComponent;
 import io.github.srcimon.screwbox.tiled.GameObject;
