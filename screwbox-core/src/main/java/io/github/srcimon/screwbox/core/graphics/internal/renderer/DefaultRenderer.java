fac3559197ae740f30483fe90ceb60339168b28b
simonbas <sebastian.simon@koeln.de>
2024-10-06 13:11:09 +0200

    fixes

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/DefaultRenderer.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/DefaultRenderer.java
@@ -34,9 +34,9 @@ public class DefaultRenderer implements Renderer {
         lastUsedColor = null;
         if (!rotation.isNone()) {
             fillWith(Color.BLACK);
-            this.graphics.rotate(rotation.radians(), canvasSize.width() / 2.0, canvasSize.height() / 2.0);
+            graphics.rotate(rotation.radians(), canvasSize.width() / 2.0, canvasSize.height() / 2.0);
         }
-        this.graphics.setClip(0, 0, canvasSize.width(), canvasSize.height());
+        graphics.setClip(0, 0, canvasSize.width(), canvasSize.height());
     }
 
     @Override
