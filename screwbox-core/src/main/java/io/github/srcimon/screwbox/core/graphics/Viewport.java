e918afc38aafb57bea446ad1a06d90b948150ad6
simonbas <sebastian.simon@koeln.de>
2024-10-15 19:57:20 +0200

    fix bug

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/Viewport.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/Viewport.java
@@ -1,5 +1,10 @@
 package io.github.srcimon.screwbox.core.graphics;
 
+/**
+ * Rendering always happens within the {@link Viewport}. Without any other specification the {@link Viewport} is
+ * identical with the {@link Screen}.
+ */
+//TODO: document how to change the viewport
 public interface Viewport extends Sizeable {
 
     Offset offset();
