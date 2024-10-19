fed4708d1e9436da5a4eb5a01c655ddf5b593a81
simonbas <sebastian.simon@koeln.de>
2024-10-19 09:47:10 +0200

    enhanced reflection

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/ReflectionRenderSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/ReflectionRenderSystem.java
@@ -26,42 +26,8 @@ public class ReflectionRenderSystem implements EntitySystem {
 
     @Override
     public void update(final Engine engine) {
-        engine.environment().removeAll(REFLECTION_RENDERERS);
-        final var reflectableEntities = engine.environment().fetchAll(RELECTED_ENTITIES);
-        for (final Entity mirror : engine.environment().fetchAll(MIRRORS)) {
-            final var expansionToMitigrateDrawingIssues = Math.max(0, -engine.graphics().toCanvas(mirror.bounds()).offset().y());
-            final var visibleArea = Pixelperfect.bounds(engine.graphics().world().visibleArea().expandTop(expansionToMitigrateDrawingIssues));
-            final var visibleAreaOfMirror = mirror.bounds().intersection(visibleArea);
-            visibleAreaOfMirror.ifPresent(reflection -> {
-                var reflectionOnScreen = engine.graphics().toCanvas(reflection);
-                final Size size = Size.of(
-                        ceil(reflectionOnScreen.width() / engine.graphics().camera().zoom()),
-                        ceil(reflectionOnScreen.height() / engine.graphics().camera().zoom()));
-                if (size.isValid()) {
-                    final var reflectionConfig = mirror.get(ReflectionComponent.class);
-                    final long seed = engine.loop().lastUpdate().milliseconds();
-                    final UnaryOperator<Bounds> entityMotion = reflectionConfig.useWaveEffect
-                            ? bounds -> bounds.moveBy(
-                            Math.sin((seed + bounds.position().y() * 100) / 320) * 2,
-                            Math.sin((seed + bounds.position().x() * 50) / 500) * 2)
-                            : null;
-                    final var reflectedBounds = reflection.moveBy(Vector.y(-reflection.height()));
-                    final var reflectedAreaOnSreen = engine.graphics().toCanvas(reflectedBounds);
-                    final var reflectionImage = new ReflectionImage(engine.graphics(), reflectionConfig.drawOrder, size, reflectedAreaOnSreen, entityMotion);
-                    for (final var entity : reflectableEntities) {
-                        reflectionImage.addEntity(entity);
-                    }
+       // engine.environment().removeAll(REFLECTION_RENDERERS);
 
-                    engine.environment().addEntity("reflection",
-                            new TransformComponent(reflection),
-                            new RenderComponent(
-                                    reflectionImage.create(reflectionConfig.blur),
-                                    reflectionConfig.drawOrder,
-                                    SpriteDrawOptions.originalSize().opacity(reflectionConfig.opacityModifier)),
-                            new ReflectionResultComponent());
-                }
-            });
-        }
     }
 
 }
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderSystem.java
@@ -2,6 +2,7 @@ package io.github.srcimon.screwbox.core.environment.rendering;
 
 import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
@@ -9,12 +10,23 @@ import io.github.srcimon.screwbox.core.environment.Order;
 import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
 import io.github.srcimon.screwbox.core.graphics.Graphics;
 import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
+import io.github.srcimon.screwbox.core.graphics.Size;
 import io.github.srcimon.screwbox.core.graphics.SpriteBatch;
+import io.github.srcimon.screwbox.core.graphics.drawoptions.SpriteDrawOptions;
+import io.github.srcimon.screwbox.core.graphics.internal.ReflectionImage;
+import io.github.srcimon.screwbox.core.utils.Pixelperfect;
+
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import static java.lang.Math.ceil;
 
 @Order(Order.SystemOrder.PRESENTATION_WORLD)
 public class RenderSystem implements EntitySystem {
 
     private static final Archetype RENDERS = Archetype.of(RenderComponent.class, TransformComponent.class);
+
+    private static final Archetype MIRRORS = Archetype.of(ReflectionComponent.class, TransformComponent.class);
 
     @Override
     public void update(final Engine engine) {
@@ -23,7 +35,8 @@ public class RenderSystem implements EntitySystem {
         final ScreenBounds visibleBounds = graphics.screen().bounds();
         double zoom = graphics.camera().zoom();
 
-        for (final Entity entity : engine.environment().fetchAll(RENDERS)) {
+        final List<Entity> renderEntities = engine.environment().fetchAll(RENDERS);
+        for (final Entity entity : renderEntities) {
             final RenderComponent render = entity.get(RenderComponent.class);
             if (mustRenderEntity(render)) {
                 final double width = render.sprite.width() * render.options.scale();
@@ -35,6 +48,42 @@ public class RenderSystem implements EntitySystem {
                     spriteBatch.add(render.sprite, entityScreenBounds.offset(), render.options.scale(render.options.scale() * zoom), render.drawOrder);
                 }
             }
+        }
+
+        for (final Entity mirror : engine.environment().fetchAll(MIRRORS)) {
+            final var expansionToMitigrateDrawingIssues = Math.max(0, -engine.graphics().toCanvas(mirror.bounds()).offset().y());
+            final var visibleArea = Pixelperfect.bounds(engine.graphics().world().visibleArea().expandTop(expansionToMitigrateDrawingIssues));
+            final var visibleAreaOfMirror = mirror.bounds().intersection(visibleArea);
+            visibleAreaOfMirror.ifPresent(reflection -> {
+                var reflectionOnScreen = engine.graphics().toCanvas(reflection);
+                final Size size = Size.of(
+                        ceil(reflectionOnScreen.width() / engine.graphics().camera().zoom()),
+                        ceil(reflectionOnScreen.height() / engine.graphics().camera().zoom()));
+                if (size.isValid()) {
+                    final var reflectionConfig = mirror.get(ReflectionComponent.class);
+                    final long seed = engine.loop().lastUpdate().milliseconds();
+                    final UnaryOperator<Bounds> entityMotion = reflectionConfig.useWaveEffect
+                            ? bounds -> bounds.moveBy(
+                            Math.sin((seed + bounds.position().y() * 100) / 320) * 2,
+                            Math.sin((seed + bounds.position().x() * 50) / 500) * 2)
+                            : null;
+                    final var reflectedBounds = reflection.moveBy(Vector.y(-reflection.height()));
+                    final var reflectedAreaOnSreen = engine.graphics().toCanvas(reflectedBounds);
+                    final var reflectionImage = new ReflectionImage(engine.graphics(), reflectionConfig.drawOrder, size, reflectedAreaOnSreen, entityMotion);
+                    for (final var entity : renderEntities) {
+                        reflectionImage.addEntity(entity);
+                    }
+
+                    spriteBatch.add(reflectionImage.create(reflectionConfig.blur),graphics.toCanvas( reflection.origin()), SpriteDrawOptions.scaled(engine.graphics().camera().zoom()).opacity(reflectionConfig.opacityModifier), reflectionConfig.drawOrder);
+//                    engine.environment().addEntity("reflection",
+//                            new TransformComponent(reflection),
+//                            new RenderComponent(
+//                                    reflectionImage.create(reflectionConfig.blur),
+//                                    reflectionConfig.drawOrder,
+//                                    SpriteDrawOptions.originalSize().opacity(reflectionConfig.opacityModifier)),
+//                            new ReflectionResultComponent());
+                }
+            });
         }
 
         graphics.screen().drawSpriteBatch(spriteBatch);
