09abadcb5ccc9c98d41ccdecab33ef9facf4267a
simonbas <sebastian.simon@koeln.de>
2024-10-29 21:34:27 +0100

    Revert "Revert "Revert "deleted todo"""
    This reverts commit a1ef68369e5d8bf207692696fff16ba4b52215f8.

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderOverLightSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/rendering/RenderOverLightSystem.java
@@ -1,22 +1,20 @@
 package io.github.srcimon.screwbox.core.environment.rendering;
 
 import io.github.srcimon.screwbox.core.Engine;
-import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.Order;
 import io.github.srcimon.screwbox.core.graphics.SpriteBatch;
-
-import java.util.List;
+import io.github.srcimon.screwbox.core.graphics.Viewport;
 
 @Order(Order.SystemOrder.PRESENTATION_ON_TOP_OF_LIGHT)
 public class RenderOverLightSystem extends RenderSystem {
 
-    @Override
-    public void update(final Engine engine) {
-        final List<Entity> entities = fetchRenderEntities(engine);
-        for (final var viewport : engine.graphics().activeViewports()) {
-            final SpriteBatch spriteBatch = renderEntitiesOnViewport(viewport, entities, render -> render.renderOverLight);
-            viewport.canvas().drawSpriteBatch(spriteBatch);
-        }
+
+    protected boolean mustRender(final RenderComponent render) {
+        return render.renderOverLight;
     }
 
+    //TODO mae the overwrite more elegant
+    protected void addReflectionsToSpriteBatch(final Engine engine, final Viewport viewport, final SpriteBatch spriteBatch) {
+        // does nothing
+    }
 }
