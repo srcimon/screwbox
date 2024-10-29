40c893d985f206e017608aa680a9179f98dd0288
simonbas <sebastian.simon@koeln.de>
2024-10-29 22:39:12 +0100

    added javadoc

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/ViewportLayout.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/ViewportLayout.java
@@ -1,7 +1,19 @@
 package io.github.srcimon.screwbox.core.graphics;
 
+/**
+ * This layout can be used to calculate the position and size of a {@link Viewport} that is present in a split screen.
+ *
+ * @see Graphics#enableSplitScreen(SplitScreenOptions)
+ */
 public interface ViewportLayout {
 
+    /**
+     * Calculate the specific bounds on the screen for a {@link Viewport}.
+     *
+     * @param index  the index of the viewport in the list of {@link Graphics#activeViewports()} (starts at 0)
+     * @param count  the total count of {@link Viewport viewports} of the split screen
+     * @param bounds the surrounding bounds of the {@link Screen}
+     */
     ScreenBounds calculateBounds(int index, int count, ScreenBounds bounds);
 
 }
