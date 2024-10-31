4e77ead40907a5aaae0d165221ddba122bb67df1
simonbas <sebastian.simon@koeln.de>
2024-11-01 00:05:53 +0100

    added javadoc to layouts

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/layouts/HorizontalLayout.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/layouts/HorizontalLayout.java
@@ -3,16 +3,19 @@ package io.github.srcimon.screwbox.core.graphics.layouts;
 import io.github.srcimon.screwbox.core.graphics.Offset;
 import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
 import io.github.srcimon.screwbox.core.graphics.Size;
+import io.github.srcimon.screwbox.core.graphics.Viewport;
 import io.github.srcimon.screwbox.core.graphics.ViewportLayout;
 
+/**
+ * Layouts {@link Viewport viewports} on next to each other.
+ */
 public class HorizontalLayout implements ViewportLayout {
 
     @Override
     public ScreenBounds calculateBounds(final int index, final int count, final ScreenBounds bounds) {
-        int width = (int) (bounds.width() / count * 1.0);
-        var offset = Offset.at(index * width, 0).add(bounds.offset());
-        var size = Size.of(width, bounds.height());
-
+        final int width = (int) (bounds.width() * 1.0 / count);
+        final var offset = Offset.at(index * width, 0).add(bounds.offset());
+        final var size = Size.of(width, bounds.height());
         return new ScreenBounds(offset, size);
     }
 
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/layouts/VerticalLayout.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/layouts/VerticalLayout.java
@@ -3,15 +3,19 @@ package io.github.srcimon.screwbox.core.graphics.layouts;
 import io.github.srcimon.screwbox.core.graphics.Offset;
 import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
 import io.github.srcimon.screwbox.core.graphics.Size;
+import io.github.srcimon.screwbox.core.graphics.Viewport;
 import io.github.srcimon.screwbox.core.graphics.ViewportLayout;
 
+/**
+ * Layouts {@link Viewport viewports} on top of each other.
+ */
 public class VerticalLayout implements ViewportLayout {
 
     @Override
-    public ScreenBounds calculateBounds(int index, int count, ScreenBounds bounds) {
-        int height = (int) (bounds.height() / count * 1.0);
-        var offset = Offset.at(0, index * height).add(bounds.offset());
-        var size = Size.of(bounds.width(), height);
+    public ScreenBounds calculateBounds(final int index, final int count, final ScreenBounds bounds) {
+        final int height = (int) (bounds.height() * 1.0 / count);
+        final var offset = Offset.at(0, index * height).add(bounds.offset());
+        final var size = Size.of(bounds.width(), height);
         return new ScreenBounds(offset, size);
     }
 }
