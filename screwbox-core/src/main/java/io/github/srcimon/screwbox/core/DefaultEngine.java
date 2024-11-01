6dee3cdae348409dfc38f80b5b4c252caa15c1a3
simonbas <sebastian.simon@koeln.de>
2024-11-01 10:39:29 +0100

    fixed issue

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/DefaultEngine.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/DefaultEngine.java
@@ -24,7 +24,6 @@ import io.github.srcimon.screwbox.core.graphics.internal.DefaultGraphics;
 import io.github.srcimon.screwbox.core.graphics.internal.DefaultLight;
 import io.github.srcimon.screwbox.core.graphics.internal.DefaultScreen;
 import io.github.srcimon.screwbox.core.graphics.internal.DefaultViewport;
-import io.github.srcimon.screwbox.core.graphics.internal.DefaultWorld;
 import io.github.srcimon.screwbox.core.graphics.internal.ViewportManager;
 import io.github.srcimon.screwbox.core.graphics.internal.renderer.AsyncRenderer;
 import io.github.srcimon.screwbox.core.graphics.internal.renderer.DefaultRenderer;
