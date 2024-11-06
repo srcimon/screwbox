4ce0349c87b43c5fd074b1c80477b4cd92d6e5ad
simonbas <sebastian.simon@koeln.de>
2024-11-06 20:40:07 +0100

    added tests

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/SplitscreenOptionsTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/SplitscreenOptionsTest.java
@@ -1,7 +1,11 @@
 package io.github.srcimon.screwbox.core.graphics;
 
+import io.github.srcimon.screwbox.core.graphics.drawoptions.LineDrawOptions;
+import io.github.srcimon.screwbox.core.graphics.layouts.TableLayout;
+import io.github.srcimon.screwbox.core.graphics.layouts.VerticalLayout;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SplitscreenOptionsTest {
@@ -18,5 +22,25 @@ class SplitscreenOptionsTest {
         assertThatThrownBy(() -> SplitscreenOptions.viewports(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("split screen must have at least one viewport");
+    }
+
+    @Test
+    void newInstance_tableLayoutWithFourViewportsAnNoBorders_setsAllProperties() {
+        var options = SplitscreenOptions.viewports(4).tableLayout().noBorders();
+
+        assertThat(options.viewportCount()).isEqualTo(4);
+        assertThat(options.layout()).isInstanceOf(TableLayout.class);
+        assertThat(options.border()).isNull();
+    }
+
+    @Test
+    void newInstance_verticalLayoutWithTwoViewportsAndBlueBorders_setsAllProperties() {
+        var borders = LineDrawOptions.color(Color.BLUE).strokeWidth(2);
+
+        var options = SplitscreenOptions.viewports(2).verticalLayout().borders(borders);
+
+        assertThat(options.viewportCount()).isEqualTo(2);
+        assertThat(options.layout()).isInstanceOf(VerticalLayout.class);
+        assertThat(options.border()).isEqualTo(borders);
     }
 }
