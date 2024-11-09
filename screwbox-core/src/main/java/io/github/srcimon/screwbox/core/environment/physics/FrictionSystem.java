b9efd3c223e20a68058b175aa9d564fdc134c887
simonbas <sebastian.simon@koeln.de>
2024-11-09 16:46:43 +0100

    prototype friction

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/physics/FrictionSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/physics/FrictionSystem.java
@@ -8,6 +8,8 @@ import io.github.srcimon.screwbox.core.environment.EntitySystem;
 
 import static io.github.srcimon.screwbox.core.utils.MathUtil.modifier;
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 /**
  * Applies friction on all {@link Entity entities} having a {@link PhysicsComponent}. Slows down entities by there specified
@@ -22,30 +24,25 @@ public class FrictionSystem implements EntitySystem {
         for (final var entity : engine.environment().fetchAll(PHYSICS)) {
             final var physicsComponent = entity.get(PhysicsComponent.class);
 
-            
             if (physicsComponent.friction != 0 && !physicsComponent.momentum.isZero()) {
                 final Vector momentum = physicsComponent.momentum;
                 final var speedChange = momentum
                         .length(abs(physicsComponent.friction) * engine.loop().delta())
                         .multiply(modifier(physicsComponent.friction));
 
-                final boolean xIsSpeedUp = physicsComponent.friction > 0
-                        ? speedChange.x() > 0
-                        : speedChange.x() < 0;
-                final double nextX = xIsSpeedUp
-                        ? Math.max(0, momentum.x() - speedChange.x())
-                        : Math.min(0, momentum.x() - speedChange.x());
-                
-                final boolean yIsSpeedUp = physicsComponent.friction > 0
-                        ? speedChange.y() > 0
-                        : speedChange.y() < 0;
-               
-                final double nextY = yIsSpeedUp
-                        ? Math.max(0, momentum.y() - speedChange.y())
-                        : Math.min(0, momentum.y() - speedChange.y());
+                final boolean frictionIsPositive = physicsComponent.friction > 0;
+                final double nextX = calculateUpdateValue(frictionIsPositive, speedChange.x(), momentum.x());
+                final double nextY = calculateUpdateValue(frictionIsPositive, speedChange.y(), momentum.y());
 
                 physicsComponent.momentum = Vector.of(nextX, nextY);
             }
         }
+    }
+
+    private double calculateUpdateValue(final boolean frictionIsPositive, final double speedChange, final double momentum) {
+        final boolean isSpeedUp = frictionIsPositive ? speedChange > 0 : speedChange < 0;
+        return isSpeedUp
+                ? max(0, momentum - speedChange)
+                : min(0, momentum - speedChange);
     }
 }
