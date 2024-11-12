af1901396b760fbff5fbeaac649620f16b657dae
simonbas <sebastian.simon@koeln.de>
2024-11-12 21:56:50 +0100

    fixup javadoc

---
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/layouts/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains all pre packed layouts used to customize layout of split screen {@link io.github.srcimon.screwbox.core.graphics.Viewport viewports}.
+ */
+package io.github.srcimon.screwbox.core.graphics.layouts;
