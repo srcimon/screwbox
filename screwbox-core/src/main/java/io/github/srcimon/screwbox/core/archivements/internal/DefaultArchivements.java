13f517432fac01b4795feb8b5452d81b2fd3db0c
simonbas <sebastian.simon@koeln.de>
2024-12-04 23:04:41 +0100

    refactor

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/archivements/internal/DefaultArchivements.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/archivements/internal/DefaultArchivements.java
@@ -88,8 +88,7 @@ public class DefaultArchivements implements Archivements, Updatable {
         final var transferItems = new ArrayList<DefaultArchivementStatus>();
 
         for (final var activeArchivement : activeArchivements) {
-            if (refreshLazyArchivements || !activeArchivement.isLazy()) {
-                var progress = activeArchivement.autoProgress(engine);
+            if (refreshLazyArchivements || !activeArchivement.isLazy()) {//TODO own list for all using auto upgrade (has method...)                 var progress = activeArchivement.autoProgress(engine);
                 activeArchivement.progress(progress);
             }
             if (activeArchivement.isCompleted()) {
