0573247def0efc07902828c0358b3ec8f69553cb
simonbas <sebastian.simon@koeln.de>
2024-12-07 12:17:33 +0100

    simplified

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/archivements/ArchivementDetailsTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/archivements/ArchivementDetailsTest.java
@@ -3,6 +3,7 @@ package io.github.srcimon.screwbox.core.archivements;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ArchivementDetailsTest {
 
@@ -17,5 +18,27 @@ class ArchivementDetailsTest {
         assertThat(archivementDetails.description()).isEqualTo("run until finished");
         assertThat(archivementDetails.title()).isEqualTo("finish level 1");
         assertThat(archivementDetails.progressionIsAbsolute()).isTrue();
+    }
+
+    @Test
+    void newInstance_titleNull_throwsException() {
+        assertThatThrownBy(() -> ArchivementDetails.title(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("title must not be null");
+    }
+
+    @Test
+    void newInstance_titleEmpty_throwsException() {
+        assertThatThrownBy(() -> ArchivementDetails.title(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("title must not be empty");
+    }
+
+    @Test
+    void goal_goalIsNegative_throwsException() {
+        var archivementDetails = ArchivementDetails.title("negative goals are not allowed");
+        assertThatThrownBy(() -> archivementDetails.goal(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("goal must be positive");
     }
 }
