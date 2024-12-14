40262e2e3c5ad70e7eabd061ae3b54b70e0fc705
simonbas <sebastian.simon@koeln.de>
2024-12-14 12:10:22 +0100

    fixed compile issues

--- examples/platformer/src/test/java/io/github/srcimon/screwbox/platformer/scenes/GameSceneTest.java
+++ examples/platformer/src/test/java/io/github/srcimon/screwbox/platformer/scenes/GameSceneTest.java
@@ -2,14 +2,13 @@ package io.github.srcimon.screwbox.platformer.scenes;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.environment.Environment;
-import io.github.srcimon.screwbox.core.environment.rendering.CameraBoundsComponent;
 import io.github.srcimon.screwbox.core.environment.internal.DefaultEnvironment;
+import io.github.srcimon.screwbox.core.environment.rendering.CameraBoundsComponent;
 import io.github.srcimon.screwbox.platformer.components.PlayerMarkerComponent;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,7 +19,7 @@ class GameSceneTest {
     Engine engine;
 
     @ParameterizedTest
-    @ValueSource(strings = { "maps/0-1_intro.json", "maps/1-1_teufelsinsel.json", "maps/1-2_misty_caves.json" })
+    @ValueSource(strings = {"maps/0-1_intro.json", "maps/1-1_teufelsinsel.json", "maps/1-2_misty_caves.json"})
     void allMapsCanBeConvertetToEntities(String mapName) {
         Environment environment = new DefaultEnvironment(engine);
 
