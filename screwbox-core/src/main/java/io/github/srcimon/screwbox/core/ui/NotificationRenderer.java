7684afc804362cae136db7b8148a63f78e776021
simonbas <sebastian.simon@koeln.de>
2024-12-18 08:16:41 +0100

    JavaDoc added

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/NotificationRenderer.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/NotificationRenderer.java
@@ -3,9 +3,18 @@ package io.github.srcimon.screwbox.core.ui;
 import io.github.srcimon.screwbox.core.graphics.Canvas;
 import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
 
-//TODO javadoc
+/**
+ * Renders a notification on the {@link Canvas} using {@link ScreenBounds} provided by {@link NotificationLayouter}.
+ */
 @FunctionalInterface
 public interface NotificationRenderer {
 
+    /**
+     * Render the {@link Notification} on the {@link Canvas}.
+     *
+     * @param notification {@link Notification} to render
+     * @param bounds       {@link ScreenBounds} calculaed by {@link NotificationLayouter}
+     * @param canvas       {@link Canvas} for drawing onto
+     */
     void render(Notification notification, ScreenBounds bounds, Canvas canvas);
 }
