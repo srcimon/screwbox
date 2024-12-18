4ef6f4222c666df96ca5942d6f0f6739967d99c0
simonbas <sebastian.simon@koeln.de>
2024-12-18 08:37:56 +0100

    added JavaDoc

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/NotificationDetails.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/ui/NotificationDetails.java
@@ -8,25 +8,39 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-//TODO javadoc
-
+/**
+ * Specifies attributes of a {@link Notification}.
+ *
+ * @param text  text of the {@link Notification}
+ * @param icon  icon of the {@link Notification}
+ * @param sound souns to be played when showing the {@link Notification}
+ * @since 2.8.0
+ */
 public record NotificationDetails(String text, Optional<Sprite> icon, Optional<Sound> sound) {
 
-    //TODO test
     public NotificationDetails {
         Objects.requireNonNull(text, "text must not be null");
         Validate.notEmpty(text, "text must not be empty");
     }
 
+    /**
+     * Creates a new instance with the specified text.
+     */
     public static NotificationDetails text(final String text) {
         return new NotificationDetails(text, Optional.empty(), Optional.empty());
     }
 
+    /**
+     * Returns a new instance with the specified icon.
+     */
     public NotificationDetails icon(final Supplier<Sprite> icon) {
         Objects.requireNonNull(text, "icon must not be null");
         return icon(icon.get());
     }
 
+    /**
+     * Returns a new instance with the specified sound.
+     */
     public NotificationDetails sound(final Supplier<Sound> sound) {
         Objects.requireNonNull(sound, "sound must not be null");
         return sound(sound.get());
