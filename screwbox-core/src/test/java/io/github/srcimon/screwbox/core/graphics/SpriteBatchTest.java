17c4347769b762bc756af95032f6dd0d047bad04
simonbas <sebastian.simon@koeln.de>
2024-12-27 18:48:39 +0100

    Support for orthographic rendering sprites

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/SpriteBatchTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/SpriteBatchTest.java
@@ -7,14 +7,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SpriteBatchTest {
 
+    private static final Sprite FIRST_SPRITE = SpriteBundle.DOT_YELLOW.get();
+    private static final Sprite SECOND_SPRITE = SpriteBundle.SLIME_MOVING.get();
+
     @Test
     void entriesInOrder_twoEntriesInWrongOrder_returnsEntriesInCorrectOrder() {
         SpriteBatch spriteBatch = new SpriteBatch();
-        spriteBatch.add(SpriteBundle.DOT_YELLOW.get(), Offset.origin(), SpriteDrawOptions.originalSize(), 2);
-        spriteBatch.add(SpriteBundle.SLIME_MOVING.get(), Offset.origin(), SpriteDrawOptions.originalSize(), 1);
+        spriteBatch.add(FIRST_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize(), 2);
+        spriteBatch.add(SECOND_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize(), 1);
 
         assertThat(spriteBatch.entriesInOrder()).hasSize(2);
         assertThat(spriteBatch.entriesInOrder().getFirst()).matches(entry -> entry.drawOrder() == 1);
+    }
+
+    @Test
+    void entriesInOrder_twoEntriesSameOrderNotOrthographic_returnsEntriesInSameOrder() {
+        SpriteBatch spriteBatch = new SpriteBatch();
+
+        spriteBatch.add(FIRST_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize(), 1);
+        spriteBatch.add(SECOND_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize(), 1);
+
+        assertThat(spriteBatch.entriesInOrder()).hasSize(2);
+        assertThat(spriteBatch.entriesInOrder().getFirst()).matches(entry -> entry.sprite().equals(FIRST_SPRITE));
+    }
+
+    @Test
+    void entriesInOrder_twoEntriesSameOrderButFirstUsesOrthographic_returnsEntriesInCorrectOrder() {
+        SpriteBatch spriteBatch = new SpriteBatch();
+
+        spriteBatch.add(FIRST_SPRITE, Offset.at(0, 10), SpriteDrawOptions.originalSize().useOrhographicSorting(), 1);
+        spriteBatch.add(SECOND_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize(), 1);
+
+        assertThat(spriteBatch.entriesInOrder()).hasSize(2);
+        assertThat(spriteBatch.entriesInOrder().getFirst()).matches(entry -> entry.sprite().equals(SECOND_SPRITE));
+    }
+
+    @Test
+    void entriesInOrder_twoEntriesSameOrderButSecondUsesOrthographic_returnsEntriesInCorrectOrder() {
+        SpriteBatch spriteBatch = new SpriteBatch();
+
+        spriteBatch.add(FIRST_SPRITE, Offset.at(0, 10), SpriteDrawOptions.originalSize(), 1);
+        spriteBatch.add(SECOND_SPRITE, Offset.origin(), SpriteDrawOptions.originalSize().useOrhographicSorting(), 1);
+
+        assertThat(spriteBatch.entriesInOrder()).hasSize(2);
+        assertThat(spriteBatch.entriesInOrder().getFirst()).matches(entry -> entry.sprite().equals(SECOND_SPRITE));
     }
 
     @Test
