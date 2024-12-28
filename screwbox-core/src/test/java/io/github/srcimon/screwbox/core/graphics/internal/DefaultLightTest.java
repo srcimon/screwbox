1455ac59276a3f6a828bf593d4002d98b0870899
simonbas <sebastian.simon@koeln.de>
2024-12-28 17:37:21 +0100

    added more tests to default light

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultLightTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/DefaultLightTest.java
@@ -1,11 +1,18 @@
 package io.github.srcimon.screwbox.core.graphics.internal;
 
+import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.graphics.GraphicsConfiguration;
+import io.github.srcimon.screwbox.core.graphics.Viewport;
+import io.github.srcimon.screwbox.core.test.TestUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -13,11 +20,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @MockitoSettings
 class DefaultLightTest {
 
-    @Mock
     GraphicsConfiguration configuration;
 
-    @InjectMocks
+    @Mock
+    Viewport viewport;
+
+    @Mock
+    Renderer renderer;
+
+    ExecutorService executor;
+
     DefaultLight light;
+
+    @BeforeEach
+    void setUp() {
+        configuration = new GraphicsConfiguration();
+        executor = Executors.newCachedThreadPool();
+        light = new DefaultLight(configuration, new ViewportManager(viewport, renderer), executor);
+    }
 
     @Test
     void setAmbientLight_ambientLightNull_throwsException() {
@@ -27,9 +47,21 @@ class DefaultLightTest {
     }
 
     @Test
+    void addOrthographicWall_wallNotNull_enablesLight() {
+        light.addOrthographicWall(Bounds.$$(20, 40, 10, 20));
+
+        assertThat(configuration.isLightEnabled()).isTrue();
+    }
+
+    @Test
     void setAmbientLight_notNull_setsLight() {
         light.setAmbientLight(Percent.half());
 
         assertThat(light.ambientLight()).isEqualTo(Percent.half());
+    }
+
+    @AfterEach
+    void tearDown() {
+        TestUtil.shutdown(executor);
     }
 }
