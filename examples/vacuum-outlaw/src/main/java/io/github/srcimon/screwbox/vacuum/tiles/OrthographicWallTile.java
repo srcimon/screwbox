d4322595330a7d3b4b5080fb2f38e4c8302fb13b
simonbas <sebastian.simon@koeln.de>
2024-12-28 12:15:46 +0100

    orthographic adjustments on vacumm outlaw level

--- examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/tiles/OrthographicWallTile.java
+++ examples/vacuum-outlaw/src/main/java/io/github/srcimon/screwbox/vacuum/tiles/OrthographicWallTile.java
@@ -3,6 +3,10 @@ package io.github.srcimon.screwbox.vacuum.tiles;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.SourceImport;
 import io.github.srcimon.screwbox.core.environment.core.TransformComponent;
+import io.github.srcimon.screwbox.core.environment.light.ShadowCasterComponent;
+import io.github.srcimon.screwbox.core.environment.light.StaticShadowCasterComponent;
+import io.github.srcimon.screwbox.core.environment.physics.ColliderComponent;
+import io.github.srcimon.screwbox.core.environment.physics.StaticColliderComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
 import io.github.srcimon.screwbox.tiled.Tile;
 
@@ -11,7 +15,11 @@ public class OrthographicWallTile implements SourceImport.Converter<Tile> {
     @Override
     public Entity convert(final Tile tile) {
         return new Entity().name("wall")
-                .add(new RenderComponent(tile.sprite(), tile.layer().order()))
+                .add(new ShadowCasterComponent())
+                .add(new StaticShadowCasterComponent())
+                .add(new StaticColliderComponent())
+                .add(new ColliderComponent())
+                .add(new RenderComponent(tile.sprite(), 3 /* order of player */))
                 .add(new TransformComponent(tile.renderBounds()));
     }
 }
