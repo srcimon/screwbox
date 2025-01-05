6992fb5fd78981c7e3262eddddf7d30e15df14a4
simonbas <sebastian.simon@koeln.de>
2025-01-05 18:48:19 +0100

    fix climb

--- examples/playgrounds/src/main/java/io/github/srcimon/screwbox/playgrounds/playercontrolls/player/dash/DashState.java
+++ examples/playgrounds/src/main/java/io/github/srcimon/screwbox/playgrounds/playercontrolls/player/dash/DashState.java
@@ -5,6 +5,7 @@ import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Time;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
+import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
 import io.github.srcimon.screwbox.playgrounds.playercontrolls.player.jump.JumpingState;
 import io.github.srcimon.screwbox.playgrounds.playercontrolls.player.move.MovementComponent;
 
@@ -15,11 +16,12 @@ public class DashState implements EntityState {
     @Override
     public void enter(Entity entity, Engine engine) {
         entity.remove(MovementComponent.class);
+        entity.get(PhysicsComponent.class).gravityModifier = 0;
     }
 
     @Override
     public EntityState update(Entity entity, Engine engine) {
-        if(Duration.between(engine.loop().time(), started).isAtLeast(Duration.ofMillis(250))) {
+        if(Duration.between(engine.loop().time(), started).isAtLeast(Duration.ofMillis(150))) {
             return new JumpingState();
         }
         return this;
@@ -28,5 +30,8 @@ public class DashState implements EntityState {
     @Override
     public void exit(Entity entity, Engine engine) {
         entity.add(new MovementComponent());
+        var physics = entity.get(PhysicsComponent.class);
+        physics.gravityModifier = 1;
+        physics.momentum = physics.momentum.multiply(0.4);
     }
 }
--- examples/playgrounds/src/main/java/io/github/srcimon/screwbox/playgrounds/playercontrolls/player/dash/DashingComponent.java
+++ examples/playgrounds/src/main/java/io/github/srcimon/screwbox/playgrounds/playercontrolls/player/dash/DashingComponent.java
@@ -3,4 +3,5 @@ package io.github.srcimon.screwbox.playgrounds.playercontrolls.player.dash;
 import io.github.srcimon.screwbox.core.environment.Component;
 
 public class DashingComponent implements Component {
+
 }
