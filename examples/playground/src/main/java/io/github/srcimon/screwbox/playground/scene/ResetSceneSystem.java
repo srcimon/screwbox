7df040b756e3d18d0f3c1166852965bcc380a765
simonbas <sebastian.simon@koeln.de>
2025-01-10 20:53:38 +0100

    fixed readme

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/ResetSceneSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/ResetSceneSystem.java
@@ -2,6 +2,9 @@ package io.github.srcimon.screwbox.playground.scene;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
+import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.scenes.SceneTransition;
+import io.github.srcimon.screwbox.core.scenes.animations.ColorFadeAnimation;
 import io.github.srcimon.screwbox.playground.scene.player.ControlKeys;
 
 public class ResetSceneSystem implements EntitySystem {
@@ -9,7 +12,11 @@ public class ResetSceneSystem implements EntitySystem {
     @Override
     public void update(Engine engine) {
         if (engine.keyboard().isPressed(ControlKeys.RESET)) {
-            engine.scenes().resetActiveScene();
+            engine.scenes().resetActiveScene(SceneTransition.custom()
+                    .outroDurationMillis(250)
+                    .outroAnimation(new ColorFadeAnimation(Color.WHITE))
+                    .introDurationMillis(250)
+                    .introAnimation(new ColorFadeAnimation(Color.WHITE)));
         }
     }
 }
