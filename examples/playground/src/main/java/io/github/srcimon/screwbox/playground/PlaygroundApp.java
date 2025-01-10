8b870a000cfc2ec87f4a48a537f5058e68a05cc9
simonbas <sebastian.simon@koeln.de>
2025-01-10 17:08:25 +0100

    added playground application

---
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/PlaygroundApp.java
@@ -0,0 +1,13 @@
+package io.github.srcimon.screwbox.playground;
+
+import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.ScrewBox;
+
+public class PlaygroundApp {
+
+    public static void main(String[] args) {
+        Engine screwBox = ScrewBox.createEngine("Playground");
+
+        screwBox.start();
+    }
+}
