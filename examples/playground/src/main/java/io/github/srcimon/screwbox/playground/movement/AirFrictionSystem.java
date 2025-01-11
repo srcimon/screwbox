ad7d3e5dfd17c11b7e728d4071bdc0fad889581e
simonbas <sebastian.simon@koeln.de>
2025-01-11 16:28:36 +0100

    fixed readme

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/movement/AirFrictionSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/movement/AirFrictionSystem.java
@@ -18,7 +18,6 @@ public class AirFrictionSystem implements EntitySystem {
             final double friction = entity.get(AirFrictionComponent.class).friction * engine.loop().delta();
             double absX = Math.abs(physicsBodyComponent.momentum.x());
             double change = Math.clamp(MathUtil.modifier(physicsBodyComponent.momentum.x()) * friction * -1, -absX, absX);
-            System.out.println(physicsBodyComponent.momentum.x() + "...." + change);
             physicsBodyComponent.momentum = physicsBodyComponent.momentum.addX(change);
         }
     }
