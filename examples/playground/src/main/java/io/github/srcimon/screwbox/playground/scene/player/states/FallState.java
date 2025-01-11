11a8b57a4cf4c4cd1a5d4d25c90353d43a8cb3ea
simonbas <sebastian.simon@koeln.de>
2025-01-11 16:48:54 +0100

    fixed readme

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/player/states/FallState.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/player/states/FallState.java
@@ -5,6 +5,7 @@ import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
 import io.github.srcimon.screwbox.core.environment.physics.CollisionDetailsComponent;
 import io.github.srcimon.screwbox.playground.movement.ClimbComponent;
+import io.github.srcimon.screwbox.playground.movement.GrabComponent;
 import io.github.srcimon.screwbox.playground.movement.JumpControlComponent;
 import io.github.srcimon.screwbox.playground.movement.MovementControlComponent;
 import io.github.srcimon.screwbox.playground.movement.WallJumpComponent;
@@ -17,6 +18,7 @@ public class FallState implements EntityState {
         entity.get(MovementControlComponent.class).isEnabled = true;
         entity.get(ClimbComponent.class).isEnabled = false;
         entity.get(WallJumpComponent.class).isEnabled = false;
+        entity.get(GrabComponent.class).isEnabled = true;
     }
 
     @Override
--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/player/states/JumpState.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/scene/player/states/JumpState.java
@@ -4,18 +4,18 @@ import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.logic.EntityState;
 import io.github.srcimon.screwbox.playground.movement.ClimbComponent;
+import io.github.srcimon.screwbox.playground.movement.GrabComponent;
 import io.github.srcimon.screwbox.playground.movement.JumpControlComponent;
 import io.github.srcimon.screwbox.playground.movement.WallJumpComponent;
 
-public class JumpState implements EntityState {
-
-    static int i = 0;
+public class JumpState implements EntityState { ;
 
     @Override
     public void enter(Entity entity, Engine engine) {
         entity.get(JumpControlComponent.class).isEnabled = false;
         entity.get(ClimbComponent.class).isEnabled = false;
         entity.get(WallJumpComponent.class).isEnabled = false;
+        entity.get(GrabComponent.class).isEnabled = false;
     }
 
     @Override
