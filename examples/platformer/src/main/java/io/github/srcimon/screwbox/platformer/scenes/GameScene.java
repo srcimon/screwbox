1ca7e17ebffe6b6693e3bc1bbda41d8ba2614bec
simonbas <sebastian.simon@koeln.de>
2025-01-14 21:56:13 +0100

    added patrolb

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/scenes/GameScene.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/scenes/GameScene.java
@@ -3,6 +3,7 @@ package io.github.srcimon.screwbox.platformer.scenes;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.environment.Environment;
+import io.github.srcimon.screwbox.core.environment.ai.AiPatrolMovementSystem;
 import io.github.srcimon.screwbox.core.environment.core.LogFpsSystem;
 import io.github.srcimon.screwbox.core.scenes.Scene;
 import io.github.srcimon.screwbox.platformer.collectables.Cherries;
@@ -76,6 +77,7 @@ public class GameScene implements Scene {
                 .addSystem(new MovingPlatformSystem())
                 .addSystem(new CollectableSystem())
                 .addSystem(new VanishingOnCollisionSystem())
+                .addSystem(new AiPatrolMovementSystem())
                 .addSystem(new ToggleLightSystem())
                 .addSystem(new KilledFromAboveSystem())
                 .addSystem(new ToggleSplitscreenSystem())
