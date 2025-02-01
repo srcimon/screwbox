44a1e0ccf7a32ee0a258127996857f251aff6ba0
simonbas <sebastian.simon@koeln.de>
2025-02-01 10:29:54 +0100

    fix typo in test class name

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/utils/ReflectionsTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/utils/ReflectionsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class RelfectionsTest {
+class ReflectionsTest {
 
     @Test
     void createInstancesFromPackage_packageNameNull_throwsException() {
