803e1e39713a71dcf42fffe1357a970c4bcb91c4
simonbas <sebastian.simon@koeln.de>
2025-02-04 22:28:44 +0100

    fix bug that crashes the example

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/menues/StartGameMenu.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/menues/StartGameMenu.java
@@ -14,7 +14,7 @@ public class StartGameMenu extends UiMenu {
         addItem("Start Level 1").onActivate(engine -> startMap("maps/1-1_teufelsinsel.json", engine));
         addItem("Start Level 2").onActivate(engine -> startMap("maps/1-2_misty_caves.json", engine));
         addItem("continue").onActivate(engine -> {
-            engine.scenes().add(new GameScene());
+            engine.scenes().addOrReplace(new GameScene());
             engine.scenes().environmentOf(GameScene.class).loadFromFile("savegame.sav");
             engine.scenes().switchTo(GameScene.class);
             engine.ui().closeMenu();
