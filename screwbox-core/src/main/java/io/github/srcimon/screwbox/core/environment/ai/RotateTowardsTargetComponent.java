d21d9deb978590bb3c14c8e2204ea614a045afc6
simonbas <sebastian.simon@koeln.de>
2025-02-15 17:29:56 +0100

    prototype new component

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/ai/RotateTowardsTargetComponent.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/ai/RotateTowardsTargetComponent.java
@@ -1,6 +1,5 @@
 package io.github.srcimon.screwbox.core.environment.ai;
 
-import io.github.srcimon.screwbox.core.Rotation;
 import io.github.srcimon.screwbox.core.environment.Component;
 
 import java.io.Serial;
@@ -10,8 +9,6 @@ public class RotateTowardsTargetComponent implements Component {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public Rotation rotation = Rotation.none();
-    public double speed = 1;
     public boolean updateSprite = true;
     public int targetId;
 
--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/ai/RotateTowardsTargetSystem.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/ai/RotateTowardsTargetSystem.java
@@ -1,34 +1,23 @@
 package io.github.srcimon.screwbox.core.environment.ai;
 
 import io.github.srcimon.screwbox.core.Engine;
-import io.github.srcimon.screwbox.core.Line;
 import io.github.srcimon.screwbox.core.Rotation;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
-import io.github.srcimon.screwbox.core.utils.ListUtil;
-
-import static java.util.Objects.nonNull;
 
 public class RotateTowardsTargetSystem implements EntitySystem {
 
-    private static final Archetype ROTORS = Archetype.of(RotateTowardsTargetComponent.class);
+    private static final Archetype ROTORS = Archetype.of(RotateTowardsTargetComponent.class, RenderComponent.class);
 
     @Override
     public void update(final Engine engine) {
-        for(final var entity : engine.environment().fetchAll(ROTORS)) {
+        for (final var entity : engine.environment().fetchAll(ROTORS)) {
             final var rotor = entity.get(RotateTowardsTargetComponent.class);
             engine.environment().tryFetchById(rotor.targetId).ifPresent(target -> {
-                final var targetRotationDegrees = Rotation.ofMovement(target.position().substract(entity.position())).degrees();
-                final var currentRotationDegrees = rotor.rotation.degrees();
-                final var nextRotationDegrees = currentRotationDegrees + (targetRotationDegrees - currentRotationDegrees) * engine.loop().delta(rotor.speed);
-                rotor.rotation = Rotation.degrees(nextRotationDegrees);
-                if(rotor.updateSprite) {
-                    var render = entity.get(RenderComponent.class);
-                    if(nonNull(render)) {
-                        render.options = render.options.rotation(rotor.rotation);
-                    }
-                }
+                final var render = entity.get(RenderComponent.class);
+                final var delta = target.position().substract(entity.position());
+                render.options = render.options.rotation(Rotation.ofMovement(delta));
             });
         }
     }
