9074134a6969777f8104c5b1a388438a24bbb30b
simonbas <sebastian.simon@koeln.de>
2025-02-17 19:16:58 +0100

    can sonar detect this?

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/controls/JumpControlComponent.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/environment/controls/JumpControlComponent.java
@@ -8,8 +8,8 @@ import java.io.Serial;
 //TODO add to reference docs
 public class JumpControlComponent implements Component {
 
-    @Serial
-    private static final long serialVersionUID = 1L;
+  //  @Serial
+   // private static final long serialVersionUID = 1L;
 
     public Enum<?> keyAlias;
     public double acceleration;
