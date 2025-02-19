b84c7ecaacf3456aaa4b2a992eba687669f0259d
simonbas <sebastian.simon@koeln.de>
2025-02-19 07:19:49 +0100

    add doc tests start

---
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/DocumentationTest.java
@@ -0,0 +1,11 @@
+package io.github.srcimon.screwbox.core;
+
+import org.junit.jupiter.api.Test;
+
+class DocumentationTest {
+
+    @Test
+    void verifyAllComponentsAreDocumentedWithinComponentOverview() {
+        //TODO
+    }
+}
