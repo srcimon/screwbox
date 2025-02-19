352bdb65e3b1c03d661ab5f09a8dc87f266cf56d
simonbas <sebastian.simon@koeln.de>
2025-02-20 00:45:31 +0100

    refactor test

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/DocumentationTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/DocumentationTest.java
@@ -2,42 +2,35 @@ package io.github.srcimon.screwbox.core;
 
 import io.github.srcimon.screwbox.core.environment.Component;
 import io.github.srcimon.screwbox.core.utils.Reflections;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DocumentationTest {
 
     //TODO move to docs?
 
     @ParameterizedTest
-    @MethodSource("allComponentNames")
-    void verifyAllComponentsAreDocumentedWithinComponentOverview(String componentName) {
+    @MethodSource("allComponentClasses")
+    void verifyAllComponentsAreDocumentedWithinComponentOverview(Class<? extends Component> componentClazz) throws Exception {
         //TODO
-        try {
-           final var allLines = Files.readAllLines(Path.of("../docs/docs/reference/components-overview.md"));
-            Assertions.assertThat(allLines).anyMatch(l -> l.contains(componentName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        final var allLines = Files.readAllLines(Path.of("../docs/docs/reference/components-overview.md"));
+        assertThat(allLines).anyMatch(l -> l.contains(componentClazz.getSimpleName()));
     }
 
-    private static Stream<Arguments> allComponentNames() {
+    private static Stream<Arguments> allComponentClasses() {
         return Reflections.findClassesInPackage("io.github.srcimon.screwbox.core.environment").stream()
                 .filter(Component.class::isAssignableFrom)
-                .map(Class::getSimpleName)
                 .map(Arguments::of);
 
 
-
-    //TODO find implementations in package...
+        //TODO find implementations in package...
 
     }
 }
