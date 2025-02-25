96711f895eb4f9aca6eab74f6098debc4cb38404
simonbas <sebastian.simon@koeln.de>
2025-02-25 20:50:00 +0100

    added tests

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/RenderPipeline.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/RenderPipeline.java
@@ -7,15 +7,13 @@ import java.util.concurrent.ExecutorService;
 
 public class RenderPipeline {
 
-    final DefaultRenderer defaultRenderer;
-    final AsyncRenderer asyncRenderer;
-    final FirewallRenderer firewallRenderer;
-    final StandbyProxyRenderer standbyProxyRenderer;
+    private final AsyncRenderer asyncRenderer;
+    private final StandbyProxyRenderer standbyProxyRenderer;
 
     public RenderPipeline(final ExecutorService executor) {
-        defaultRenderer = new DefaultRenderer();
+        DefaultRenderer defaultRenderer = new DefaultRenderer();
         asyncRenderer = new AsyncRenderer(defaultRenderer, executor);
-        firewallRenderer = new FirewallRenderer(asyncRenderer);
+        FirewallRenderer firewallRenderer = new FirewallRenderer(asyncRenderer);
         standbyProxyRenderer = new StandbyProxyRenderer(firewallRenderer);
     }
 
--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/RendererPipelineTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/internal/renderer/RendererPipelineTest.java
@@ -1,12 +1,17 @@
 package io.github.srcimon.screwbox.core.graphics.internal.renderer;
 
 import io.github.srcimon.screwbox.core.Duration;
+import io.github.srcimon.screwbox.core.graphics.ScreenBounds;
+import io.github.srcimon.screwbox.core.graphics.Size;
+import io.github.srcimon.screwbox.core.graphics.SpriteBundle;
+import io.github.srcimon.screwbox.core.graphics.drawoptions.SpriteFillOptions;
 import io.github.srcimon.screwbox.core.test.TestUtil;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,11 +21,14 @@ class RendererPipelineTest {
 
     RenderPipeline renderPipeline;
     ExecutorService executorService;
+    Image image;
 
     @BeforeEach
     void setUp() {
         executorService = Executors.newSingleThreadExecutor();
         renderPipeline = new RenderPipeline(executorService);
+        image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_ARGB);
+        updateContext();
     }
 
     @Test
@@ -28,7 +36,26 @@ class RendererPipelineTest {
         assertThat(renderPipeline.renderDuration()).isEqualTo(Duration.none());
     }
 
-    //TODO add real tests
+    @Test
+    void renderDuration_renderingHappened_hasDuration() {
+        renderPipeline.toggleOnOff();
+        updateContext();
+
+        renderPipeline.renderer().fillWith(SpriteBundle.EXPLOSION.get(), SpriteFillOptions.scale(2), new ScreenBounds(Size.of(640, 480)));
+        assertThat(renderPipeline.renderDuration().nanos()).isPositive();
+    }
+
+    @Test
+    void renderDuration_rendererOffline_isNone() {
+        updateContext();
+
+        renderPipeline.renderer().fillWith(SpriteBundle.EXPLOSION.get(), SpriteFillOptions.scale(2), new ScreenBounds(Size.of(640, 480)));
+        assertThat(renderPipeline.renderDuration()).isEqualTo(Duration.none());
+    }
+
+    void updateContext() {
+        renderPipeline.renderer().updateContext(() -> (Graphics2D) image.getGraphics());
+    }
 
     @AfterEach
     void tearDown() {
