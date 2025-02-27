ce082bba2753d4b0e37b51d09aadbed3aba6b844
simonbas <sebastian.simon@koeln.de>
2025-02-27 18:09:58 +0100

    added shader bundle

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/ShaderBundle.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/ShaderBundle.java
@@ -1,17 +1,24 @@
 package io.github.srcimon.screwbox.core.graphics;
 
-import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.assets.AssetBundle;
 import io.github.srcimon.screwbox.core.graphics.drawoptions.ShaderOptions;
+import io.github.srcimon.screwbox.core.graphics.shader.GrayscaleShader;
 import io.github.srcimon.screwbox.core.graphics.shader.WaterDistortionShader;
+
+import static io.github.srcimon.screwbox.core.Duration.ofSeconds;
+import static io.github.srcimon.screwbox.core.graphics.drawoptions.ShaderOptions.shader;
 
 //TODO add to bundle documentation
 //TODO add test for AssetBundles in bundle documentation
 public enum ShaderBundle implements AssetBundle<ShaderOptions> {
 
-    WATER(ShaderOptions.shader(new WaterDistortionShader()).cacheSize(30)),
-    BREEZE(ShaderOptions.shader(new WaterDistortionShader(2, 0.25)).duration(Duration.ofSeconds(4)).cacheSize(30));
+    WATER(shader(new WaterDistortionShader())
+            .cacheSize(30)),
+    BREEZE(shader(new WaterDistortionShader(2, 0.25))
+            .duration(ofSeconds(4))
+            .cacheSize(30)),
+    GRAYSCALE(shader(new GrayscaleShader()));
 
     private final Asset<ShaderOptions> options;
 
