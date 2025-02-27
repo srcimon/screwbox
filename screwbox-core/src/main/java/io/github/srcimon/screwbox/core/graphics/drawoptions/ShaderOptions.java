d3cdebf07a76e48264e85c1b4445c70ba256b7d4
simonbas <sebastian.simon@koeln.de>
2025-02-27 15:26:21 +0100

    refactorings

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/drawoptions/ShaderOptions.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/drawoptions/ShaderOptions.java
@@ -6,4 +6,20 @@ import io.github.srcimon.screwbox.core.Time;
 import io.github.srcimon.screwbox.core.graphics.Shader;
 
 public record ShaderOptions(Shader shader, Time offset, Duration duration, Ease ease) {
+
+    public static ShaderOptions shader(Shader shader) {
+        return new ShaderOptions(shader, Time.unset(), Duration.oneSecond(), Ease.LINEAR_IN);
+    }
+
+    public ShaderOptions offset(Time offset) {
+        return new ShaderOptions(shader, offset, duration, ease);
+    }
+
+    public ShaderOptions duration(Duration duration) {
+        return new ShaderOptions(shader, offset, duration, ease);
+    }
+
+    public ShaderOptions ease(Ease ease) {
+        return new ShaderOptions(shader, offset, duration, ease);
+    }
 }
