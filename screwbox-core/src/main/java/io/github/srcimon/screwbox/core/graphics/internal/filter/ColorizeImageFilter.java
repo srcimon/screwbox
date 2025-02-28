d17ea4bf9ec7e221b10cdb62b5a8dad65e79009a
simonbas <sebastian.simon@koeln.de>
2025-02-28 15:44:46 +0100

    fixed sonar issues

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/filter/ColorizeImageFilter.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/filter/ColorizeImageFilter.java
@@ -12,10 +12,10 @@ public class ColorizeImageFilter extends RGBImageFilter {
 
     public ColorizeImageFilter(int deltaRed, int deltaGreen, int deltaBlue) {
         Validate.range(deltaRed, 0, 255, "deltaRed must be in range 0 to 255");
-        this.deltaRed = deltaRed;
         Validate.range(deltaGreen, 0, 255, "deltaGreen must be in range 0 to 255");
-        this.deltaGreen = deltaGreen;
         Validate.range(deltaBlue, 0, 255, "deltaBlue must be in range 0 to 255");
+        this.deltaRed = deltaRed;
+        this.deltaGreen = deltaGreen;
         this.deltaBlue = deltaBlue;
     }
 
@@ -26,9 +26,9 @@ public class ColorizeImageFilter extends RGBImageFilter {
         final int green = (rgb & 0x00ff00) >> 8;
         final int blue = (rgb & 0x0000ff);
 
-        final int targetRed = Math.max(0, Math.min(0xff, red + deltaRed));
-        final int targetGreen = Math.max(0, Math.min(0xff, green + deltaGreen));
-        final int targetBlue = Math.max(0, Math.min(0xff, blue + deltaBlue));
+        final int targetRed = Math.clamp(red + deltaRed, 0, 0xff);
+        final int targetGreen = Math.clamp(green + deltaGreen, 0, 0xff);
+        final int targetBlue = Math.clamp(blue + deltaBlue, 0, 0xff);
 
         return alpha | (targetRed << 16) | (targetGreen << 8) | targetBlue;
     }
