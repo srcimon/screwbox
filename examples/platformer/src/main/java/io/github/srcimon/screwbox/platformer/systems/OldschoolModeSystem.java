0f7af95e47aceb18b38c9743564da8c7e29a4f70
simonbas <sebastian.simon@koeln.de>
2025-03-03 21:35:52 +0100

    prototype

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/systems/OldschoolModeSystem.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/systems/OldschoolModeSystem.java
@@ -19,11 +19,11 @@ public class OldschoolModeSystem implements EntitySystem {
             if (hasShader) {
                 engine.graphics().configuration().disableOverlayShader();
             } else {
-                engine.graphics().configuration().setOverlayShader(ShaderBundle.CHROMATIC_ABBERATION);
+                engine.graphics().configuration().setOverlayShader(ShaderBundle.GRAYSCALE);
             }
         }
         if (hasShader != crtSystemPresent) {
-            //engine.environment().toggleSystem(new CrtMonitorOverlaySystem());
+            engine.environment().toggleSystem(new CrtMonitorOverlaySystem());
         }
     }
 }
