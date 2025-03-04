f52e45f9c2b4b1412d38f8dd3226078aaba826d7
simonbas <sebastian.simon@koeln.de>
2025-03-04 22:15:03 +0100

    nicer shader previews

--- screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/ShaderBundleTest.java
+++ screwbox-core/src/test/java/io/github/srcimon/screwbox/core/graphics/ShaderBundleTest.java
@@ -11,7 +11,6 @@ class ShaderBundleTest {
     @EnumSource(ShaderBundle.class)
     void verifyAllShadersAreValid(ShaderBundle shader) {
         assertThatNoException().isThrownBy(shader::get);
-        shader.get().createPreview(SpriteBundle.BOX_STRIPED.get().addBorder(2, Color.TRANSPARENT).singleImage(), SpriteBundle.SHADER_PREVIEW.get().singleImage(), 20).scaled(2).exportGif(shader.name());
     }
 
 }
