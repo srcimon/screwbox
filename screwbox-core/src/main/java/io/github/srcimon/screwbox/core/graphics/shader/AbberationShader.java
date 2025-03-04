59f8e737f565525b22cea33b1e85507fb8841357
simonbas <sebastian.simon@koeln.de>
2025-03-04 18:52:54 +0100

    prototype

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/shader/AbberationShader.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/shader/AbberationShader.java
@@ -2,40 +2,37 @@ package io.github.srcimon.screwbox.core.graphics.shader;
 
 import io.github.srcimon.screwbox.core.Percent;
 import io.github.srcimon.screwbox.core.graphics.Shader;
-import io.github.srcimon.screwbox.core.utils.Pixelperfect;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class AbberationShader extends Shader {
 
+    private static final int SIZE_INCREASE = 8;
+    private static final int OFFSET_CORRECT = SIZE_INCREASE / 2;
     //TODO initialize via constructor
 
     public AbberationShader() {
         super("abberation");
     }
 
-//    double x = Pixelperfect.value(Math.sin(20 * progress.value()) );
-//    double y = Pixelperfect.value(Math.sin(20 * progress.value()) * 3);
-//    double x2 = Pixelperfect.value(Math.sin(15 * progress.value()) * -2);
-//    double y2 = Pixelperfect.value(Math.sin(15 * progress.value()) * -3);
-
     @Override
     public Image apply(Image source, Percent progress) {
-        int added = 8;
-        final var result = new BufferedImage(source.getWidth(null) + added, source.getHeight(null) + added, BufferedImage.TYPE_INT_ARGB);
-        int xo = added / 2;
+        final var result = new BufferedImage(
+                source.getWidth(null) + SIZE_INCREASE,
+                source.getHeight(null) + SIZE_INCREASE, BufferedImage.TYPE_INT_ARGB);
+
         Graphics2D graphics = (Graphics2D) result.getGraphics();
-        double x =Math.sin(10 * progress.value()) *3;
+        double x = Math.sin(10 * progress.value()) * 3;
         double y = Math.sin(20 * progress.value()) * 5;
         double x2 = Math.sin(20 * progress.value()) * -5;
         double y2 = Math.sin(10 * progress.value()) * -3;
         graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (0.10f + progress.value() / 20.0)));
-        graphics.drawImage(source, xo+(int) x, xo+(int) y, null);
-        graphics.drawImage(source, xo+(int) x2, xo+(int) y2, null);
+        graphics.drawImage(source, OFFSET_CORRECT + (int) x, OFFSET_CORRECT + (int) y, null);
+        graphics.drawImage(source, OFFSET_CORRECT + (int) x2, OFFSET_CORRECT + (int) y2, null);
         graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (0.20f + -progress.value() / 20.0)));
-        graphics.drawImage(source, xo+(int) x, xo+(int) y2, null);
-        graphics.drawImage(source, xo+(int) x2, xo+(int) y2, null);
+        graphics.drawImage(source, OFFSET_CORRECT + (int) x, OFFSET_CORRECT + (int) y2, null);
+        graphics.drawImage(source, OFFSET_CORRECT + (int) x2, OFFSET_CORRECT + (int) y2, null);
         graphics.dispose();
         return result;
     }
