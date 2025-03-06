62bcfc39ef2e2d4b60151733b7179504ffe0125b
simonbas <sebastian.simon@koeln.de>
2025-03-06 22:23:15 +0100

    refactor old filter code

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/filter/ReplaceColorFilter.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/internal/filter/ReplaceColorFilter.java
@@ -1,23 +1,21 @@
 package io.github.srcimon.screwbox.core.graphics.internal.filter;
 
 import io.github.srcimon.screwbox.core.graphics.Color;
-import io.github.srcimon.screwbox.core.graphics.internal.AwtMapper;
 
 import java.awt.image.RGBImageFilter;
 
 public class ReplaceColorFilter extends RGBImageFilter {
 
-    private final int oldColorAwt;
-    private final int newColorAwt;
+    private final int oldRgb;
+    private final int newRgb;
 
     public ReplaceColorFilter(final Color oldColor, final Color newColor) {
-        oldColorAwt = AwtMapper.toAwtColor(oldColor).getRGB();
-        newColorAwt = AwtMapper.toAwtColor(newColor).getRGB();
+        oldRgb = oldColor.rgb();
+        newRgb = newColor.rgb();
     }
 
     @Override
     public int filterRGB(final int x, final int y, final int rgb) {
-        return rgb == oldColorAwt ? newColorAwt : rgb;
+        return rgb == oldRgb ? newRgb : rgb;
     }
-
 }
