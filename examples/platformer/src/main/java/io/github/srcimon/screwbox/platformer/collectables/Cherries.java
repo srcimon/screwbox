9622bde215ef1d0299b9cb7b51fb213504ce8981
simonbas <sebastian.simon@koeln.de>
2025-03-08 21:57:24 +0100

    fixed cherries!

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/collectables/Cherries.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/collectables/Cherries.java
@@ -1,5 +1,6 @@
 package io.github.srcimon.screwbox.platformer.collectables;
 
+import io.github.srcimon.screwbox.core.Duration;
 import io.github.srcimon.screwbox.core.assets.Asset;
 import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.SourceImport.Converter;
@@ -9,9 +10,12 @@ import io.github.srcimon.screwbox.core.environment.light.PointLightComponent;
 import io.github.srcimon.screwbox.core.environment.physics.CollisionSensorComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.FixedSpinComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
+import io.github.srcimon.screwbox.core.environment.tweening.TweenComponent;
 import io.github.srcimon.screwbox.core.environment.tweening.TweenOrbitPositionComponent;
 import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.graphics.ShaderBundle;
 import io.github.srcimon.screwbox.core.graphics.Sprite;
+import io.github.srcimon.screwbox.core.graphics.drawoptions.SpriteDrawOptions;
 import io.github.srcimon.screwbox.platformer.components.CollectableComponent;
 import io.github.srcimon.screwbox.tiled.GameObject;
 
@@ -19,17 +23,25 @@ import static io.github.srcimon.screwbox.tiled.Tileset.spriteAssetFromJson;
 
 public class Cherries implements Converter<GameObject> {
 
-    private static final Asset<Sprite> SPRITE = spriteAssetFromJson("tilesets/collectables/cherries.json");
+    private static final Asset<Sprite> SPRITE = spriteAssetFromJson("tilesets/collectables/cherries.json", "animation");
 
     @Override
     public Entity convert(final GameObject object) {
+        int x = 0;
+        System.out.println(SPRITE.get().allFrames().size());
+        for(final var i : SPRITE.get().allFrames()) {
+           for(int xp = 0; xp < i.image().getWidth(null); xp++) {
+               System.out.println(i.colorAt(xp,0).opacity().value());
+               x++;
+           }
+        }
         return new Entity().add(
-                new TweenOrbitPositionComponent(object.position(), 2),
-                new FixedSpinComponent(0.4),
+             //   new FixedSpinComponent(0.4),
                 new PointLightComponent(20, Color.RED),
                 new GlowComponent(60, Color.RED.opacity(0.5)),
                 new TransformComponent(object.bounds()),
-                new RenderComponent(SPRITE.get(), object.layer().order()),
+//                new RenderComponent(SPRITE.get(), object.layer().order(), SpriteDrawOptions.originalSize().shaderSetup(ShaderBundle.DISSOLVE)),
+                new RenderComponent(SPRITE.get(), object.layer().order(), SpriteDrawOptions.originalSize()),
                 new CollisionSensorComponent(),
                 new CollectableComponent());
     }
