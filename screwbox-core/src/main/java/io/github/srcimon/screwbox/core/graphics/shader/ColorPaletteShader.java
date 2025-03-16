2b7fafa5353b6d0684ab3435537feb83d9eb2bed
simonbas <sebastian.simon@koeln.de>
2025-03-16 12:23:05 +0100

    added ColorPaletteShader

--- screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/shader/ColorPaletteShader.java
+++ screwbox-core/src/main/java/io/github/srcimon/screwbox/core/graphics/shader/ColorPaletteShader.java
@@ -1,23 +1,58 @@
 package io.github.srcimon.screwbox.core.graphics.shader;
 
 import io.github.srcimon.screwbox.core.Percent;
+import io.github.srcimon.screwbox.core.graphics.Color;
 import io.github.srcimon.screwbox.core.graphics.Shader;
+import io.github.srcimon.screwbox.core.graphics.internal.ImageOperations;
 
 import java.awt.*;
+import java.awt.image.RGBImageFilter;
 import java.io.Serial;
+import java.util.Set;
+
+import static java.util.stream.Collectors.joining;
+
 //TODO document
 //TODO implement
 public class ColorPaletteShader extends Shader {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private final Set<Color> colorPalette;
 
-    protected ColorPaletteShader() {
-        super("ColorPaletteShader");
+    public ColorPaletteShader(final Set<Color> colorPalette) {
+        super("ColorPaletteShader-" + colorPalette.stream().map(Color::hex).collect(joining("-")));
+        //TODO validate not empty!
+        this.colorPalette = colorPalette;
     }
 
     @Override
-    public Image apply(Image source, Percent progress) {
-        return source;
+    public Image apply(final Image source, final Percent progress) {
+        return ImageOperations.applyFilter(source, new RGBImageFilter() {
+
+            @Override
+            public int filterRGB(int x, int y, int rgb) {
+                //TODO really helps?
+                if(rgb==0) {
+                    return 0;
+                }
+
+                var in = Color.rgb(rgb);
+                return getNearestColor(in);
+            }
+        });
+    }
+
+    private int getNearestColor(final Color color) {
+        Color nearest = null;
+        double nearestDistance = Double.MAX_VALUE;
+        for(var paletteColor : colorPalette) {
+            double difference = paletteColor.difference(color);
+            if(nearest==null || nearestDistance > difference) {
+                nearest = paletteColor;
+                nearestDistance = difference;
+            }
+        }
+        return nearest.rgb();
     }
 }
