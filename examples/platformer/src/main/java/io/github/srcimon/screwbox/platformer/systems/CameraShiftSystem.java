666d91ed83444ef246570d0695a031788fe5015f
simonbas <sebastian.simon@koeln.de>
2025-03-18 23:03:23 +0100

    refactor CameraShiftSystem

--- examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/systems/CameraShiftSystem.java
+++ examples/platformer/src/main/java/io/github/srcimon/screwbox/platformer/systems/CameraShiftSystem.java
@@ -3,14 +3,11 @@ package io.github.srcimon.screwbox.platformer.systems;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
-import io.github.srcimon.screwbox.core.environment.Entity;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.environment.Order;
 import io.github.srcimon.screwbox.core.environment.rendering.CameraTargetComponent;
 import io.github.srcimon.screwbox.core.environment.rendering.RenderComponent;
 import io.github.srcimon.screwbox.platformer.components.PlayerMarkerComponent;
-
-import java.util.Optional;
 
 @Order(Order.SystemOrder.SIMULATION_EARLY)
 public class CameraShiftSystem implements EntitySystem {
@@ -19,26 +16,14 @@ public class CameraShiftSystem implements EntitySystem {
 
     @Override
     public void update(Engine engine) {
-        Optional<Entity> playerEntity = engine.environment().tryFetchSingleton(PLAYER);
-        if (playerEntity.isEmpty()) {
-            return;
-        }
-
-        double delta = engine.loop().delta();
-        Entity player = playerEntity.get();
-        for (var target : engine.environment().fetchAllHaving(CameraTargetComponent.class)) {
-            var configuration = target.get(CameraTargetComponent.class);
-            if (player.get(RenderComponent.class).options.isFlipHorizontal()) {
-                configuration.shift = Vector.of(
-                        Math.max(-50,
-                                configuration.shift.x() - configuration.followSpeed * delta * 100),
-                        0);
-            } else {
-                configuration.shift = Vector.of(
-                        Math.min(50,
-                                configuration.shift.x() + configuration.followSpeed * delta * 100),
-                        0);
+        engine.environment().tryFetchSingleton(PLAYER).ifPresent(player -> {
+            final double delta = engine.loop().delta(100);
+            for (var target : engine.environment().fetchAllHaving(CameraTargetComponent.class)) {
+                var configuration = target.get(CameraTargetComponent.class);
+                configuration.shift = player.get(RenderComponent.class).options.isFlipHorizontal()
+                        ? Vector.x(Math.max(-50, configuration.shift.x() - configuration.followSpeed * delta))
+                        : Vector.x(Math.min(50, configuration.shift.x() + configuration.followSpeed * delta));
             }
-        }
+        });
     }
 }
