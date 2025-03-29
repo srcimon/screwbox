17bfa20e23c12be78f192cfda7e722c28be3642c
simonbas <sebastian.simon@koeln.de>
2025-03-29 23:27:45 +0100

    prototype interactive water

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/DrawWaterSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/DrawWaterSystem.java
@@ -2,24 +2,33 @@ package io.github.srcimon.screwbox.playground;
 
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Line;
-import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.graphics.World;
 import io.github.srcimon.screwbox.core.graphics.drawoptions.CircleDrawOptions;
 import io.github.srcimon.screwbox.core.graphics.drawoptions.LineDrawOptions;
 
+import static io.github.srcimon.screwbox.core.Vector.$;
+
 public class DrawWaterSystem implements EntitySystem {
 
-    private WaterSurface waterSurface = new WaterSurface(Line.between(Vector.$(-240, 0), Vector.$(240, 0)), 8);
+    private WaterSurface waterSurface = new WaterSurface(Line.between($(-240, 0), $(240, 0)), 8);
 
     @Override
     public void update(Engine engine) {
-        var nodeOptions = CircleDrawOptions.filled(Color.WHITE);
+        var nodeOptions = CircleDrawOptions.filled(Color.hex("#7878e7"));
+        var nodeOutlineOptions = CircleDrawOptions.outline(Color.WHITE).strokeWidth(2);
+        var segmentOptions = LineDrawOptions.color(Color.WHITE).strokeWidth(2);
+
+        World world = engine.graphics().world();
+
         for (final var segment : waterSurface.surface().segments()) {
-            engine.graphics().world().drawLine(segment, LineDrawOptions.color(Color.WHITE).strokeWidth(2));
+            world.drawLine(segment, segmentOptions);
         }
+
         for (final var node : waterSurface.nodes()) {
-            engine.graphics().world().drawCircle(node.position(), 8, nodeOptions);
+            world.drawCircle(node.position(), 8, nodeOptions);
+            world.drawCircle(node.position(), 8, nodeOutlineOptions);
         }
     }
 }
--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/WaterSurface.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/WaterSurface.java
@@ -2,15 +2,35 @@ package io.github.srcimon.screwbox.playground;
 
 import io.github.srcimon.screwbox.core.Line;
 import io.github.srcimon.screwbox.core.Path;
+import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.utils.Validate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 public class WaterSurface {
 
-    private final List<WaterNode> nodes = new ArrayList<>();
+    class Node {
+
+        private Vector restingPosition;
+        private double height;
+        private double speed;
+
+        Node(final Vector restingPosition) {
+            this.restingPosition = restingPosition;
+        }
+
+        Vector position() {
+            return restingPosition.addY(height);
+        }
+
+        public void update(double delta) {
+            height = height + delta * speed;
+        }
+    }
+
+
+    private final List<Node> nodes = new ArrayList<>();
 
     public WaterSurface(Line waterLine, int nodeCount) {
         Validate.isTrue(() -> waterLine.from().y() == waterLine.to().y(), "water line must be horizontal");
@@ -19,16 +39,20 @@ public class WaterSurface {
         double distance = waterLine.length() / nodeCount;
 
         for (int i = 0; i < nodeCount; i++) {
-            nodes.add(new WaterNode(waterLine.from().addX(x)));
+            nodes.add(new Node(waterLine.from().addX(x)));
             x += distance;
         }
     }
 
-    public List<WaterNode> nodes() {
+    public void update(double delta) {
+        nodes().forEach(node -> node.update(delta));
+    }
+
+    public List<Node> nodes() {
         return nodes;
     }
 
     public Path surface() {
-        return Path.withNodes(nodes().stream().map(WaterNode::position).toList());
+        return Path.withNodes(nodes().stream().map(Node::position).toList());
     }
 }
