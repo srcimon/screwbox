461c521e5a059733cc7ce0d0a876b374e6f74776
simonbas <sebastian.simon@koeln.de>
2025-03-30 16:47:00 +0200

    prototype interactive water

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
@@ -1,9 +1,13 @@
 package io.github.srcimon.screwbox.playground;
 
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
+import io.github.srcimon.screwbox.core.environment.physics.GravityComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
+
+import static io.github.srcimon.screwbox.core.utils.MathUtil.modifier;
 
 public class FloatSystem implements EntitySystem {
 
@@ -15,12 +19,19 @@ public class FloatSystem implements EntitySystem {
     public void update(Engine engine) {
         final var fluids = engine.environment().fetchAll(FLUIDS);
         final var floatings = engine.environment().fetchAll(FLOATINGS);
-
-        for(final var fluid : fluids) {
-            for(final var floating : floatings) {
+        var gravity = engine.environment().tryFetchSingletonComponent(GravityComponent.class).map(g -> g.gravity).orElse(Vector.zero());
+        for (final var floating : floatings) {
+            var physics = floating.get(PhysicsComponent.class);
+            for (final var fluid : fluids) {
                 if (fluid.bounds().intersects(floating.bounds())) {
-                    var physics = floating.get(PhysicsComponent.class);
-                    physics.momentum = physics.momentum.addY(engine.loop().delta(-800));
+                    physics.momentum = physics.momentum.addY(engine.loop().delta(-400)).add(gravity.multiply(engine.loop().delta()).invert());
+
+                    final double friction = 80 * engine.loop().delta();
+                    final double absX = Math.abs(physics.momentum.x());
+                    final double absY = Math.abs(physics.momentum.y());
+                    final double changeX = Math.clamp(modifier(physics.momentum.x()) * friction * -1, -absX, absX);
+                    final double changeY = Math.clamp(modifier(physics.momentum.y()) * friction * -1, -absY, absY);
+                    physics.momentum =physics.momentum.add(changeX, changeY);
                 }
             }
         }
