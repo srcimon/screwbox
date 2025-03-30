d3c0109cf37dfd48576f1c2315c4be52dfbee2e1
simonbas <sebastian.simon@koeln.de>
2025-03-30 17:13:04 +0200

    prototype interactive water

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
@@ -1,11 +1,15 @@
 package io.github.srcimon.screwbox.playground;
 
 import io.github.srcimon.screwbox.core.Engine;
+import io.github.srcimon.screwbox.core.Line;
+import io.github.srcimon.screwbox.core.Path;
 import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
 import io.github.srcimon.screwbox.core.environment.physics.GravityComponent;
 import io.github.srcimon.screwbox.core.environment.physics.PhysicsComponent;
+import io.github.srcimon.screwbox.core.graphics.Color;
+import io.github.srcimon.screwbox.core.graphics.drawoptions.LineDrawOptions;
 
 import static io.github.srcimon.screwbox.core.utils.MathUtil.modifier;
 
@@ -25,18 +29,32 @@ public class FloatSystem implements EntitySystem {
             for (final var floating : floatings) {
                 var physics = floating.get(PhysicsComponent.class);
                 FluidSurface surface = fluid.get(FluidComponent.class).surface;
-                if (fluid.bounds().intersects(floating.bounds().expandTop(surface.maxHeight()))) {
+                if (fluid.bounds().intersects(floating.bounds().expandTop(surface.maxHeight()))) {//TODO necessary?
 
+                    var surfacePath = surface.surface(fluid.origin(), fluid.bounds().width());
+                    double height = getHeight(surfacePath, floating.bounds().position());
+                    if(height < 0) {
                     physics.momentum = physics.momentum.addY(engine.loop().delta(-400)).add(gravity.multiply(engine.loop().delta()).invert());
-
-                    final double friction = 300 * engine.loop().delta();
+                    final double friction = 200 * engine.loop().delta();
                     final double absX = Math.abs(physics.momentum.x());
                     final double absY = Math.abs(physics.momentum.y());
                     final double changeX = Math.clamp(modifier(physics.momentum.x()) * friction * -1, -absX, absX);
                     final double changeY = Math.clamp(modifier(physics.momentum.y()) * friction * -1, -absY, absY);
                     physics.momentum = physics.momentum.add(changeX, changeY);
+                    }
                 }
             }
         }
+    }
+
+    private double getHeight(Path surfacePath, Vector position) {
+        var normal = Line.normal(position.addX(1.111), -1000);
+        for(var segment : surfacePath.segments()) {
+            var point = segment.intersectionPoint(normal);
+            if(point != null) {
+                return point.y() - position.y();
+            }
+        }
+        return 0;
     }
 }
