45177711bff9f197a09a6f508eadcee62b2fe06b
simonbas <sebastian.simon@koeln.de>
2025-04-02 18:25:59 +0200

    refactor

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/FloatSystem.java
@@ -1,8 +1,8 @@
 package io.github.srcimon.screwbox.playground;
 
+import io.github.srcimon.screwbox.core.Bounds;
 import io.github.srcimon.screwbox.core.Engine;
 import io.github.srcimon.screwbox.core.Line;
-import io.github.srcimon.screwbox.core.Path;
 import io.github.srcimon.screwbox.core.Vector;
 import io.github.srcimon.screwbox.core.environment.Archetype;
 import io.github.srcimon.screwbox.core.environment.EntitySystem;
@@ -24,29 +24,27 @@ public class FloatSystem implements EntitySystem {
 
         for (final var fluidEntity : fluids) {
             final Fluid fluid = fluidEntity.get(FluidComponent.class).fluid;
-            final var surface = fluid.surface(fluidEntity.bounds());
             for (final var floating : floatings) {
-                if (fluidEntity.bounds().intersects(floating.bounds().expandTop(fluid.maxHeight()))) {
-                    final var floatOptions = floating.get(FloatComponent.class);
-                    final double height = getHeight(surface, floating.bounds().position());
-                    if (height < 0) {
-                        final var physics = floating.get(PhysicsComponent.class);
-                        physics.momentum = physics.momentum.addY(engine.loop().delta(-floatOptions.buoyancy)).add(gravity.multiply(engine.loop().delta()).invert());
-                        final double friction = floatOptions.friction * engine.loop().delta();
-                        final double absX = Math.abs(physics.momentum.x());
-                        final double absY = Math.abs(physics.momentum.y());
-                        final double changeX = Math.clamp(modifier(physics.momentum.x()) * friction * -1, -absX, absX);
-                        final double changeY = Math.clamp(modifier(physics.momentum.y()) * friction * -1, -absY, absY);
-                        physics.momentum = physics.momentum.add(changeX, changeY);
-                    }
+                final var floatOptions = floating.get(FloatComponent.class);
+                final double height = getHeight(fluid, fluidEntity.bounds(), floating.bounds());
+                if (height < 0) {
+                    final var physics = floating.get(PhysicsComponent.class);
+                    physics.momentum = physics.momentum.addY(engine.loop().delta(-floatOptions.buoyancy)).add(gravity.multiply(engine.loop().delta()).invert());
+                    final double friction = floatOptions.friction * engine.loop().delta();
+                    final double absX = Math.abs(physics.momentum.x());
+                    final double absY = Math.abs(physics.momentum.y());
+                    final double changeX = Math.clamp(modifier(physics.momentum.x()) * friction * -1, -absX, absX);
+                    final double changeY = Math.clamp(modifier(physics.momentum.y()) * friction * -1, -absY, absY);
+                    physics.momentum = physics.momentum.add(changeX, changeY);
                 }
             }
         }
     }
 
-    private double getHeight(final Path surface, final Vector position) {
+    private double getHeight(final Fluid fluid, final Bounds fluidBounds, final Bounds bounds) {
+        var position = bounds.position();
         var normal = Line.normal(position, -1000);
-        for (var segment : surface.segments()) {
+        for (var segment : fluid.surface(fluidBounds).segments()) {
             var point = segment.intersectionPoint(normal);
             if (point != null) {
                 return point.y() - position.y();
