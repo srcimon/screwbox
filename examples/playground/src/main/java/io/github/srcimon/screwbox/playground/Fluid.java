50c91c68f17eb0505895df77ace50e69c1dd48dd
simonbas <sebastian.simon@koeln.de>
2025-04-02 19:31:31 +0200

    refactor

--- examples/playground/src/main/java/io/github/srcimon/screwbox/playground/Fluid.java
+++ examples/playground/src/main/java/io/github/srcimon/screwbox/playground/Fluid.java
@@ -5,8 +5,6 @@ import io.github.srcimon.screwbox.core.Vector;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 //TODO convert to simple component
 public class Fluid implements Serializable {
@@ -17,42 +15,34 @@ public class Fluid implements Serializable {
     private final FluidOptions options;
 
     public double getHeight(int nodeNr) {
-        return nodes.get(nodeNr).height;
+        return height[nodeNr];
     }
 
     public int nodeCount() {
-        return nodes.size();
+        return nodeCount;
     }
 
-    private class Node implements Serializable {
-
-        @Serial
-        private static final long serialVersionUID = 1L;
-
-        private double height;
-        private double speed;
-    }
-
-    private final List<Node> nodes = new ArrayList<>();
+    public final double[] height;
+    public final double[] speed;
+    public final int nodeCount;
 
     public Fluid(final FluidOptions options) {
         this.options = options;
-        for (int i = 0; i < options.nodeCount(); i++) {
-            nodes.add(new Node());
-        }
+        height = new double[options.nodeCount()];
+        speed = new double[options.nodeCount()];
+        nodeCount = options.nodeCount();
     }
 
     public void update(final double delta) {
-        for (int i = 0; i < nodes.size(); i++) {
-            var node = nodes.get(i);
-            final double deltaLeft = i > 0 ? node.height - nodes.get(i - 1).height : 0;
-            final double deltaRight = i < nodes.size() - 1 ? node.height - nodes.get(i + 1).height : 0;
-            node.height = node.height + delta * node.speed;
+        for (int i = 0; i < nodeCount; i++) {
+            final double deltaLeft = i > 0 ? height[i] - height[i - 1] : 0;
+            final double deltaRight = i < nodeCount - 1 ? height[i] - height[i + 1] : 0;
+            height[i] += +delta * speed[i];
 
             final double sidePull = deltaLeft * delta * options.transmission() + deltaRight * delta * options.transmission();
-            final double retract = node.height * options.retract() * delta;
-            final double dampen = options.dampening() * node.speed * delta;
-            node.speed += -sidePull -retract -dampen;
+            final double retract = height[i] * options.retract() * delta;
+            final double dampen = options.dampening() * speed[i] * delta;
+            speed[i] += -sidePull - retract - dampen;
         }
 
     }
@@ -60,10 +50,10 @@ public class Fluid implements Serializable {
     public void interact(final Bounds projection, final Bounds interaction, final double strength) {
         var nodePositions = FluidSupport.calculateSurface(projection, this);
 
-        for (int i = 0; i < nodes.size(); i++) {
+        for (int i = 0; i < nodeCount; i++) {
             final Vector nodePosition = nodePositions.get(i);
             if (interaction.contains(nodePosition)) {
-                nodes.get(i).speed += strength;
+                speed[i] += strength;
             }
         }
     }
