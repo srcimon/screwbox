cb7be2866d2165d35bb1d801a14805e0bc35cc87
simonbas <sebastian.simon@koeln.de>
2025-04-21 21:12:18 +0200

    Added depth property to `FloatComponent`

--- screwbox-core/src/main/java/dev/screwbox/core/physics/internal/CollisionResolver.java
+++ screwbox-core/src/main/java/dev/screwbox/core/physics/internal/CollisionResolver.java
@@ -50,15 +50,15 @@ public final class CollisionResolver {
         if (MathUtil.sameSign(correction.x(), physicsBodyComponent.momentum.x())) {
             return;
         }
-        physicsBodyComponent.momentum = Vector.of(
-                physicsBodyComponent.momentum.x() * -1 * colliderComponent.bounce.value(),
-                physicsBodyComponent.momentum.y());
+        physicsBodyComponent.momentum = physicsBodyComponent.momentum.replaceX(
+                physicsBodyComponent.momentum.x() * -1 * colliderComponent.bounce.value());
     }
 
     private static void reactOnVerticalCollision(final CollisionCheck pair, final double updateFactor) {
         final var physicsBodyComponent = pair.physicsBodyComponent();
         final var colliderComponent = pair.colliderComponent();
-        final Vector newMomentum = physicsBodyComponent.momentum.replaceY(physicsBodyComponent.momentum.y() * -1 * colliderComponent.bounce.value());
+        final Vector newMomentum = physicsBodyComponent.momentum.replaceY(
+                physicsBodyComponent.momentum.y() * -1 * colliderComponent.bounce.value());
 
         final double absX = abs(newMomentum.x());
         final double friction = Math.min(absX, colliderComponent.friction * updateFactor);
