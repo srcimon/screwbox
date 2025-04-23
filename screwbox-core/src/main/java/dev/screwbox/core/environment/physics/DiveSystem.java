427bd51d0f575626ad15ed80614b6bf3798470ad
simonbas <sebastian.simon@koeln.de>
2025-04-23 20:08:33 +0200

    added tests

--- screwbox-core/src/main/java/dev/screwbox/core/environment/physics/DiveSystem.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/physics/DiveSystem.java
@@ -18,21 +18,21 @@ public class DiveSystem implements EntitySystem {
     @Override
     public void update(Engine engine) {
         final var physicsEntities = engine.environment().fetchAll(Archetype.ofSpacial(PhysicsComponent.class));
-        for (var submergeEntity : engine.environment().fetchAll(Archetype.ofSpacial(DiveComponent.class, FloatComponent.class))) {
-            final var floatComponent = submergeEntity.get(FloatComponent.class);
-            final var diveComponent = submergeEntity.get(DiveComponent.class);
-            final var sensorBounds = Bounds.atOrigin(submergeEntity.origin().add(1, -0.5), submergeEntity.bounds().width() - 2, 1);
+        for (var divinEntity : engine.environment().fetchAll(Archetype.ofSpacial(DiveComponent.class, FloatComponent.class))) {
+            final var floatComponent = divinEntity.get(FloatComponent.class);
+            final var diveComponent = divinEntity.get(DiveComponent.class);
+
             if (isNull(diveComponent.inactiveDepth)) {
                 diveComponent.inactiveDepth = floatComponent.dive;
+            } else if (diveComponent.isDiving) {
+                floatComponent.dive = diveComponent.inactiveDepth;
             } else {
-                if(diveComponent.isDiving) {
-                    floatComponent.dive = diveComponent.inactiveDepth;
-                } else {
-                    diveComponent.inactiveDepth = floatComponent.dive;
-                }
+                diveComponent.inactiveDepth = floatComponent.dive;
             }
+
+            final var sensorBounds = Bounds.atOrigin(divinEntity.origin().add(1, -0.5), divinEntity.bounds().width() - 2, 1);
             for (final var physicsEntity : physicsEntities) {
-                if (submergeEntity != physicsEntity && sensorBounds.touches(physicsEntity.bounds())) {
+                if (divinEntity != physicsEntity && sensorBounds.touches(physicsEntity.bounds())) {
                     diveComponent.isDiving = true;
                     floatComponent.dive = diveComponent.maxDepth;
                 }
