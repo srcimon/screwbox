3406e7d5e88ac80eb6db7833fa4bafdca2b930e3
simonbas <sebastian.simon@koeln.de>
2025-05-13 22:39:38 +0200

    prototype new feature

--- examples/playground/src/main/java/dev/screwbox/playground/SplashSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/SplashSystem.java
@@ -8,30 +8,29 @@ import dev.screwbox.core.Percent;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.physics.FloatComponent;
-import dev.screwbox.core.environment.physics.FluidComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
 import dev.screwbox.core.graphics.SpriteBundle;
 import dev.screwbox.core.particles.ParticleOptions;
 import dev.screwbox.core.particles.SpawnMode;
-import dev.screwbox.core.utils.Scheduler;
 
 public class SplashSystem implements EntitySystem {
 
-//TODO sound!
+    //TODO sound!
     @Override
     public void update(Engine engine) {
         for (final var entity : engine.environment().fetchAll(Archetype.of(SplashComponent.class, FloatComponent.class, PhysicsComponent.class))) {
             var floatComponent = entity.get(FloatComponent.class);
-            if(floatComponent.attachedWave != null && entity.get(PhysicsComponent.class).momentum.length() > 15) {
-                if(entity.get(SplashComponent.class).scheduler.isTick()) {
+            if (floatComponent.attachedWave != null && entity.get(PhysicsComponent.class).momentum.length() > 15) {
+                if (entity.get(SplashComponent.class).scheduler.isTick()) {
                     engine.particles().spawn(Bounds.atOrigin(entity.bounds().minX(), floatComponent.attachedWave.middle().y(), entity.bounds().width(), 2)
-                            , SpawnMode.BOTTOM_SIDE, ParticleOptions.particleSource(entity).chaoticMovement(60, Duration.ofSeconds(1))
-                            .animateOpacity(Percent.half(), Percent.quarter())
-                                            .sprite(SpriteBundle.SMOKE)
-                            .ease(Ease.SINE_IN_OUT)
-                            .randomRotation(0.25)
-                                    .randomLifeTimeMilliseconds(100, 400)
-                            .animateScale(0.5, 1));
+                            , SpawnMode.BOTTOM_SIDE, ParticleOptions.particleSource(entity)
+                                    .chaoticMovement(60, Duration.ofSeconds(1))
+                                    .animateOpacity(Percent.quarter(), Percent.of(0.1))
+                                    .sprite(SpriteBundle.SMOKE)
+                                    .ease(Ease.SINE_IN_OUT)
+                                    .randomRotation(0.25)
+                                    .randomLifeTimeMilliseconds(200, 500)
+                                    .animateScale(0.5, 1));
                 }
             }
         }
