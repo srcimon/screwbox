abbefc25426c9f5372b09ca39458e4c706892a24
simonbas <sebastian.simon@koeln.de>
2025-05-22 20:03:39 +0200

    Added `SoundBundle.FLUID`

--- examples/playground/src/main/java/dev/screwbox/playground/FluidEffectsSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/FluidEffectsSystem.java
@@ -2,17 +2,22 @@ package dev.screwbox.playground;
 
 import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
+import dev.screwbox.core.audio.SoundOptions;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.fluids.FluidComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
 import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.particles.SpawnMode;
+import dev.screwbox.core.utils.ListUtil;
+
+import java.util.Random;
 
 public class FluidEffectsSystem implements EntitySystem {
 
     private static final Archetype FLUIDS = Archetype.ofSpacial(FluidEffectsComponent.class, FluidComponent.class);
     private static final Archetype PHYSICS = Archetype.ofSpacial(PhysicsComponent.class, RenderComponent.class);
+    private static final Random RANDOM = new Random();
 
     @Override
     public void update(Engine engine) {
@@ -39,6 +44,12 @@ public class FluidEffectsSystem implements EntitySystem {
                     if (isInside) {//TODO configure threshold
                         Bounds bounds = Bounds.atOrigin(physicsEntity.bounds().minX(), y, physicsEntity.bounds().width(), 2);
                         engine.particles().spawn(bounds, SpawnMode.BOTTOM_SIDE, effects.particleOptions.source(entity));
+if(effects.soundScheduler.isTick()) {
+    engine.audio().playSound(ListUtil.randomFrom(effects.sounds), SoundOptions.playOnce()
+            .speed(RANDOM.nextDouble(0.5, 1.2))//TODO configure ranges in splashcomponent
+            .position(physicsEntity.position())
+    );
+}
                     }
                 }
             }
