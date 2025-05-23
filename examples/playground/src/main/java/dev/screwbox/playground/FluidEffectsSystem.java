39c5ffe43d3fb2761907769916767c96828e3f01
simonbas <sebastian.simon@koeln.de>
2025-05-23 17:32:48 +0200

    Unified MacOs and Windows tray icon

--- examples/playground/src/main/java/dev/screwbox/playground/FluidEffectsSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/FluidEffectsSystem.java
@@ -2,6 +2,7 @@ package dev.screwbox.playground;
 
 import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
+import dev.screwbox.core.Vector;
 import dev.screwbox.core.audio.SoundOptions;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
@@ -13,6 +14,7 @@ import dev.screwbox.core.particles.SpawnMode;
 import dev.screwbox.core.utils.ListUtil;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public class FluidEffectsSystem implements EntitySystem {
@@ -30,29 +32,33 @@ public class FluidEffectsSystem implements EntitySystem {
                 final List<Entity> physics = engine.environment().fetchAll(PHYSICS);
                 final var surfaceNodes = entity.get(FluidComponent.class).surface.nodes();
                 for (var physicsEntity : physics) {
-                    Double y = null;
-                    PhysicsComponent physicsComponent = physicsEntity.get(PhysicsComponent.class);
-                    if (!physicsComponent.ignoreCollisions && physicsComponent.momentum.length() > effects.speedThreshold) {
-                        for (final var node : surfaceNodes) {
-                            if (physicsEntity.bounds().contains(node)) {
-                                y = node.y();
-                            }
-                            //TODO optimize performance
-//TODO ParticleOptions.relativeOrigin(+1)
-                        }
-                    }
-                    if (y != null) {
-                        Bounds bounds = Bounds.atOrigin(physicsEntity.bounds().minX(), y, physicsEntity.bounds().width(), 2);
-                        engine.particles().spawn(bounds, SpawnMode.BOTTOM_SIDE, effects.particleOptions.drawOrder(entity.get(RenderComponent.class) == null ? 0 : entity.get(RenderComponent.class).drawOrder + 1));
+                    getInteractingNode(physicsEntity, effects, surfaceNodes).ifPresent(node -> {
+                        Bounds bounds = Bounds.atOrigin(physicsEntity.bounds().minX(), node.y(), physicsEntity.bounds().width(), 2);
+                        engine.particles().spawn(bounds, SpawnMode.BOTTOM_SIDE, effects.particleOptions
+                                .drawOrder(entity.get(RenderComponent.class) == null ? 0 : entity.get(RenderComponent.class).drawOrder + 1));    //TODO ParticleOptions.relativeOrigin(+1)
                         if (effects.soundScheduler.isTick()) {//TODO move sheduler out of other sceduler
                             engine.audio().playSound(ListUtil.randomFrom(effects.sounds), SoundOptions.playOnce()
                                     .speed(RANDOM.nextDouble(0.6, 1.2))//TODO configure ranges in splashcomponent
                                     .position(physicsEntity.position())
                             );
                         }
-                    }
+                    });
                 }
             }
         }
+    }
+
+
+    private Optional<Vector> getInteractingNode(final Entity physicsEntity, final FluidEffectsComponent effects, final List<Vector> surfaceNodes) {
+        PhysicsComponent physicsComponent = physicsEntity.get(PhysicsComponent.class);
+        if (!physicsComponent.ignoreCollisions && physicsComponent.momentum.length() > effects.speedThreshold) {
+            for (final var node : surfaceNodes) {
+                if (physicsEntity.bounds().contains(node)) {
+                    return Optional.of(node);
+                }
+
+            }
+        }
+        return Optional.empty();
     }
 }
