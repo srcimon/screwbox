bf8fad38803d36a6a4d62c241a122a890d4782cb
simonbas <sebastian.simon@koeln.de>
2025-05-30 09:25:34 +0200

    refactored MovingPlatformSystem

--- examples/platformer/src/main/java/dev/screwbox/platformer/systems/PrintSystem.java
+++ examples/platformer/src/main/java/dev/screwbox/platformer/systems/PrintSystem.java
@@ -5,6 +5,7 @@ import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.Order;
 import dev.screwbox.core.graphics.Canvas;
+import dev.screwbox.core.graphics.options.TextDrawOptions;
 import dev.screwbox.platformer.components.TextComponent;
 
 import static dev.screwbox.core.assets.FontBundle.BOLDZILLA;
@@ -14,14 +15,18 @@ import static dev.screwbox.core.graphics.options.TextDrawOptions.font;
 public class PrintSystem implements EntitySystem {
 
     private static final Archetype TEXTS = Archetype.of(TextComponent.class);
+    private static final TextDrawOptions TITLE = font(BOLDZILLA).alignCenter().scale(7);
+    private static final TextDrawOptions SUBTITLE = font(BOLDZILLA).alignCenter().scale(4);
 
     @Override
     public void update(final Engine engine) {
         for (var entity : engine.environment().fetchAll(TEXTS)) {
-            TextComponent textComponent = entity.get(TextComponent.class);
-            Canvas canvas = engine.graphics().canvas();
-            canvas.drawText(canvas.center(), textComponent.text, font(BOLDZILLA).alignCenter().scale(7));
-            canvas.drawText(canvas.center().addY(80), textComponent.subtext, font(BOLDZILLA).alignCenter().scale(4));
+            if (engine.graphics().isWithinDistanceToVisibleArea(entity.position(), 1024)) {
+                TextComponent textComponent = entity.get(TextComponent.class);
+                Canvas canvas = engine.graphics().canvas();
+                canvas.drawText(canvas.center(), textComponent.text, TITLE);
+                canvas.drawText(canvas.center().addY(80), textComponent.subtext, SUBTITLE);
+            }
         }
     }
 }
