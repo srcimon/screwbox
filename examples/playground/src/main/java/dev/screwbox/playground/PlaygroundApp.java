75446528dafc4c2d2347d020349aac612eafc087
simonbas <sebastian.simon@koeln.de>
2025-06-08 17:00:54 +0200

    cleaned playground

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -8,9 +8,6 @@ public class PlaygroundApp {
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Playground");
 
-        engine.scenes().add(new PlaygroundScene())
-                .switchTo(PlaygroundScene.class);
-
         engine.assets()
                 .enableLogging()
                 .prepareClassPackageAsync(PlaygroundApp.class);
