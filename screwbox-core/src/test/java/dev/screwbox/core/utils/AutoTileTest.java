e5357571fe7bb77f204ed889ab28ce5e0f263f26
simonbas <sebastian.simon@koeln.de>
2025-06-11 22:03:27 +0200

    fix

--- screwbox-core/src/test/java/dev/screwbox/core/utils/AutoTileTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/utils/AutoTileTest.java
@@ -1,27 +1,28 @@
 package dev.screwbox.core.utils;
 
 import dev.screwbox.core.graphics.Offset;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AutoTileTest {
 
     //TODO ramp up tests and rename all
     @Test
     void createIndex_XXX() {
-        var index = AutoTile.createIndex(Offset.at(10,10), offset -> offset.equals(Offset.at(11,10)));
-        Assertions.assertThat(index.index3x3Minimal()).isEqualTo(4);
+        var index = AutoTile.createIndex(Offset.at(10, 10), offset -> offset.equals(Offset.at(11, 10)));
+        assertThat(index.index3x3Minimal()).isEqualTo(4);
     }
 
     @Test
     void createIndex_XXX2() {
-        var index = AutoTile.createIndex(Offset.at(10,10), offset -> false);
-        Assertions.assertThat(index.index3x3Minimal()).isEqualTo(0);
+        var index = AutoTile.createIndex(Offset.at(10, 10), offset -> false);
+        assertThat(index.index3x3Minimal()).isEqualTo(0);
     }
 
     @Test
     void createIndex_XXX3() {
-        var index = AutoTile.createIndex(Offset.at(10,10), offset -> true);
-        Assertions.assertThat(index.index3x3Minimal()).isEqualTo(255);
+        var index = AutoTile.createIndex(Offset.at(10, 10), offset -> true);
+        assertThat(index.index3x3Minimal()).isEqualTo(255);
     }
 }
