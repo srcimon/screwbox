53e73f1fe17d272e651b502b6009171b6b099186
simonbas <sebastian.simon@koeln.de>
2025-06-13 21:57:09 +0200

    dynamic mask type

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/AutoTile.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/AutoTile.java
@@ -3,6 +3,7 @@ package dev.screwbox.core.graphics;
 import dev.screwbox.core.assets.Asset;
 import dev.screwbox.core.utils.Validate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -23,70 +24,76 @@ public class AutoTile {
     }
 
     private enum MaskType {
-        MASK_2X2,
-        MASK_3X3
+        MASK_2X2(2, Map.ofEntries()),
+        MASK_3X3(3, Map.ofEntries(
+                entry(16, Offset.at(0, 0)),
+                entry(17, Offset.at(0, 1)),
+                entry(1, Offset.at(0, 2)),
+                entry(0, Offset.at(0, 3)),
+
+                entry(20, Offset.at(1, 0)),
+                entry(21, Offset.at(1, 1)),
+                entry(5, Offset.at(1, 2)),
+                entry(4, Offset.at(1, 3)),
+
+                entry(84, Offset.at(2, 0)),
+                entry(85, Offset.at(2, 1)),
+                entry(69, Offset.at(2, 2)),
+                entry(68, Offset.at(2, 3)),
+
+                entry(80, Offset.at(3, 0)),
+                entry(81, Offset.at(3, 1)),
+                entry(65, Offset.at(3, 2)),
+                entry(64, Offset.at(3, 3)),
+
+                entry(213, Offset.at(4, 0)),
+                entry(29, Offset.at(4, 1)),
+                entry(23, Offset.at(4, 2)),
+                entry(117, Offset.at(4, 3)),
+
+                entry(92, Offset.at(5, 0)),
+                entry(127, Offset.at(5, 1)),
+                entry(223, Offset.at(5, 2)),
+                entry(71, Offset.at(5, 3)),
+
+                entry(116, Offset.at(6, 0)),
+                entry(253, Offset.at(6, 1)),
+                entry(247, Offset.at(6, 2)),
+                entry(197, Offset.at(6, 3)),
+
+                entry(87, Offset.at(7, 0)),
+                entry(113, Offset.at(7, 1)),
+                entry(209, Offset.at(7, 2)),
+                entry(93, Offset.at(7, 3)),
+
+                entry(28, Offset.at(8, 0)),
+                entry(31, Offset.at(8, 1)),
+                entry(95, Offset.at(8, 2)),
+                entry(7, Offset.at(8, 3)),
+
+                entry(125, Offset.at(9, 0)),
+                entry(119, Offset.at(9, 1)),
+                entry(255, Offset.at(9, 2)),
+                entry(199, Offset.at(9, 3)),
+
+                entry(124, Offset.at(10, 0)),
+                entry(221, Offset.at(10, 2)),
+                entry(215, Offset.at(10, 3)),
+
+                entry(112, Offset.at(11, 0)),
+                entry(245, Offset.at(11, 1)),
+                entry(241, Offset.at(11, 2)),
+                entry(193, Offset.at(11, 3))));
+
+        private final int aspectRatio;
+        private final Map<Integer, Offset> mappings;
+
+        MaskType(final int aspectRatio, final Map<Integer, Offset> mappings) {
+            this.aspectRatio = aspectRatio;
+            this.mappings = mappings;
+        }
+
     }
-
-    private static final Map<Integer, Offset> MAPPINGS = Map.ofEntries(
-            entry(16, Offset.at(0, 0)),
-            entry(17, Offset.at(0, 1)),
-            entry(1, Offset.at(0, 2)),
-            entry(0, Offset.at(0, 3)),
-
-            entry(20, Offset.at(1, 0)),
-            entry(21, Offset.at(1, 1)),
-            entry(5, Offset.at(1, 2)),
-            entry(4, Offset.at(1, 3)),
-
-            entry(84, Offset.at(2, 0)),
-            entry(85, Offset.at(2, 1)),
-            entry(69, Offset.at(2, 2)),
-            entry(68, Offset.at(2, 3)),
-
-            entry(80, Offset.at(3, 0)),
-            entry(81, Offset.at(3, 1)),
-            entry(65, Offset.at(3, 2)),
-            entry(64, Offset.at(3, 3)),
-
-            entry(213, Offset.at(4, 0)),
-            entry(29, Offset.at(4, 1)),
-            entry(23, Offset.at(4, 2)),
-            entry(117, Offset.at(4, 3)),
-
-            entry(92, Offset.at(5, 0)),
-            entry(127, Offset.at(5, 1)),
-            entry(223, Offset.at(5, 2)),
-            entry(71, Offset.at(5, 3)),
-
-            entry(116, Offset.at(6, 0)),
-            entry(253, Offset.at(6, 1)),
-            entry(247, Offset.at(6, 2)),
-            entry(197, Offset.at(6, 3)),
-
-            entry(87, Offset.at(7, 0)),
-            entry(113, Offset.at(7, 1)),
-            entry(209, Offset.at(7, 2)),
-            entry(93, Offset.at(7, 3)),
-
-            entry(28, Offset.at(8, 0)),
-            entry(31, Offset.at(8, 1)),
-            entry(95, Offset.at(8, 2)),
-            entry(7, Offset.at(8, 3)),
-
-            entry(125, Offset.at(9, 0)),
-            entry(119, Offset.at(9, 1)),
-            entry(255, Offset.at(9, 2)),
-            entry(199, Offset.at(9, 3)),
-
-            entry(124, Offset.at(10, 0)),
-            entry(221, Offset.at(10, 2)),
-            entry(215, Offset.at(10, 3)),
-
-            entry(112, Offset.at(11, 0)),
-            entry(245, Offset.at(11, 1)),
-            entry(241, Offset.at(11, 2)),
-            entry(193, Offset.at(11, 3))
-            );
 
     private final Map<Integer, Sprite> tileset = new HashMap<>();
     private final Sprite defaultTile; //Make empty sprite default sprite
@@ -99,14 +106,16 @@ public class AutoTile {
     }
 
     private AutoTile(final Frame frame) {
-        Validate.isTrue(() -> frame.width() == 3 * frame.height(), "image width must be three times image height");
-        this.maskType = MaskType.MASK_3X3;//TODO automatically detect maskType
+        final int aspectRatio = frame.width() / frame.height();
+        //Validate.isTrue(() -> frame.width() == 3 * frame.height(), "image width must be three times image height");
+       this.maskType = Arrays.stream(MaskType.values()).filter(type -> type.aspectRatio == aspectRatio).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("image aspect ratio not supported by any mask type"));//TODO: log supported ratios
         int tileWidth = frame.height() / 4;
-        for (final var mapping : MAPPINGS.entrySet()) {
+        for (final var mapping : maskType.mappings.entrySet()) {
             Offset value = mapping.getValue();
             tileset.put(mapping.getKey(), new Sprite(frame.extractArea(Offset.at(value.x() * tileWidth, value.y() * tileWidth), Size.square(tileWidth))));
         }
-        defaultTile = new Sprite(frame.extractArea(Offset.at(10 * tileWidth, tileWidth), Size.square(tileWidth)));
+        defaultTile = Sprite.pixel(Color.RED).scaled(tileWidth); //TODO? new Sprite(frame.extractArea(Offset.at(10 * tileWidth, tileWidth), Size.square(tileWidth)));
     }
 
     public Sprite findSprite(final Mask mask) {
