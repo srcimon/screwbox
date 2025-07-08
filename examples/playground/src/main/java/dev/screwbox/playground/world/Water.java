5c0cccb6742123cf34481ee4048c85d9470f4042
simonbas <sebastian.simon@koeln.de>
2025-07-08 19:00:04 +0200

    enhance playground

--- examples/playground/src/main/java/dev/screwbox/playground/world/Water.java
+++ examples/playground/src/main/java/dev/screwbox/playground/world/Water.java
@@ -3,6 +3,10 @@ package dev.screwbox.playground.world;
 import dev.screwbox.core.Percent;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.SourceImport;
+import dev.screwbox.core.environment.fluids.FluidComponent;
+import dev.screwbox.core.environment.fluids.FluidEffectsComponent;
+import dev.screwbox.core.environment.fluids.FluidRenderComponent;
+import dev.screwbox.core.environment.fluids.FluidTurbulenceComponent;
 import dev.screwbox.core.environment.rendering.ReflectionComponent;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.utils.TileMap;
@@ -12,12 +16,16 @@ public class Water implements SourceImport.Converter<TileMap.Block<Color>> {
     @Override
     public Entity convert(TileMap.Block<Color> block) {
         return new Entity().name("water")
-                .bounds(block.bounds())
-                .add(new ReflectionComponent(Percent.half(), 0), c -> {
-                    c.applyWaveDistortionPostFilter = true;
-                    c.frequencyX = 0.2;
-                    c.frequencyY = 0.4;
-                    c.speed = 0.002;
+                .bounds(block.bounds().expandTop(-8))
+                .add(new FluidEffectsComponent())
+                .add(new FluidRenderComponent())
+                .add(new FluidComponent((int)(block.bounds().width() / 16.0)))
+                .add(new FluidTurbulenceComponent(80))
+                .add(new ReflectionComponent(Percent.half(), 0), config -> {
+                    config.applyWaveDistortionPostFilter = true;
+                    config.frequencyX = 0.2;
+                    config.frequencyY = 0.4;
+                    config.speed = 0.002;
                 });
     }
 }
