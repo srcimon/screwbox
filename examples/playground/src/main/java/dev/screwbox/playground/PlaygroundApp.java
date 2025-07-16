5ded3f7553f87d6a375e2a75c7ad2e78f49e1948
simonbas <sebastian.simon@koeln.de>
2025-07-16 21:33:15 +0200

    added missing final modifiers

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -20,23 +20,26 @@ import dev.screwbox.core.utils.PerlinNoise;
 
 public class PlaygroundApp {
 
+    static  double l = 0;
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Playground");
 
+
         engine.environment()
+                .addSystem(new LogFpsSystem())
                 .addSystem(e -> {
                     final Bounds area = e.graphics().visibleArea();
                     final double size = 8;
                     final double padding = 0;
-                    final double divisor = 80.0;
+                    final double divisor = 120.0;
                     final var options = RectangleDrawOptions.filled(Color.RED);
 
-                    var z = e.loop().runningTime().milliseconds() / 1000.0;
+                    var z = e.loop().runningTime().milliseconds() / 10000.0;
                     for (double y = area.minY(); y < area.maxY(); y += size + padding) {
                         for (double x = area.minX(); x < area.maxX(); x += size + padding) {
                             var noise = (PerlinNoise.generatePerlinNoise3D(123123L, x / divisor+10000, y  / divisor+10000,z) + 1) / 2.0;
-                           // var noise = Math.abs(noiseB * Math.sin(z) + noiseA * Math.cos(z));
-                            e.graphics().world().drawRectangle(Bounds.atOrigin(x, y, size, size), RectangleDrawOptions.filled(Color.rgb((int)((noise + 1) * 127), 0, 0)));//TODO FIXME
+                            e.graphics().world().drawRectangle(Bounds.atOrigin(x, y, size, size), RectangleDrawOptions.filled(Color.rgb(
+                                    (int)(noise * 255), 0, 0)));//TODO FIXME
                         }
                     }
                 });
