e8c19e2715a400d4fdd5b58bfe4a9303a11bc808
simonbas <sebastian.simon@koeln.de>
2025-07-16 23:13:16 +0200

    added missing final modifiers

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -16,15 +16,16 @@ public class PlaygroundApp {
                 .addSystem(new LogFpsSystem())
                 .addSystem(e -> {
                     final Bounds area = e.graphics().visibleArea();
-                    final double size = 20;
+                    final double size = 10;
                     final double padding = 0;
-                    final double divisor = 120.0;
-                    var z = e.loop().runningTime().milliseconds() / 1000.0;
+                    final double divisor = 220.0;
+                    var z = e.loop().runningTime().milliseconds() / 200.0;
                     for (double y = area.minY(); y < area.maxY(); y += size + padding) {
                         for (double x = area.minX(); x < area.maxX(); x += size + padding) {
                             var noise = (PerlinNoise.generatePerlinNoise3D(123123L, x / divisor + 10000, y / divisor + 10000, z) + 1) / 2.0;
+                            Color rgb = Color.rgb(0, 0, Math.clamp((int) (noise * 500)-200, 0, 255));
                             e.graphics().world().drawRectangle(Bounds.atOrigin(x, y, size, size),
-                                    RectangleDrawOptions.filled(Color.rgb(Math.max(0,(int) (noise * 255-200)), 0, 0)));//TODO FIXME
+                                    RectangleDrawOptions.filled(rgb));//TODO FIXME
                         }
                     }
                 });
