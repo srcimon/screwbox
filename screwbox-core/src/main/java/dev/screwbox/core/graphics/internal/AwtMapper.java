3fa6dbcbc7ef706f0ed272e8d6d015adb2859c16
simonbas <sebastian.simon@koeln.de>
2025-07-16 22:47:27 +0200

    added missing final modifiers

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/AwtMapper.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/AwtMapper.java
@@ -7,7 +7,8 @@ public final class AwtMapper {
     private AwtMapper() {
     }
 
+    //TODO I dont understand that workaround!
     public static java.awt.Color toAwtColor(final Color color) {
-        return new java.awt.Color(color.r(), color.g(), color.b(), (int) (color.opacity().value() * 255.0));
+        return new java.awt.Color(color.r(), color.g(), color.b(), color.opacity().rangeValue(0, 254));
     }
 }
