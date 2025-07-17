ab60688469b74ba4efec078030c67c2255862878
simonbas <sebastian.simon@koeln.de>
2025-07-17 21:05:05 +0200

    fixup

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -6,6 +6,7 @@ import dev.screwbox.core.ScrewBox;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.core.LogFpsSystem;
 import dev.screwbox.core.graphics.Color;
+import dev.screwbox.core.graphics.World;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 import dev.screwbox.core.graphics.options.RectangleDrawOptions;
 import dev.screwbox.core.utils.PerlinNoise;
@@ -19,14 +20,16 @@ public class PlaygroundApp {
                 .addSystem(e -> {
                     final Bounds area = e.graphics().visibleArea();
                     final double size = 10;
-                    final double padding = 0;
+                    final double padding = 10;
                     final double divisor = 220.0;
                     var z = e.loop().runningTime().milliseconds() / 400.0;
+                    World world = e.graphics().world();
                     for (double y = area.minY(); y < area.maxY(); y += size + padding) {
                         for (double x = area.minX(); x < area.maxX(); x += size + padding) {
                             var noise = (PerlinNoise.generatePerlinNoise3D(123123L, x / divisor + 10000, y / divisor + 10000, z) + 1) / 2.0;
-                            e.graphics().world().drawCircle(Vector.of(x, y), noise * size,
-                                    CircleDrawOptions.filled(Color.WHITE));//TODO FIXME
+
+                            world.drawRectangle(Bounds.atOrigin(x, y, size * noise, size * noise),
+                                    RectangleDrawOptions.filled(noise > 0.8 ? Color.WHITE : Color.BLUE));//TODO FIXME
                         }
                     }
                 });
