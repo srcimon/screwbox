fdba53afa831eaa79c64b7b56f8f2532bc9a6d4e
Sebastian Simon <104482889+srcimon@users.noreply.github.com>
2025-07-19 08:06:24 +0200

    less heap usage (#684)

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/Size.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/Size.java
@@ -133,7 +133,7 @@ public class Size implements Serializable, Comparable<Size> {
      * Returns a list off all pixel {@link Offset}s within this {@link Size}.
      */
     public List<Offset> allPixels() {
-        final List<Offset> allPixels = new ArrayList<>();
+        final List<Offset> allPixels = new ArrayList<>(width * height);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 allPixels.add(Offset.at(x, y));
