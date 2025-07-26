6d9418c00ad61185a70973a127a7d6c8bfebfea0
simonbas <sebastian.simon@koeln.de>
2025-07-26 11:24:49 +0200

    tuned performance even further

--- examples/playground/src/main/java/dev/screwbox/playground/VisualizerSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/VisualizerSystem.java
@@ -2,36 +2,40 @@ package dev.screwbox.playground;
 
 import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
+import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.World;
 import dev.screwbox.core.graphics.options.RectangleDrawOptions;
-import dev.screwbox.core.utils.PerlinNoise;
+import dev.screwbox.core.utils.FractalNoise;
 
 public class VisualizerSystem implements EntitySystem {
 
-    private double plus;
+    private Vector offset = Vector.of(1000, 1000);
 
     @Override
     public void update(Engine engine) {
-        final Bounds area = engine.graphics().visibleArea().expand(-96);
+        offset = offset.add(engine.keyboard().arrowKeysMovement(2));
+        final Bounds area = engine.graphics().visibleArea();
         final double size = 8;
-        final double padding = 3;
-        final double divisor = 250.0;
-        plus += engine.audio().microphoneLevel().value()  * engine.loop().delta();
-        var z = plus + engine.loop().runningTime().milliseconds() / 50000.0;
+        final double padding = 0;
+        final double divisor = 4.0;
+        var z = engine.loop().runningTime().milliseconds() / 50.0;
         World world = engine.graphics().world();
 
         for (double y = area.minY(); y < area.maxY(); y += size + padding) {
             for (double x = area.minX(); x < area.maxX(); x += size + padding) {
-                final double x1 = x / divisor + 10000;
-                final double y1 = y / divisor + 10000;
-                final var noise = (PerlinNoise.generatePerlinNoise3d(123123L, x1, y1, z) + 1) / 2.0;
-                final var noise2 = (PerlinNoise.generatePerlinNoise3d(112353L, x1, y1, z) + 1) / 2.0;
-
-                Color color = noise2 > 0.5 && noise2 < 0.55
-                        ? Color.YELLOW
-                        : Color.rgb((int) (noise2 * 255), 0, (int) (noise * 255));
+                final double x1 = x / divisor + offset.x();
+                final double y1 = y / divisor + offset.y();
+                final var noise = FractalNoise.generateFractalNoise(100.123123, 123123L, x1, y1, z).value();
+                final var noise2 = FractalNoise.generateFractalNoise(100.123123, 12234233L, x1, y1, z).value();
+                Color color = Color.DARK_BLUE;
+                if (noise2 > 0.4 && noise2 < 0.5) {
+                    color = Color.YELLOW;
+                }
+                if (noise2 < 0.4) {
+                    color = Color.RED;
+                }
 
                 Bounds rectangle = Bounds.atOrigin(x + (noise - 0.5) * 100, y + (noise2 - 0.5) * 200, size * noise, size * noise);
                 world.drawRectangle(rectangle, RectangleDrawOptions.filled(color));
