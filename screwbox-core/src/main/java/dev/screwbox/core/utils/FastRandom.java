1ba27cd10eadd61989e8ff3289730822e898ad94
simonbas <sebastian.simon@koeln.de>
2025-07-26 15:08:34 +0200

    added todos

--- screwbox-core/src/main/java/dev/screwbox/core/utils/FastRandom.java
+++ screwbox-core/src/main/java/dev/screwbox/core/utils/FastRandom.java
@@ -2,27 +2,33 @@ package dev.screwbox.core.utils;
 
 import java.util.Random;
 
-public class FastRandom {
+/**
+ * A non threadsafe {@link Random} alternative used for fast random value generation.
+ */
+public final class FastRandom {
+
+    private static final boolean[] HASHES = createInitialHashes();
 
     private final long seed;
+    private int index = 0;
 
-    public FastRandom(long seed) {
+    public FastRandom(final long seed) {
         this.seed = seed;
     }
 
-    private static final boolean[] BOOLS = new boolean[1024];
-
-    static {
-        var r = new Random(123);
-        for (int i = 0; i < BOOLS.length; i++) {
-            BOOLS[i] = r.nextBoolean();
-        }
-    }
-
-    int index = 0;
-
+    //TODO Documentation and tests
+    //TODO mention in changelog
     public boolean nextBoolean() {
         index++;
-        return BOOLS[Math.abs((int) (seed + index)) % BOOLS.length];
+        return HASHES[Math.abs((int) (seed + index)) % HASHES.length];
+    }
+
+    private static boolean[] createInitialHashes() {
+        final boolean[] hashes = new boolean[1024];
+        var seededRandom = new Random(123);
+        for (int i = 0; i < hashes.length; i++) {
+            hashes[i] = seededRandom.nextBoolean();
+        }
+        return hashes;
     }
 }
