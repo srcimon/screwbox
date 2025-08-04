664e7c55b2be809fd4fb363e527503a2d6ac8ea6
simonbas <sebastian.simon@koeln.de>
2025-08-04 20:05:58 +0200

    prototype and add todos

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/shader/GlowShader.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/shader/GlowShader.java
@@ -1,27 +1,44 @@
 package dev.screwbox.core.graphics.shader;
 
 import dev.screwbox.core.Percent;
+import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.Shader;
 import dev.screwbox.core.graphics.ShaderSetup;
 import dev.screwbox.core.graphics.SpriteBundle;
+import dev.screwbox.core.graphics.internal.ImageOperations;
 
 import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.RGBImageFilter;
 //TODO Add NoArgsConstructor to Shader
 //TODO normalize all Shader CacheKeys
 //TODO delete main
 
 public class GlowShader extends Shader {
 
-    public GlowShader() {
-        super("GlowShader", false);
+    private final int threshold;
+
+    public GlowShader(final Percent threshold) {
+        super("GlowShader-" + threshold.value(), false);
+        this.threshold = threshold.rangeValue(0, 255);
     }
 
     @Override
     public Image apply(final Image source, final Percent progress) {
-        return source;
+        var filter = new RGBImageFilter() {
+
+            @Override
+            public int filterRGB(int x, int y, int rgb) {
+                var brightness = Color.rgb(rgb).brightness();
+                return brightness > threshold ? rgb : 0;
+            }
+        };
+
+        var result = ImageOperations.applyFilter(source, filter);
+        return result;
     }
 
     public static void main(String[] args) {
-        ShaderSetup.shader(new GlowShader()).createPreview(SpriteBundle.ACHIEVEMENT.get().singleImage()).exportGif("shader.png");
+        ShaderSetup.shader(new GlowShader(Percent.of(0.75))).createPreview(SpriteBundle.ACHIEVEMENT.get().singleImage()).exportGif("shader.png");
     }
 }
