7ab3a4cbe7cab8dbefcd8967c4d1271904b76115
simonbas <sebastian.simon@koeln.de>
2025-08-15 12:49:27 +0200

    prototype new playground

--- examples/playground/src/main/java/dev/screwbox/playground/JointsSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/JointsSystem.java
@@ -24,8 +24,7 @@ public class JointsSystem implements EntitySystem {
                     boolean isRetracted = distance-joint.length < 0;
                     Vector motion = delta.multiply(-1 * (joint.length - distance) * engine.loop().delta() * (isRetracted ? joint.retractStength : joint.expandStrength));
                         physics.momentum = physics.momentum.add(motion);
-                        targetPhysics.momentum = targetPhysics.momentum.applyFriction(engine.loop().delta(100), engine.loop().delta(100)).add(motion.invert())
-                                ;
+                        targetPhysics.momentum = targetPhysics.momentum.applyFriction(engine.loop().delta(100), engine.loop().delta(100)).add(motion.invert().multiply(1));
                 });
             }
         });
