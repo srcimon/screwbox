9e7fec7a9f2af697981e70b815d01d9834887905
simonbas <sebastian.simon@koeln.de>
2025-08-17 18:18:47 +0200

    fixup

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
@@ -4,8 +4,6 @@ import dev.screwbox.core.Percent;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 
-import javax.swing.text.View;
-
 //TODO document and test
 public class LensFlare {
 
@@ -15,15 +13,16 @@ public class LensFlare {
      * Renders the lens flare on the specified {@link Canvas}.
      */
     public void renderGlowLensFlareTo(Vector position, Color color, double radius, Viewport viewport) {
-        var delta = viewport.camera().position().substract(position);
-        var a = viewport.camera().position().add(delta.multiply(-2.5));
-        var b = viewport.camera().position().add(delta.multiply(-2.0));
-        var c = viewport.camera().position().add(delta.multiply(2.5));
-        var d = viewport.camera().position().add(delta.multiply(2.0));
+        Vector camPosition = viewport.camera().position();
+        var delta = camPosition.substract(position);
+        var a = camPosition.add(delta.multiply(-2.5));
+        var b = camPosition.add(delta.multiply(-2.0));
+        var c = camPosition.add(delta.multiply(2.5));
+        var d = camPosition.add(delta.multiply(2.0));
 
         CircleDrawOptions options2 = CircleDrawOptions.fading(color.opacity(color.opacity().value() * opacity.value()));
         viewport.canvas().drawCircle(viewport.toCanvas(a), viewport.toCanvas(radius * 0.5), options2);
-       viewport.canvas().drawCircle(viewport.toCanvas(b), viewport.toCanvas(radius * 1.5), options2);
+        viewport.canvas().drawCircle(viewport.toCanvas(b), viewport.toCanvas(radius * 1.5), options2);
         viewport.canvas().drawCircle(viewport.toCanvas(c), viewport.toCanvas(radius * 0.5), options2);
         viewport.canvas().drawCircle(viewport.toCanvas(d), viewport.toCanvas(radius * 1.5), options2);
     }
