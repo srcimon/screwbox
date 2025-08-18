dd70a585424d3b6a3ff6793c9196e6b78168598f
simonbas <sebastian.simon@koeln.de>
2025-08-18 22:56:11 +0200

    prototype

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
@@ -5,9 +5,12 @@ import dev.screwbox.core.Rotation;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 import dev.screwbox.core.graphics.options.LineDrawOptions;
+import dev.screwbox.core.utils.FractalNoise;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static dev.screwbox.core.graphics.Color.WHITE;
 
 //TODO document and test
 //TODO document in docusaurus
@@ -48,5 +51,16 @@ public class LensFlare {
             viewport.canvas().drawLine(viewport.toCanvas(position), viewport.toCanvas(result.to()), LineDrawOptions.color(color.opacity(color.opacity().value() * rayOpacity)).strokeWidth(rayWidth));
         }
 
+        for (int i = -60; i < 60; i += 20) {
+            for (int y = -60; y < 60; y += 20) {
+                Offset offset = viewport.toCanvas(position.addX(i).addY(y));
+                var noise = FractalNoise.generateFractalNoise(800, 12394L, offset);
+                var noise2 = FractalNoise.generateFractalNoise(800, 12342344L, offset);
+                viewport.canvas().drawCircle(offset.add(
+                        noise.rangeValue(-100, 100),
+                        noise2.rangeValue(-400, 400)
+                ), 20, CircleDrawOptions.filled(color.opacity(color.opacity().value() * 0.03)));
+            }
+        }
     }
 }
