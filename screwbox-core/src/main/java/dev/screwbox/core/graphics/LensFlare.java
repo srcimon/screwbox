ab6e96baf7873cf99ef18e93b0b4de9c0f14aba9
simonbas <sebastian.simon@koeln.de>
2025-08-19 21:04:03 +0200

    prototype

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
@@ -6,7 +6,6 @@ import dev.screwbox.core.Rotation;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 import dev.screwbox.core.graphics.options.LineDrawOptions;
-import dev.screwbox.core.utils.FractalNoise;
 import dev.screwbox.core.utils.PerlinNoise;
 
 import java.util.ArrayList;
@@ -17,21 +16,21 @@ import java.util.List;
 //TODO avoid this to have no code at all
 public class LensFlare {
 
-    private record Halo(double distance, double size, double opacity) {
+    private record Orb(double distance, double size, double opacity) {
 
     }
 
-    private List<Halo> halos = new ArrayList<>();
+    private final List<Orb> orbs = new ArrayList<>();
 
-    public LensFlare addHalo(final double distance, final double size, final double opacity) {
-        halos.add(new Halo(distance, size, opacity));
+    public LensFlare addOrb(final double distance, final double size, final double opacity) {
+        orbs.add(new Orb(distance, size, opacity));
         return this;
     }
 
     public void render(final Vector position, final double radius, final Color color, final Viewport viewport) {
         final Vector cameraPosition = viewport.camera().position();
         final var positionToCamera = cameraPosition.substract(position);
-        for (final var orb : halos) {
+        for (final var orb : orbs) {
             final var orbPosition = cameraPosition.add(positionToCamera.multiply(orb.distance()));
             final double orbRadius = radius * orb.size();
             final var orbOptions = CircleDrawOptions.fading(color.opacity(color.opacity().value() * orb.opacity()));
@@ -39,34 +38,41 @@ public class LensFlare {
         }
 
         //TODO extract ray config
-        int numberOfRays = 4;
-        double rayRotationSpeed = 0.1;
-        double rayOpacity = 0.03;
-        int rayWidth = 3;
-        double rayLength = 1.5;
+        int rayCount = 2;
+        double rayRelativeRotation = 0.1;
+        double rayOpacity = 0.04;
+        int rayWidthPixels = 3;
+        double rayLength = 1.1;
 
         // rays
-        for (int i = 0; i < numberOfRays; i++) {
+        for (int i = 0; i < rayCount; i++) {
             var line = Line.normal(position, rayLength * radius);
-            var result = Rotation.degrees(i * 360.0 / numberOfRays + rayRotationSpeed * viewport.toCanvas(position).x()).applyOn(line);
-            viewport.canvas().drawLine(viewport.toCanvas(position), viewport.toCanvas(result.to()), LineDrawOptions.color(color.opacity(color.opacity().value() * rayOpacity)).strokeWidth(rayWidth));
+            var result = Rotation.degrees(i * 360.0 / rayCount + rayRelativeRotation * viewport.toCanvas(position).x()).applyOn(line);
+            LineDrawOptions options = LineDrawOptions.color(color.opacity(color.opacity().value() * rayOpacity)).strokeWidth(rayWidthPixels);
+            viewport.canvas().drawLine(viewport.toCanvas(result.from()), viewport.toCanvas(result.to()), options);
         }
 
+
+        renderOrbs(position, radius, color, viewport);
+    }
+
+    private static void renderOrbs(Vector position, double radius, Color color, Viewport viewport) {
         int distance = 60;
         int spacing = 30;
         double zoom = 1000;
-        double opacity = 0.14;
-        double size = 0.15;
-int count = 5;
-
-
+        double opacity = 0.08;
+        double size = 0.1;
+        int count = 5;
 
         // orbs
-        for(double d = 0; d < 360; d+= 360.0/count) {
+        for (double d = 0; d < 360; d += 360.0 / count) {
             var to = Rotation.degrees(d).applyOn(Line.normal(position, spacing)).to();
             Offset offset = viewport.toCanvas(to);
-            var noise = Percent.of((PerlinNoise.generatePerlinNoise(12394L, (position.y() +d+offset.x()) / zoom, d+offset.y() / zoom) +1) / 2.0);
-            var noise2 = Percent.of((PerlinNoise.generatePerlinNoise(534345L, (position.y() + d+offset.x()) / zoom, d+offset.y() / zoom) +1) / 2.0);
+            double z = radius / 10.0 + color.rgb()/ 10.0 + position.x() / 100.0+ position.y() / 100.0;
+            double z2 = radius / 3.0 + color.rgb()/ 8.0+ position.x() / 100.0+ position.y() / 100.0;
+
+            var noise = Percent.of((PerlinNoise.generatePerlinNoise3d((int)d + 1938891L, offset.x() / zoom, offset.y() / zoom, z) + 1) / 2.0);
+            var noise2 = Percent.of((PerlinNoise.generatePerlinNoise3d((int)d + 1485952L, offset.x() / zoom, offset.y() / zoom, z2) + 1) / 2.0);
             Offset dotOffset = viewport.toCanvas(to.add(noise.rangeValue(-distance, distance), noise2.rangeValue(-distance, distance)));
 
             viewport.canvas().drawCircle(dotOffset, (int) (viewport.toCanvas(radius) * size), CircleDrawOptions.filled(color.opacity(color.opacity().value() * opacity * noise.value())));
