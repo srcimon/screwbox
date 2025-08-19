cb0b257156937843bcf08738c6085680438da6c7
simonbas <sebastian.simon@koeln.de>
2025-08-19 19:52:09 +0200

    prototype

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
@@ -1,11 +1,13 @@
 package dev.screwbox.core.graphics;
 
 import dev.screwbox.core.Line;
+import dev.screwbox.core.Percent;
 import dev.screwbox.core.Rotation;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 import dev.screwbox.core.graphics.options.LineDrawOptions;
 import dev.screwbox.core.utils.FractalNoise;
+import dev.screwbox.core.utils.PerlinNoise;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,28 +52,22 @@ public class LensFlare {
             viewport.canvas().drawLine(viewport.toCanvas(position), viewport.toCanvas(result.to()), LineDrawOptions.color(color.opacity(color.opacity().value() * rayOpacity)).strokeWidth(rayWidth));
         }
 
-        int distance = 100;
+        int distance = 200;
         int spacing = 40;
-        int zoom = 2500;
+        double zoom = 4000;
         double opacity = 0.07;
         double size = 0.1;
+int count = 12;
 
         // orbs
-        for (int i = -distance; i < distance; i += spacing) {
-            for (int y = -distance; y < distance; y += spacing) {
-                Offset offset = viewport.toCanvas(position.addX(i).addY(y));
-                var noise = FractalNoise.generateFractalNoise(zoom, 12394L, offset);
-                var noise2 = FractalNoise.generateFractalNoise(zoom, 12342344L, offset);
-                Offset dotOffset = offset.add(
-                        noise.rangeValue(-distance*2, distance*2),
-                        noise2.rangeValue(-distance*2, distance*2)
-                );
-                var dotdistance = viewport.toCanvas(position).distanceTo(dotOffset);
+        for(double d = 0; d < 360; d+= 360.0/count) {
+            var to = Rotation.degrees(d).applyOn(Line.normal(position, spacing)).to();
+            Offset offset = viewport.toCanvas(to);
+            var noise = Percent.of((PerlinNoise.generatePerlinNoise(12394L, d+offset.x() / zoom, d+offset.y() / zoom) +1) / 2.0);
+            var noise2 = Percent.of((PerlinNoise.generatePerlinNoise(534345L, d+offset.x() / zoom, d+offset.y() / zoom) +1) / 2.0);
+            Offset dotOffset = viewport.toCanvas(to.add(noise.rangeValue(-distance, distance), noise2.rangeValue(-distance, distance)));
 
-                var modifier = Math.max(0, distance * 2 - dotdistance) / dotdistance * opacity;
-
-                viewport.canvas().drawCircle(dotOffset, (int) (viewport.toCanvas(radius) * size), CircleDrawOptions.filled(color.opacity(color.opacity().value() * modifier)));
-            }
+            viewport.canvas().drawCircle(dotOffset, (int) (viewport.toCanvas(radius) * size), CircleDrawOptions.filled(color.opacity(color.opacity().value() * opacity)));
         }
     }
 }
