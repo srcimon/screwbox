dd60485a070a53220d22eb0d308b7504a997bc2c
simonbas <sebastian.simon@koeln.de>
2025-08-20 22:19:27 +0200

    kick out orbs

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/LensFlare.java
@@ -1,12 +1,11 @@
 package dev.screwbox.core.graphics;
 
 import dev.screwbox.core.Line;
-import dev.screwbox.core.Percent;
 import dev.screwbox.core.Rotation;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.options.CircleDrawOptions;
 import dev.screwbox.core.graphics.options.LineDrawOptions;
-import dev.screwbox.core.utils.PerlinNoise;
+import dev.screwbox.core.utils.Validate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +21,11 @@ public record LensFlare(List<Reflection> reflections, int rayCount) {
 
     private record Reflection(double distance, double size, double opacity) {
 
+        private Reflection {
+            Validate.range(distance, -10, 10, "distance must be in range -10 to 10");
+            Validate.range(size, 0.01, 10, "size must be in range 0.01 to 10");
+            Validate.range(opacity, 0.01, 10, "opacity must be in range 0.01 to 10");
+        }
     }
 
     public static LensFlare noRays() {
@@ -42,7 +46,6 @@ public record LensFlare(List<Reflection> reflections, int rayCount) {
     public void render(final Vector position, final double radius, final Color color, final Viewport viewport) {
         renderRays(position, radius, color, viewport);
         renderReflections(radius, color, viewport, position);
-        renderOrbs(position, radius, color, viewport);
     }
 
     private void renderReflections(double radius, Color color, Viewport viewport, Vector position) {
@@ -57,7 +60,7 @@ public record LensFlare(List<Reflection> reflections, int rayCount) {
         }
     }
 
-    private  void renderRays(Vector position, double radius, Color color, Viewport viewport) {
+    private void renderRays(Vector position, double radius, Color color, Viewport viewport) {
         double rayRotationSpeed = 0.1;
         double rayOpacity = 0.14;
         int rayWidth = 1;
@@ -68,30 +71,6 @@ public record LensFlare(List<Reflection> reflections, int rayCount) {
             var result = Rotation.degrees(i * 360.0 / rayCount + rayRotationSpeed * viewport.toCanvas(position).x()).applyOn(line);
             LineDrawOptions options = LineDrawOptions.color(color.opacity(color.opacity().value() * rayOpacity)).strokeWidth(rayWidth);
             viewport.canvas().drawLine(viewport.toCanvas(result.from()), viewport.toCanvas(result.to()), options);
-        }
-    }
-
-    private  void renderOrbs(Vector position, double radius, Color color, Viewport viewport) {
-
-        int distance = 60;
-        int spacing = 30;
-        double zoom = 1000;
-        double opacity = 0.2;
-        double size = 0.1;
-        int count = 8;
-
-        // orbs
-        for (double d = 0; d < 360; d += 360.0 / count) {
-            var to = Rotation.degrees(d).applyOn(Line.normal(position, spacing)).to();
-            Offset offset = viewport.toCanvas(to);
-            double z = radius / 10.0 + color.rgb() / 10.0;
-            double z2 = radius / 3.0 + color.rgb() / 8.0;
-
-            var noise = Percent.of((PerlinNoise.generatePerlinNoise3d((int) d + 1938891L, offset.x() / zoom, offset.y() / zoom, z) + 1) / 2.0);
-            var noise2 = Percent.of((PerlinNoise.generatePerlinNoise3d((int) d + 1485952L, offset.x() / zoom, offset.y() / zoom, z2) + 1) / 2.0);
-            Offset dotOffset = viewport.toCanvas(to.add(noise.rangeValue(-distance, distance), noise2.rangeValue(-distance, distance)));
-
-            viewport.canvas().drawCircle(dotOffset, (int) (viewport.toCanvas(radius) * size), CircleDrawOptions.filled(color.opacity(color.opacity().value() * opacity * noise.value())));
         }
     }
 }
