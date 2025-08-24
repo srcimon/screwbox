f04e7854a0d4bdbfb50e31ba15ad0f3e6c0e848b
simonbas <sebastian.simon@koeln.de>
2025-08-24 13:18:17 +0200

    wtf

--- screwbox-core/src/main/java/dev/screwbox/core/ScrewBox.java
+++ screwbox-core/src/main/java/dev/screwbox/core/ScrewBox.java
@@ -25,11 +25,21 @@ public final class ScrewBox {
 
     /**
      * Creates an {@link Engine} instance. Sets the {@link Engine#name()} to the
-     * given value. The {@link Engine#name()} is used to initialize the
-     * {@link Window#title()}.
+     * specified value. The {@link Engine#name()} is used to initialize the
+     * {@link Window#title()}. Auto detects recommended {@link RenderingMode}.
      */
     public static Engine createEngine(final String name) {
+        return createEngine(name, RenderingMode.autodetect());
+    }
+
+    /**
+     * Creates an {@link Engine} instance. Sets the {@link Engine#name()} to the
+     * specified value and tries to use the specified {@link RenderingMode}.
+     * The {@link Engine#name()} is used to initialize the s{@link Window#title()}.
+     */
+    public static Engine createEngine(final String name, final RenderingMode renderingMode) {
         requireNonNull(name, "name must not be null");
+        renderingMode.tryToApply();
         return new DefaultEngine(name);
     }
 
