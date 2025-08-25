aed301ff6f0d9541ac3900d6869c39174a3fa7b8
simonbas <sebastian.simon@koeln.de>
2025-08-25 08:42:22 +0200

    set headless

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/shader/NeonShaderTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/shader/NeonShaderTest.java
@@ -2,21 +2,26 @@ package dev.screwbox.core.graphics.shader;
 
 import dev.screwbox.core.Percent;
 import dev.screwbox.core.graphics.Color;
-import dev.screwbox.core.graphics.Frame;
 import dev.screwbox.core.graphics.SpriteBundle;
 import dev.screwbox.core.test.TestUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
 class NeonShaderTest {
 
+    @BeforeEach
+    void setUp() {
+        System.setProperty("java.awt.headless", "true");
+    }
+
     @Test
     void apply_yellow_createsNeonEffect() {
         Image image = SpriteBundle.ACHIEVEMENT.get().singleImage();
 
         var result = new NeonShader(Color.WHITE, Color.YELLOW).apply(image, Percent.half());
-        Frame.fromImage(result).exportPng("apply_yellow_createsNeonEffect.png");
+
         TestUtil.verifyIsSameImage(result, "shader/apply_yellow_createsNeonEffect.png");
     }
 }
