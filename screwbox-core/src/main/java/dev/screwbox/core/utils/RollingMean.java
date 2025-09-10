4f6e648f5562d99d1d63bf36ee234f6046e28767
simonbas <sebastian.simon@koeln.de>
2025-09-10 13:28:39 +0200

    added rolling mean

---
+++ screwbox-core/src/main/java/dev/screwbox/core/utils/RollingMean.java
@@ -0,0 +1,33 @@
+package dev.screwbox.core.utils;
+
+public class RollingMean {
+
+    private final double[] samples;
+    private int index = 0;
+    private double value;
+    private boolean initialized;
+
+    public RollingMean(final int size) {
+        Validate.positive(size, "size must be positive");
+        samples = new double[size];
+    }
+
+    public int intValue() {
+        return (int) (this.value / samples.length);
+    }
+
+    public void record(final double value) {
+        this.value -= samples[index];
+        this.value += value;
+        samples[index] = value;
+        index++;
+        if (index >= samples.length) {
+            index = 0;
+        }
+    }
+
+    public int average(int value) {
+        record(value);
+        return intValue();
+    }
+}
