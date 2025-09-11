bd0379635a842699f93bd0d7923e9b70cd9a78be
simonbas <sebastian.simon@koeln.de>
2025-09-11 17:49:44 +0200

    added tests

--- screwbox-core/src/main/java/dev/screwbox/core/loop/Loop.java
+++ screwbox-core/src/main/java/dev/screwbox/core/loop/Loop.java
@@ -28,8 +28,8 @@ public interface Loop {
     void setSpeed(double speed);
 
     /**
-     * Sets the games target frames per second. Default value is
-     * {@link #MIN_TARGET_FPS}. Setting target fps below that value is not allowed.
+     * Sets the games target frames per second. Default value is 120.
+     * Setting target fps below that value is not allowed.
      */
     Loop setTargetFps(int targetFps);
 
