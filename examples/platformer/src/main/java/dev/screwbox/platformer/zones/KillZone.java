fdddb46400ca30fd43cf825fb246ac9825c90d66
simonbas <sebastian.simon@koeln.de>
2025-09-22 19:09:41 +0200

    prototyping code

--- examples/platformer/src/main/java/dev/screwbox/platformer/zones/KillZone.java
+++ examples/platformer/src/main/java/dev/screwbox/platformer/zones/KillZone.java
@@ -4,7 +4,10 @@ import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.SourceImport.Converter;
 import dev.screwbox.core.environment.core.TransformComponent;
+import dev.screwbox.core.environment.light.AerialLightComponent;
+import dev.screwbox.core.environment.light.GlowComponent;
 import dev.screwbox.core.environment.logic.TriggerAreaComponent;
+import dev.screwbox.core.graphics.Color;
 import dev.screwbox.platformer.components.DeathEventComponent.DeathType;
 import dev.screwbox.platformer.components.KillZoneComponent;
 import dev.screwbox.platformer.components.PlayerMarkerComponent;
@@ -16,10 +19,16 @@ public class KillZone implements Converter<GameObject> {
     public Entity convert(GameObject object) {
         var deathType = object.properties().tryGetEnum("death-type", DeathType.class).orElse(DeathType.SPIKES);
 
-        return new Entity().add(
+        Entity add = new Entity().add(
                 new TriggerAreaComponent(Archetype.ofSpacial(PlayerMarkerComponent.class)),
                 new KillZoneComponent(deathType),
                 new TransformComponent(object.bounds()));
+        //TODO remove pseudocode below
+        if(deathType.equals(DeathType.LAVA)) {
+            add.add(new GlowComponent(40, Color.YELLOW.opacity(0.3)), glow -> glow.isRectangular = true);
+            add.add(new AerialLightComponent(Color.BLACK.opacity(0.7)));
+        }
+        return add;
     }
 
 }
