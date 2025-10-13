0b676f10a0df817d1f9754b3e7e2f685693f0789
simonbas <sebastian.simon@koeln.de>
2025-10-13 22:38:13 +0200

    Simplify Astar

--- screwbox-core/src/main/java/dev/screwbox/core/navigation/AStarAlgorithm.java
+++ screwbox-core/src/main/java/dev/screwbox/core/navigation/AStarAlgorithm.java
@@ -22,59 +22,34 @@ public class AStarAlgorithm<T> implements PathfindingAlgorithm<T> {
 
     @Override
     public List<T> findPath(final NodeGraph<T> nodeGraph, final T start, final T end) {
-        return new AStarSearch<>(nodeGraph, start, end).findPath();
-    }
-
-    private static class AStarSearch<T> {
-
-        private final NodeGraph<T> nodeGraph;
-        private final T start;
-        private final T end;
-        private final Set<T> closed;
-        private final Map<T, Double> costs;
-        private final Map<T, Double> costsToStart;
-        private final Queue<ChainedNode<T>> open;
-
-        public AStarSearch(final NodeGraph<T> nodeGraph, final T start, final T end) {
-            this.nodeGraph = nodeGraph;
-            this.start = start;
-            this.end = end;
-            closed = new HashSet<>();
-            costs = new HashMap<>(Map.of(start, 0.0));
-            costsToStart = new HashMap<>();
-            open = new PriorityQueue<>(List.of(new ChainedNode<>(start, null)));
-        }
-
-        public List<T> findPath() {
-            while (!open.isEmpty()) {
-                final ChainedNode<T> currentNode = open.remove();
-                if (!closed.contains(currentNode.node())) {
-                    closed.add(currentNode.node());
-                    if (currentNode.node().equals(end)) {
-                        return currentNode.backtrack();
-                    }
-                    processNode(currentNode);
+        Set<T> closed = new HashSet<>();
+        Map<T, Double> costs = new HashMap<>(Map.of(start, 0.0));
+        Map<T, Double> costsToStart = new HashMap<>();
+        Queue<ChainedNode<T>> open = new PriorityQueue<>(List.of(new ChainedNode<>(start, null)));
+        while (!open.isEmpty()) {
+            final ChainedNode<T> currentNode = open.remove();
+            if (!closed.contains(currentNode.node())) {
+                closed.add(currentNode.node());
+                if (currentNode.node().equals(end)) {
+                    return currentNode.backtrack();
                 }
-            }
-            return emptyList();
-        }
-
-        private void processNode(final ChainedNode<T> currentNode) {
-            final var costToStart = costsToStart.get(currentNode.node());
-            for (final var neighbor : nodeGraph.adjacentNodes(currentNode.node())) {
-                if (!closed.contains(neighbor)) {
-                    final double startCost = isNull(costToStart) ? nodeGraph.traversalCost(currentNode.node(), start) : costToStart;
-                    final double totalCost = startCost + nodeGraph.traversalCost(neighbor, currentNode.node())
-                                             + nodeGraph.traversalCost(neighbor, end);
-                    final Double costNeighbour = costs.get(neighbor);
-                    if (isNull(costNeighbour) || totalCost < costNeighbour) {
-                        costsToStart.put(neighbor, totalCost);
-                        costs.put(neighbor, totalCost);
-                        open.add(new ChainedNode<>(neighbor, currentNode, totalCost));
+                final var costToStart = costsToStart.get(currentNode.node());
+                for (final var neighbor : nodeGraph.adjacentNodes(currentNode.node())) {
+                    if (!closed.contains(neighbor)) {
+                        final double startCost = isNull(costToStart) ? nodeGraph.traversalCost(currentNode.node(), start) : costToStart;
+                        final double totalCost = startCost + nodeGraph.traversalCost(neighbor, currentNode.node())
+                                                 + nodeGraph.traversalCost(neighbor, end);
+                        final Double costNeighbour = costs.get(neighbor);
+                        if (isNull(costNeighbour) || totalCost < costNeighbour) {
+                            costsToStart.put(neighbor, totalCost);
+                            costs.put(neighbor, totalCost);
+                            open.add(new ChainedNode<>(neighbor, currentNode, totalCost));
+                        }
                     }
                 }
             }
         }
+        return emptyList();
     }
 
 }
