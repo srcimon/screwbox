5bae09b3cb909c5f6b548998cd50d84c3edda641
simonbas <sebastian.simon@koeln.de>
2025-10-14 22:11:53 +0200

    added JavaDoc to Graph and PathfindingAlgorithm

--- screwbox-core/src/main/java/dev/screwbox/core/navigation/Graph.java
+++ screwbox-core/src/main/java/dev/screwbox/core/navigation/Graph.java
@@ -2,9 +2,21 @@ package dev.screwbox.core.navigation;
 
 import java.util.List;
 
+/**
+ * Graph used for pathfinding using a {@link PathfindingAlgorithm}.
+ *
+ * @since 3.12.0
+ */
 public interface Graph<T> {
 
+    /**
+     * Returns a list of all adjacent nodes to specified node.
+     */
     List<T> adjacentNodes(T node);
 
+    /**
+     * Returns the relative cost of traversal from start to end. Cost should always be returned by implementations,
+     * even when there is no connection between start and end.
+     */
     double traversalCost(T start, T end);
 }
