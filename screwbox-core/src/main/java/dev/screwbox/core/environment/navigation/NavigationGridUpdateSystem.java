d1c2f2915a2ddfe9fd11042bc1ea260d5c7b6ba9
simonbas <sebastian.simon@koeln.de>
2025-10-18 11:33:17 +0200

    fixup

--- screwbox-core/src/main/java/dev/screwbox/core/environment/navigation/NavigationGridUpdateSystem.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/navigation/NavigationGridUpdateSystem.java
@@ -1,15 +1,12 @@
 package dev.screwbox.core.environment.navigation;
 
 import dev.screwbox.core.Bounds;
-import dev.screwbox.core.Duration;
 import dev.screwbox.core.Engine;
-import dev.screwbox.core.Time;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.Order;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Order(Order.SystemOrder.PREPARATION)
@@ -21,17 +18,11 @@ public class NavigationGridUpdateSystem implements EntitySystem {
     public void update(final Engine engine) {
         engine.environment().tryFetchSingletonComponent(PhysicsGridConfigurationComponent.class).ifPresent(config -> {
             if (config.updateScheduler.isTick(engine.loop().time())) {
-                Time t = Time.now();
-                List<Entity> obstacleEntities = engine.environment().fetchAll(OBSTACLES);
+                final List<Entity> obstacleEntities = engine.environment().fetchAll(OBSTACLES);
                 engine.async().runExclusive(NavigationGridUpdateSystem.class, () -> {
-                    final List<Bounds> obstacles = new ArrayList<>();
-                    for(var entity : obstacleEntities) {
-                        obstacles.add(entity.bounds());
-                    }
+                    final List<Bounds> obstacles = obstacleEntities.stream().map(Entity::bounds).toList();
                     engine.navigation().setNavigationRegion(config.worldBounds, config.gridSize, obstacles);
                 });
-                System.out.println(Duration.since(t).nanos());
-
             }
         });
 
