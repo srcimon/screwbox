68330c723817100d427021e8fc936f28c2d3a985
simonbas <sebastian.simon@koeln.de>
2025-10-22 22:00:16 +0200

    revert playground

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -1,51 +1,94 @@
 package dev.screwbox.playground;
 
-import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.ScrewBox;
 import dev.screwbox.core.Vector;
-import dev.screwbox.core.assets.FontBundle;
-import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.core.LogFpsSystem;
-import dev.screwbox.core.environment.rendering.AutoTileComponent;
+import dev.screwbox.core.environment.fluids.FloatComponent;
+import dev.screwbox.core.environment.physics.ColliderComponent;
+import dev.screwbox.core.environment.physics.GravityComponent;
+import dev.screwbox.core.environment.physics.PhysicsComponent;
+import dev.screwbox.core.environment.physics.StaticColliderComponent;
 import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.graphics.AutoTileBundle;
-import dev.screwbox.core.graphics.Offset;
-import dev.screwbox.core.graphics.Sprite;
-import dev.screwbox.core.graphics.SpriteBundle;
-import dev.screwbox.core.graphics.options.SpriteDrawOptions;
-import dev.screwbox.core.graphics.options.TextDrawOptions;
+import dev.screwbox.core.utils.TileMap;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaygroundApp {
 
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Playground");
-//TODO add dynmaic auto tiles to demo game
-        engine.loop().unlockFps();
         engine.graphics().camera().setZoom(3);
-        engine.environment().enableAllFeatures()
+
+        var map = TileMap.fromString("""
+                
+                
+                
+                1   4      # X#
+                          ####
+                        c
+                2   3
+                #######
+                ###   ####
+                """);
+
+        engine.environment()
+                .enableAllFeatures()
+                .addSystem(new DebugJointsSystem())
+                .addSystem(new JointsSystem())
+                .addSystem(new PhysicsInteractionSystem())
                 .addSystem(new LogFpsSystem())
-                .addSystem(e -> {
-                    e.graphics().canvas().drawText(Offset.at(10, 10), "Count: " + e.environment().entityCount(), TextDrawOptions.font(FontBundle.SKINNY_SANS).scale(5));
-                    e.graphics().camera().setZoom(e.graphics().camera().zoom() + e.mouse().unitsScrolled() / 20.0);
-                    Vector position = e.mouse().position().snap(16);
-                    e.graphics().world().drawSprite(SpriteBundle.MARKER_CROSSHAIR, position.substract(Vector.$(8, 8)), SpriteDrawOptions.originalSize().opacity(0.4));
-                    if (e.mouse().isDownLeft()) {
-                        if (e.navigation().searchAtPosition(position).checkingFor(Archetype.ofSpacial(RenderComponent.class)).selectAll().isEmpty()) {
-                            e.environment().addEntity(new Entity()
-                                    .bounds(Bounds.atPosition(position, 16, 16))
-                                    .add(new AutoTileComponent(AutoTileBundle.BUBBLEGUM))
-                                    .add(new RenderComponent(Sprite.invisible())));
-                        }
-                    } else if (e.mouse().isDownRight()) {
-                        e.navigation().searchAtPosition(position).checkingFor(Archetype.ofSpacial(RenderComponent.class)).selectAll().forEach(en -> {
-                            e.environment().remove(en);
-                        });
-                    }
-                });
+                .addEntity(new Entity().add(new GravityComponent(Vector.y(400))));
+
+        var xEntity = map.tiles().stream().filter(tile -> tile.value().equals('X')).findFirst().orElseThrow();
+        double dist = 0;
+        int max = 8;
+        for (int i = max; i >= 0; i--) {
+            Entity add = new Entity(100 + i)
+                    .name(i == 0 ? "start" : "node")
+                    .add(new FloatComponent())
+                    .bounds(xEntity.bounds().moveBy(0, dist).expand(-12))
+                    .add(new PhysicsComponent(), p -> p.friction = 80);
+
+            if (i != max) {
+                add.add(new JointComponent(List.of(new Joint(100 + i + 1))));
+            } else {
+                add.add(new JointComponent(new ArrayList<>()));
+            }
+            engine.environment().addEntity(add);
+            dist += 12;
+        }
+
+        engine.environment()
+                .importSource(map.tiles())
+                .usingIndex(TileMap.Tile::value)
+                .when('#').as(tile -> new Entity().bounds(tile.bounds())
+                        .add(new RenderComponent(tile.findSprite(AutoTileBundle.ROCKS)))
+                        .add(new ColliderComponent())
+                        .add(new StaticColliderComponent()))
+
+                .when('1').as(tile -> new Entity(1).bounds(tile.bounds().expand(-12))
+                        .add(new JointComponent(List.of(new Joint(3), new Joint(4))))
+                        .add(new PhysicsComponent(), p -> p.friction = 80))
+
+                .when('2').as(tile -> new Entity(2).bounds(tile.bounds().expand(-12))
+                        .add(new JointComponent(List.of(new Joint(1), new Joint(3))))
+                        .add(new PhysicsComponent(), p -> p.friction = 80))
+
+                .when('3').as(tile -> new Entity(3).bounds(tile.bounds().expand(-12))
+                        .add(new JointComponent(List.of(new Joint(4))))
+                        .add(new PhysicsComponent(), p -> p.friction = 80))
+
+                .when('4').as(tile -> new Entity(4).bounds(tile.bounds().expand(-12))
+                        .add(new JointComponent(List.of(new Joint(2))))
+                        .add(new PhysicsComponent(), p -> p.friction = 80))
+
+                .when('5').as(tile -> new Entity(5).bounds(tile.bounds().expand(-12))
+                        .add(new JointComponent(List.of(new Joint(1), new Joint(2))))
+                        .add(new PhysicsComponent(), p -> p.friction = 80));
 
         engine.start();
     }
