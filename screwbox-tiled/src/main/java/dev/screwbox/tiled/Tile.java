437c389afec9a3fd74549e0c746f7a8ce32802cd
simonbas <sebastian.simon@koeln.de>
2025-10-22 19:56:19 +0200

    candyland

--- screwbox-tiled/src/main/java/dev/screwbox/tiled/Tile.java
+++ screwbox-tiled/src/main/java/dev/screwbox/tiled/Tile.java
@@ -2,7 +2,7 @@ package dev.screwbox.tiled;
 
 import dev.screwbox.core.Bounds;
 import dev.screwbox.core.graphics.Sprite;
-
+//TODO rename renderBounds
 public record Tile(Sprite sprite, Bounds renderBounds, Layer layer, Properties properties) {
 
 }
