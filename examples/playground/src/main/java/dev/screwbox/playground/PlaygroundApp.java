6b71fe0b96d74c8ba077e9ed0b383e24ccffbcb3
simonbas <sebastian.simon@koeln.de>
2025-11-01 10:59:55 +0100

    cleanup

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -6,6 +6,9 @@ import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.core.LogFpsSystem;
 import dev.screwbox.core.environment.fluids.FloatComponent;
+import dev.screwbox.core.environment.fluids.FluidComponent;
+import dev.screwbox.core.environment.fluids.FluidRenderComponent;
+import dev.screwbox.core.environment.fluids.FluidTurbulenceComponent;
 import dev.screwbox.core.environment.physics.ColliderComponent;
 import dev.screwbox.core.environment.physics.GravityComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
@@ -33,6 +36,8 @@ public class PlaygroundApp {
                 
                 #######
                 ###   ####
+                WWWWWWWWWWWWWWWWWWWWWWWWWW
+                WWWWWWWWWWWWWWWWWWWWWWWWWW
                 """);
 
         engine.environment()
@@ -62,6 +67,14 @@ public class PlaygroundApp {
             engine.environment().addEntity(add);
             dist += 12;
         }
+
+        engine.environment()
+                .importSource(map.blocks())
+                .usingIndex(TileMap.Block::value)
+                        .when('W').as(tile -> new Entity().bounds(tile.bounds())
+                        .add(new FluidComponent(20))
+                        .add(new FluidRenderComponent())
+                        .add(new FluidTurbulenceComponent()));
 
         engine.environment()
                 .importSource(map.tiles())
