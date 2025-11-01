57152b2bd9e86c8747636a90833552ae7a392e5d
simonbas <sebastian.simon@koeln.de>
2025-11-01 12:51:35 +0100

    refactor addTask

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/renderer/AsyncRenderer.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/renderer/AsyncRenderer.java
@@ -1,22 +1,22 @@
 package dev.screwbox.core.graphics.internal.renderer;
 
-import dev.screwbox.core.Duration;
 import dev.screwbox.core.Angle;
+import dev.screwbox.core.Duration;
 import dev.screwbox.core.Time;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.Offset;
 import dev.screwbox.core.graphics.ScreenBounds;
 import dev.screwbox.core.graphics.Size;
 import dev.screwbox.core.graphics.Sprite;
-import dev.screwbox.core.graphics.options.OvalDrawOptions;
+import dev.screwbox.core.graphics.internal.Renderer;
 import dev.screwbox.core.graphics.options.LineDrawOptions;
+import dev.screwbox.core.graphics.options.OvalDrawOptions;
 import dev.screwbox.core.graphics.options.PolygonDrawOptions;
 import dev.screwbox.core.graphics.options.RectangleDrawOptions;
 import dev.screwbox.core.graphics.options.SpriteDrawOptions;
 import dev.screwbox.core.graphics.options.SpriteFillOptions;
 import dev.screwbox.core.graphics.options.SystemTextDrawOptions;
 import dev.screwbox.core.graphics.options.TextDrawOptions;
-import dev.screwbox.core.graphics.internal.Renderer;
 import dev.screwbox.core.utils.Latch;
 
 import java.awt.*;
@@ -57,57 +57,61 @@ public class AsyncRenderer implements Renderer {
 
     @Override
     public void fillWith(final Color color, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.fillWith(color, clip));
+        addTask(() -> next.fillWith(color, clip));
     }
 
     @Override
     public void rotate(final Angle rotation, final ScreenBounds clip, final Color backgroundColor) {
-        renderTasks.active().add(() -> next.rotate(rotation, clip, backgroundColor));
+        addTask(() -> next.rotate(rotation, clip, backgroundColor));
     }
 
     @Override
     public void fillWith(final Sprite sprite, final SpriteFillOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.fillWith(sprite, options, clip));
+        addTask(() -> next.fillWith(sprite, options, clip));
     }
 
     @Override
     public void drawText(final Offset offset, final String text, final SystemTextDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawText(offset, text, options, clip));
+        addTask(() -> next.drawText(offset, text, options, clip));
     }
 
     @Override
     public void drawRectangle(final Offset offset, final Size size, final RectangleDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawRectangle(offset, size, options, clip));
+        addTask(() -> next.drawRectangle(offset, size, options, clip));
     }
 
     @Override
     public void drawLine(final Offset from, final Offset to, final LineDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawLine(from, to, options, clip));
+        addTask(() -> next.drawLine(from, to, options, clip));
     }
 
     @Override
     public void drawOval(final Offset offset, final int radiusX, final int radiusY, final OvalDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawOval(offset, radiusX, radiusY, options, clip));
+        addTask(() -> next.drawOval(offset, radiusX, radiusY, options, clip));
     }
 
     @Override
     public void drawSprite(final Supplier<Sprite> sprite, final Offset origin, final SpriteDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawSprite(sprite, origin, options, clip));
+        addTask(() -> next.drawSprite(sprite, origin, options, clip));
     }
 
     @Override
     public void drawSprite(final Sprite sprite, final Offset origin, final SpriteDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawSprite(sprite, origin, options, clip));
+        addTask(() -> next.drawSprite(sprite, origin, options, clip));
     }
 
     @Override
     public void drawText(final Offset offset, final String text, final TextDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawText(offset, text, options, clip));
+        addTask(() -> next.drawText(offset, text, options, clip));
     }
 
     @Override
     public void drawPolygon(final List<Offset> nodes, final PolygonDrawOptions options, final ScreenBounds clip) {
-        renderTasks.active().add(() -> next.drawPolygon(nodes, options, clip));
+        addTask(() -> next.drawPolygon(nodes, options, clip));
+    }
+
+    private void addTask(final Runnable runnable) {
+        renderTasks.active().add(runnable);
     }
 
     private FutureTask<Void> finishRenderTasks() {
