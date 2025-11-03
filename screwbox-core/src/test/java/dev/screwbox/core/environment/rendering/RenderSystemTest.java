8f6085cd6884057572230c3c7d2f3f593d8fed18
simonbas <sebastian.simon@koeln.de>
2025-11-03 20:14:43 +0100

    revive test

--- screwbox-core/src/test/java/dev/screwbox/core/environment/rendering/RenderSystemTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/environment/rendering/RenderSystemTest.java
@@ -1,5 +1,6 @@
 package dev.screwbox.core.environment.rendering;
 
+import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.core.TransformComponent;
 import dev.screwbox.core.environment.internal.DefaultEnvironment;
 import dev.screwbox.core.graphics.Camera;
@@ -59,23 +60,21 @@ class RenderSystemTest {
         verify(canvas).drawSprite(sprite, Offset.at(20, 20), SpriteDrawOptions.scaled(2).drawOrder(5));
     }
 
-//    @Test
-//    void update_spriteOnTopOfLight_drawsNoSprite(DefaultEnvironment environment) {
-//        var sprite = SpriteBundle.ICON.get();
-//        when(camera.zoom()).thenReturn(2.0);
-//
-//        when(viewport.visibleArea()).thenReturn($$(176, 176, 48, 48));
-//
-//        environment
-//                .addEntity(new Entity()
-//                        .add(new TransformComponent(200, 200, 16, 16))
-//                        .add(new RenderComponent(sprite, 5), render -> render.renderInForeground = true))
-//                .addSystem(new RenderSystem());
-//
-//        environment.update();
-//
-//        verify(canvas).drawSpriteBatch(spriteBatch.capture());
-//
-//        assertThat(spriteBatch.getValue().isEmpty()).isTrue();
-//    }
+    @Test
+    void update_sortOrthographic_drawsNoSpriteUsingZIndex(DefaultEnvironment environment) {
+        var sprite = SpriteBundle.ICON.get();
+        when(camera.zoom()).thenReturn(2.0);
+        when(viewport.toCanvas($$(184, 184, 32, 32), 1, 1)).thenReturn(new ScreenBounds(20, 20, 8, 8));
+        when(canvas.size()).thenReturn(Size.of(800, 800));
+
+        environment
+                .addEntity(new Entity()
+                        .add(new TransformComponent(200, 200, 16, 16))
+                        .add(new RenderComponent(sprite, 5), render -> render.isSortOrthographic = true))
+                .addSystem(new RenderSystem());
+
+        environment.update();
+
+        verify(canvas).drawSprite(sprite, Offset.at(20, 20), SpriteDrawOptions.scaled(2).drawOrder(5).zIndex(28));
+    }
 }
