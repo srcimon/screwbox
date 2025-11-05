1b0ba726449c66e0185843bbf3bcf2292f041d16
simonbas <sebastian.simon@koeln.de>
2025-11-05 17:42:52 +0100

    added test

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/options/PolygonDrawOptionsTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/options/PolygonDrawOptionsTest.java
@@ -3,6 +3,8 @@ package dev.screwbox.core.graphics.options;
 import dev.screwbox.core.graphics.Color;
 import org.junit.jupiter.api.Test;
 
+import static dev.screwbox.core.graphics.options.PolygonDrawOptions.Smoothing.SPLINE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PolygonDrawOptionsTest {
@@ -23,5 +25,18 @@ class PolygonDrawOptionsTest {
         assertThatThrownBy(() -> options.smoothing(null))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("smoothing must not be null");
+    }
+
+    @Test
+    void newInstance_allPropertiesSet_hasValues() {
+        var options = PolygonDrawOptions.outline(Color.RED)
+                .strokeWidth(4)
+                .smoothing(SPLINE)
+                .drawOrder(9);
+
+        assertThat(options.smoothing()).isEqualTo(SPLINE);
+        assertThat(options.drawOrder()).isEqualTo(9);
+        assertThat(options.strokeWidth()).isEqualTo(4);
+        assertThat(options.style()).isEqualTo(PolygonDrawOptions.Style.OUTLINE);
     }
 }
