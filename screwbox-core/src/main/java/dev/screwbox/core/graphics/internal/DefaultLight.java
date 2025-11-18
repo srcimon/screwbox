5f9d9a0d3067e66ad8512c14becfc95405ac4234
simonbas <sebastian.simon@koeln.de>
2025-11-18 20:01:55 +0100

    Revert "Revert "Cleaned up light update""
    This reverts commit 9955c8e8aebcd837fb9432267a2de48c4b429779.

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/DefaultLight.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/DefaultLight.java
@@ -13,6 +13,7 @@ import dev.screwbox.core.graphics.Offset;
 import dev.screwbox.core.graphics.Viewport;
 import dev.screwbox.core.graphics.internal.filter.SizeIncreasingBlurImageFilter;
 import dev.screwbox.core.graphics.internal.filter.SizeIncreasingImageFilter;
+import dev.screwbox.core.loop.internal.Updatable;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ import static dev.screwbox.core.graphics.options.SpriteDrawOptions.scaled;
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
-public class DefaultLight implements Light {
+public class DefaultLight implements Light, Updatable {
 
     private final ViewportManager viewportManager;
     private final ExecutorService executor;
@@ -208,6 +209,7 @@ public class DefaultLight implements Light {
         return scale;
     }
 
+    @Override
     public void update() {
         lightPhysics = new LightPhysics();
         lightRenderers.clear();
