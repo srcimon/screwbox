73952d524e017fb061044df14c3566e79f199a1c
simonbas <sebastian.simon@koeln.de>
2025-11-21 21:32:22 +0100

    prototype dynamic ui

--- screwbox-core/src/main/java/dev/screwbox/core/window/internal/WindowFrame.java
+++ screwbox-core/src/main/java/dev/screwbox/core/window/internal/WindowFrame.java
@@ -15,6 +15,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 import java.io.Serial;
+import java.util.ArrayList;
 
 public class WindowFrame extends JFrame implements WindowFocusListener {
 
@@ -26,7 +27,8 @@ public class WindowFrame extends JFrame implements WindowFocusListener {
     private final Canvas canvas;
     private final Size initialSize;
     private Panel inputPanel;
-private KeyListener keyListener;
+    private KeyListener keyListener;
+
     public WindowFrame(final KeyListener keyListener, final Size initialSize) {
         this.keyListener = keyListener;
         setLayout(new BorderLayout());
@@ -35,31 +37,36 @@ private KeyListener keyListener;
         this.initialSize = initialSize;
         addWindowFocusListener(this);
         canvas = new Canvas();
-        canvas.setPreferredSize(new Dimension(initialSize.width() - 200, initialSize.height()));
+        canvas.setPreferredSize(new Dimension(initialSize.width(), initialSize.height()));
         add(canvas, BorderLayout.WEST);
         canvas.setBackground(Color.BLACK);
         inputPanel = new Panel();
-        inputPanel.setLayout(new GridLayout(5, 1));
-        inputPanel.setPreferredSize(new Dimension(200, initialSize.height()));
-        inputPanel.add(new Button("Input 2"), BorderLayout.SOUTH);
-        inputPanel.add(new Button("Input 2"), BorderLayout.SOUTH);
-        inputPanel.add(new Button("Input 2"), BorderLayout.SOUTH);
-        inputPanel.add(new Button("Input 2"), BorderLayout.SOUTH);
-        inputPanel.add(new Button("Input 2"), BorderLayout.SOUTH);
+        inputPanel.setLayout(new GridLayout(10, 1));
+        inputPanel.setVisible(false);
         add(inputPanel, BorderLayout.EAST);
         pack();
         canvas.addKeyListener(keyListener);
+        inputPanel.addKeyListener(keyListener);
         addKeyListener(keyListener);
     }
 
+    java.util.List<Component> components = new ArrayList<>();
     public void showInputs() {
         canvas.setPreferredSize(new Dimension(initialSize.width() - 200, initialSize.height()));
+        components.add(new Button("Input 2"));
+        components.add(new Button("Input 2"));
+        for(var c : components) {
+            inputPanel.add(c, BorderLayout.SOUTH);
+            c.addKeyListener(keyListener);
+        }
         inputPanel.setPreferredSize(new Dimension(200, initialSize.height()));
         inputPanel.setVisible(true);
         pack();
     }
 
     public void hideInputs() {
+        inputPanel.removeAll();
+        components.clear();
         canvas.setPreferredSize(new Dimension(initialSize.width(), initialSize.height()));
         inputPanel.setPreferredSize(new Dimension(0, initialSize.height()));
         inputPanel.setVisible(false);
