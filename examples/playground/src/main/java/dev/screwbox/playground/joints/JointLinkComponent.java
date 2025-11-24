cccb1516551136e73f5e9ab35b5581d41890a31e
simonbas <sebastian.simon@koeln.de>
2025-11-24 22:54:34 +0100

    refacot

--- examples/playground/src/main/java/dev/screwbox/playground/joints/JointLinkComponent.java
+++ examples/playground/src/main/java/dev/screwbox/playground/joints/JointLinkComponent.java
@@ -4,7 +4,7 @@ import dev.screwbox.core.environment.Component;
 
 public class JointLinkComponent implements Component {
 
-    public final Joint link;
+    public final Joint link;//TODO inline Joint inside component
 
     public JointLinkComponent(final Joint link) {
         this.link = link;
