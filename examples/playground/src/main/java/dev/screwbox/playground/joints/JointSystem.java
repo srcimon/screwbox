413c1febcbd07a015e5b8e745ac2525d1de2f5f0
simonbas <sebastian.simon@koeln.de>
2025-11-25 21:12:31 +0100

    refactor

--- examples/playground/src/main/java/dev/screwbox/playground/joints/JointSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/joints/JointSystem.java
@@ -6,7 +6,6 @@ import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.EntitySystem;
-import dev.screwbox.core.environment.Environment;
 import dev.screwbox.core.environment.ExecutionOrder;
 import dev.screwbox.core.environment.Order;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
@@ -19,41 +18,39 @@ public class JointSystem implements EntitySystem {
 
     @Override
     public void update(final Engine engine) {
-        double deltaTime = engine.loop().delta();
-
         for (final var linkEntity : engine.environment().fetchAll(LINKS)) {
             final var jointLink = linkEntity.get(JointLinkComponent.class);
-            final var jointTarget = engine.environment().fetchById(jointLink.targetId);
-            updateJoint(jointTarget, linkEntity, jointLink, deltaTime);
+            updateJoint(linkEntity, jointLink, engine);
         }
 
         for (final var structureEntity : engine.environment().fetchAll(STRUCTURES)) {
             final var jointStructure = structureEntity.get(JointStructureComponent.class);
             for (int index = 0; index < jointStructure.targetIds.length; index++) {
-                final var jointTarget = engine.environment().fetchById(jointStructure.targetIds[index]);
-                final var mapped = new JointLinkComponent(jointStructure.targetIds[index]);
-                mapped.length = jointStructure.lengths[index];
-                mapped.stiffness = jointStructure.stiffness;
-                mapped.expand = jointStructure.expand;
-                mapped.retract = jointStructure.retract;
-                updateJoint(jointTarget, structureEntity, mapped, deltaTime);
-                jointStructure.lengths[index] = mapped.length;
+                final var link1 = new JointLinkComponent(jointStructure.targetIds[index]);
+                link1.length = jointStructure.lengths[index];
+                link1.stiffness = jointStructure.stiffness;
+                link1.expand = jointStructure.expand;
+                link1.retract = jointStructure.retract;
+                final var link = link1;
+                updateJoint(structureEntity, link, engine);
+                jointStructure.lengths[index] = link.length;
             }
         }
     }
 
-    private static void updateJoint(final Entity jointTarget, final Entity jointEntity, final JointLinkComponent joint, double deltaTime) {
+    private static void updateJoint(final Entity jointEntity, final JointLinkComponent joint, Engine engine) {
+        final var jointTarget = engine.environment().fetchById(joint.targetId);
         final double distance = jointEntity.position().distanceTo(jointTarget.position());
         if (joint.length == 0) {
             joint.length = distance;
         }
-        Vector delta = jointTarget.position().substract(jointEntity.position());
+        final Vector delta = jointTarget.position().substract(jointEntity.position());
         final boolean isRetracted = distance - joint.length > 0;
         final double strength = isRetracted ? joint.retract : joint.expand;
-        final Vector motion = delta.limit(joint.stiffness).multiply((distance - joint.length) * deltaTime * strength);
+        final Vector motion = delta.limit(joint.stiffness).multiply((distance - joint.length) * engine.loop().delta() * strength);
         final var physics = jointEntity.get(PhysicsComponent.class);
-        final var targetPhysics = jointTarget.get(PhysicsComponent.class);
         physics.velocity = physics.velocity.add(motion);
+        final var targetPhysics = jointTarget.get(PhysicsComponent.class);
         targetPhysics.velocity = targetPhysics.velocity.add(motion.invert());
         joint.angle = Angle.ofVector(delta);
     }
