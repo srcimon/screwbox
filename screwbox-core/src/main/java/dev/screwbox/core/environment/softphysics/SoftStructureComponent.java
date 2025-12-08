ee17fd7fd7abe3a35541d14f4cd40b35f1a081cc
simonbas <sebastian.simon@koeln.de>
2025-12-08 22:40:24 +0100

    prototype

--- screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftStructureComponent.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftStructureComponent.java
@@ -11,7 +11,6 @@ import java.util.List;
  *
  * @since 3.16.0
  */
-//TODO rename SoftIntegrityComponent?
 public class SoftStructureComponent implements Component {
 
     public SoftStructureComponent(final List<Integer> targetIds) {
