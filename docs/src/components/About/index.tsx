a4fbddcaf815477dc5159cf4507cfa9746f6dec5
simonbas <sebastian.simon@koeln.de>
2025-12-09 22:54:11 +0100

    fix imports

--- docs/src/components/About/index.tsx
+++ docs/src/components/About/index.tsx
@@ -1,17 +1,17 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
 import Heading from '@theme/Heading';
-import Link from '@docusaurus/Link';
 
 export default function About(): ReactNode {
-  return (
-    <section>
-      <div className="container">
-        <Heading as="h1" className="subheading">Feature Overview</Heading>
-        <p className="hero__subtitle subheading">
-        <b class="hero__slogan">ScrewBox uses a purely code based approach on creating games to not force you into using a proprietary content editor.</b> These features will be available when coding with ScrewBox:
-        </p>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <div className="container">
+                <Heading as="h1" className="subheading">Feature Overview</Heading>
+                <p className="hero__subtitle subheading">
+                    <b class="hero__slogan">ScrewBox uses a purely code based approach on creating games to not force
+                        you into using a proprietary content editor.</b> These features will be available when coding
+                    with ScrewBox:
+                </p>
+            </div>
+        </section>
+    );
 }
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -4,7 +4,6 @@ import Layout from '@theme/Layout';
 import Features from '@site/src/components/Features';
 import Header from '@site/src/components/Header';
 import About from '@site/src/components/About';
-import Heading from '@theme/Heading';
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
