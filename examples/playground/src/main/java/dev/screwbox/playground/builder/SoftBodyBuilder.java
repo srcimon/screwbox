e78e6e34b3e80663581c29d03e5bb2e6ea6b099b
simonbas <sebastian.simon@koeln.de>
2025-12-11 15:38:20 +0100

    prototype

--- examples/playground/src/main/java/dev/screwbox/playground/builder/SoftBodyBuilder.java
+++ examples/playground/src/main/java/dev/screwbox/playground/builder/SoftBodyBuilder.java
@@ -42,7 +42,7 @@ public class SoftBodyBuilder {
 
             var node = nodes.get(i);
             var entity = new Entity(environment.allocateId());
-            entity.add(new TransformComponent(node));
+            entity.add(new TransformComponent(node,2,2));
             entity.add(new PhysicsComponent(), p -> p.friction = 3);
             entity.add(new SoftLinkComponent(isLast ? environment.peekId() : entities.getFirst().id().get()));
             entities.add(entity);
