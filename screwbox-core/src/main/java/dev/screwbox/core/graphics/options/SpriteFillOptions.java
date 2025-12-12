8aef18352d089abbbf60c352fd2b2a258090de09
simonbas <sebastian.simon@koeln.de>
2025-12-12 08:51:53 +0100

    fixed bug

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/options/SpriteFillOptions.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/options/SpriteFillOptions.java
@@ -24,6 +24,7 @@ import static java.util.Objects.requireNonNull;
  */
 public record SpriteFillOptions(Offset offset, double scale, Percent opacity, ShaderSetup shaderSetup, int drawOrder) {
 
+    //TODO Polygon for rendering area
     public SpriteFillOptions {
         Validate.positive(scale, "scale must be positive");
         requireNonNull(offset, "offset must not be null");
