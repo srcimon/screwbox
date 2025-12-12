c1f1a40ea77132a60728d686d0bc6bde121b16b8
simonbas <sebastian.simon@koeln.de>
2025-12-12 10:22:58 +0100

    fixups

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -22,7 +22,6 @@ import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.graphics.AutoTileBundle;
 import dev.screwbox.core.utils.TileMap;
 import dev.screwbox.playground.builder.BuilderSystem;
-import dev.screwbox.playground.builder.OldSoftbodyBuilder;
 import dev.screwbox.playground.builder.RopeBuilder;
 import dev.screwbox.playground.misc.DebugJointsSystem;
 import dev.screwbox.playground.misc.PhysicsInteractionSystem;
