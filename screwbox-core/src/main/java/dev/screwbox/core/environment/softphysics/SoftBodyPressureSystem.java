93c7b057a8b5b5d863ecb58720d1fcc573106012
simonbas <sebastian.simon@koeln.de>
2025-12-14 12:17:27 +0100

    added docs

--- screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyPressureSystem.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyPressureSystem.java
@@ -7,7 +7,6 @@ import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.ExecutionOrder;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
-import dev.screwbox.core.keyboard.Key;
 
 import static dev.screwbox.core.environment.Order.SIMULATION_LATE;
 
@@ -22,15 +21,24 @@ public class SoftBodyPressureSystem implements EntitySystem {
             final var softBody = body.get(SoftBodyComponent.class);
             final var polygon = SoftPhysicsSupport.toPolygon(softBody);
             Vector sum = Vector.zero();
+            final Vector[] appliedPressures = new Vector[polygon.nodes().size()];
+
+            // calculate pressure according to position
             for (int i = 0; i < polygon.nodes().size(); i++) {
-                Entity entity = softBody.nodes.get(i);
-                var delta = polygon.center().substract(entity.position()).length(1).multiply(-engine.loop().delta() * body.get(SoftBodyPressureComponent.class).pressure);
-                sum = sum.add(delta);
-                entity.get(PhysicsComponent.class).velocity = entity.get(PhysicsComponent.class).velocity.add(delta);
+                final Entity entity = softBody.nodes.get(i);
+                final var appliedPressure = polygon.center().substract(entity.position()).length(1)
+                        .multiply(-engine.loop().delta() * body.get(SoftBodyPressureComponent.class).pressure);
+                sum = sum.add(appliedPressure);
+                appliedPressures[i] = appliedPressure;
             }
+
+            // add pressure and rebalance velocity to avoid adding movement to the body
             for (int i = 0; i < polygon.nodes().size(); i++) {
                 Entity entity = softBody.nodes.get(i);
-                entity.get(PhysicsComponent.class).velocity = entity.get(PhysicsComponent.class).velocity.add(sum.multiply(1.0 / polygon.nodes().size()).invert());
+                final var physics = entity.get(PhysicsComponent.class);
+                physics.velocity = physics.velocity
+                        .add(appliedPressures[i])
+                        .add(sum.multiply(1.0 / polygon.nodes().size()).invert());
             }
         }
     }
