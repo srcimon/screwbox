3229a0820f396d57638df260224838ed49d925e3
simonbas <sebastian.simon@koeln.de>
2025-12-17 11:33:48 +0100

    clean up bisectorRay Code

--- screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
@@ -2,9 +2,21 @@ package dev.screwbox.core.environment.softphysics;
 
 import dev.screwbox.core.Polygon;
 import dev.screwbox.core.environment.Component;
+import dev.screwbox.core.environment.Entity;
 
+import java.io.Serial;
+
+/**
+ * Adds shape matching to a soft body {@link Entity} to keep the body in the original shape.
+ *
+ * @since 3.18.0
+ */
 public class SoftBodyShapeComponent implements Component {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    //TODO boolean allowRotation = true;
     public Polygon shape;
 
 }
--- screwbox-core/src/test/java/dev/screwbox/core/environment/internal/DefaultEnvironmentTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/environment/internal/DefaultEnvironmentTest.java
@@ -32,6 +32,7 @@ import dev.screwbox.core.environment.softphysics.RopeSystem;
 import dev.screwbox.core.environment.softphysics.SoftBodyCollisionSystem;
 import dev.screwbox.core.environment.softphysics.SoftBodyPressureSystem;
 import dev.screwbox.core.environment.softphysics.SoftBodyRenderSystem;
+import dev.screwbox.core.environment.softphysics.SoftBodyShapeSystem;
 import dev.screwbox.core.environment.softphysics.SoftBodySystem;
 import dev.screwbox.core.environment.softphysics.SoftPhysicsSystem;
 import dev.screwbox.core.environment.tweening.TweenDestroySystem;
@@ -460,9 +461,10 @@ class DefaultEnvironmentTest {
     void enableSoftPhysics_addsSoftPhysicsSystems() {
         environment.enableSoftPhysics();
 
-        assertThat(environment.systems()).hasSize(7)
+        assertThat(environment.systems()).hasSize(8)
                 .anyMatch(system -> system.getClass().equals(SoftPhysicsSystem.class))
                 .anyMatch(system -> system.getClass().equals(SoftBodySystem.class))
+                .anyMatch(system -> system.getClass().equals(SoftBodyShapeSystem.class))
                 .anyMatch(system -> system.getClass().equals(SoftBodyPressureSystem.class))
                 .anyMatch(system -> system.getClass().equals(SoftBodyCollisionSystem.class))
                 .anyMatch(system -> system.getClass().equals(SoftBodyRenderSystem.class))
@@ -648,7 +650,7 @@ class DefaultEnvironmentTest {
     void enableAllFeatures_noSystemPresent_addsAllSystems() {
         environment.enableAllFeatures();
 
-        assertThat(environment.systems()).hasSize(59)
+        assertThat(environment.systems()).hasSize(60)
                 .anyMatch(system -> system.getClass().equals(PhysicsSystem.class));
     }
 
