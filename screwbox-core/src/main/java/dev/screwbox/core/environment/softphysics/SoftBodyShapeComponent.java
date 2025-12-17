6fb41ddc449997712f5f1c34f61d764882009ba0
simonbas <sebastian.simon@koeln.de>
2025-12-17 11:34:55 +0100

    clean up bisectorRay Code

--- screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
@@ -7,12 +7,12 @@ import dev.screwbox.core.environment.Entity;
 import java.io.Serial;
 
 /**
- * Adds shape matching to a soft body {@link Entity} to keep the body in the original shape.
+ * Adds shape matching to a soft body {@link Entity} to keep the body in the original shape during simulation.
  *
  * @since 3.18.0
  */
 public class SoftBodyShapeComponent implements Component {
-
+//TODO add to guide
     @Serial
     private static final long serialVersionUID = 1L;
 
