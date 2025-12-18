a63674778aac18afc6743c4441f0f93c13e77ac2
simonbas <sebastian.simon@koeln.de>
2025-12-18 12:05:39 +0100

    added javadoc

--- screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/softphysics/SoftBodyShapeComponent.java
@@ -7,27 +7,38 @@ import dev.screwbox.core.environment.Entity;
 import java.io.Serial;
 
 /**
- * Adds shape matching to a soft body {@link Entity} to keep the body in the original shape during simulation.
+ * Adds shape matching to a soft body {@link Entity} to preserve the body in the original shape during simulation.
  *
  * @since 3.18.0
  */
 public class SoftBodyShapeComponent implements Component {
-//TODO add to guide
+    //TODO add to guide
     @Serial
     private static final long serialVersionUID = 1L;
 
-    //TODO document
+    /**
+     * The shape the soft body tries to preserve. Will be automatically added using the current {@link SoftBodyComponent#shape}.
+     */
     public Polygon shape;
 
-    //TODO document
+    /**
+     * Specify if rotation will be used to preserve the soft body shape. Turning rotation of keeps body upright.
+     */
     public boolean isRotationAllowed = true;
 
-    //TODO document
+    /**
+     * Specify a minimum distance between projected node and current node of the soft body to take action. Can be used
+     * to prevent ghost motion.
+     */
     public double deadZone = 4;
 
-    //TODO document
+    /**
+     * The strength used to keep the soft body nodes within shape.
+     */
     public double strength = 20;
 
-    //TODO document
+    /**
+     * The flexibility used to keep the soft body nodes within shape.
+     */
     public double flexibility = 20;
 }
