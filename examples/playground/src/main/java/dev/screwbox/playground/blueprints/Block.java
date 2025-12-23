62db5b5029c159442459486d7401a74ebc42ffce
simonbas <sebastian.simon@koeln.de>
2025-12-23 12:08:36 +0100

    prototype pseudo code

--- examples/playground/src/main/java/dev/screwbox/playground/blueprints/Block.java
+++ examples/playground/src/main/java/dev/screwbox/playground/blueprints/Block.java
@@ -9,10 +9,10 @@ import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.graphics.AutoTileBundle;
 import dev.screwbox.core.utils.TileMap;
 
-public class Block implements EntityBlueprint<TileMap.Tile> {
+public class Block implements EntityBlueprint<TileMap.Tile<Character>> {
 
     @Override
-    public Entity createFrom(final TileMap.Tile tile, final ImportContext context) {
+    public Entity createFrom(final TileMap.Tile<Character> tile, final ImportContext context) {
         return new Entity()
                 .name("block")
                 .bounds(tile.bounds())
