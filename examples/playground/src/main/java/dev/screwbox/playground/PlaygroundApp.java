a1aa9aa2030411ed4f395f7de4c3663212576d8c
simonbas <sebastian.simon@koeln.de>
2025-12-23 19:36:42 +0100

    prototype pseudo code;

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -64,7 +64,7 @@ public class PlaygroundApp {
 
         Environment environment = engine.environment();
 
-        environment.importSourcesUsingRuleset(map.tiles())
+        environment.importFromSource(map.tiles())
                 .indexBy(TileMap.Tile::value)
                 .when('X').as(new Block())
                 .when('Y').as(new Block())
