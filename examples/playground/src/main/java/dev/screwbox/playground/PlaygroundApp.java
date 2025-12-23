280177c78b63319c3d1e9e706ba117764a91580a
simonbas <sebastian.simon@koeln.de>
2025-12-23 17:48:50 +0100

    prototype pseudo code;

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -64,14 +64,13 @@ public class PlaygroundApp {
 
         Environment environment = engine.environment();
 
-        environment.importSource(map.tiles())
-                .indexBy(TileMap.Tile::value)
-                .rule(matchIndex('X')).useBlueprint(new Block())
-                .rule(allOf(matchProbability(0.4), matchIndex('X'))).useBlueprint(new Block())
-                .rule(allOf(matchProbability(0.4), matchIndex('X'))).useBlueprint((a, b) -> new Entity(a.column()))
-                .rule(lastRuleDidNotApply()).useBlueprint(new Block())
-                .rule(RuleCriteria.matchSource(tile -> tile.value().equals('X'))).useBlueprint(new Block())
-                .rule(RuleCriteria.entityCoundBelow(20)).fail("something went wrong");
+        environment.importSource(map.tiles()).indexBy(TileMap.Tile::value)
+                .when(matchIndex('X')).useBlueprint(new Block())
+                .when(allOf(matchProbability(0.4), matchIndex('X'))).useBlueprint(new Block())
+                .when(allOf(matchProbability(0.4), matchIndex('X'))).useBlueprint((a, b) -> new Entity(a.column()))
+                .when(lastRuleDidNotApply()).useBlueprint(new Block())
+                .when(RuleCriteria.matchSource(tile -> tile.value().equals('X'))).useBlueprint(new Block())
+        //        .when(RuleCriteria.entityCoundBelow(20)).fail("something went wrong");
 
 
 //        environment.importSource(map.objects())
