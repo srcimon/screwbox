b025e82eee8fa33989590615c6182996865b3513
simonbas <sebastian.simon@koeln.de>
2025-12-23 12:07:15 +0100

    prototype pseudo code

--- screwbox-core/src/main/java/dev/screwbox/core/environment/population/Asssociate.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/population/Asssociate.java
@@ -2,7 +2,7 @@ package dev.screwbox.core.environment.population;
 
 public record Asssociate<T>(EntityBlueprint<T> blueprint, Object index) {
 
-    public static Asssociate<T> blueprint(EntityBlueprint blueprint) {
+    public static <T> Asssociate<T> blueprint(EntityBlueprint<T> blueprint) {
         return new Asssociate<>(blueprint, null);
     }
 
