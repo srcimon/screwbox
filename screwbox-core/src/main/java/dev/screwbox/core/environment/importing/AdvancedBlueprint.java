1f9c4c3824c8f550898b6acefbf7597c1111de26
simonbas <sebastian.simon@koeln.de>
2025-12-29 18:43:23 +0100

    moved and javadoc

--- screwbox-core/src/main/java/dev/screwbox/core/environment/importing/AdvancedBlueprint.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/importing/AdvancedBlueprint.java
@@ -2,9 +2,20 @@ package dev.screwbox.core.environment.importing;
 
 import dev.screwbox.core.environment.Entity;
 
+/**
+ * An advanced {@link Blueprint} that can make use of the {@link ImportContext} to assemble an specific {@link Entity}
+ * e.g. an enemy from a specific source.
+ *
+ * @see Blueprint
+ * @see ComplexBlueprint
+ * @since 3.19.0
+ */
 @FunctionalInterface
 public interface AdvancedBlueprint<T> {
 
+    /**
+     * Assemble the {@link Entity} from the source.
+     */
     Entity assembleFrom(T source, ImportContext context);
 
 }
--- screwbox-core/src/main/java/dev/screwbox/core/environment/importing/ComplexBlueprint.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/importing/ComplexBlueprint.java
@@ -4,8 +4,19 @@ import dev.screwbox.core.environment.Entity;
 
 import java.util.List;
 
+/**
+ * A complex {@link Blueprint} that can make use of the {@link ImportContext} to assemble multiple related {@link Entity entities}
+ * e.g. a soft body enemy.
+ *
+ * @see AdvancedBlueprint
+ * @see ComplexBlueprint
+ * @since 3.19.0
+ */
 @FunctionalInterface
 public interface ComplexBlueprint<T> {
 
+    /**
+     * Assemble the {@link Entity entities} from the source.
+     */
     List<Entity> assembleFrom(T source, ImportContext context);
 }
