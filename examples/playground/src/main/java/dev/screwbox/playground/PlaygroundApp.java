4515ecc4a32c9636f53459d72c27b33dc1a08d9b
simonbas <sebastian.simon@koeln.de>
2026-01-02 13:16:39 +0100

    resize

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -3,9 +3,12 @@ package dev.screwbox.playground;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.Line;
 import dev.screwbox.core.ScrewBox;
+import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
 import dev.screwbox.core.environment.softphysics.RopeRenderComponent;
 import dev.screwbox.core.graphics.Color;
+
+import java.util.List;
 
 import static dev.screwbox.core.Vector.$;
 
@@ -15,19 +18,22 @@ public class PlaygroundApp {
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Playground");
 
+        engine.environment()
+            .enableAllFeatures()
+            .addSystem(new InteractionSystem())
+            .addEntities(createRope(engine));
+
+        engine.start();
+    }
+
+    private static List<Entity> createRope(Engine engine) {
         var rope = SoftPhysicsSupport.createRope($(4, 10), $(30, 130), 8, engine.environment());
 
         rope.getFirst().add(new RopeRenderComponent(Color.MAGENTA, 2));
         rope.forEach(node -> node.resize(4, 4));
-        rope.forEach(node -> node.get(PhysicsComponent.class).friction = 3);
+        rope.forEach(node -> node.get(PhysicsComponent.class).friction = 1);
         rope.getFirst().remove(PhysicsComponent.class);
-
-        engine.environment()
-            .enableAllFeatures()
-            .addSystem(new InteractionSystem())
-            .addEntities(rope);
-
-        engine.start();
+        return rope;
     }
 
 }
