2566670153915dd30bccb9b14c05e9dfcdbf3da4
simonbas <sebastian.simon@koeln.de>
2026-01-02 12:27:28 +0100

    prototype code

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -8,39 +8,44 @@ import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.SoftPhysicsSupport;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
-import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.environment.softphysics.RopeRenderComponent;
 import dev.screwbox.core.graphics.Color;
-import dev.screwbox.core.graphics.SpriteBundle;
+import dev.screwbox.core.graphics.Size;
 
 import java.util.List;
 
+import static dev.screwbox.core.Vector.$;
+
 public class PlaygroundApp {
 
-    static  private Entity grabbed;
+    static private Entity grabbed;
 
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Playground");
 
-        List<Entity> rope = SoftPhysicsSupport.createRope(Line.between(Vector.of(4, 10), Vector.of(30, 30)), 10, engine.environment());
-        rope.getFirst().add(new RopeRenderComponent(Color.ORANGE, 4)).add(new RenderComponent(SpriteBundle.DOT_WHITE));
-rope.forEach(r -> r.get(PhysicsComponent.class).friction = 4);
-        engine.graphics().camera().setPosition(Vector.of(4, 10));
+        var ropeOptions = SoftPhysicsSupport.RopeOptions.nodeCount(10)
+            .fixedEnd()
+            .nodeSize(Size.square(4))
+            .friction(3);
+
+        List<Entity> rope = SoftPhysicsSupport.createRope(Line.between($(4, 10), $(30, 130)), ropeOptions, engine.environment());
+        rope.getFirst().add(new RopeRenderComponent(Color.ORANGE, 4));
+
         engine.environment().addEntities(rope);
         engine.environment().enableAllFeatures();
 
         engine.environment().addSystem(s -> {
-            if(engine.mouse().isPressedLeft()) {
+            if (engine.mouse().isPressedLeft()) {
                 engine.navigation().searchAtPosition(engine.mouse().position())
                     .checkingFor(Archetype.ofSpacial(PhysicsComponent.class))
                     .selectAll().forEach(e -> grabbed = e);
             }
 
-            if(grabbed != null) {
+            if (grabbed != null) {
                 grabbed.get(PhysicsComponent.class).velocity = Vector.zero();
                 grabbed.moveTo(engine.mouse().position());
             }
-            if(!engine.mouse().isDownLeft()) {
+            if (!engine.mouse().isDownLeft()) {
                 grabbed = null;
             }
             engine.graphics().camera().move(engine.keyboard().wsadMovement(engine.loop().delta(300)));
