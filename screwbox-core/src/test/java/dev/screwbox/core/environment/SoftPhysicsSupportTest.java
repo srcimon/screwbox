d2994ef87b64a46a93424c228f02fd13e21f6155
simonbas <sebastian.simon@koeln.de>
2026-01-03 11:07:08 +0100

    added tests

--- screwbox-core/src/test/java/dev/screwbox/core/environment/SoftPhysicsSupportTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/environment/SoftPhysicsSupportTest.java
@@ -5,7 +5,10 @@ import dev.screwbox.core.test.EnvironmentExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.List;
+
 import static dev.screwbox.core.Vector.$;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(EnvironmentExtension.class)
@@ -26,9 +29,23 @@ class SoftPhysicsSupportTest {
     }
 
     @Test
+    void createRope_twoNodes_throwsException(DefaultEnvironment environment) {
+        assertThatThrownBy(() -> SoftPhysicsSupport.createRope($(20, 10), $(20, 10), 2, environment))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("nodeCount must be between 3 and 4096 (actual value: 2)");
+    }
+
+    @Test
     void createRope_idPool_throwsException() {
         assertThatThrownBy(() -> SoftPhysicsSupport.createRope($(20, 10), $(20, 10), 4, null))
             .isInstanceOf(NullPointerException.class)
             .hasMessageContaining("idPool must not be null");
+    }
+
+
+    @Test
+    void xxxxx(DefaultEnvironment environment) {
+        List<Entity> rope = SoftPhysicsSupport.createRope($(20, 10), $(20, 10), 8, environment);
+        assertThat(rope).hasSize(8);
     }
 }
