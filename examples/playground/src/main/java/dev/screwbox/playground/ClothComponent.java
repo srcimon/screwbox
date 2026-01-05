42bece85455a35c7db6e8c682dda154489b0d4bd
simonbas <sebastian.simon@koeln.de>
2026-01-05 22:31:30 +0100

    rename

--- examples/playground/src/main/java/dev/screwbox/playground/ClothComponent.java
+++ examples/playground/src/main/java/dev/screwbox/playground/ClothComponent.java
@@ -13,4 +13,11 @@ public class ClothComponent implements Component {
         this.nodes = nodes;
         this.normalSize = normalSize;
     }
+
+
+
+    //TODO define this component first. How to auto populate component using an entity system interpreting links and structures?
+    //NOTES:
+    //U need a single clothrendercomponent to fix rendering of cloth nodes in same order.
+    //  splitting to ClothNodeComponent(peerA.position, peerB.position) seems nice but would not alloe ordered rendering
 }
