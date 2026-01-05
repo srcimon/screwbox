9abd5f16ba05ad72f80c114f48fac55e22f4bede
simonbas <sebastian.simon@koeln.de>
2026-01-05 22:09:08 +0100

    rename

--- examples/playground/src/main/java/dev/screwbox/playground/ClothRenderComponent.java
+++ examples/playground/src/main/java/dev/screwbox/playground/ClothRenderComponent.java
@@ -4,4 +4,8 @@ import dev.screwbox.core.environment.Component;
 
 public class ClothRenderComponent implements Component {
 
+    /**
+     * Draw order used for rendering.
+     */
+    public int drawOrder = 0;
 }
