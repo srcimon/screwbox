2e9a0b93879e6e0f27d3547a460f9b3cd241da92
simonbas <sebastian.simon@koeln.de>
2026-01-13 19:42:47 +0100

    added cloth rendering test

--- screwbox-core/src/test/java/dev/screwbox/core/environment/softphysics/ClothRenderSystemTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/environment/softphysics/ClothRenderSystemTest.java
@@ -6,6 +6,7 @@ import dev.screwbox.core.environment.internal.DefaultEnvironment;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.Graphics;
 import dev.screwbox.core.graphics.Size;
+import dev.screwbox.core.graphics.SpriteBundle;
 import dev.screwbox.core.graphics.World;
 import dev.screwbox.core.graphics.options.PolygonDrawOptions;
 import dev.screwbox.core.test.EnvironmentExtension;
@@ -37,5 +38,26 @@ class ClothRenderSystemTest {
         verify(world, times(64)).drawPolygon(
             argThat((ArgumentMatcher<Polygon>) polygon -> polygon.isClosed() && polygon.nodeCount() == 3),
             argThat(options -> options.equals(PolygonDrawOptions.filled(Color.rgb(253, 0, 0)))));
+    }
+
+    @Test
+    void update_nonDetailedTexturedCloth_rendersOnPolygonPerMeshCell(DefaultEnvironment environment, World world, Graphics graphics) {
+        var cloth = SoftPhysicsSupport.createCloth(Bounds.$$(0, 0, 128, 128), Size.of(4, 8), environment);
+        cloth.root().add(new ClothRenderComponent(), config -> {
+            config.detailed = false;
+            config.texture = SpriteBundle.BOX.get();
+        });
+
+        when(graphics.isWithinDistanceToVisibleArea(any(), anyDouble())).thenReturn(true);
+        environment
+            .addEntities(cloth)
+            .addSystem(new ClothRenderSystem());
+        environment.update();
+
+        verify(world, times(32)).drawPolygon(any(Polygon.class), any());
+
+        verify(world, times(5)).drawPolygon(
+            argThat((ArgumentMatcher<Polygon>) polygon -> polygon.isClosed() && polygon.nodeCount() == 4),
+            argThat(options -> options.equals(PolygonDrawOptions.filled(Color.rgb(35, 36, 34)))));
     }
 }
