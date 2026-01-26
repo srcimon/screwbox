948bb1e2d8b03e9b62a17d92a1cbd3e006bf6849
simonbas <sebastian.simon@koeln.de>
2026-01-26 20:11:46 +0100

    Renamed `ExpandShader`

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/shader/ExpandShaderTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/shader/ExpandShaderTest.java
@@ -10,13 +10,13 @@ import java.awt.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class SizeIncreaseShaderTest {
+class ExpandShaderTest {
 
     private static final Image SOURCE = Frame.fromFile("tile.bmp").image();
 
     @Test
     void apply_sameIncreaseXandY_increasesBoth() {
-        var result = new SizeIncreaseShader(4).apply(SOURCE, Percent.zero());
+        var result = new ExpandShader(4).apply(SOURCE, Percent.zero());
 
         var resultFrame = Frame.fromImage(result);
         assertThat(resultFrame.size()).isEqualTo(Size.of(24, 24));
@@ -24,7 +24,7 @@ class SizeIncreaseShaderTest {
 
     @Test
     void apply_increaseXonly_doesntChangeY() {
-        var result = new SizeIncreaseShader(4, 0).apply(SOURCE, Percent.zero());
+        var result = new ExpandShader(4, 0).apply(SOURCE, Percent.zero());
 
         var resultFrame = Frame.fromImage(result);
         assertThat(resultFrame.size()).isEqualTo(Size.of(24, 16));
@@ -32,21 +32,21 @@ class SizeIncreaseShaderTest {
 
     @Test
     void newInstance_zeroIncrease_throwsException() {
-        assertThatThrownBy(() -> new SizeIncreaseShader(0))
+        assertThatThrownBy(() -> new ExpandShader(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("at least one axis should be size increased");
     }
 
     @Test
     void newInstance_xIncreaseInvalid_throwsException() {
-        assertThatThrownBy(() -> new SizeIncreaseShader(-2, 2))
+        assertThatThrownBy(() -> new ExpandShader(-2, 2))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("only size increase from 1 to 32 is supported (actual value: -2)");
     }
 
     @Test
     void newInstance_yIncreaseInvalid_throwsException() {
-        assertThatThrownBy(() -> new SizeIncreaseShader(2, 33))
+        assertThatThrownBy(() -> new ExpandShader(2, 33))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("only size increase from 1 to 32 is supported (actual value: 33)");
     }
