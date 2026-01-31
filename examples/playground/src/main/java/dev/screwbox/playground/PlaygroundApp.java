5b8f16502f2a6eb8845b057196a842e2eb6c2373
simonbas <sebastian.simon@koeln.de>
2026-01-31 17:04:34 +0100

    add lots of prototyping code

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -5,7 +5,7 @@ import dev.screwbox.core.Engine;
 import dev.screwbox.core.Line;
 import dev.screwbox.core.Percent;
 import dev.screwbox.core.ScrewBox;
-import dev.screwbox.core.Vector;
+import dev.screwbox.core.Time;
 import dev.screwbox.core.environment.Order;
 import dev.screwbox.core.environment.core.LogFpsSystem;
 import dev.screwbox.core.graphics.Color;
@@ -28,11 +28,11 @@ public class PlaygroundApp {
             .addSystem(e -> {
                 Line between = rotation.applyOn(Line.between(e.mouse().position(), e.mouse().position().add(50, -10)));
                 e.graphics().world().drawLine(between, LineDrawOptions.color(Color.WHITE).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()));
-                e.graphics().light().addDirectionalLight(between, 80, Color.BLACK);
-                if(e.keyboard().isDown(Key.Q)) {
+                e.graphics().light().addDirectionalLight(between, 80, Angle.of(between).addDegrees(270 + Math.sin(e.loop().runningTime().milliseconds() / 1000.0) * 45), Color.BLACK);
+                if (e.keyboard().isDown(Key.Q)) {
                     rotation = rotation.addDegrees(e.loop().delta(-40));
                 }
-                if(e.keyboard().isDown(Key.E)) {
+                if (e.keyboard().isDown(Key.E)) {
                     rotation = rotation.addDegrees(e.loop().delta(40));
                 }
             });
