bfbe1dce4179ebba768843996a220c4b228a240d
simonbas <sebastian.simon@koeln.de>
2026-02-01 11:57:55 +0100

    test perpendicular

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/LightPhysics.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/LightPhysics.java
@@ -117,10 +117,18 @@ public class LightPhysics {
             }
         }
         for (final var p : poi) {
-            lightBox.source().perpendicular(p).ifPresent(perpendicular ->
-                DefaultWorld.DEBUG_WORKAROUND.drawLine(perpendicular, LineDrawOptions.color(Color.WHITE.opacity(0.25)).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()))
-           );
+            lightBox.source().perpendicular(p).ifPresent(perpendicular -> {
+                    DefaultWorld.DEBUG_WORKAROUND.drawLine(perpendicular, LineDrawOptions.color(Color.WHITE.opacity(0.25)).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()));
+                    for (final var segment : extractLines(occluders)) {
+                        var ix = segment.intersectionPoint(perpendicular);
+                        if(ix != null) {
+                            DefaultWorld.DEBUG_WORKAROUND.drawOval(ix, 1, 1, OvalDrawOptions.filled(Color.YELLOW.opacity(0.5)).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()));
+                        }
+                    }
+                }
+            );
             DefaultWorld.DEBUG_WORKAROUND.drawOval(p, 1, 1, OvalDrawOptions.outline(Color.WHITE.opacity(0.25)).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()));
+
         }
         final List<Vector> area = new ArrayList<>();
         area.add(lightBox.origin());
