8f11bf0a34b318556e96e9e21a6bc6fb42f295e4
simonbas <sebastian.simon@koeln.de>
2026-02-05 11:22:39 +0100

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/LightPhysics.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/LightPhysics.java
@@ -33,25 +33,25 @@ public class LightPhysics {
         }
     }
 
-    private static class NoSelfOccluder extends Occluder {
+    private static final class NoSelfOccluder extends Occluder {
         NoSelfOccluder(final Bounds bounds) {
             super(bounds);
         }
 
-        public List<Line> lines(Vector lightPosition) {
+        public List<Line> lines(final Vector sourcePosition) {
             if (lines == null) {
                 lines = new ArrayList<>(Borders.ALL.extractFrom(bounds));
             }
-            final List<Line> lightDependendLines = new ArrayList<>();
-            final boolean isBetweenX = lightPosition.x() > bounds.minX() && lightPosition.x() < bounds.maxX();
-            final boolean isBetweenY = lightPosition.y() > bounds.minY() && lightPosition.y() < bounds.maxY();
-            lines.sort(comparingDouble(border -> border.center().distanceTo(lightPosition)));
+            final List<Line> pointSpecificLights = new ArrayList<>();
+            final boolean isBetweenX = sourcePosition.x() > bounds.minX() && sourcePosition.x() < bounds.maxX();
+            final boolean isBetweenY = sourcePosition.y() > bounds.minY() && sourcePosition.y() < bounds.maxY();
+            lines.sort(comparingDouble(border -> border.center().distanceTo(sourcePosition)));
             if (isBetweenX != isBetweenY) {
-                lightDependendLines.add(lines.get(lines.get(1).intersects(Line.between(bounds.position(), lightPosition)) ? 0 : 1));
+                pointSpecificLights.add(lines.get(lines.get(1).intersects(Line.between(bounds.position(), sourcePosition)) ? 0 : 1));
             }
-            lightDependendLines.add(lines.get(2));
-            lightDependendLines.add(lines.get(3));
-            return lightDependendLines;
+            pointSpecificLights.add(lines.get(2));
+            pointSpecificLights.add(lines.get(3));
+            return pointSpecificLights;
         }
     }
 
@@ -93,7 +93,7 @@ public class LightPhysics {
         final var relevantOccluders = allIntersecting(lightBox);
         for (long angle = Math.round(minAngle); angle < maxAngle; angle++) {
             final Line raycast = Angle.degrees(angle).rotate(normal);
-            area.add(findClosest(relevantOccluders, raycast));
+            area.add(findNearest(raycast, relevantOccluders));
         }
         return area;
     }
@@ -118,7 +118,7 @@ public class LightPhysics {
 
         final List<Line> definitionLines = new ArrayList<>();
         for (final var probe : calculateLightProbes(lightBox, relevantOccluders)) {
-            Vector closest1 = findClosest(relevantOccluders, probe);
+            Vector closest1 = findNearest(probe, relevantOccluders);
             definitionLines.add(closest1 == null ? probe : Line.between(probe.start(), closest1));
         }
         List<ScoredPoint> points = new ArrayList<>();
@@ -135,25 +135,24 @@ public class LightPhysics {
         return area;
     }
 
-    record ScoredPoint(Vector position, double cost) implements Comparable<ScoredPoint> {
+    private record ScoredPoint(Vector position, double cost) implements Comparable<ScoredPoint> {
         @Override
         public int compareTo(ScoredPoint o) {
             return Double.compare(cost, o.cost);
         }
     }
 
-    private static Vector findClosest(List<Occluder> relevantOccluders, Line raycast) {
+    private static Vector findNearest(final Line raycast, final List<Occluder> rayOccluders) {
         double minDist = Double.MAX_VALUE;
         Vector nearest = null;
-        for (final var occluder : relevantOccluders) {
-            final List<Line> lines = occluder.lines(raycast.start());
-            for (final var other : lines) {
-                final var intersectionPoint = raycast.intersectionPoint(other);
-                if (nonNull(intersectionPoint)) {
-                    var distance = raycast.start().distanceTo(intersectionPoint);
+        for (final var occluder : rayOccluders) {
+            for (final var other : occluder.lines(raycast.start())) {
+                final var intersection = raycast.intersectionPoint(other);
+                if (nonNull(intersection)) {
+                    var distance = raycast.start().distanceTo(intersection);
                     if (distance < minDist) {
                         minDist = distance;
-                        nearest = intersectionPoint;
+                        nearest = intersection;
                     }
                 }
             }
@@ -161,16 +160,16 @@ public class LightPhysics {
         return nearest == null ? raycast.end() : nearest;
     }
 
-    private static List<Line> calculateLightProbes(DirectionalLightBox lightBox, List<Occluder> relevantOccluders) {
+    private static List<Line> calculateLightProbes(final DirectionalLightBox lightBox, final List<Occluder> lightOccluders) {
         final List<Line> lightProbes = new ArrayList<>();
-        final var a = lightBox.source().end().substract(lightBox.source().start()).length(0.000000000001);
-        final var b = lightBox.source().start().substract(lightBox.source().end()).length(0.000000000001);
+        final var left = lightBox.source().end().substract(lightBox.source().start()).length(0.000000000001);
+        final var right = lightBox.source().start().substract(lightBox.source().end()).length(0.000000000001);
 
-        for (final var occluder : relevantOccluders) {
-            processPoint(lightBox, occluder.bounds.origin(), lightProbes, a, b);
-            processPoint(lightBox, occluder.bounds.topRight(), lightProbes, a, b);
-            processPoint(lightBox, occluder.bounds.bottomRight(), lightProbes, a, b);
-            processPoint(lightBox, occluder.bounds.bottomLeft(), lightProbes, a, b);
+        for (final var occluder : lightOccluders) {
+            addProbes(lightBox, occluder.bounds.origin(), lightProbes, left, right);
+            addProbes(lightBox, occluder.bounds.topRight(), lightProbes, left, right);
+            addProbes(lightBox, occluder.bounds.bottomRight(), lightProbes, left, right);
+            addProbes(lightBox, occluder.bounds.bottomLeft(), lightProbes, left, right);
         }
 
         lightProbes.add(Line.between(lightBox.origin(), lightBox.bottomLeft()));
@@ -178,11 +177,11 @@ public class LightPhysics {
         return lightProbes;
     }
 
-    private static void processPoint(DirectionalLightBox lightBox, Vector p, List<Line> lightProbes, Vector a, Vector b) {
-        lightBox.source().perpendicular(p).ifPresent(perpendicular -> {
-                lightProbes.add(perpendicular.move(a).length(lightBox.distance()));
-                lightProbes.add(perpendicular);
-                lightProbes.add(perpendicular.move(b).length(lightBox.distance()));
+    private static void addProbes(final DirectionalLightBox lightBox, final Vector point, final List<Line> probes, final Vector left, final Vector right) {
+        lightBox.source().perpendicular(point).ifPresent(perpendicular -> {
+                probes.add(perpendicular.move(left).length(lightBox.distance()));
+                probes.add(perpendicular);
+                probes.add(perpendicular.move(right).length(lightBox.distance()));
             }
         );
     }
