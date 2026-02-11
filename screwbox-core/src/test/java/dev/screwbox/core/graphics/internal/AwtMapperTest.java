864231bbc6dd408b6b837531f1bb28056dc9d0b9
simonbas <sebastian.simon@koeln.de>
2026-02-11 19:53:50 +0100

    Moved path creation to `AwtMapper`

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/internal/AwtMapperTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/internal/AwtMapperTest.java
@@ -2,7 +2,12 @@ package dev.screwbox.core.graphics.internal;
 
 import dev.screwbox.core.Percent;
 import dev.screwbox.core.graphics.Color;
+import dev.screwbox.core.graphics.Offset;
 import org.junit.jupiter.api.Test;
+
+import java.awt.geom.GeneralPath;
+import java.awt.geom.PathIterator;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,5 +35,43 @@ class AwtMapperTest {
         assertThat(awtColor.getGreen()).isEqualTo(40);
         assertThat(awtColor.getBlue()).isEqualTo(60);
         assertThat(awtColor.getAlpha()).isEqualTo(127);
+    }
+
+    @Test
+    void toPath_validPoint_createsPath() {
+        GeneralPath path = AwtMapper.toPath(List.of(Offset.at(10, 2), Offset.at(19, 2), Offset.at(21, 3)));
+
+        PathIterator iterator = path.getPathIterator(null);
+        float[] coords = new float[6];
+
+        assertThat(iterator.isDone()).isFalse();
+        assertThat(iterator.currentSegment(coords)).isEqualTo(PathIterator.SEG_MOVETO);
+        assertThat(coords[0]).isEqualTo(10);
+        assertThat(coords[1]).isEqualTo(2);
+
+        iterator.next();
+
+        assertThat(iterator.isDone()).isFalse();
+        assertThat(iterator.currentSegment(coords)).isEqualTo(PathIterator.SEG_LINETO);
+        assertThat(coords[0]).isEqualTo(10);
+        assertThat(coords[1]).isEqualTo(2);
+
+        iterator.next();
+
+        assertThat(iterator.isDone()).isFalse();
+        assertThat(iterator.currentSegment(coords)).isEqualTo(PathIterator.SEG_LINETO);
+        assertThat(coords[0]).isEqualTo(19);
+        assertThat(coords[1]).isEqualTo(2);
+
+        iterator.next();
+
+        assertThat(iterator.isDone()).isFalse();
+        assertThat(iterator.currentSegment(coords)).isEqualTo(PathIterator.SEG_LINETO);
+        assertThat(coords[0]).isEqualTo(21);
+        assertThat(coords[1]).isEqualTo(3);
+
+        iterator.next();
+
+        assertThat(iterator.isDone()).isTrue();
     }
 }
