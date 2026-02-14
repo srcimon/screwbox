0722482d8082d072c080547323b327afaed190be
simonbas <sebastian.simon@koeln.de>
2026-02-14 17:24:54 +0100

    Fixed light going to occluder edges

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/internal/DefaultLightTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/internal/DefaultLightTest.java
@@ -52,7 +52,7 @@ class DefaultLightTest {
 
     @Test
     void addBackgdropOccluder_occluderNotNull_enablesLight() {
-        light.addBackgdropOccluder(Polygon.ofNodes($(10, 2), $(4, 2), $(10, 2)), 0.75, false);
+        light.addBackgdropOccluder(Polygon.ofNodes($(10, 2), $(4, 2), $(10, 2)), 0.75, false, true);
 
         assertThat(configuration.isLightEnabled()).isTrue();
     }
