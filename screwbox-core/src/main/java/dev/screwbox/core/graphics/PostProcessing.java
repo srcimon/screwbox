ec3467caf2efee1ac4f6eeab8f70036abca55b43
simonbas <sebastian.simon@koeln.de>
2026-03-01 09:41:11 +0100

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/PostProcessing.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/PostProcessing.java
@@ -1,11 +1,22 @@
 package dev.screwbox.core.graphics;
 
 import dev.screwbox.core.Vector;
+import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.graphics.filter.PostProcessingFilter;
 import dev.screwbox.core.graphics.options.ShockwaveOptions;
 
+/**
+ * Add {@link PostProcessingFilter} to create optical effects on the {@link Screen}. Also provides some easy-to-use
+ * functions to create more sofisticated optical effects like {@link #triggerShockwave(Vector, ShockwaveOptions)}.
+ *
+ * @since 3.24.0
+ */
 public interface PostProcessing {
 
+    /**
+     * Creates a shockwave effect using an internal {@link PostProcessingFilter}. The effect
+     * is purly optical and does not affect physics {@link Entity entities}.
+     */
     PostProcessing triggerShockwave(Vector position, ShockwaveOptions options);
 
     int shockwaveCount();
@@ -13,6 +24,8 @@ public interface PostProcessing {
     PostProcessing addFilter(PostProcessingFilter filter);
 
     PostProcessing addViewportFilter(PostProcessingFilter filter);
+
+    //TODO PostProcessing addViewportFilter(int viewportId, PostProcessingFilter filter);
 
     PostProcessing clearFilters();
 
--- screwbox-core/src/main/java/dev/screwbox/core/graphics/filter/UnderwaterPostFilter.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/filter/UnderwaterPostFilter.java
@@ -1,7 +1,5 @@
 package dev.screwbox.core.graphics.filter;
 
-import dev.screwbox.core.graphics.ScreenBounds;
-
 import java.awt.*;
 import java.awt.image.VolatileImage;
 
