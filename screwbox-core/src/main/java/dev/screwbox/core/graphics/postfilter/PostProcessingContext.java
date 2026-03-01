3538e121809e82c7ec9fbfee6e2b5897aef143c6
simonbas <sebastian.simon@koeln.de>
2026-03-01 16:14:52 +0100

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/PostProcessingContext.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/PostProcessingContext.java
@@ -3,19 +3,20 @@ package dev.screwbox.core.graphics.postfilter;
 import dev.screwbox.core.Duration;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.ScreenBounds;
+import dev.screwbox.core.graphics.Size;
+import dev.screwbox.core.graphics.Sizeable;
 import dev.screwbox.core.graphics.Viewport;
 
-public record PostProcessingContext(Color backgroundColor, Duration filterActiveDuration, Viewport viewport) {
+public record PostProcessingContext(Color backgroundColor, Duration filterActiveDuration,
+                                    Viewport viewport) implements Sizeable {
 
     public ScreenBounds bounds() {
         return viewport.canvas().bounds();
     }
 
-    public int width() {
-        return bounds().width();
+    @Override
+    public Size size() {
+        return bounds().size();
     }
 
-    public int height() {
-        return bounds().height();
-    }
 }
