1efc26a49d5cab70c13f21d0d59ae74c1ec5647a
simonbas <sebastian.simon@koeln.de>
2026-03-04 18:02:56 +0100

    added test

--- screwbox-core/src/test/java/dev/screwbox/core/graphics/postfilter/BeeEyePostFilterTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/postfilter/BeeEyePostFilterTest.java
@@ -1,14 +1,29 @@
 package dev.screwbox.core.graphics.postfilter;
 
-import org.assertj.core.api.Assertions;
+import dev.screwbox.core.Duration;
+import dev.screwbox.core.graphics.Color;
+import dev.screwbox.core.graphics.Frame;
 import org.junit.jupiter.api.Test;
 
-class BeeEyePostFilterTest {
+import static dev.screwbox.core.test.TestUtil.verifyIsSameImage;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BeeEyePostFilterTest extends PostFilterTest {
 
     @Test
     void newInstance_eyesTooSmall_throwsException() {
-        Assertions.assertThatThrownBy(() -> new BeeEyePostFilter(2))
+        assertThatThrownBy(() -> new BeeEyePostFilter(2))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("eye size must be in range 8 to 128 (actual value: 2)");
+    }
+
+    @Test
+    void applyBeeEyePostFilter_validInput_isCorrect() {
+        var filter = new BeeEyePostFilter(10);
+        var context = new PostProcessingContext(Color.BLACK, Duration.ofMillis(10), viewport);
+
+        filter.apply(source, target, context);
+
+        verifyIsSameImage(targetImage, "postfilter/applyBeeEyePostFilter_validInput_isCorrect.png");
     }
 }
