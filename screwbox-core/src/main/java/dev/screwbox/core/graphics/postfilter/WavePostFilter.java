ca508c586b841874ed242696d600b28b26e0e435
simonbas <sebastian.simon@koeln.de>
2026-03-06 17:16:13 +0100

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/WavePostFilter.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/WavePostFilter.java
@@ -1,21 +1,23 @@
 package dev.screwbox.core.graphics.postfilter;
 
 import dev.screwbox.core.Duration;
+import dev.screwbox.core.Percent;
 import dev.screwbox.core.graphics.internal.AwtMapper;
 import dev.screwbox.core.utils.Validate;
 
 import java.awt.*;
 
 //TODO Test and document
-public record WavePostFilter(int rowHeight, Duration interval, double intensity) implements PostProcessingFilter {
+public record WavePostFilter(int rowHeight, Duration interval, double intensity, Percent frequency) implements PostProcessingFilter {
 
     public WavePostFilter {
         Validate.range(rowHeight, 1, 16, "row height must be in range 1 to 16");
         Validate.range(intensity, 1, 32, "intensity must be in range 1 to 32");
+        Validate.range(intensity, 1, 32, "intensity must be in range 1 to 32");
     }
 
     public WavePostFilter() {
-        this(2, Duration.ofMillis(500), 10);
+        this(2, Duration.ofMillis(500), 10, Percent.of(0.05));
     }
 
     @Override
@@ -23,13 +25,11 @@ public record WavePostFilter(int rowHeight, Duration interval, double intensity)
         final var area = context.bounds();
         target.setColor(AwtMapper.toAwtColor(context.backgroundColor()));
         target.fillRect(area.x(), area.y(), area.width(), area.height());
-        int h = area.height();
 
-        double time = context.lifetime().milliseconds() / (double) interval.milliseconds();
-        double frequency = 0.05;//TODO configure
+        final double time = context.lifetime().milliseconds() / (double) interval.milliseconds();
 
-        for (int y = 0; y < h; y += rowHeight) {
-            int offsetX = (int) (Math.sin((y * frequency) + time) * intensity);
+        for (int y = 0; y < area.height(); y += rowHeight) {
+            int offsetX = (int) (Math.sin((y * frequency.value()) + time) * intensity);
 
             target.drawImage(source,
                 area.x() + offsetX, area.y() + y, area.maxX() + offsetX, area.y() + y + rowHeight,
