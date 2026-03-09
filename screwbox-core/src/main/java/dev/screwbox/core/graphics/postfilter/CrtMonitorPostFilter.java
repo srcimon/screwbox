6c301f117e4a4d8b2cafc2dd6e1edad54e35ca8f
simonbas <sebastian.simon@koeln.de>
2026-03-09 20:13:54 +0100

    finished

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/CrtMonitorPostFilter.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/CrtMonitorPostFilter.java
@@ -1,9 +1,6 @@
 package dev.screwbox.core.graphics.postfilter;
 
-import dev.screwbox.core.Duration;
-import dev.screwbox.core.Time;
 import dev.screwbox.core.assets.Asset;
-import dev.screwbox.core.graphics.ScreenBounds;
 import dev.screwbox.core.graphics.Sprite;
 import dev.screwbox.core.graphics.SpriteBundle;
 import dev.screwbox.core.utils.MathUtil;
@@ -24,7 +21,7 @@ public class CrtMonitorPostFilter implements PostProcessingFilter {
     @Override
     public void apply(final Image source, final Graphics2D target, final PostProcessingContext context) {
         final long seed = context.lifetime().milliseconds();
-        ScreenBounds area = context.bounds();
+        final var area = context.bounds();
         target.drawImage(source, area.x(), area.y(), area.maxX(), area.maxY(), area.x(), area.y(), area.maxX(), area.maxY(), null);
         target.setStroke(new BasicStroke(2));
         for (int y = area.y(); y < area.height(); y += 4) {
@@ -33,11 +30,12 @@ public class CrtMonitorPostFilter implements PostProcessingFilter {
             target.drawLine(area.x(), y, area.maxX(), y);
         }
         target.setComposite(AlphaComposite.getInstance(SRC_OVER, 1f));
-        var size = EDGE.get().size();
-//        SpriteDrawOptions baseOptions = SpriteDrawOptions.originalSize().ignoreOverlayShader();
-//        canvas.drawSprite(EDGE, Offset.origin(), baseOptions);
-//        canvas.drawSprite(EDGE, Offset.at(canvas.width() - size.width(), 0), baseOptions.flippedHorizontal(true));
-//        canvas.drawSprite(EDGE, Offset.at(0, canvas.height() - size.height()), baseOptions.flippedVertical(true));
-//        canvas.drawSprite(EDGE, Offset.at(canvas.width() - size.width(), canvas.height() - size.height()), baseOptions.flippedHorizontal(true).flippedVertical(true));
+        final var size = EDGE.get().size();
+        final var image = EDGE.get().singleImage();
+
+        target.drawImage(image, area.x(), area.y(), null);
+        target.drawImage(image, area.maxX(), area.y(), area.maxX() - size.width(), area.y() + size.height(), 0, 0, size.width(), size.height(), null);
+        target.drawImage(image, area.x(), area.maxY(), area.x() + size.width(), area.maxY() - size.height(), 0, 0, size.width(), size.height(), null);
+        target.drawImage(image, area.maxX(), area.maxY(), area.maxX() - size.width(), area.maxY() - size.height(), 0, 0, size.width(), size.height(), null);
     }
 }
