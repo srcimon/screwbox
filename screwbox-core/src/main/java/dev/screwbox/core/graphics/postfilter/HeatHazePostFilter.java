d497dd83f93eb5454938456d28847c6b8b378d1a
simonbas <sebastian.simon@koeln.de>
2026-04-03 13:20:38 +0200

    fixed todos

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/HeatHazePostFilter.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/postfilter/HeatHazePostFilter.java
@@ -10,7 +10,6 @@ import java.awt.*;
  *
  * @since 3.24.0
  */
-//TODO apply resolutionscale
 public record HeatHazePostFilter(Duration interval, int segmentHeight) implements PostProcessingFilter {
 
     public HeatHazePostFilter {
@@ -26,20 +25,23 @@ public record HeatHazePostFilter(Duration interval, int segmentHeight) implement
         drawSourceImage(source, target, context);
 
         final var area = context.bounds();
+        final double scale = context.resolutionScale();
         final double time = context.lifetime().milliseconds() / (double) interval.milliseconds();
-        for (int y = area.y(); y < area.maxY(); y += segmentHeight) {
-            final double verticalFactor = (double) (y - area.y()) / area.height();
-            final int offsetX = (int) ((Math.sin(time * 1.5 + y * 0.1) * 4 +
-                                        Math.sin(time * 3.7 + y * 0.5) * 2) * verticalFactor);
 
-            final int offsetY = (int) (Math.abs(Math.sin(time * 0.5 + y * 0.05)) * 3 * verticalFactor);
+        final int scaledSegmentHeight = (int) Math.max(1, segmentHeight * scale);
+
+        for (int y = area.y(); y < area.maxY(); y += scaledSegmentHeight) {
+            final double verticalFactor = (double) (y - area.y()) / area.height();
+
+            final int offsetX = (int) (((Math.sin(time * 1.5 + y * (0.1 / scale)) * 4 +
+                                         Math.sin(time * 3.7 + y * (0.5 / scale)) * 2) * verticalFactor) * scale);
+
+            final int offsetY = (int) ((Math.abs(Math.sin(time * 0.5 + y * (0.05 / scale))) * 3 * verticalFactor) * scale);
 
             target.drawImage(source,
-                area.x(), y, area.maxX(), y + segmentHeight,
-                area.x() + offsetX, y + offsetY, area.maxX() + offsetX, y + segmentHeight + offsetY,
+                area.x(), y, area.maxX(), y + scaledSegmentHeight,
+                area.x() + offsetX, y + offsetY, area.maxX() + offsetX, y + scaledSegmentHeight + offsetY,
                 null);
-
         }
     }
-
 }
