81f21e3f61ab9b0acd6b4f7334101f3f22dfda7d
simonbas <sebastian.simon@koeln.de>
2026-04-11 10:33:36 +0200

    fixup

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/Color.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/Color.java
@@ -391,9 +391,9 @@ public final class Color implements Serializable {
      * @since 3.27.0
      */
     public boolean isNearlyIdenticalTo(final Color other) {
-        return Math.abs(r() - other.r()) < 3
-               && Math.abs(g() - other.g()) < 3
-               && Math.abs(b() - other.b()) < 3
-               && Math.abs(opacity().value() - other.opacity().value()) < 0.1;
+        return Math.abs(r() - other.r()) < 6
+               && Math.abs(g() - other.g()) < 6
+               && Math.abs(b() - other.b()) < 6
+               && Math.abs(opacity().value() - other.opacity().value()) <= 0.1;
     }
 }
