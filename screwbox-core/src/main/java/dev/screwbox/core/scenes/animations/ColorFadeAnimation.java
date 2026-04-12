5a6484835cde5e86d0712af31b20844ad5bff4d9
simonbas <sebastian.simon@koeln.de>
2026-04-12 11:58:53 +0200

    Revert "Test"
    This reverts commit 860f934ac9a01ac0ebdd0658c5f80cd36d226d21.

--- screwbox-core/src/main/java/dev/screwbox/core/scenes/animations/ColorFadeAnimation.java
+++ screwbox-core/src/main/java/dev/screwbox/core/scenes/animations/ColorFadeAnimation.java
@@ -23,6 +23,7 @@ public record ColorFadeAnimation(Color color) implements TransitionAnimation {
     }
 
     @Override
+    public void apply(final Image source, final Graphics2D target, final AnimationContext context) {
         target.drawImage(source, 0, 0, null);
         target.setColor(AwtMapper.toAwtColor(color.opacity(context.progress())));
         target.fillRect(0, 0, context.width(), context.height());
