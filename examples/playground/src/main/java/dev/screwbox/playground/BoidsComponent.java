ec3e0fb41d49b4041341646cc2603ab164a523b5
simonbas <sebastian.simon@koeln.de>
2026-04-12 15:30:13 +0200

    add boids system prototype

---
+++ examples/playground/src/main/java/dev/screwbox/playground/BoidsComponent.java
@@ -0,0 +1,9 @@
+package dev.screwbox.playground;
+
+import dev.screwbox.core.environment.Component;
+
+public class BoidsComponent implements Component {
+
+    public double visionRadius = 20;
+
+}
