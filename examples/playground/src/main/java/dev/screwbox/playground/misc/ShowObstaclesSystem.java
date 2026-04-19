2ec06becd22b333a6b65a8d8f22b04443cc75a32
simonbas <sebastian.simon@koeln.de>
2026-04-19 10:44:04 +0200

    add final modifiers

--- examples/playground/src/main/java/dev/screwbox/playground/misc/ShowObstaclesSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/misc/ShowObstaclesSystem.java
@@ -3,9 +3,9 @@ package dev.screwbox.playground.misc;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.EntitySystem;
+import dev.screwbox.core.environment.ai.BoidObstacleComponent;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.options.RectangleDrawOptions;
-import dev.screwbox.core.environment.ai.BoidObstacleComponent;
 
 public class ShowObstaclesSystem implements EntitySystem {
 
@@ -14,7 +14,7 @@ public class ShowObstaclesSystem implements EntitySystem {
     @Override
     public void update(Engine engine) {
         for (var obstacle : engine.environment().fetchAll(OBSTACLES)) {
-        //    engine.graphics().world().drawRectangle(obstacle.bounds(), RectangleDrawOptions.outline(Color.WHITE).strokeWidth(2));
+            engine.graphics().world().drawRectangle(obstacle.bounds(), RectangleDrawOptions.outline(Color.WHITE).strokeWidth(2));
         }
     }
 }
