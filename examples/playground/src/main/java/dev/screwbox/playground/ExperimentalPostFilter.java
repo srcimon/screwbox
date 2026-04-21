125c891d58957e6035889c80452c2f0b5d18a000
simonbas <sebastian.simon@koeln.de>
2026-04-21 19:54:43 +0200

    start new prototype

--- examples/playground/src/main/java/dev/screwbox/playground/ExperimentalPostFilter.java
+++ examples/playground/src/main/java/dev/screwbox/playground/ExperimentalPostFilter.java
@@ -1,14 +1,75 @@
 package dev.screwbox.playground;
 
+import dev.screwbox.core.Duration;
+import dev.screwbox.core.Percent;
+import dev.screwbox.core.Polygon;
+import dev.screwbox.core.graphics.Offset;
+import dev.screwbox.core.graphics.internal.AwtMapper;
 import dev.screwbox.core.graphics.postfilter.PostProcessingContext;
 import dev.screwbox.core.graphics.postfilter.PostProcessingFilter;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExperimentalPostFilter implements PostProcessingFilter {
 
+    private final Polygon outline;
+    private final Polygon surface;
+    Percent strength = Percent.of(0.3);
+    private static final int ITERATIONS = 30;
+    Duration interval = Duration.ofMillis(500);
+    public ExperimentalPostFilter(Polygon outline, Polygon surface) {
+        this.outline = outline;
+        this.surface = surface;
+    }
+
     @Override
     public void apply(Image source, Graphics2D target, PostProcessingContext context) {
+        drawSourceImage(source, target, context);
 
+        final var area = context.bounds();
+        final double scale = context.resolutionScale();
+        final var originalClip = target.getClip();
+
+
+        List<Offset> offsets = new ArrayList<>();
+        for(var node : outline.definitionNotes()) {
+            offsets.add(context.viewport().toCanvas(node));
+        }
+        var outline = AwtMapper.toPath(offsets);
+        // Polygon-Setup (ohne die letzten beiden Knoten)
+
+        final int stepX = (int) Math.ceil(area.center().x() / (double) ITERATIONS);
+        final int stepY = (int) Math.ceil(area.center().y() / (double) ITERATIONS);
+        final double time = context.lifetime().milliseconds() / (double) interval.milliseconds();
+
+        // Effekt auf Polygon beschränken
+        target.setClip(outline);
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            final double wave = Math.sin(time + (i * strength.value()));
+            final int offset = (int) (wave * 12 * scale);
+
+            final int localSx1 = i * stepX;
+            final int localSy1 = i * stepY;
+            final int localSx2 = context.width() - localSx1;
+            final int localSy2 = context.height() - localSy1;
+
+            final int localDx1 = localSx1 - offset;
+            final int localDy1 = localSy1 - offset;
+            final int localDx2 = localSx2 + offset;
+            final int localDy2 = localSy2 + offset;
+
+            target.drawImage(source,
+                area.x() + localDx1, area.y() + localDy1,
+                area.x() + localDx2, area.y() + localDy2,
+                area.x() + localSx1, area.y() + localSy1,
+                area.x() + localSx2, area.y() + localSy2,
+                null);
+        }
+
+        // Clip zurücksetzen für nachfolgende Effekte
+        target.setClip(originalClip);
     }
 }
--- examples/playground/src/main/java/dev/screwbox/playground/FluidPostfilterSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/FluidPostfilterSystem.java
@@ -12,11 +12,12 @@ public class FluidPostfilterSystem implements EntitySystem {
 
     @Override
     public void update(Engine engine) {
+        engine.graphics().postProcessing().clearFilters();
         //TODO FLuidComponent.fluidPolygon;
         for(final var fluid : engine.environment().fetchAll(Archetype.ofSpacial(FluidComponent.class))) {
             var surface = fluid.get(FluidComponent.class).surface;
             var outline = surface.addNode(fluid.bounds().bottomRight()).addNode(fluid.bounds().bottomLeft()).close(); // TODO addNodes()
-            engine.graphics().world().drawPolygon(outline, PolygonDrawOptions.outline(Color.RED).strokeWidth(3).drawOrder(Order.DEBUG_OVERLAY_LATE.drawOrder()));
+            engine.graphics().postProcessing().addScreenFilter(new ExperimentalPostFilter(outline, surface));
         }
     }
 }
