2f51b316c11b9303a2a19a4e062b951bb630cd0b
simonbas <sebastian.simon@koeln.de>
2026-04-21 19:28:33 +0200

    start new prototype

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -1,84 +1,82 @@
 package dev.screwbox.playground;
 
 import dev.screwbox.core.Bounds;
-import dev.screwbox.core.Duration;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.ScrewBox;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.ai.BoidComponent;
 import dev.screwbox.core.environment.ai.BoidObstacleComponent;
-import dev.screwbox.core.environment.core.LogFpsSystem;
-import dev.screwbox.core.environment.physics.ChaoticMovementComponent;
-import dev.screwbox.core.environment.physics.CursorAttachmentComponent;
+import dev.screwbox.core.environment.fluids.FluidComponent;
+import dev.screwbox.core.environment.fluids.FluidRenderComponent;
+import dev.screwbox.core.environment.fluids.FluidTurbulenceComponent;
+import dev.screwbox.core.environment.physics.ColliderComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
+import dev.screwbox.core.environment.physics.StaticColliderComponent;
+import dev.screwbox.core.environment.rendering.CameraTargetComponent;
 import dev.screwbox.core.environment.rendering.RenderComponent;
+import dev.screwbox.core.graphics.AutoTileBundle;
 import dev.screwbox.core.graphics.Color;
-import dev.screwbox.core.graphics.Sprite;
+import dev.screwbox.core.graphics.SpriteBundle;
+import dev.screwbox.core.utils.TileMap;
 
-import java.util.Random;
+import static dev.screwbox.core.environment.importing.ImportOptions.indexedSources;
 
 public class PlaygroundApp {
 
-    private static final Random RANDOM = new Random();
-
     public static void main(String[] args) {
-        Engine engine = ScrewBox.createEngine("Playground");
+        Engine screwBox = ScrewBox.createEngine("Playground");
 
-        engine.environment()
+        var map = TileMap.fromString("""
+                                                    ##
+               ##   #         C            ##      ####
+            ###########WWWWWWWWWWWWWWW###################
+            ###########WWWWWWWWWWWWWWW###################
+            ###########WWWWWWWWWWWWWWW###################
+            ###########WWWWWWWWWWWWWWW###################
+            ###########WWWWWWWWWWWWWWW###################
+            #############################################
+            #############################################
+            #############################################
+            """);
+
+
+        screwBox.graphics().camera().setZoom(3);
+        screwBox.environment()
             .enableAllFeatures()
-            .addSystem(new LogFpsSystem())
-            .addSystemsFromPackage("dev.screwbox.playground")
-            .addEntity(new Entity().name("container")
-                .add(new BoidObstacleComponent(), config -> config.isContainer = true)
-                .bounds(engine.graphics().visibleArea())
+            .importSource(indexedSources(map.blocks(), TileMap.Block::value)
+                .assign('W', block -> new Entity().name("water")
+                    .bounds(block.bounds().expandTop(-8))
+                    .add(new FluidComponent((int) (block.bounds().width() / 16)))
+                    .add(new BoidObstacleComponent(), config -> config.isContainer = true)
+                    .add(new FluidRenderComponent(), config -> {
+                        config.surfaceColor = Color.WHITE.opacity(0.5);
+                        config.surfaceStrokeWidth = 1;
+                    })
+                    .add(new FluidTurbulenceComponent(300))
+                )
+                .assign('W', block -> new Entity().name("boid1")
+                    .bounds(Bounds.atPosition(block.bounds().position(), 8, 8))
+                    .add(new RenderComponent(SpriteBundle.DOT_RED.get().scaled(0.5)))
+                    .add(new PhysicsComponent())
+                    .add(new BoidComponent(), config -> {
+                        config.obstaclePerceptionRadius = 10;
+                    })
+                )
             )
-            .addEntity(new Entity().name("cursor")
-                .add(new CursorAttachmentComponent())
-                .add(new BoidObstacleComponent(), config -> config.isContainer = true)
-                .bounds(Bounds.atPosition(engine.mouse().position(), 120, 200))
-            )
-            .addEntity(new Entity().name("obstacle")
-                .add(new BoidObstacleComponent())
-                .add(new PhysicsComponent())
-                .add(new ChaoticMovementComponent(200, Duration.ofSeconds(2)))
-                .bounds(Bounds.atOrigin(40, 40, 80, 80)))
+            .importSource(indexedSources(map.tiles(), TileMap.Tile::value)
+                .assign('#', tile -> new Entity().name("earth")
+                    .bounds(tile.bounds())
+                    .add(new RenderComponent(AutoTileBundle.ROCKS.get().findSprite(tile.autoTileMask())))
+                    .add(new StaticColliderComponent())
+                    .add(new ColliderComponent())
+                )
+                .assign('C', tile -> new Entity().name("camera")
+                    .bounds(tile.bounds())
+                    .add(new CameraTargetComponent())
+                )
+            );
 
-            .addEntity(new Entity().name("obstacle")
-                .add(new BoidObstacleComponent())
-                .add(new PhysicsComponent())
-                .add(new ChaoticMovementComponent(200, Duration.ofSeconds(2)))
-                .bounds(Bounds.atOrigin(-140, -40, 100, 40)))
-        ;
-        populateWithBoids(engine, 3000);
-
-        engine.start();
-    }
-
-    private static void populateWithBoids(Engine engine, int boidCount) {
-        Sprite boidSprite = Sprite.pixel(Color.RED).scaled(1);
-        for (int i = 0; i < boidCount; i++) {
-
-            Bounds area = engine.graphics().visibleArea();
-            Entity boid1 = new Entity()
-                .name("boid")
-                .bounds(Bounds.atPosition(
-                    RANDOM.nextDouble(area.minX(), area.maxX()),
-                    RANDOM.nextDouble(area.minY(), area.maxY()),
-                    16, 16))
-                .add(new RenderComponent(boidSprite))
-                .add(new PhysicsComponent())
-                .add(new BoidComponent(), boid -> {
-                    boid.velocity = 120;
-                    boid.alignmentStrenth = 8.2;
-                    boid.separationStrength = 10.3;
-                    boid.cohesionStrength = 5;
-
-                    boid.obstacleAvoidanceStrength = 48;
-                    boid.obstaclePerceptionRadius = 100;
-                    boid.perceptionRadius = 100;
-                });
-            engine.environment().addEntity(boid1);
-        }
+        screwBox.start();
     }
 
 }
