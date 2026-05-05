f233ff62e711cf218d2b2158b24c8a5046e1d8e4
simonbas <sebastian.simon@koeln.de>
2026-05-05 20:26:59 +0200

    prototype json parsing

--- screwbox-core/src/main/java/dev/screwbox/core/utils/JSonLoader.java
+++ screwbox-core/src/main/java/dev/screwbox/core/utils/JSonLoader.java
@@ -2,11 +2,22 @@ package dev.screwbox.core.utils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class JSonLoader {
+
+    private record JsonContent(String content) {
+
+            private JsonContent(final String content) {
+                Validate.isTrue(() -> content.startsWith("{"), "input is no json string");
+                Validate.isTrue(() -> content.endsWith("}"), "input is no json string");
+                this.content = content.substring(1, content.length() - 1);
+            }
+
+            public <T> T getValue(final String name, final Class<T> type) {
+                return null;
+            }
+        }
 
     public static <T> T load(final String json, final Class<T> type) {
         Objects.requireNonNull(json, "json must not be null");
@@ -15,24 +26,21 @@ public class JSonLoader {
         final var allArgsConstructor = Reflections.findAllArgsConstructor(type)
             .orElseThrow(() -> new IllegalArgumentException(type.getSimpleName() + " is missing all args constructor"));
 
-        final Map<String, String> fieldData = getFieldData(json);
-
-
-        return createInstance(allArgsConstructor);
+        final var root = new JsonContent(json);
+        return createInstance(allArgsConstructor, root);
     }
 
-    private static HashMap<String, String> getFieldData(final String json) {
-        Validate.isTrue(() -> json.startsWith("{"), "input is no json string");
-        Validate.isTrue(() -> json.endsWith("}"), "input is no json string");
-        var jsonContent = json.substring(1, json.length() - 1);
-        return new HashMap<>();
-    }
-
-    private static <T> T createInstance(final Constructor<T> allArgsConstructor) {
+    private static <T> T createInstance(final Constructor<T> constructor, final JsonContent content) {
+        final Class<T> type = constructor.getDeclaringClass();
         try {
-            return (T) allArgsConstructor.newInstance((String) null);
+            final Object[] values = new Object[type.getDeclaredFields().length];
+            for (int i = 0; i < values.length; i++) {
+                final var field = type.getDeclaredFields()[i];
+                values[i] = content.getValue(field.getName(), field.getType());
+            }
+            return (T) constructor.newInstance(values);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalArgumentException("could not instantiate type " + allArgsConstructor.getDeclaringClass().getSimpleName(), e);
+            throw new IllegalArgumentException("could not instantiate type " + type.getSimpleName(), e);
         }
     }
 }
