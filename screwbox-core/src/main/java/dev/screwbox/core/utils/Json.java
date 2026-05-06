1c3265f3382c3f6b0a6417b587420b9bfd9db0c9
simonbas <sebastian.simon@koeln.de>
2026-05-06 22:01:12 +0200

    prototype json parsing

--- screwbox-core/src/main/java/dev/screwbox/core/utils/Json.java
+++ screwbox-core/src/main/java/dev/screwbox/core/utils/Json.java
@@ -2,9 +2,15 @@ package dev.screwbox.core.utils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Json {
+
+    private record Attribute(int endIndex, String name, String value) {
+
+    }
 
     private record JsonObject(String content) {
 
@@ -12,14 +18,34 @@ public class Json {
             Validate.isTrue(() -> content.startsWith("{"), "input is no json string");
             Validate.isTrue(() -> content.endsWith("}"), "input is no json string");
             this.content = content.substring(1, content.length() - 1).trim();
+
+        }
+
+        private List<Attribute> getAllAttributes() {
+            List<Attribute> attributes = new ArrayList<>();
+            int index = 0;
+            while (index < content.length()) {
+                var next = fetchAttribute(index);
+                attributes.add(next);
+                index = next.endIndex()+1;
+                System.out.println(next);
+            }
+            return attributes;
+        }
+
+        private Attribute fetchAttribute(int index) {
+            var attributeStart = content.indexOf('\"', index);
+            var attributeEnd = content.indexOf('\"', attributeStart + 1);
+            var name = content.substring(attributeStart + 1, attributeEnd);
+
+            var attributeValueStart = content.indexOf('\"', attributeEnd+1);
+            var attributeValueEnd = content.indexOf('\"', attributeValueStart + 1);
+            var value = content.substring(attributeValueStart + 1, attributeValueEnd);
+            return new Attribute(attributeValueEnd+1, name, value);
         }
 
         public <T> T getValue(final String name, final Class<T> type) {
-            return (T) findAttributeValue(name);
-        }
-
-        private String findAttributeValue(String name) {
-            return null;
+            return (T)getAllAttributes().stream().filter(attribute -> attribute.name().equals(name)).findFirst().map(Attribute::value).orElse(null);
         }
     }
 
