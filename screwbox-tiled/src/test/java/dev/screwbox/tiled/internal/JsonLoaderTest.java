9f673f280d6efdb0099eb32bce97f97aeb65296b
simonbas <sebastian.simon@koeln.de>
2026-05-11 19:52:51 +0200

    fix Test

--- screwbox-tiled/src/test/java/dev/screwbox/tiled/internal/JsonLoaderTest.java
+++ screwbox-tiled/src/test/java/dev/screwbox/tiled/internal/JsonLoaderTest.java
@@ -7,35 +7,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JsonLoaderTest {
 
-    private record JsonDemo(String name) {
+    public record JsonDemo(String name) {
     }
 
     @Test
     void loadJson_fileNameNull_throwsException() {
         assertThatThrownBy(() -> JsonLoader.loadJson(null, String.class))
-                .isInstanceOf(NullPointerException.class)
-                .hasMessage("file name must not be null");
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("file name must not be null");
     }
 
     @Test
     void loadJson_typeNull_throwsException() {
         assertThatThrownBy(() -> JsonLoader.loadJson("tile.bmp", null))
-                .isInstanceOf(NullPointerException.class)
-                .hasMessage("type must not be null");
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("type must not be null");
     }
 
     @Test
     void loadJson_noJson_throwsException() {
         assertThatThrownBy(() -> JsonLoader.loadJson("tile.bmp", String.class))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("tile.bmp is not a JSON-File");
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("tile.bmp is not a JSON-File");
     }
 
     @Test
     void loadJson_corruptJson_throwsException() {
         assertThatThrownBy(() -> JsonLoader.loadJson("fake.json", String.class))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("file could not be deserialized: fake.json");
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("file could not be deserialized: fake.json");
     }
 
     @Test
