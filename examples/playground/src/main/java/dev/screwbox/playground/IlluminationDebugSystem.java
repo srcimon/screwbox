8fbb920d9ae0f66e71cf613ec854f59f1633d115
simonbas <sebastian.simon@koeln.de>
2026-05-15 22:08:09 +0200

    prototype

--- examples/playground/src/main/java/dev/screwbox/playground/IlluminationDebugSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/IlluminationDebugSystem.java
@@ -2,16 +2,14 @@ package dev.screwbox.playground;
 
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.environment.EntitySystem;
-import dev.screwbox.core.environment.ExecutionOrder;
-import dev.screwbox.core.environment.Order;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.keyboard.Key;
 
-@ExecutionOrder(Order.DEBUG_OVERLAY)
 public class IlluminationDebugSystem implements EntitySystem {
 
     @Override
     public void update(final Engine engine) {
+        engine.graphics().camera().changeZoomBy(engine.mouse().unitsScrolled() / 20.0);
         for (var entity : engine.environment().fetchAllHaving(IlluminationComponent.class)) {
             if (!engine.keyboard().isDown(Key.SPACE)) {
                 engine.graphics().light().addIllumination(entity.position(), entity.get(IlluminationComponent.class).radius, Color.BLACK);
