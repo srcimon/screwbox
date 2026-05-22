946bbb6a59ab7717614237c4fbeb2cc7b6d1c15c
simonbas <sebastian.simon@koeln.de>
2026-05-22 16:16:46 +0200

    imrpove demo

--- examples/playground/src/main/java/dev/screwbox/playground/DebugSystem.java
+++ examples/playground/src/main/java/dev/screwbox/playground/DebugSystem.java
@@ -4,13 +4,17 @@ import dev.screwbox.core.Angle;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.light.ConeLightComponent;
+import dev.screwbox.core.environment.physics.AttachmentComponent;
+import dev.screwbox.core.environment.softphysics.SoftLinkComponent;
 
 public class DebugSystem implements EntitySystem {
 
     @Override
     public void update(final Engine engine) {
         for (var cone : engine.environment().fetchAllHaving(ConeLightComponent.class)) {
-            cone.get(ConeLightComponent.class).direction = Angle.degrees(cone.get(ConeLightComponent.class).direction.degrees() + engine.mouse().unitsScrolled());
+
+            Angle dir = engine.environment().fetchById(cone.get(AttachmentComponent.class).targetId).get(SoftLinkComponent.class).angle.addDegrees(180);
+            cone.get(ConeLightComponent.class).direction = Angle.degrees(Math.clamp(dir.degrees(), 90, 270));
         }
     }
 }
--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -9,16 +9,20 @@ import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.ai.BoidObstacleComponent;
 import dev.screwbox.core.environment.core.LogFpsSystem;
+import dev.screwbox.core.environment.light.ConeLightComponent;
 import dev.screwbox.core.environment.light.DirectionalLightComponent;
 import dev.screwbox.core.environment.light.GlowComponent;
 import dev.screwbox.core.environment.light.OccluderComponent;
 import dev.screwbox.core.environment.light.PointLightComponent;
 import dev.screwbox.core.environment.light.StaticOccluderComponent;
+import dev.screwbox.core.environment.physics.AttachmentComponent;
 import dev.screwbox.core.environment.physics.ColliderComponent;
 import dev.screwbox.core.environment.physics.CursorAttachmentComponent;
 import dev.screwbox.core.environment.physics.GravityComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
 import dev.screwbox.core.environment.physics.StaticColliderComponent;
+import dev.screwbox.core.environment.physics.TailwindComponent;
+import dev.screwbox.core.environment.physics.TailwindPropelledComponent;
 import dev.screwbox.core.environment.rendering.CameraTargetComponent;
 import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.environment.softphysics.RopeOccluderComponent;
@@ -27,6 +31,7 @@ import dev.screwbox.core.environment.softphysics.SoftPhysicsSupport;
 import dev.screwbox.core.graphics.AutoTileBundle;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.options.ShadowOptions;
+import dev.screwbox.core.utils.ListUtil;
 import dev.screwbox.core.utils.TileMap;
 import dev.screwbox.core.window.MouseCursor;
 
@@ -39,9 +44,9 @@ public class PlaygroundApp {
 
         var map = TileMap.fromString("""
             ############
-            #         #
-            ###   B  ##
-            ###  B ########
+            #     R    #
+            ###        ##
+            ###     #######
             ##        ##  ########
             ######    #C    #    #
              #           #    #  #
@@ -57,7 +62,7 @@ public class PlaygroundApp {
             .addSystem(new LogFpsSystem())
             .addEntity(new Entity().bounds(map.bounds().scale(3)).add(new DirectionalLightComponent(), d -> d.angle = Angle.degrees(-10)))
             .addEntity(new Entity().add(new GravityComponent(Vector.y(200))))
-            .addEntity(new Entity().add(new CursorAttachmentComponent()).bounds(Bounds.$$(0, 0, 1, 1)).add(new GlowComponent(60, Color.WHITE.opacity(0.3))).add(new PointLightComponent(80, Color.BLACK)))
+            .addEntity(new Entity().add(new CursorAttachmentComponent()).bounds(Bounds.$$(0, 0, 1, 1)).add(new TailwindComponent(40)).add(new GlowComponent(60, Color.WHITE.opacity(0.3))).add(new PointLightComponent(80, Color.BLACK)))
             .importSource(indexedSources(map.tiles(), TileMap.Tile::value)
                 .assign('#', tile -> new Entity().name("wall")
                     .bounds(tile.bounds())
@@ -72,13 +77,19 @@ public class PlaygroundApp {
                     .bounds(tile.bounds())
                     .add(new CameraTargetComponent(), c -> c.followSpeed = 10000))
                 .assignComplex('R', (source, idPool) -> {
-                    var rope = SoftPhysicsSupport.createRope(source.position().addY(-source.bounds().height() / 2.0), source.position().addY(10), 3, idPool);
-                    rope.root().remove(PhysicsComponent.class);
-                    rope.root().add(new RopeRenderComponent(Color.ORANGE, 2));
-                    rope.root().add(new RopeOccluderComponent(ShadowOptions.rounded()));
-                    return rope;
-                })
-            );
+                    var rope = SoftPhysicsSupport.createRope(source.position().addY(-source.bounds().height() / 2.0), source.position().addY(10), 6, idPool);
+                    rope.end().remove(PhysicsComponent.class);
+                    rope.end().moveTo(rope.root().position());
+                    rope.root().add(new RopeRenderComponent(Color.BLACK, 1));
+                    rope.end().add(new RopeOccluderComponent(ShadowOptions.rounded()));
+                    rope.connectors().forEach(r -> r.get(PhysicsComponent.class).friction = 1);
+                    rope.connectors().forEach(r -> r.add(new TailwindPropelledComponent()));
+                    Entity lamp = new Entity().name("lamp")
+                        .add(new AttachmentComponent(rope.root().forceId()))
+                        .bounds(Bounds.atPosition(rope.end().position(), 1, 1))
+                        .add(new GlowComponent(30, Color.WHITE.opacity(0.5))).add(new PointLightComponent(20)).add(new ConeLightComponent(Angle.degrees(0), Angle.degrees(120), 100, Color.BLACK));
+                    return ListUtil.combine(rope, lamp);
+                }));
 
         screwBox.graphics().configuration().setBackgroundColor(Color.DARK_BLUE);
         screwBox.start();
