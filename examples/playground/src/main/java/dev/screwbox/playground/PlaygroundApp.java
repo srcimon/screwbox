b6ce253c14f3122163ab0672a6401276d0dbdf17
simonbas <sebastian.simon@koeln.de>
2026-06-03 20:41:37 +0200

    refactor

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -45,7 +45,11 @@ public class PlaygroundApp {
         Engine screwBox = ScrewBox.createEngine("Playground");
 
         screwBox.environment().addSystem(e -> {
-            e.graphics().world().drawText(e.mouse().position(), "This is a test text that is needed to debug text drawing.", TextDrawOptions.font(FontBundle.BOLDZILLA).scale(1.5).lineSpacing(2).charactersPerLine(10));
+            e.graphics().world().drawText(e.mouse().position(), "This is a test text that is needed to debug text drawing. Either you supplied the wrong credentials", TextDrawOptions
+                .font(FontBundle.BOLDZILLA)
+                .scale(1.5)
+                .lineSpacing(10)
+                .charactersPerLine(20));
         });
         screwBox.graphics().configuration().setBackgroundColor(Color.DARK_BLUE);
         screwBox.start();
