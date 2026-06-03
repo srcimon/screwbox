6bdbc8e3c9ea10a6eb049bf5ce0565f7e11f98ee
simonbas <sebastian.simon@koeln.de>
2026-06-03 20:14:55 +0200

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/RichTextBlock.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/RichTextBlock.java
@@ -10,31 +10,25 @@ import java.util.List;
 
 public class RichTextBlock {
 
-    public record Part(Offset offset, Sprite sprite, int characterNr) {
+    public record Character(Offset offset, Sprite sprite, int characterNr) {
 
     }
 
     private final String text;
     private final TextDrawOptions options;
-    private final List<String> lines;
 
     public RichTextBlock(final String text, final TextDrawOptions options) {
         this.text = text;
         this.options = options;
-        this.lines = TextUtil.lineWrap(text, options.charactersPerLine());
+
     }
 
-    public List<String> lines() {
-        return lines;
-    }
-
-    public List<Part> parts() {
-        List<Part> parts = new ArrayList<>();
-
+    public List<Character> characters() {
+        List<Character> characters = new ArrayList<>();
+        var lines = TextUtil.lineWrap(text, options.charactersPerLine());
         int y = 0;
         int characterNr = 0;
-        final var textBlock = new RichTextBlock(text, options);
-        for (final String line : textBlock.lines()) {
+        for (final String line : lines) {
             double x = switch (options.alignment()) {
                 case LEFT -> 0;
                 case CENTER -> -options.widthOf(line) / 2.0;
@@ -42,12 +36,12 @@ public class RichTextBlock {
             };
             final List<Sprite> allSprites = options.font().spritesFor(options.isUppercase() ? line.toUpperCase() : line);
             for (final var sprite : allSprites) {
-                parts.add(new Part(Offset.at(x, y), sprite, characterNr));
+                characters.add(new Character(Offset.at(x, y), sprite, characterNr));
                 x += (sprite.width() + options.padding()) * options.scale();
                 characterNr++;
             }
             y += (int) (1.0 * options.font().height() * options.scale() + options.lineSpacing());
         }
-        return parts;
+        return characters;
     }
 }
