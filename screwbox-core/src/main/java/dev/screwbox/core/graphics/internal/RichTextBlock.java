8328765ffb4ad3822c8f1c63f875b644764b3d5d
simonbas <sebastian.simon@koeln.de>
2026-06-03 21:40:57 +0200

    refactor

--- screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/RichTextBlock.java
+++ screwbox-core/src/main/java/dev/screwbox/core/graphics/internal/RichTextBlock.java
@@ -10,21 +10,24 @@ import java.util.List;
 
 public class RichTextBlock {
 
+
     public record Glyph(Offset offset, Sprite sprite, int characterNr) {
 
     }
 
     private final String text;
+    private final String pureText;
     private final TextDrawOptions options;
 
     public RichTextBlock(final String text, final TextDrawOptions options) {
         this.text = text;
+        this.pureText = text.replace("{", "").replace("}", "");
         this.options = options;
     }
 
     public List<Glyph> glyphs() {
         List<Glyph> glyphs = new ArrayList<>();
-        var lines = TextUtil.lineWrap(text, options.charactersPerLine());
+        var lines = TextUtil.lineWrap(pureText, options.charactersPerLine());
         int y = 0;
         int characterNr = 0;
         for (final String line : lines) {
@@ -33,11 +36,13 @@ public class RichTextBlock {
                 case CENTER -> -options.widthOf(line) / 2.0;
                 case RIGHT -> -options.widthOf(line);
             };
-            final List<Sprite> allSprites = options.font().spritesFor(options.isUppercase() ? line.toUpperCase() : line);
-            for (final var sprite : allSprites) {
-                glyphs.add(new Glyph(Offset.at(x, y), sprite, characterNr));
-                x += (sprite.width() + options.padding()) * options.scale();
-                characterNr++;
+            for (Character character : line.toCharArray()) {
+                var sprite = options.font().spriteFor(options.isUppercase() ? Character.toUpperCase(character) : character);
+                if (sprite.isPresent()) {
+                    glyphs.add(new Glyph(Offset.at(x, y), sprite.get(), characterNr));
+                    x += (sprite.get().width() + options.padding()) * options.scale();
+                    characterNr++;
+                }
             }
             y += (int) (1.0 * options.font().height() * options.scale() + options.lineSpacing());
         }
