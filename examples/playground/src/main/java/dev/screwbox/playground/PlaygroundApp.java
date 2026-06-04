9c50a08af2878e6da11b597fe8317332ba792bb5
simonbas <sebastian.simon@koeln.de>
2026-06-04 17:08:02 +0200

    Lis

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -9,24 +9,24 @@ import dev.screwbox.core.graphics.options.TextDrawOptions;
 
 public class PlaygroundApp {
     //TODO use alternate colors to enhance uis in example apps
+
     public static void main(String[] args) {
         Engine screwBox = ScrewBox.createEngine("Playground");
 
-        screwBox.environment().addSystem(e -> {
-            e.graphics().world().drawText(e.mouse().position(), """
-                {Debo} is the best {{girlfriend}} in the {world! That} is nice""", TextDrawOptions
-                .font(FontBundle.BOLDZILLA)
-                .scale(1.5)
+        TextDrawOptions options = TextDrawOptions
+            .font(FontBundle.BOLDZILLA)
+            .scale(1.5)
 
-                .alternativeFont(FontBundle.BOLDZILLA.getCustomColor(Color.YELLOW))
-                .alternativeShader(ShaderBundle.DISSOLVE)
+            .alternativeFont(FontBundle.BOLDZILLA.getCustomColor(Color.YELLOW))
+            .alternativeShader(ShaderBundle.DISSOLVE)
 
-                .alternativeFont(FontBundle.SKINNY_SANS.getCustomColor(Color.RED))
-                .alternativeShader( ShaderBundle.UNDERWATER)
+            .alternativeFont(FontBundle.SKINNY_SANS.getCustomColor(Color.RED))
+            .alternativeShader(ShaderBundle.UNDERWATER)
 
-                .lineSpacing(10)
-                .charactersPerLine(20));
-        });
+            .lineSpacing(10)
+            .charactersPerLine(20);
+
+        screwBox.environment().addSystem(e -> e.graphics().world().drawText(e.mouse().position(), "{Debo} is the best {{girlfriend}} in {the {{world!}} That} is nice", options));
         screwBox.graphics().configuration().setBackgroundColor(Color.DARK_BLUE);
         screwBox.start();
     }
