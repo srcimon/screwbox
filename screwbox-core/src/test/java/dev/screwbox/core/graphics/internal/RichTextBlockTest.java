d1533bf9a3d067d1dd68b1f400fea9f98b8df69a
simonbas <sebastian.simon@koeln.de>
2026-06-04 18:04:08 +0200

    added test

---
+++ screwbox-core/src/test/java/dev/screwbox/core/graphics/internal/RichTextBlockTest.java
@@ -0,0 +1,26 @@
+package dev.screwbox.core.graphics.internal;
+
+import dev.screwbox.core.assets.FontBundle;
+import dev.screwbox.core.graphics.options.TextDrawOptions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RichTextBlockTest {
+
+    @Test
+    void glyphs_noHighlights_isSameAsTextLength() {
+        var block = new RichTextBlock("This is a Test", TextDrawOptions.font(FontBundle.BOLDZILLA));
+
+        assertThat(block.glyphs()).hasSize(14)
+            .anyMatch(glyph -> glyph.characterNr() == 0);
+    }
+
+    @Test
+    void glyphs_twoHighlights_isSameAsTextLengthWithoutMarkers() {
+        var block = new RichTextBlock("This {is} a {{Test}}", TextDrawOptions.font(FontBundle.BOLDZILLA));
+
+        assertThat(block.glyphs()).hasSize(14)
+            .anyMatch(glyph -> glyph.characterNr() == 0);
+    }
+}
