3a2f1dd38ab46d0bf9abefa851a8a6fb61b487d2
simonbas <sebastian.simon@koeln.de>
2026-06-08 19:18:12 +0200

    Bump Node dependencies

--- screwbox-core/src/main/java/dev/screwbox/core/ui/UiLayout.java
+++ screwbox-core/src/main/java/dev/screwbox/core/ui/UiLayout.java
@@ -2,6 +2,7 @@ package dev.screwbox.core.ui;
 
 import dev.screwbox.core.graphics.ScreenBounds;
 
+@FunctionalInterface
 public interface UiLayout {
 
     ScreenBounds layout(UiMenuItem item, UiMenu menu, ScreenBounds bounds);
