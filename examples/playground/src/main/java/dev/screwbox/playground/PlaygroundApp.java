90d67533290f3d12a853c64650d714917dd4a3a9
simonbas <sebastian.simon@koeln.de>
2026-06-16 22:03:27 +0200

    prototype hit detection

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -2,46 +2,12 @@ package dev.screwbox.playground;
 
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.ScrewBox;
-import dev.screwbox.core.assets.FontBundle;
-import dev.screwbox.core.environment.Entity;
-import dev.screwbox.core.environment.importing.Blueprint;
-import dev.screwbox.core.environment.importing.ImportOptions;
-import dev.screwbox.core.environment.physics.ColliderComponent;
-import dev.screwbox.core.environment.rendering.CameraTargetComponent;
-import dev.screwbox.core.environment.rendering.RenderComponent;
-import dev.screwbox.core.graphics.Color;
-import dev.screwbox.core.graphics.Sprite;
-import dev.screwbox.core.utils.TileMap;
 
 public class PlaygroundApp {
 
     public static void main(String[] args) {
 
         Engine screwBox = ScrewBox.createEngine("Playground");
-        screwBox.graphics().camera().setZoom(3);
-
-        var map = TileMap.fromString("""
-            #####
-            #          #
-            #          #
-            #          #
-            #    T     #
-            #          #
-            #          #
-            ####   #####
-            """);
-
-        screwBox.environment()
-            .enableAllFeatures()
-            .addSystem(new AudioTraceDebugSystem())
-            .importSource(ImportOptions.indexedSources(map.tiles(), TileMap.Tile::value)
-                .assign('#', source -> new Entity().bounds(source.bounds())
-                    .add(new ColliderComponent())
-                    .add(new RenderComponent(Sprite.placeholder(Color.RED, source.size()))))
-                .assign('T', source -> new Entity().bounds(source.bounds())
-                    .add(new CameraTargetComponent())
-                    .add(new RenderComponent(FontBundle.BOLDZILLA.get().spriteFor('T').get())))
-            );
 
         screwBox.start();
     }
