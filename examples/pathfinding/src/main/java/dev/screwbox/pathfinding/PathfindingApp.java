14fc10450ba62694898a7a3b5a192e0924861ff5
simonbas <sebastian.simon@koeln.de>
2026-07-07 20:17:55 +0200

    Enhance Pathfinding example

--- examples/pathfinding/src/main/java/dev/screwbox/pathfinding/PathfindingApp.java
+++ examples/pathfinding/src/main/java/dev/screwbox/pathfinding/PathfindingApp.java
@@ -1,7 +1,12 @@
 package dev.screwbox.pathfinding;
 
 import dev.screwbox.core.Engine;
+import dev.screwbox.core.Percent;
 import dev.screwbox.core.ScrewBox;
+import dev.screwbox.core.graphics.postfilter.CrtMonitorPostFilter;
+import dev.screwbox.core.graphics.postfilter.WarpPostFilter;
+import dev.screwbox.core.scenes.SceneTransition;
+import dev.screwbox.core.scenes.animations.BlackHoleAnimation;
 import dev.screwbox.pathfinding.scenes.DemoScene;
 
 public class PathfindingApp {
@@ -9,10 +14,14 @@ public class PathfindingApp {
     public static void main(String[] args) {
         Engine engine = ScrewBox.createEngine("Pathfinding");
 
-        engine.scenes().add(new DemoScene())
-                .switchTo(DemoScene.class);
+        engine.graphics().postProcessing().addViewportFilter(new WarpPostFilter(Percent.of(0.1))).addViewportFilter(new CrtMonitorPostFilter());
+        engine.scenes()
+            .add(new DemoScene())
+            .switchTo(DemoScene.class, SceneTransition.custom().introAnimation(new BlackHoleAnimation()).introDurationMillis(1500));
 
-        engine.assets().enableLogging().prepareClassPackageAsync(PathfindingApp.class);
+        engine.assets()
+            .enableLogging()
+            .prepareClassPackageAsync(PathfindingApp.class);
 
         engine.graphics().camera().setZoom(2.7);
 
