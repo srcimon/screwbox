69fdd72091f26fb5e73edb3babc9b0c25b411ca7
Sebastian Simon <104482889+srcimon@users.noreply.github.com>
2026-07-17 21:02:33 +0200

    Experimental (#1038)

--- UNRELEASED-CHANGES.md
+++ UNRELEASED-CHANGES.md
@@ -12,4 +12,6 @@
 
 ### 📦 Dependency updates
 
-- ...
+- Bump Node dependencies
+- Bump JUnit to 6.1.2
+- Bump Docusaurus to 3.10.2
--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -1,45 +1,50 @@
 package dev.screwbox.playground;
 
-import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.ScrewBox;
-import dev.screwbox.core.Vector;
-import dev.screwbox.core.environment.Entity;
-import dev.screwbox.core.environment.ai.BoidComponent;
-import dev.screwbox.core.environment.ai.BoidObstacleComponent;
-import dev.screwbox.core.environment.physics.PhysicsComponent;
-import dev.screwbox.core.environment.rendering.CameraTargetComponent;
-import dev.screwbox.core.environment.rendering.RenderComponent;
-import dev.screwbox.core.graphics.SpriteBundle;
+import dev.screwbox.core.environment.core.LogFpsSystem;
+import dev.screwbox.core.graphics.GraphicsConfiguration;
+import dev.screwbox.core.graphics.Offset;
+import dev.screwbox.core.graphics.Sprite;
+import dev.screwbox.core.graphics.options.SpriteDrawOptions;
 
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public class PlaygroundApp {
 
+    private static final int WIDTH = GraphicsConfiguration.DEFAULT_RESOLUTION.width();
+    private static final int HEIGHT = GraphicsConfiguration.DEFAULT_RESOLUTION.height();
+    private static int[] pixels;
+
     public static void main(String[] args) {
         Engine screwBox = ScrewBox.createEngine("Playground");
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
+        pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        screwBox.loop().unlockFps();
         screwBox.environment()
-            .enableAllFeatures();
+            .enableAllFeatures()
+            .addSystem(new LogFpsSystem())
+            .addSystem(e -> {
+                e.graphics().canvas().drawSprite(Sprite.fromImage(image), Offset.origin(), SpriteDrawOptions.originalSize());
+                updateAndDrawPixels((int) e.loop().frameNumber());
+            });
 
-        var offset= Vector.$(2000,2000);
-
-        for (int i = 0; i < 4000; i++) {
-            screwBox.environment()
-                .addEntity(new Entity()
-                    .add(new BoidComponent(), b -> {
-                        b.velocity = 100;
-                        b.perceptionRadius = 25;
-                        b.alignmentStrenth = 25;
-                        b.separationStrength = 28;
-                        b.cohesionStrength = 3;
-                    })
-                    .add(new PhysicsComponent())
-                    .bounds(Bounds.atPosition(new Random().nextDouble(-400, 400), new Random().nextDouble(-400, 400), 2, 2).moveBy(offset))
-                    .add(new RenderComponent(SpriteBundle.DOT_WHITE.get().scaled(0.25))));
-        }
-        screwBox.environment().addEntity(new Entity().add(new CameraTargetComponent()).bounds(Bounds.atPosition(offset, 800, 800)).add(new BoidObstacleComponent(), b -> b.isContainer = true));
         screwBox.start();
     }
 
+    private static void updateAndDrawPixels(int frame) {
+        Arrays.parallelSetAll(pixels, gid -> {
+            int x = gid % WIDTH;
+            int y = gid / WIDTH;
+
+            int r = (x + frame) & 0xFF;
+            int g = (y - frame) & 0xFF;
+            int b = (x * y + frame) & 0xFF;
+
+            return (r << 16) | (g << 8) | b;
+        });
+    }
 }
