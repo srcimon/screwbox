9f1104f41609e43f7ad5c356dc0ec879374f09a6
simonbas <sebastian.simon@koeln.de>
2026-07-20 13:21:07 +0200

    try fixing compile issues

--- screwbox-core/src/test/java/dev/screwbox/core/navigation/GridTest.java
+++ screwbox-core/src/test/java/dev/screwbox/core/navigation/GridTest.java
@@ -5,7 +5,10 @@ import dev.screwbox.core.Vector;
 import dev.screwbox.core.graphics.Offset;
 import dev.screwbox.core.graphics.Size;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static dev.screwbox.core.Bounds.$$;
 import static dev.screwbox.core.Vector.$;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -79,82 +82,79 @@ class GridTest {
         assertThat(grid.cellHeight()).isEqualTo(8.0);
     }
 
-//    @Test
-//    void fill_areaInGrid_setsValuesWithinArea() {
-//        Bounds area = $$(0, 0, 12, 12);
-//        var grid = Grid.booleanGrid(area, 4);
-//
-//        grid.fill($$(3, 2, 2, 3), true);
-//
-//        assertThat(grid.get(0, 0)).isTrue();
-//        assertThat(grid.get(0, 1)).isTrue();
-//        assertThat(grid.get(1, 0)).isTrue();
-//        assertThat(grid.get(1, 1)).isTrue();
-//        assertThat(grid.get(1, 1)).isTrue();
-//        assertThat(grid.hasValue(2, 0)).isFalse();
-//        assertThat(grid.hasValue(2, 1)).isFalse();
-//        assertThat(grid.hasValue(0, 2)).isFalse();
-//        assertThat(grid.hasValue(1, 2)).isFalse();
-//        assertThat(grid.hasValue(2, 2)).isFalse();
-//    }
-//
-//
-//    @Test
-//    void cellBounds_cellOutOfGrid_returnsBoundsInWorld() {
-//        Bounds area = $$(0, 0, 12, 12);
-//        var grid = Grid.booleanGrid(area, 4);
-//
-//        var result = grid.cellBounds(Offset.at(30, 30));
-//        assertThat(result).isEqualTo($$(120, 120, 4, 4));
-//    }
-//
-//
-//    @Test
-//    void cellCount_3X3Area_returns9() {
-//        Bounds area = $$(0, 0, 12, 12);
-//        var grid = Grid.booleanGrid(area, 4);
-//
-//        assertThat(grid.cellCount()).isEqualTo(9);
-//    }
-//
-//
-//    @ParameterizedTest
-//    @CsvSource({
-//        "0, 0, 0, 0",
-//        "-10, 10, -1, 0",
-//        "-17, 50.123, -2, 3",
-//    })
-//    void findCell_validPosition_returnsCell(double x, double y, int cellX, int cellY) {
-//        Offset cell = Grid.findCell($(x, y), 16);
-//
-//        assertThat(cell).isEqualTo(Offset.at(cellX, cellY));
-//    }
-//
-//    @Test
-//    void findCell_invalidCellSize_throwsException() {
-//        Vector vector = $(4, 1);
-//
-//        assertThatThrownBy(() -> Grid.findCell(vector, 0))
-//            .isInstanceOf(IllegalArgumentException.class)
-//            .hasMessage("cell size must be positive (actual value: 0)");
-//    }
-//
-//    @Test
-//    void fill_emptyGrid_fillsAllsCells() {
-//        var grid = new Grid<>(Bounds.atOrigin(4, 4, 4, 4), 4, String.class);
-//        grid.fill("test");
-//
-//        assertThat(grid.cells()).allMatch(cell -> grid.get(cell).equals("test"));
-//    }
-//
-//    @Test
-//    void clear_cellWithinGrid_clearsCellData() {
-//        var grid = new Grid<>(Bounds.atOrigin(4, 4, 4, 4), 1, String.class);
-//        var cell = Offset.at(2, 0);
-//        grid.set(cell, "test");
-//
-//        grid.clear(cell);
-//
-//        assertThat(grid.hasValue(cell)).isFalse();
-//    }
+    @Test
+    void fill_areaInGrid_setsValuesWithinArea() {
+        Bounds area = $$(0, 0, 12, 12);
+        var grid = Grid.createByApproxCellSize(Size.square(4), area, Boolean.class);
+
+        grid.fill($$(3, 2, 2, 3), true);
+
+        assertThat(grid.get(0, 0)).isTrue();
+        assertThat(grid.get(0, 1)).isTrue();
+        assertThat(grid.get(1, 0)).isTrue();
+        assertThat(grid.get(1, 1)).isTrue();
+        assertThat(grid.get(1, 1)).isTrue();
+        assertThat(grid.hasValue(2, 0)).isFalse();
+        assertThat(grid.hasValue(2, 1)).isFalse();
+        assertThat(grid.hasValue(0, 2)).isFalse();
+        assertThat(grid.hasValue(1, 2)).isFalse();
+        assertThat(grid.hasValue(2, 2)).isFalse();
+    }
+
+    @Test
+    void cellBounds_cellOutOfGrid_returnsBoundsInWorld() {
+        Bounds area = $$(0, 0, 12, 12);
+        var grid = Grid.createByApproxCellSize(Size.square(4), area, Boolean.class);
+
+        var result = grid.cellBounds(Offset.at(30, 30));
+        assertThat(result).isEqualTo($$(120, 120, 4, 4));
+    }
+
+    @Test
+    void cellCount_3X3Area_returns9() {
+        Bounds area = $$(0, 0, 12, 12);
+        var grid = Grid.createByApproxCellSize(Size.square(4), area, Boolean.class);
+
+        assertThat(grid.cellCount()).isEqualTo(9);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, 0, 0, 0",
+        "-10, 10, -1, 0",
+        "-17, 50.123, -2, 3",
+    })
+    void findCell_validPosition_returnsCell(double x, double y, int cellX, int cellY) {
+        Offset cell = Grid.findCell($(x, y), 16);
+
+        assertThat(cell).isEqualTo(Offset.at(cellX, cellY));
+    }
+
+    @Test
+    void findCell_invalidCellSize_throwsException() {
+        Vector vector = $(4, 1);
+
+        assertThatThrownBy(() -> Grid.findCell(vector, 0))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("cell size must be positive (actual value: 0)");
+    }
+
+    @Test
+    void fill_emptyGrid_fillsAllsCells() {
+        var grid = Grid.createByApproxCellSize(Size.square(4), Bounds.atOrigin(4, 4, 4, 4), String.class);
+        grid.fill("test");
+
+        assertThat(grid.cells()).allMatch(cell -> grid.get(cell).equals("test"));
+    }
+
+    @Test
+    void clear_cellWithinGrid_clearsCellData() {
+        var grid = Grid.createByApproxCellSize(Size.square(1), Bounds.atOrigin(4, 4, 4, 4), String.class);
+        var cell = Offset.at(2, 0);
+        grid.set(cell, "test");
+
+        grid.clear(cell);
+
+        assertThat(grid.hasValue(cell)).isFalse();
+    }
 }
