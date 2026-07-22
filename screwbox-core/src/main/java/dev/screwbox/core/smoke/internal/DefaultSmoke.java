a65c8def5c54db97d4788752e998e8f46433e90f
simonbas <sebastian.simon@koeln.de>
2026-07-22 21:39:11 +0200

    start prototyping

--- screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/DefaultSmoke.java
+++ screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/DefaultSmoke.java
@@ -5,9 +5,9 @@ import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.Order;
 import dev.screwbox.core.graphics.Color;
 import dev.screwbox.core.graphics.Offset;
-import dev.screwbox.core.graphics.ScreenBounds;
 import dev.screwbox.core.graphics.Size;
 import dev.screwbox.core.graphics.Sprite;
+import dev.screwbox.core.graphics.internal.ImageOperations;
 import dev.screwbox.core.graphics.internal.ViewportManager;
 import dev.screwbox.core.graphics.options.RectangleDrawOptions;
 import dev.screwbox.core.graphics.options.SpriteDrawOptions;
@@ -21,8 +21,8 @@ public class DefaultSmoke implements Smoke, Updatable {
 
 //TODO support split screen
     private final ViewportManager viewportManager;
-    private int cellSize = 8;
-    private int screenBorder=-32;
+    private int cellSize = 4;
+    private int screenBorder=32;
 
     private Vector worldAnchor;
     private FluidSimulation simulation;
@@ -33,15 +33,22 @@ public class DefaultSmoke implements Smoke, Updatable {
 
     @Override
     public Smoke enable() {
+        reassignGrid();
+        return this;
+    }
+
+    private void reassignGrid() {
+        var boundsArea = calculateBestBounds();
+        worldAnchor = boundsArea.origin();
+        simulation = new FluidSimulation((int)(boundsArea.width() / cellSize));
+    }
+
+    private Bounds calculateBestBounds() {
         Bounds visibleArea = viewportManager.defaultViewport().visibleArea().expand(screenBorder);//TODO remove expand
         var boundsArea = visibleArea.snapExpand(cellSize);
-        boundsArea = boundsArea.resize(
+        return boundsArea.resize(
             Math.max(boundsArea.width(), boundsArea.height()),
             Math.max(boundsArea.width(), boundsArea.height()));
-        Vector origin = boundsArea.origin();
-        worldAnchor = origin;
-        simulation = new FluidSimulation((int)(boundsArea.width() / cellSize));
-        return this;
     }
 
     @Override
@@ -54,20 +61,22 @@ public class DefaultSmoke implements Smoke, Updatable {
     @Override
     public void update() {
         if(simulation != null) {
+            if(calculateBestBounds().origin().distanceTo(worldAnchor) > 0) {//TODO > border
+                reassignGrid();
+            }
             //TODO get delta from update()
             simulation.step(0.002 , 0.0004,0.0003, 4);
-            simulation.addDensity(10,10, 2);
-            simulation.addVelocity(10,10, 8,8);
+            simulation.addDensity(30,30, 2);
+            simulation.addVelocity(30,30, 8,8);
 
             BufferedImage image = createImage();
 
-            double scale = simulation.size() / viewportManager.defaultViewport().camera().zoom();
+            double scale = cellSize * viewportManager.defaultViewport().camera().zoom();
             Offset origin = viewportManager.defaultViewport().toCanvas(worldAnchor);
-
             viewportManager.defaultViewport().canvas().drawSprite(Sprite.fromImage(image), origin, SpriteDrawOptions
                 .scaled(scale)
                 .drawOrder(Order.DEBUG_OVERLAY.drawOrder()));//TODO size
-            viewportManager.defaultViewport().canvas().drawRectangle(origin, Size.of(100,100), RectangleDrawOptions.filled(Color.BLUE.opacity(0.9)));
+            System.out.println(scale);
             //TODO handle zoom changes
         }
     }
@@ -89,7 +98,7 @@ public class DefaultSmoke implements Smoke, Updatable {
                 pixels[pixelIndex + x] = (a << 24) | (r << 16) | (g << 8) | b;
             }
         }
-        //                ImageOperations.blurImage(image, 3);
+                        ImageOperations.blurImage(image, 3);
         return image;
     }
 }
