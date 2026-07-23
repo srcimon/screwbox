28eca6442cbe9e00a03615ddb32452fae4f95d76
simonbas <sebastian.simon@koeln.de>
2026-07-23 08:39:29 +0200

    small performance tunes

--- screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/FluidSimulation.java
+++ screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/FluidSimulation.java
@@ -131,8 +131,9 @@ public class FluidSimulation {
 
         for (int j = 1; j < cells - 1; j++) {
             for (int i = 1; i < cells - 1; i++) {
-                velocX[IX(i, j)] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
-                velocY[IX(i, j)] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
+                int ix = IX(i, j);
+                velocX[ix] -= 0.5 * (p[IX(i + 1, j)] - p[IX(i - 1, j)]) / h;
+                velocY[ix] -= 0.5 * (p[IX(i, j + 1)] - p[IX(i, j - 1)]) / h;
             }
         }
         set_bnd(1, velocX);
@@ -154,8 +155,9 @@ public class FluidSimulation {
 
         for (j = 1, jfloat = 1; j < cells - 1; j++, jfloat++) {
             for (i = 1, ifloat = 1; i < cells - 1; i++, ifloat++) {
-                tmp1 = dtx * velocX[IX(i, j)];
-                tmp2 = dty * velocY[IX(i, j)];
+                int ix = IX(i, j);
+                tmp1 = dtx * velocX[ix];
+                tmp2 = dty * velocY[ix];
                 x = ifloat - tmp1;
                 y = jfloat - tmp2;
 
@@ -178,7 +180,7 @@ public class FluidSimulation {
                 int j0i = (int) (j0);
                 int j1i = (int) (j1);
 
-                d[IX(i, j)] = s0 * (t0 * d0[IX(i0i, j0i)] + t1 * d0[IX(i0i, j1i)]) +
+                d[ix] = s0 * (t0 * d0[IX(i0i, j0i)] + t1 * d0[IX(i0i, j1i)]) +
                               s1 * (t0 * d0[IX(i1i, j0i)] + t1 * d0[IX(i1i, j1i)]);
             }
         }
