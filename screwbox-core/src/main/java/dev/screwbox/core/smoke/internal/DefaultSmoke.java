90678513a47fe88e376b4a1253c25ae9c3bd8202
simonbas <sebastian.simon@koeln.de>
2026-07-25 20:27:06 +0200

    finally !

--- screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/DefaultSmoke.java
+++ screwbox-core/src/main/java/dev/screwbox/core/smoke/internal/DefaultSmoke.java
@@ -31,7 +31,7 @@ public class DefaultSmoke implements Smoke, Updatable {
     //TODO support split screen
     private final ViewportManager viewportManager;
     private final ExecutorService executor;
-    private int cellSize = 10;
+    private int cellSize = 8;
     private int screenBorder = 256;
     private Vector worldAnchor;
     private Vector imageWorldAnchor = Vector.zero();
@@ -163,8 +163,8 @@ public class DefaultSmoke implements Smoke, Updatable {
     public void update() {
     }
 
-    private static int upscale = 4;
-    private static int blur = 4;
+    private static int upscale = 3;
+    private static int blur = 6;
 
     static Percent maxOpacity = Percent.max();
     //TODO reuse bufferimage
