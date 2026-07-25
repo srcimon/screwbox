acb1ab63c0c4d06bcfec2d4f9cad7d8bd105f14a
simonbas <sebastian.simon@koeln.de>
2026-07-25 11:36:46 +0200

    get rid completely

--- screwbox-core/src/main/java/dev/screwbox/core/environment/smoke/SmokeSystem.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/smoke/SmokeSystem.java
@@ -11,16 +11,16 @@ public class SmokeSystem implements EntitySystem {
     @Override
     public void update(Engine engine) {
         for(final var entity : engine.environment().fetchAll(Archetype.ofSpacial(SmokeEmitterComponent.class))) {
-            if(engine.graphics().isWithinDistanceToVisibleArea(entity.position(), 32)) {
-                engine.graphics().smoke().emit(entity.position(), entity.get(SmokeEmitterComponent.class).amount * engine.loop().delta(), entity.get(SmokeEmitterComponent.class).color);
+            if(engine.graphics().isWithinDistanceToVisibleArea(entity.position(), 128)) {
+                engine.graphics().smoke().emit(entity.position(), entity.get(SmokeEmitterComponent.class).amount , entity.get(SmokeEmitterComponent.class).color);
             }
         }
 
         for(final var entity : engine.environment().fetchAll(Archetype.ofSpacial(SmokeAffectorComponent.class))) {
-            if(engine.graphics().isWithinDistanceToVisibleArea(entity.position(), 32)) {
+            if(engine.graphics().isWithinDistanceToVisibleArea(entity.position(), 128)) {
                 var affector = entity.get(SmokeAffectorComponent.class);
                 Vector speed = affector.speed == null ? entity.get(PhysicsComponent.class).velocity.multiply(0.1) : affector.speed;
-                engine.graphics().smoke().affect(entity.position(), speed.multiply(engine.loop().delta()));
+                engine.graphics().smoke().affect(entity.position(), speed);
             }
         }
     }
