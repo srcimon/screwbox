185efb0efa3d6e583bcc44780bb0cb66251cd5cc
simonbas <sebastian.simon@koeln.de>
2026-08-08 20:51:10 +0200

    added todo

--- examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
+++ examples/playground/src/main/java/dev/screwbox/playground/PlaygroundApp.java
@@ -1,12 +1,14 @@
 package dev.screwbox.playground;
 
 import dev.screwbox.core.Bounds;
+import dev.screwbox.core.Duration;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.ScrewBox;
 import dev.screwbox.core.Vector;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.core.LogFpsSystem;
 import dev.screwbox.core.environment.importing.ImportOptions;
+import dev.screwbox.core.environment.particles.ParticleEmitterComponent;
 import dev.screwbox.core.environment.physics.PhysicsComponent;
 import dev.screwbox.core.environment.rendering.RenderComponent;
 import dev.screwbox.core.environment.smoke.SmokeObstacleComponent;
@@ -22,6 +24,7 @@ import dev.screwbox.core.graphics.layouts.VerticalLayout;
 import dev.screwbox.core.graphics.smoke.SmokeOptions;
 import dev.screwbox.core.graphics.smoke.styles.NettrunnerSmokeStyle;
 import dev.screwbox.core.keyboard.Key;
+import dev.screwbox.core.particles.ParticleOptions;
 import dev.screwbox.core.utils.ListUtil;
 import dev.screwbox.core.utils.TileMap;
 
@@ -51,7 +54,8 @@ public class PlaygroundApp {
 
         screwBox.graphics().smoke().setOptions(SmokeOptions.slowFade().style(new NettrunnerSmokeStyle()));
         screwBox.environment().importSource(ImportOptions.indexedSources(map.tiles(), TileMap.Tile::value)
-            .assign('G', (source, idPool) -> new Entity().bounds(Bounds.atPosition(source.position(), 16,16)).add(new RenderComponent(Sprite.placeholder(Color.YELLOW, 16))).add(new PhysicsComponent()))
+            .assign('G', (source, idPool) -> new Entity().bounds(Bounds.atPosition(source.position(), 16,16))
+                .add(new RenderComponent(Sprite.placeholder(Color.YELLOW, 16))).add(new PhysicsComponent()))
             .assign('#', (source, idPool) -> new Entity().bounds(source.bounds()).add(new SmokeObstacleComponent()).add(new RenderComponent(Sprite.placeholder(Color.DARK_GREEN, 32))))
             .assign('W', (source, idPool) -> new Entity().bounds(source.bounds())
                 .add(new RenderComponent(Sprite.placeholder(Color.WHITE.opacity(0.2), Size.square(32)))).add(new WindComponent(Vector.y(2)))));
@@ -63,7 +67,7 @@ public class PlaygroundApp {
         screwBox.environment().addSystem(x -> {
             x.mouse().hoverViewport().camera().changeZoomBy(x.mouse().unitsScrolled() / -20.0);
             if(x.mouse().isDownRight()) {
-                x.graphics().smoke().push(screwBox.mouse().position(), x.keyboard().arrowKeysMovement(1000).multiply(screwBox.loop().delta()));
+                x.graphics().smoke().push(screwBox.mouse().position(), Vector.x(500).multiply(screwBox.loop().delta()));
                 x.graphics().smoke().emit(screwBox.mouse().position(), 0.4 * screwBox.loop().delta(), color);
             }
 
