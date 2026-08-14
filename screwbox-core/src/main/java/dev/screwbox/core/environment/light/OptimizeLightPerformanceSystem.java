d8cd766d6bc733d5b8f533569e3228b92dbc470c
simonbas <sebastian.simon@koeln.de>
2026-08-14 20:03:01 +0200

    refactored OptimizeLightPerformanceSystem

--- screwbox-core/src/main/java/dev/screwbox/core/environment/light/OptimizeLightPerformanceSystem.java
+++ screwbox-core/src/main/java/dev/screwbox/core/environment/light/OptimizeLightPerformanceSystem.java
@@ -1,16 +1,13 @@
 package dev.screwbox.core.environment.light;
 
-import dev.screwbox.core.Bounds;
 import dev.screwbox.core.Engine;
 import dev.screwbox.core.environment.Archetype;
 import dev.screwbox.core.environment.Entity;
 import dev.screwbox.core.environment.EntitySystem;
 import dev.screwbox.core.environment.ExecutionOrder;
-import dev.screwbox.core.environment.core.TransformComponent;
-import dev.screwbox.core.utils.GeometryUtil;
+import dev.screwbox.core.environment.internal.EntityBakery;
 
 import java.util.List;
-import java.util.Optional;
 
 import static dev.screwbox.core.environment.Order.OPTIMIZATION;
 
@@ -24,8 +21,13 @@ public class OptimizeLightPerformanceSystem implements EntitySystem {
     public void update(final Engine engine) {
         final List<Entity> combinables = engine.environment().fetchAll(COMBINABLES);
         for (final var entity : combinables) {
-            for (final var combinable : combinables) {
-                if (tryToCombine(entity, combinable, engine)) {
+            for (final var peer : combinables) {
+                final var baked = EntityBakery.tryToBake(entity, peer, OccluderComponent.class);
+                if (baked.isPresent()) {
+                    entity.remove(StaticOccluderComponent.class);
+                    peer.remove(StaticOccluderComponent.class);
+                    baked.get().add(new StaticOccluderComponent());
+                    engine.environment().addEntity(baked.get());
                     return; // only one combination per frame
                 }
             }
@@ -35,29 +37,5 @@ public class OptimizeLightPerformanceSystem implements EntitySystem {
             entity.remove(StaticOccluderComponent.class);
         }
         engine.environment().remove(OptimizeLightPerformanceSystem.class);
-    }
-
-    private static boolean tryToCombine(final Entity first, Entity second, final Engine engine) {
-        if (first == second) {
-            return false;
-        }
-        final var firstOccluder = first.get(OccluderComponent.class);
-        final var seccondOccluder = second.get(OccluderComponent.class);
-        if (firstOccluder.isAffectedByShadow != seccondOccluder.isAffectedByShadow || firstOccluder.expand != 0 || seccondOccluder.expand != 0) {
-            return false;
-        }
-        Optional<Bounds> result = GeometryUtil.tryToCombine(first.bounds(), second.bounds());
-        if (result.isPresent()) {
-            engine.environment().addEntity(
-                new OccluderComponent(firstOccluder.isAffectedByShadow),
-                new StaticOccluderComponent(),
-                new TransformComponent(result.get()));
-            first.remove(StaticOccluderComponent.class);
-            first.remove(OccluderComponent.class);
-            second.remove(StaticOccluderComponent.class);
-            second.remove(OccluderComponent.class);
-            return true;
-        }
-        return false;
     }
 }
