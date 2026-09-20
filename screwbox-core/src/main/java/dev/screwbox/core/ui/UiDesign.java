d2cb34861ab4ee83b2fb7498448358e068514126
simonbas <sebastian.simon@koeln.de>
2026-09-20 19:43:45 +0200

    add more javadoc

--- screwbox-core/src/main/java/dev/screwbox/core/ui/UiDesign.java
+++ screwbox-core/src/main/java/dev/screwbox/core/ui/UiDesign.java
@@ -2,8 +2,13 @@ package dev.screwbox.core.ui;
 
 import dev.screwbox.core.graphics.Canvas;
 
+/**
+ * Specifies the design of ui menues.
+ *
+ * @see Ui#setDesign(UiDesign)
+ */
 public interface UiDesign {
-
+//TODO finish javadoc
     void renderSelectableItem(String label, Canvas canvas);
 
     void renderSelectedItem(String label, Canvas canvas);
